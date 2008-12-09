4574722c3163f3ff75745ba68334fd66e634e235
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2008-12-09 04:26:45 +0000

    git-svn-id: https://scalaz.googlecode.com/svn/trunk@24 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

---
+++ src/main/scalaz/database/ResultSetFetchDirection.scala
@@ -0,0 +1,22 @@
+package scalaz.database
+
+import java.sql.ResultSet.{FETCH_FORWARD, FETCH_REVERSE, FETCH_UNKNOWN}
+
+sealed trait ResultSetFetchDirection {
+  def asInt: Int
+}
+final case object Forward extends ResultSetFetchDirection {
+  def asInt = FETCH_FORWARD
+}
+final case object Reverse extends ResultSetFetchDirection {
+  def asInt = FETCH_REVERSE
+}
+final case object Unknown extends ResultSetFetchDirection {
+  def asInt = FETCH_UNKNOWN
+}
+
+object ResultSetFetchDirection {
+  def resultSetFetchDirections = List(Forward, Reverse, Unknown)
+
+  def fromInt(n: Int) = resultSetFetchDirections find (_.asInt == n)
+}
