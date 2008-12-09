8672738073103559cf2a42dfaf119298e3de3aed
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2008-12-09 05:33:12 +0000

    git-svn-id: https://scalaz.googlecode.com/svn/trunk@28 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scalaz/database/sql/Predicate.scala
+++ src/main/scalaz/database/sql/Predicate.scala
@@ -2,6 +2,12 @@ package scalaz.database.sql
 
 sealed trait Predicate {
   def toSQL: String
+
+  import Predicate.predicate
+
+  def |||(p: Predicate) = predicate("(" + toSQL + " OR " + p.toSQL + ")")
+
+  def &&&(p: Predicate) = predicate("(" + toSQL + " AND " + p.toSQL + ")") 
 }
 
 object Predicate {
