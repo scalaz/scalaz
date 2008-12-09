e131e4c1e8cc6f0736c7615e95ff4e9c18a633ce
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2008-12-09 04:29:27 +0000

    git-svn-id: https://scalaz.googlecode.com/svn/trunk@25 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

---
+++ src/main/scalaz/database/ResultSetW.scala
@@ -0,0 +1,23 @@
+package scalaz.database
+
+import java.sql.ResultSet
+
+sealed trait ResultSetW {
+  val resultset: ResultSet
+
+  def ~>[T](f: ImmutableResultSet => T) = new Iterator[T] {
+    var h = resultset.next
+    def next = {
+      val t = f(resultset)
+      h = resultset.next
+      t
+    }
+    def hasNext = h
+  }
+}
+
+object ResultSetW {
+  implicit def resultset(r: ResultSet) = new ResultSetW {
+    val resultset = r
+  }
+}
