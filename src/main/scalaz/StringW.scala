39343e4c420475eed184ac04f602d0596f9567b2
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-01-22 03:58:33 +0000

    git-svn-id: https://scalaz.googlecode.com/svn/trunk@92 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scalaz/StringW.scala
+++ src/main/scalaz/StringW.scala
@@ -59,6 +59,15 @@ sealed trait StringW {
    */
   def parseShort: Validation[NumberFormatException, Short] = EitherW.throws(java.lang.Short.parseShort(s)).left.map(_.asInstanceOf[NumberFormatException])
 
+  /**
+   * Returns the same String value if the given value is 1 otherwise pluralises this String by appending an "s" unless
+   * this String ends with "y" and not one of ["ay", "ey", "iy", "oy", "uy"] in which case the 'y' character is chopped and "ies"
+   * is appended.
+   */
+  def plural(n: Int) = if(n == 1) s else
+                       if((s endsWith "y") && (List("ay", "ey","iy", "oy", "uy") forall (!s.endsWith(_)))) s.take(s.length - 1) + "ies"
+                       else s + "s" 
+
   import xml._
 
   /**
