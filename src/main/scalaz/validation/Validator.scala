32167559ae9e2fbf60dd956fdc25155e5487dce0
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-02-25 22:12:28 +0000

    git-svn-id: https://scalaz.googlecode.com/svn/trunk@147 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scalaz/validation/Validator.scala
+++ src/main/scalaz/validation/Validator.scala
@@ -12,6 +12,8 @@ package scalaz.validation
 object Validator {
   import control.Kleisli
   import control.Kleisli._
+  import control.FoldLeft
+  import control.FoldLeftW._
   import list.NonEmptyList
 
   import Validation.{success, fail}
@@ -39,12 +41,41 @@ object Validator {
   def isDigits = kleisli[Option]((ds: List[Char]) => isDigit.traverses(ds))
 
   /**
-   * A validator for a maximum length of a list.
+   * A validator for a minimum value of an integer.
    */
-  def lengthMax[A](max: Int) = filter((as: List[A]) => as.length <= max)
+  def intMin(min: Int) = filter((n: Int) => n >= min)
+
+  /**
+   * A validator for a maximum value of an integer.
+   */
+  def intMax(max: Int) = filter((n: Int) => n <= max)
 
   /**
    * A validator for ensuring an int value is within a range.
    */
-  def intRange(min: Int, max: Int) = filter((n: Int) => n >= min && n <= max)
+  def intRange(min: Int, max: Int) = intMin(min) >=> intMax(max)
+
+  /**
+   * A validator for a minimum length of a list.
+   */
+  def lengthMin[F[_]](min: Int)(implicit f: FoldLeft[F]) = intMin(min) comap ((x: F[_]) => {
+    implicit val k = foldleft[F](x)
+    k.items
+  })
+
+  /**
+   * A validator for a maximum length of a list.
+   */
+  def lengthMax[F[_]](max: Int)(implicit f: FoldLeft[F]) = intMax(max) comap ((x: F[_]) => {
+    implicit val k = foldleft[F](x)
+    k.items
+  })
+
+  /**
+   * A validator for a range of the length of a list.
+   */
+  def lengthRange[F[_]](min: Int, max: Int)(implicit f: FoldLeft[F]) = intRange(min, max) comap ((x: F[_]) => {
+    implicit val k = foldleft[F](x)
+    k.items
+  })
 }
