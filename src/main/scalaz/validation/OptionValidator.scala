84344f7799f1bae1122439ef5b4653511731d776
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-03-03 07:03:46 +0000

    git-svn-id: https://scalaz.googlecode.com/svn/trunk@165 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scalaz/validation/OptionValidator.scala
+++ src/main/scalaz/validation/OptionValidator.scala
@@ -30,6 +30,19 @@ sealed trait OptionValidator[-A, B] {
    * a non-empty list.
    */
   def ^^^[E](e: => E) = k.compose[PartialType[Validation, NonEmptyList[E]]#Apply](_ toSuccess nel(e))
+
+  /**
+   * Lift the underlying kleisli structure from <code>Option</code> to <code>Validation</code> using the given error.
+   */
+  def ^^-[E, AA <: A](e: AA => E) = kleisli[PartialType[Validation, E]#Apply]((a: AA) =>
+    k(a) toSuccess (e(a)))
+
+  /**
+   * Lift the underlying kleisli structure from <code>Option</code> to <code>Validation</code> using the given error in
+   * a non-empty list.
+   */
+  def ^^^-[E, AA <: A](e: AA => E) = kleisli[PartialType[Validation, NonEmptyList[E]]#Apply]((a: AA) =>
+    k(a) toSuccess (nel(e(a))))
 }
 
 /**
