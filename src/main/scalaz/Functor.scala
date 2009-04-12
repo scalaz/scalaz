4b2fbc3e8117220e872590108b7181f3f6601144
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-04-12 07:30:00 +0000

    git-svn-id: https://scalaz.googlecode.com/svn/branches/scalaz4@264 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scalaz/Functor.scala
+++ src/main/scalaz/Functor.scala
@@ -13,7 +13,53 @@ object Functor {
     def fmap[A, B](r: Continuation[R, A], f: A => B) = Continuation.continuation[R, B](k => r(k compose f))
   }
 
+  implicit val NonEmptyListFunctor = new Functor[NonEmptyList] {
+    def fmap[A, B](r: NonEmptyList[A], f: A => B) = r map f
+  }
+
+  implicit def StateFunctor[S] = new Functor[PartialApply1Of2[State, S]#Apply] {
+    def fmap[A, B](r: State[S, A], f: A => B) = r map f
+  }
+
+  implicit val Tuple1Functor = new Functor[Tuple1] {
+    def fmap[A, B](r: Tuple1[A], f: A => B) = Tuple1(f(r._1))
+  }
+
+  implicit def Tuple2Functor[R] = new Functor[PartialApply1Of2[Tuple2, R]#Apply] {
+    def fmap[A, B](r: (R, A), f: A => B) = (r._1, f(r._2))
+  }
+
+  implicit val Function0Functor = new Functor[Function0] {
+    def fmap[A, B](r: Function0[A], f: A => B) = new Function0[B] {
+      def apply = f(r.apply)
+    }
+  }
+
+  implicit def Function1Functor[R] = new Functor[PartialApply1Of2[Function1, R]#Apply] {
+    def fmap[A, B](r: R => A, f: A => B) = r andThen f
+  }
+
+  implicit val ListFunctor = new Functor[List] {
+    def fmap[A, B](r: List[A], f: A => B) = r map f
+  }
+
+  implicit val StreamFunctor = new Functor[Stream] {
+    def fmap[A, B](r: Stream[A], f: A => B) = r map f
+  }
+
   implicit val OptionFunctor = new Functor[Option] {
     def fmap[A, B](r: Option[A], f: A => B) = r map f
+  }
+
+  implicit val ArrayFunctor = new Functor[Array] {
+    def fmap[A, B](r: Array[A], f: A => B) = r map f
+  }
+
+  implicit def EitherLeftFunctor[X] = new Functor[PartialApply1Of2[Either.LeftProjection, X]#Flip] {
+    def fmap[A, B](r: Either.LeftProjection[A, X], f: A => B) = r.map(f).left
+  }
+
+  implicit def EitherRightFunctor[X] = new Functor[PartialApply1Of2[Either.RightProjection, X]#Apply] {
+    def fmap[A, B](r: Either.RightProjection[X, A], f: A => B) = r.map(f).right
   }
 }
