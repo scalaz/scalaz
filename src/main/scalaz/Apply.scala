0d44f199154be0ec892444e4e2121be57aba58c8
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-04-12 10:30:54 +0000

    git-svn-id: https://scalaz.googlecode.com/svn/branches/scalaz4@270 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scalaz/Apply.scala
+++ src/main/scalaz/Apply.scala
@@ -15,5 +15,36 @@ object Apply {
 
   implicit def ContinuationApply[R] = FunctorBindApply[PartialApply1Of2[Continuation, R]#Apply]
 
-  implicit def OptionApply = FunctorBindApply[Option]
+  implicit val NonEmptyListApply = FunctorBindApply[NonEmptyList]
+
+  implicit def StateApply[S] = FunctorBindApply[PartialApply1Of2[State, S]#Apply]
+
+  implicit val Tuple1Apply = FunctorBindApply[Tuple1]
+
+  implicit val Function0Apply = FunctorBindApply[Function0]
+
+  implicit def Function1Apply[R] = FunctorBindApply[PartialApply1Of2[Function1, R]#Apply]
+
+  implicit def Function2Apply[R, S] = FunctorBindApply[PartialApply2Of3[Function2, R, S]#Apply]
+
+  implicit def Function3Apply[R, S, T] = FunctorBindApply[PartialApply3Of4[Function3, R, S, T]#Apply]
+
+  implicit def Function4Apply[R, S, T, U] = FunctorBindApply[PartialApply4Of5[Function4, R, S, T, U]#Apply]
+
+  implicit def Function5Apply[R, S, T, U, V] = FunctorBindApply[PartialApply5Of6[Function5, R, S, T, U, V]#Apply]
+
+  implicit def Function6Apply[R, S, T, U, V, W] = FunctorBindApply[PartialApply6Of7[Function6, R, S, T, U, V, W]#Apply]
+
+  implicit val ListApply = FunctorBindApply[List]
+
+  implicit val StreamApply = FunctorBindApply[Stream]
+
+  implicit val OptionApply = FunctorBindApply[Option]
+
+  implicit val ArrayApply = FunctorBindApply[Array]
+
+  implicit def EitherLeftApply[X] = FunctorBindApply[PartialApply1Of2[Either.LeftProjection, X]#Flip]
+
+  implicit def EitherRightApply[X] = FunctorBindApply[PartialApply1Of2[Either.RightProjection, X]#Apply]
+
 }
