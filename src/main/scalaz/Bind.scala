ed316c43e90203d259d67fe3d68b32e25b9cfaf4
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-04-12 10:18:47 +0000

    git-svn-id: https://scalaz.googlecode.com/svn/branches/scalaz4@268 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scalaz/Bind.scala
+++ src/main/scalaz/Bind.scala
@@ -5,6 +5,7 @@ trait Bind[Z[_]] {
 }
 
 object Bind {
+  /*
   implicit val IdentityBind: Bind[Identity] = new Bind[Identity] {
     def bind[A, B](a: Identity[A], f: A => Identity[B]) = f(a.value)
   }
@@ -15,5 +16,73 @@ object Bind {
 
   implicit def OptionBind: Bind[Option] = new Bind[Option] {
     def bind[A, B](a: Option[A], f: A => Option[B]) = a flatMap f
+  }
+    */
+  implicit val IdentityBind: Bind[Identity] = new Bind[Identity] {
+    def bind[A, B](a: Identity[A], f: A => Identity[B]) = f(a.value)
+  }
+
+  implicit def ContinuationBind[R]: Bind[PartialApply1Of2[Continuation, R]#Apply] = new Bind[PartialApply1Of2[Continuation, R]#Apply] {
+    def bind[A, B](a: Continuation[R, A], f: A => Continuation[R, B]) = Continuation.continuation[R, B](c => a(p => f(p)(c)))
+  }
+
+  implicit val NonEmptyListBind = new Bind[NonEmptyList] {
+    def bind[A, B](r: NonEmptyList[A], f: A => NonEmptyList[B]) = r flatMap f
+  }
+
+  implicit def StateBind[S] = new Bind[PartialApply1Of2[State, S]#Apply] {
+    def bind[A, B](r: State[S, A], f: A => State[S, B]) = r flatMap f
+  }
+
+  implicit val Function0Bind = new Bind[Function0] {
+    def bind[A, B](r: Function0[A], f: A => Function0[B]) = f(r.apply)
+  }
+
+  implicit def Function1Bind[R] = new Bind[PartialApply1Of2[Function1, R]#Apply] {
+    def bind[A, B](r: R => A, f: A => R => B) = (t: R) => f(r(t))(t)
+  }
+
+  implicit def Function2Bind[R, S] = new Bind[PartialApply2Of3[Function2, R, S]#Apply] {
+    def bind[A, B](r: (R, S) => A, f: A => (R, S) => B) = (t1: R, t2: S) => f(r(t1, t2))(t1, t2)
+  }
+
+  implicit def Function3Bind[R, S, T] = new Bind[PartialApply3Of4[Function3, R, S, T]#Apply] {
+    def bind[A, B](r: (R, S, T) => A, f: A => (R, S, T) => B) = (t1: R, t2: S, t3: T) => f(r(t1, t2, t3))(t1, t2, t3)
+  }
+
+  implicit def Function4Bind[R, S, T, U] = new Bind[PartialApply4Of5[Function4, R, S, T, U]#Apply] {
+    def bind[A, B](r: (R, S, T, U) => A, f: A => (R, S, T, U) => B) = (t1: R, t2: S, t3: T, t4: U) => f(r(t1, t2, t3, t4))(t1, t2, t3, t4)
+  }
+
+  implicit def Function5Bind[R, S, T, U, V] = new Bind[PartialApply5Of6[Function5, R, S, T, U, V]#Apply] {
+    def bind[A, B](r: (R, S, T, U, V) => A, f: A => (R, S, T, U, V) => B) = (t1: R, t2: S, t3: T, t4: U, t5: V) => f(r(t1, t2, t3, t4, t5))(t1, t2, t3, t4, t5)
+  }
+
+  implicit def Function6Bind[R, S, T, U, V, W] = new Bind[PartialApply6Of7[Function6, R, S, T, U, V, W]#Apply] {
+    def bind[A, B](r: (R, S, T, U, V, W) => A, f: A => (R, S, T, U, V, W) => B) = (t1: R, t2: S, t3: T, t4: U, t5: V, t6: W) => f(r(t1, t2, t3, t4, t5, t6))(t1, t2, t3, t4, t5, t6)
+  }
+
+  implicit val ListBind = new Bind[List] {
+    def bind[A, B](r: List[A], f: A => List[B]) = r flatMap f
+  }
+
+  implicit val StreamBind = new Bind[Stream] {
+    def bind[A, B](r: Stream[A], f: A => Stream[B]) = r flatMap f
+  }
+
+  implicit val OptionBind = new Bind[Option] {
+    def bind[A, B](r: Option[A], f: A => Option[B]) = r flatMap f
+  }
+
+  implicit val ArrayBind = new Bind[Array] {
+    def bind[A, B](r: Array[A], f: A => Array[B]) = r flatMap f
+  }
+
+  implicit def EitherLeftBind[X] = new Bind[PartialApply1Of2[Either.LeftProjection, X]#Flip] {
+    def bind[A, B](r: Either.LeftProjection[A, X], f: A => Either.LeftProjection[B, X]) = r.flatMap(f(_).e).left
+  }
+
+  implicit def EitherRightBind[X] = new Bind[PartialApply1Of2[Either.RightProjection, X]#Apply] {
+    def bind[A, B](r: Either.RightProjection[X, A], f: A => Either.RightProjection[X, B]) = r.flatMap(f(_).e).right
   }
 }
--- src/main/scalaz/Pure.scala
+++ src/main/scalaz/Pure.scala
@@ -5,7 +5,7 @@ trait Pure[P[_]] {
 }
 
 object Pure {
-  implicit val IdentityPure = new Pure[Identity] {
+  implicit val IdentityPure: Pure[Identity] = new Pure[Identity] {
     def pure[A](a: A) = Identity.id(a)
   }
 
@@ -13,7 +13,73 @@ object Pure {
     def pure[A](a: A) = Continuation.continuation[R, A](_(a))
   }
 
-  implicit def OptionPure = new Pure[Option] {
+  implicit val NonEmptyListPure = new Pure[NonEmptyList] {
+    def pure[A](a: A) = NonEmptyList.nel(a)
+  }
+
+  implicit def StatePure[S] = new Pure[PartialApply1Of2[State, S]#Apply] {
+    def pure[A](a: A) = State.value[S](a)
+  }
+
+  implicit val Tuple1Pure = new Pure[Tuple1] {
+    def pure[A](a: A) = Tuple1(a)
+  }
+
+  implicit val Function0Pure = new Pure[Function0] {
+    def pure[A](a: A) = new Function0[A] {
+      def apply = a
+    }
+  }
+
+  implicit def Function1Pure[R] = new Pure[PartialApply1Of2[Function1, R]#Apply] {
+    def pure[A](a: A) = (_: R) => a
+  }
+
+  implicit def Function2Pure[R, S] = new Pure[PartialApply2Of3[Function2, R, S]#Apply] {
+    def pure[A](a: A) = (_: R, _: S) => a
+  }
+
+  implicit def Function3Pure[R, S, T] = new Pure[PartialApply3Of4[Function3, R, S, T]#Apply] {
+    def pure[A](a: A) = (_: R, _: S , _: T ) => a
+  }
+
+  implicit def Function4Pure[R, S, T, U] = new Pure[PartialApply4Of5[Function4, R, S, T, U]#Apply] {
+    def pure[A](a: A) = (_: R, _: S , _: T , _: U) => a
+  }
+
+  implicit def Function5Pure[R, S, T, U, V] = new Pure[PartialApply5Of6[Function5, R, S, T, U, V]#Apply] {
+    def pure[A](a: A) = (_: R, _: S , _: T , _: U, _: V) => a
+  }
+
+  implicit def Function6Pure[R, S, T, U, V, W] = new Pure[PartialApply6Of7[Function6, R, S, T, U, V, W]#Apply] {
+    def pure[A](a: A) = (_: R, _: S , _: T , _: U, _: V, _: W) => a
+  }
+
+  implicit val ListPure = new Pure[List] {
+    def pure[A](a: A) = List(a)
+  }
+
+  implicit val StreamPure = new Pure[Stream] {
+    def pure[A](a: A) = Stream(a)
+  }
+
+  implicit val OptionPure = new Pure[Option] {
     def pure[A](a: A) = Some(a)
+  }
+
+  implicit val ArrayPure = new Pure[Array] {
+    def pure[A](a: A) = Array.make(1, a)
+  }
+
+  implicit def EitherLeftPure[X] = new Pure[PartialApply1Of2[Either.LeftProjection, X]#Flip] {
+    def pure[A](a: A) = Left(a).left
+  }
+
+  implicit def EitherRightPure[X] = new Pure[PartialApply1Of2[Either.RightProjection, X]#Apply] {
+    def pure[A](a: A) = Right(a).right
+  }
+
+  implicit val RandomAccessSeqPure = new Pure[RandomAccessSeq] {
+    def pure[A](a: A) = RandomAccessSeq(a)
   }
 }
