1339a9882638ab913b6e3b5d9a4f8cb10b9b007b
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-04-13 05:13:26 +0000

    git-svn-id: https://scalaz.googlecode.com/svn/branches/scalaz4@285 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scalaz/FoldLeft.scala
+++ src/main/scalaz/FoldLeft.scala
@@ -9,7 +9,7 @@ object FoldLeft {
     def foldLeft[B, A](t: Identity[A], b: B, f: (B, A) => B) = f(b, t.value)
   }
 
-  implicit def NonEmptyListFoldLeft[A] = new FoldLeft[NonEmptyList] {
+  implicit val NonEmptyListFoldLeft = new FoldLeft[NonEmptyList] {
     def foldLeft[B, A](t: NonEmptyList[A], b: B, f: (B, A) => B) = t.list.foldLeft(b)(f)
   }
 
@@ -17,38 +17,38 @@ object FoldLeft {
     def foldLeft[B, A](t: State[Unit, A], b: B, f: (B, A) => B) = f(b, t(())._2)
   }
 
-  implicit def Tuple1FoldLeft[A] = new FoldLeft[Tuple1] {
+  implicit val Tuple1FoldLeft = new FoldLeft[Tuple1] {
     def foldLeft[B, A](t: Tuple1[A], b: B, f: (B, A) => B) = f(b, t._1)
   }
 
-  implicit def Function0FoldLeft[A] = new FoldLeft[Function0] {
+  implicit val Function0FoldLeft = new FoldLeft[Function0] {
     def foldLeft[B, A](t: Function0[A], b: B, f: (B, A) => B) = f(b, t.apply)
   }
 
-  implicit def OptionFoldLeft[A] = new FoldLeft[Option] {
+  implicit val OptionFoldLeft = new FoldLeft[Option] {
     def foldLeft[B, A](t: Option[A], b: B, f: (B, A) => B) = t match {
       case Some(a) => f(b, a)
       case None => b
     }
   }
 
-  implicit def ArrayFoldLeft[A] = new FoldLeft[Array] {
+  implicit val ArrayFoldLeft = new FoldLeft[Array] {
     def foldLeft[B, A](t: Array[A], b: B, f: (B, A) => B) = t.foldLeft(b)(f)
   }
 
   implicit def EitherLeftFoldLeft[X] = new FoldLeft[PartialApply1Of2[Either.LeftProjection, X]#Flip] {
-    def foldLeft[B, A](e: Either.LeftProjection[A, X], b: B, f: (B, A) => B) = error("")
+    def foldLeft[B, A](e: Either.LeftProjection[A, X], b: B, f: (B, A) => B) = OptionFoldLeft.foldLeft(e.toOption, b ,f)
   }
 
   implicit def EitherRightFoldLeft[X] = new FoldLeft[PartialApply1Of2[Either.RightProjection, X]#Apply] {
-    def foldLeft[B, A](e: Either.RightProjection[X, A], b: B, f: (B, A) => B) = error("")
+    def foldLeft[B, A](e: Either.RightProjection[X, A], b: B, f: (B, A) => B) = OptionFoldLeft.foldLeft(e.toOption, b ,f)
   }
 
-  implicit def IterableFoldLeft[A] = new FoldLeft[Iterable] {
+  implicit val IterableFoldLeft = new FoldLeft[Iterable] {
     def foldLeft[B, A](t: Iterable[A], b: B, f: (B, A) => B) = t.foldLeft(b)(f)
   }
 
-  implicit def JavaIterableFoldLeft[A] = new FoldLeft[java.lang.Iterable] {
+  implicit val JavaIterableFoldLeft = new FoldLeft[java.lang.Iterable] {
     def foldLeft[B, A](t: java.lang.Iterable[A], b: B, f: (B, A) => B) = {
       var x = b
       val i = t.iterator
--- src/main/scalaz/FoldRight.scala
+++ src/main/scalaz/FoldRight.scala
@@ -8,4 +8,57 @@ object FoldRight {
   implicit val IdentityFoldRight = new FoldRight[Identity] {
     def foldRight[A, B](t: Identity[A], b: B, f: (A, => B) => B) = f(t.value, b)
   }
+
+  implicit val NonEmptyListFoldRight = new FoldRight[NonEmptyList] {
+    def foldRight[A, B](t: NonEmptyList[A], b: B, f: (A, => B) => B) = IterableFoldRight.foldRight(t.list, b, f)
+  }
+
+  implicit val StateFoldRight = new FoldRight[PartialApply1Of2[State, Unit]#Apply] {
+    def foldRight[A, B](t: State[Unit, A], b: B, f: (A, => B) => B) = f(t(())._2, b)
+  }
+
+  implicit val Tuple1FoldRight = new FoldRight[Tuple1] {
+    def foldRight[A, B](t: Tuple1[A], b: B, f: (A, => B) => B) = f(t._1, b)
+  }
+
+  implicit val Function0FoldRight = new FoldRight[Function0] {
+    def foldRight[A, B](t: Function0[A], b: B, f: (A, => B) => B) = f(t.apply, b)
+  }
+
+  implicit val OptionFoldRight = new FoldRight[Option] {
+    def foldRight[A, B](t: Option[A], b: B, f: (A, => B) => B) = t match {
+      case Some(a) => f(a, b)
+      case None => b
+    }
+  }
+
+  implicit val ArrayFoldRight = new FoldRight[Array] {
+    def foldRight[A, B](t: Array[A], b: B, f: (A, => B) => B) = t.foldRight(b)(f(_, _))
+  }
+
+  implicit def EitherLeftFoldRight[X] = new FoldRight[PartialApply1Of2[Either.LeftProjection, X]#Flip] {
+    def foldRight[A, B](e: Either.LeftProjection[A, X], b: B, f: (A, => B) => B) = OptionFoldRight.foldRight(e.toOption, b, f)
+  }
+
+  implicit def EitherRightFoldRight[X] = new FoldRight[PartialApply1Of2[Either.RightProjection, X]#Apply] {
+    def foldRight[A, B](e: Either.RightProjection[X, A], b: B, f: (A, => B) => B) = OptionFoldRight.foldRight(e.toOption, b, f)
+  }
+
+  implicit val IterableFoldRight = new FoldRight[Iterable] {
+    def foldRight[A, B](t: Iterable[A], b: B, f: (A, => B) => B): B = t.foldRight(b)(f(_, _))
+  }
+
+  implicit def JavaListFoldRight[A] = new FoldRight[java.util.List] {
+    def foldRight[A, B](t: java.util.List[A], b: B, f: (A, => B) => B) = {
+      var x = b
+      val i = t.listIterator
+
+      while(i.hasPrevious) {
+        val n = i.previous
+        x = f(n, x)
+      }
+
+      x
+    }
+  }
 }
