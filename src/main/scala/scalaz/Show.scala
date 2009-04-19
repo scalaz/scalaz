b912db34e5411f0103520af51724105f730b52bb
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-04-19 11:43:28 +0000

    .
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@366 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scala/scalaz/Show.scala
+++ src/main/scala/scalaz/Show.scala
@@ -13,7 +13,40 @@ object Show {
 
   def shows[A](f: A => String) = show[A](f(_).toList)
 
-  def showA[A] = show[A](_.toString.toList)
+  def showA[A] = shows[A](_.toString)
 
-  implicit def StringShow = showA[String]
+  implicit val IntShow = showA[Int]
+
+  implicit val BooleanShow = showA[Boolean]
+
+  implicit val CharShow = showA[Char]
+
+  implicit val ByteShow = showA[Byte]
+
+  implicit val LongShow = showA[Long]
+
+  implicit val ShortShow = showA[Short]
+
+  implicit val FloatShow = showA[Float]
+
+  implicit val DoubleShow = showA[Double]
+
+  implicit val StringShow = showA[String]
+
+  import S._
+  
+  implicit def IterableShow[A](implicit sa: Show[A]) = show[Iterable[A]](as => {
+    val i = as.elements
+    val k = new collection.mutable.ListBuffer[Char]
+    while(i.hasNext) {
+      val n = i.next
+      k ++= n.show
+      if(i.hasNext)
+        k += ','
+    }
+    k.toList
+  })
+
+  implicit def OptionShow[A](implicit sa: Show[A]) = shows[Option[A]](_ map (_.shows) toString)
+
 }
