231b018288d9201b0c0aeff6a4a28736f0cff8a5
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-04-19 07:54:43 +0000

    .
    git-svn-id: https://scalaz.googlecode.com/svn/branches/scalaz4@346 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scalaz/Equal.scala
+++ src/main/scalaz/Equal.scala
@@ -9,5 +9,61 @@ object Equal {
     def equal(a1: A, a2: A) = f(a1, a2)
   }
 
-  implicit val StringEqual = equal[String](_ == _)
+  def equalA[A] = equal[A](_ == _)
+
+  implicit val StringEqual = equalA[String]
+
+  implicit val IntEqual = equalA[Int]
+
+  implicit val BooleanEqual = equalA[Boolean]
+
+  implicit val CharEqual = equalA[Char]
+
+  implicit val ByteEqual = equalA[Byte]
+
+  implicit val LongEqual = equalA[Long]
+  
+  implicit val ShortEqual = equalA[Short]
+
+  implicit val FloatEqual = equalA[Float]
+  
+  implicit val DoubleEqual = equalA[Double]
+
+  implicit def IterableEqual[A](implicit ea: Equal[A]) = equal[Iterable[A]]((a1, a2) => {
+    import S._
+    
+    val i1 = a1.elements
+    val i2 = a2.elements
+    var b = false
+
+    while(i1.hasNext && i2.hasNext && !b) {
+      val x1 = i1.next
+      val x2 = i2.next
+
+      if(x1 /= x2) {
+        b = true
+      }
+    }
+
+    !(b || i1.hasNext || i2.hasNext) 
+  })
+
+  implicit def JavaIterableEqual[A](implicit ea: Equal[A]) = equal[java.lang.Iterable[A]]((a1, a2) => {
+    import S._
+
+    val i1 = a1.iterator
+    val i2 = a2.iterator
+    var b = false
+
+    while(i1.hasNext && i2.hasNext && !b) {
+      val x1 = i1.next
+      val x2 = i2.next
+
+      if(x1 /= x2) {
+        b = true
+      }
+    }
+
+    !(b || i1.hasNext || i2.hasNext)
+  })
 }
