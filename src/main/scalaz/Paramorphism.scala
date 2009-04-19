d2d69d9989c978ded8a8eb1e1ecd790bdb7122ff
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-04-19 06:14:26 +0000

    .
    git-svn-id: https://scalaz.googlecode.com/svn/branches/scalaz4@341 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scalaz/Paramorphism.scala
+++ src/main/scalaz/Paramorphism.scala
@@ -5,6 +5,21 @@ trait Paramorphism[P[_]] {
 }
 
 object Paramorphism {
+  implicit val ListParamorphism = new Paramorphism[List] {
+    override def para[A, B](as: List[A], b: B, f: ((=> A, => List[A], B) => B)): B = as match {
+      case Nil => b
+      case a :: as => f(a, as, para(as, b, f))
+    }
+  }
+
+  implicit val StreamParamorphism = new Paramorphism[Stream] {
+    override def para[A, B](as: Stream[A], b: B, f: ((=> A, => Stream[A], B) => B)): B =
+      if(as.isEmpty)
+        b
+      else
+        f(as.head, as.tail, para(as.tail, b, f))
+  }
+
   implicit val OptionParamorphism = new Paramorphism[Option] {
     def para[A, B](as: Option[A], b: B, f: ((=> A, => Option[A], B) => B)): B = as match {
       case None => b
