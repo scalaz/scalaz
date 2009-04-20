f27588944274a3af50db89425683193e1238156f
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-04-20 22:24:20 +0000

    Zipper constructor
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@396 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scala/scalaz/MA.scala
+++ src/main/scala/scalaz/MA.scala
@@ -238,7 +238,9 @@ object MA {
   implicit def EitherLeftMA[X, A](a: Either.LeftProjection[A, X]) = ma[PartialApply1Of2[Either.LeftProjection, X]#Flip](a)
 
   implicit def EitherRightMA[X, A](a: Either.RightProjection[X, A]) = ma[PartialApply1Of2[Either.RightProjection, X]#Apply](a)
-  
+
+  implicit def ZipperMA[A](a: Zipper[A]) = ma[Zipper](a)
+
   import java.util._
   import java.util.concurrent._
 
