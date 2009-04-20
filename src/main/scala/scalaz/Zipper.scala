2a66065f6a46be7799aa4596ff9eadafd287c4df
runarorama <runarorama@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-04-20 22:56:02 +0000

    Zipper
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@397 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scala/scalaz/Zipper.scala
+++ src/main/scala/scalaz/Zipper.scala
@@ -1,5 +1,7 @@
 package scalaz
 
+import MA._
+
 /**
  * Provides a pointed stream, which is a non-empty zipper-like stream structure that tracks an index (focus)
  * position in a stream. Focus can be moved forward and backwards through the stream, elements can be inserted
@@ -14,6 +16,50 @@ sealed trait Zipper[+A] extends Iterable[A] {
   val rights: Stream[A]
 
   def elements = (lefts.reverse ++ Stream.cons(focus, rights)).elements
+
+  def next = rights match {
+    case Stream.empty => None
+    case rs => Some(tryNext)
+  }
+
+  def tryNext = rights match {
+    case Stream.empty => error("cannot move to next element")
+    case Stream.cons(r, rs) => zipper(Stream.cons(focus, lefts), r, rs)
+  }
+
+  def previous = lefts match {
+    case Stream.empty => None
+    case ls => Some(tryPrevious)
+  }
+
+  def tryPrevious = lefts match {
+    case Stream.empty => error("cannot move to previous element")
+    case Stream.cons(l, ls) => zipper(ls, l, Stream.cons(focus, rights))
+  }
+
+  def insert = insertRight
+
+  def insertLeft(y: A) = zipper(lefts, y, Stream.cons(focus, rights))
+
+  def insertRight(y: A) = zipper(Stream.cons(focus, lefts), y, rights)
+
+  def delete = deleteRight
+
+  def deleteLeft = (lefts, rights) match {
+    case (Stream.empty, Stream.empty) => None
+    case (Stream.cons(l, ls), rs) => Some(zipper(ls, l, rs))
+    case (Stream.empty, Stream.cons(r, rs)) => Some(zipper(Stream.empty, r, rs))
+  }
+
+  def deleteRight = (lefts, rights) match {
+    case (Stream.empty, Stream.empty) => None
+    case (Stream.cons(l, ls), rs) => Some(zipper(ls, l, rs))
+    case (Stream.empty, Stream.cons(r, rs)) => Some(zipper(Stream.empty, r, rs))
+  }
+
+  def deleteOthers = zipper(Stream.empty, focus, Stream.empty)
+
+  def length = foldr(const(_ + 1)) 0
 }
 
 object Zipper {
