e28dc097bdbfc90acf3c5228db330e4fa0128e44
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-04-20 09:52:53 +0000

    .
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@386 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scala/scalaz/Zero.scala
+++ src/main/scala/scalaz/Zero.scala
@@ -32,4 +32,62 @@ object Zero {
   implicit val FloatZero = zero(0F)
 
   implicit val DoubleZero = zero(0D)
+
+  implicit def ListZero[A] = zero[List[A]](Nil)
+
+  implicit def StreamZero[A] = zero[Stream[A]](Stream.empty)
+
+  implicit def OptionZero[A] = zero[Option[A]](None)
+
+  implicit def ArrayZero[A] = zero(new Array[A](0))
+
+  implicit def EitherLeftZero[A, B](implicit za: Zero[A]) = zero[Either.LeftProjection[A, B]](Left(za.zero).left)
+
+  implicit def EitherRightZero[A, B](implicit za: Zero[A]) = zero[Either.RightProjection[B, A]](Right(za.zero).right)
+
+  import java.util._
+  import java.util.concurrent._
+
+  implicit def JavaArrayListZero[A] = zero(new ArrayList[A])
+
+  implicit def JavaHashMapZero[K, V] = zero(new HashMap[K, V])
+
+  implicit def JavaHashSetZero[A] = zero(new HashSet[A])
+
+  implicit def JavaHashtableZero[K, V] = zero(new Hashtable[K, V])
+
+  implicit def JavaIdentityHashMapZero[K, V] = zero(new IdentityHashMap[K, V])
+
+  implicit def JavaLinkedHashMapZero[K, V] = zero(new LinkedHashMap[K, V])
+
+  implicit def JavaLinkedHashSetZero[A] = zero(new LinkedHashSet[A])
+
+  implicit def JavaLinkedListZero[A] = zero(new LinkedList[A])
+
+  implicit def JavaPriorityQueueZero[A] = zero(new PriorityQueue[A])
+
+  implicit def JavaStackZero[A] = zero(new Stack[A])
+
+  implicit def JavaTreeMapZero[K, V] = zero(new TreeMap[K, V])
+
+  implicit def JavaTreeSetZero[A] = zero(new TreeSet[A])
+
+  implicit def JavaVectorZero[A] = zero(new Vector[A])
+
+  implicit def JavaWeakHashMapZero[K, V] = zero(new WeakHashMap[K, V])
+
+  implicit def JavaArrayBlockingQueueZero[A] = zero(new ArrayBlockingQueue[A](0))
+
+  implicit def JavaConcurrentHashMapZero[K, V] = zero(new ConcurrentHashMap[K, V])
+
+  implicit def JavaConcurrentLinkedQueueZero[A] = zero(new ConcurrentLinkedQueue[A])
+
+  implicit def JavaCopyOnWriteArrayListZero[A] = zero(new CopyOnWriteArrayList[A])
+
+  implicit def JavaCopyOnWriteArraySetZero[A] = zero(new CopyOnWriteArraySet[A])
+
+  implicit def JavaLinkedBlockingQueueZero[A] = zero(new LinkedBlockingQueue[A])
+
+  implicit def JavaSynchronousQueueZero[A] = zero(new SynchronousQueue[A])
+
 }
