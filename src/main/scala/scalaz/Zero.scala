68ae45bee33ba3d57a95dd2ccab43db5661c0ad2
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-04-20 08:16:18 +0000

    .
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@380 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scala/scalaz/Zero.scala
+++ src/main/scala/scalaz/Zero.scala
@@ -11,7 +11,25 @@ object Zero {
     val zero = z
   }
 
+  implicit val OrderingZero = zero(EQ)
+
   implicit val UnitZero = zero(())
 
   implicit val StringZero = zero("")
+
+  implicit val IntZero = zero(0)
+
+  implicit val BooleanZero = zero(false)
+
+  implicit val CharZero = zero(0.toChar)
+
+  implicit val ByteZero = zero(0.toByte)
+
+  implicit val LongZero = zero(0L)
+
+  implicit val ShortZero = zero(0.toShort)
+
+  implicit val FloatZero = zero(0F)
+
+  implicit val DoubleZero = zero(0D)
 }
