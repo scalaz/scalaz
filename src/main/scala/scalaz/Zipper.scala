ab8b4a9dc55dd338facaabcd2aea87309bc0c0cd
runarorama <runarorama@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-04-21 17:11:09 +0000

    git-svn-id: https://scalaz.googlecode.com/svn/trunk@432 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scala/scalaz/Zipper.scala
+++ src/main/scala/scalaz/Zipper.scala
@@ -20,50 +20,97 @@ sealed trait Zipper[A] extends Iterable[A] {
   import MA._
   import S._
 
+  /**
+   * Possibly moves to next element to the right of focus.
+   */
   def next = rights match {
     case Stream.empty => None
     case Stream.cons(r, rs) => Some(zipper(Stream.cons(focus, lefts), r, rs))
   }
 
+  /**
+   * Moves to the next element to the right of focus, or error if there is no element on the right.
+   */
   def tryNext = next err "cannot move to next element"
 
+  /**
+   * Possibly moves to the previous element to the left of focus.
+   */
   def previous = lefts match {
     case Stream.empty => None
     case Stream.cons(l, ls) => Some(zipper(ls, l, Stream.cons(focus, rights)))
   }
 
+  /**
+   * Moves to the previous element to the left of focus, or error if there is no element on the left.
+   */
   def tryPrevious = previous err "cannot move to previous element"
 
+  /**
+   * An alias for insertRight
+   */
   def insert = insertRight(_)
 
+  /**
+   * Inserts an element to the left of focus and focuses on the new element.
+   */
   def insertLeft(y: A) = zipper(lefts, y, Stream.cons(focus, rights))
 
+  /**
+   * Inserts an element to the right of focus and focuses on the new element.
+   */
   def insertRight(y: A) = zipper(Stream.cons(focus, lefts), y, rights)
 
+  /**
+   * An alias for deleteRigth
+   */
   def delete = deleteRight
 
+  /**
+   * Deletes the element at focus and moves the focus to the left. If there is no element on the left,
+   * focus is moved to the right.
+   */
   def deleteLeft = (lefts, rights) match {
     case (Stream.empty, Stream.empty) => None
     case (Stream.cons(l, ls), rs) => Some(zipper(ls, l, rs))
     case (Stream.empty, Stream.cons(r, rs)) => Some(zipper(Stream.empty, r, rs))
   }
 
+  /**
+   * Deletes the element at focus and moves the focus to the right. If there is no element on the right,
+   * focus is moved to the left.
+   */
   def deleteRight = (lefts, rights) match {
     case (Stream.empty, Stream.empty) => None
     case (Stream.cons(l, ls), rs) => Some(zipper(ls, l, rs))
     case (Stream.empty, Stream.cons(r, rs)) => Some(zipper(Stream.empty, r, rs))
   }
 
+  /**
+   * Deletes all elements except the focused element.
+   */
   def deleteOthers = zipper(Stream.empty, focus, Stream.empty)
 
   def length = this.foldr[Int](0, ((a: A, b: Int) => b + 1)(_, _))
 
+  /**
+   * Whether the focus is on the first element in the zipper.
+   */
   def atStart = lefts.isEmpty
 
+  /**
+   * Whether the focus is on the last element in the zipper.
+   */
   def atEnd = rights.isEmpty
 
+  /**
+   * Pairs each element with a boolean indicating whether that element has focus.
+   */
   def withFocus = zipper(lefts.zip(Stream.const(false)), (focus, true), rights.zip(Stream.const(false)))
 
+  /**
+   * Moves focus to the nth element of the zipper, or None if there is no such element.
+   */
   def move(n: Int): Option[Zipper[A]] =
     if (n < 0 || n >= length) None
     else {
@@ -73,6 +120,10 @@ sealed trait Zipper[A] extends Iterable[A] {
       else tryNext.move(n)
     }
 
+  /**
+   * Moves focus to the nearest element matching the given predicate, preferring the left,
+   * or None if no element matches.
+   */
   import StreamW._
   def findZ(p: A => Boolean): Option[Zipper[A]] =
     if (p(focus)) Some(this)
@@ -81,19 +132,33 @@ sealed trait Zipper[A] extends Iterable[A] {
       c.lefts.merge(c.rights).find((x => p(x.focus)))
     }
 
+  /**
+   * Given a traversal function, find the first element along the traversal that matches a given predicate. 
+   */
   def findBy(f: Zipper[A] => Option[Zipper[A]])(p: A => Boolean): Option[Zipper[A]] = {
     f(this) >>= (x => if (p(x.focus)) Some(x) else x.findBy(f)(p))
   }
 
+  /**
+   * Moves focus to the nearest element on the right that matches the given predicate,
+   * or None if there is no such element.
+   */
   def findNext = findBy((z: Zipper[A]) => z.next)(_)
 
+  /**
+   * Moves focus to the previous element on the left that matches the given predicate,
+   * or None if there is no such element. 
+   */
   def findPrevious = findBy((z: Zipper[A]) => z.previous)(_)
 
+  /**
+   * Provides a zipper of all positions of the zipper, with focus on the current position.
+   */
   def positions = {
     val left = unfoldr(((p: Zipper[A]) => p.previous.map(x => (x, x))), this)
     val right = unfoldr(((p: Zipper[A]) => p.next.map(x => (x, x))), this)
     zipper(left, this, right)
-  }  
+  }
 }
 
 object Zipper {
