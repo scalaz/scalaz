906bf783164059d5518e973e415b6b3187f9e32f
runarorama <runarorama@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-04-21 10:17:18 +0000

    git-svn-id: https://scalaz.googlecode.com/svn/trunk@427 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

---
+++ src/main/scala/scalaz/StreamW.scala
@@ -0,0 +1,23 @@
+package scalaz
+
+sealed trait StreamW[A] {
+  val value: Stream[A]
+
+  def string(f: A => Char) = value map f mkString
+
+  def stringj(f: A => Stream[Char]) = value flatMap f mkString
+
+  import StreamW._
+
+  def merge(s: Stream[A]): Stream[A] =
+    if (value.isEmpty) Stream.empty
+    else Stream.cons(value.head, s.merge(value.tail))
+}
+
+object StreamW {
+  implicit def StreamTo[A](as: Stream[A]): StreamW[A] = new StreamW[A] {
+    val value = as
+  }
+
+  implicit def StreamFrom[A](as: StreamW[A]): Stream[A] = as.value
+}
