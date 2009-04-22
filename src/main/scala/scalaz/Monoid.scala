74950f2a3de8fb9c5d654aff387ea2c4405a37bc
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-04-22 04:28:48 +0000

    .
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@467 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scala/scalaz/Monoid.scala
+++ src/main/scala/scalaz/Monoid.scala
@@ -3,87 +3,8 @@ package scalaz
 trait Monoid[M] extends Zero[M] with Semigroup[M]
 
 object Monoid {
-  def monoid[M](implicit s: Semigroup[M], z: Zero[M]) = new Monoid[M] {
+  implicit def monoid[M](implicit s: Semigroup[M], z: Zero[M]) = new Monoid[M] {
     def append(s1: M, s2: => M) = s append (s1, s2)
     val zero = z.zero
   }
-
-  implicit val DigitMonoid = monoid[Digit]
-  
-  implicit val OrderingMonoid = monoid[Ordering]
-
-  implicit val UnitMonoid = monoid[Unit]
-
-  implicit val StringMonoid = monoid[String]
-
-  implicit val IntMonoid = monoid[Int]
-
-  implicit val IntMultiplicationMonoid = monoid[IntMultiplication]
-
-  implicit val BooleanConjunctionMonoid = monoid[BooleanConjunction]
-
-  implicit val BooleanMonoid = monoid[Boolean]
-
-  implicit val CharMonoid = monoid[Char]
-
-  implicit val CharMultiplicationMonoid = monoid[CharMultiplication]
-
-  implicit val ByteMonoid = monoid[Byte]
-
-  implicit val ByteMultiplicationMonoid = monoid[ByteMultiplication]
-
-  implicit val LongMonoid = monoid[Long]
-
-  implicit val LongMultiplicationMonoid = monoid[LongMultiplication]
-
-  implicit val ShortMonoid = monoid[Short]
-
-  implicit val ShortMultiplicationMonoid = monoid[ShortMultiplication]
-
-  implicit val FloatMonoid = monoid[Float]
-
-  implicit val DoubleMonoid = monoid[Double]
-
-  implicit val BigIntegerMonoid = monoid[java.math.BigInteger]
-
-  implicit val BigIntMonoid = monoid[BigInt]
-
-  implicit val NodeSeqMonoid = monoid[xml.NodeSeq]
-
-  implicit def ZipStreamMonoid[A] = monoid[ZipStream[A]]
-  
-  implicit def ListMonoid[A] = monoid[List[A]]
-
-  implicit def StreamMonoid[A] = monoid[Stream[A]]
-
-  implicit def OptionMonoid[A] = monoid[Option[A]]
-
-  implicit def ArrayMonoid[A] = monoid[Array[A]]
-
-  implicit def EitherLeftMonoid[A, B](implicit za: Zero[A]) = monoid[Either.LeftProjection[A, B]]
-
-  implicit def EitherRightMonoid[A, B](implicit za: Zero[A]) = monoid[Either.RightProjection[B, A]]
-
-  import java.util._
-  import java.util.concurrent._
-
-  implicit def JavaArrayListMonoid[A] = monoid[ArrayList[A]]
-
-  implicit def JavaLinkedListMonoid[A] = monoid[LinkedList[A]]
-
-  implicit def JavaPriorityQueueMonoid[A] = monoid[PriorityQueue[A]]
-
-  implicit def JavaStackMonoid[A] = monoid[Stack[A]]
-
-  implicit def JavaVectorMonoid[A] = monoid[Vector[A]]
-
-  implicit def JavaArrayBlockingQueueMonoid[A] = monoid[ArrayBlockingQueue[A]]
-
-  implicit def JavaConcurrentLinkedQueueMonoid[A] = monoid[ConcurrentLinkedQueue[A]]
-
-  implicit def JavaCopyOnWriteArrayListMonoid[A] = monoid[CopyOnWriteArrayList[A]]
-
-  implicit def JavaLinkedBlockingQueueMonoid[A] = monoid[LinkedBlockingQueue[A]]
-
-  implicit def JavaSynchronousQueueMonoid[A] = monoid[SynchronousQueue[A]]
 }
