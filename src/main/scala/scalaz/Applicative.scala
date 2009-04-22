7cd9903ff2850859021adda0735f5c0a9c226e48
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-04-22 01:47:45 +0000

    .
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@436 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scala/scalaz/Applicative.scala
+++ src/main/scala/scalaz/Applicative.scala
@@ -1,10 +1,11 @@
 package scalaz
 
-trait Applicative[Z[_]] extends Pointed[Z] with Apply[Z]
+trait Applicative[Z[_]] extends Pointed[Z] with Apply[Z] {
+  override def fmap[A, B](fa: Z[A], f: A => B) = this(pure(f), fa)  
+}
 
 object Applicative {
   def applicative[Z[_]](implicit p: Pure[Z], a: Apply[Z]) = new Applicative[Z] {
-    def fmap[A, B](fa: Z[A], f: A => B) = a(p.pure(f), fa)
     def pure[A](a: A) = p.pure(a)
     def apply[A, B](f: Z[A => B], x: Z[A]) = a(f, x)
   }
--- src/main/scala/scalaz/Monad.scala
+++ src/main/scala/scalaz/Monad.scala
@@ -1,11 +1,12 @@
 package scalaz
 
-trait Monad[M[_]] extends Applicative[M] with Bind[M]
+trait Monad[M[_]] extends Applicative[M] with Bind[M] {
+  override def fmap[A, B](fa: M[A], f: A => B) = bind(fa, (a: A) => pure(f(a)))
+  override def apply[A, B](f: M[A => B], a: M[A]): M[B] = bind(f, (k: A => B) => fmap(a, k(_: A)))  
+}
 
 object Monad {
   def monad[M[_]](implicit b: Bind[M], p: Pure[M]) = new Monad[M] {
-    def fmap[A, B](fa: M[A], f: A => B) = b.bind(fa, (a: A) => p.pure(f(a)))
-    def apply[A, B](f: M[A => B], a: M[A]): M[B] = b.bind(f, (k: A => B) => fmap(a, k(_: A)))
     def pure[A](a: A) = p.pure(a)
     def bind[A, B](a: M[A], f: A => M[B]) = b.bind(a, f)
   }
