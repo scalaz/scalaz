9cdeb32815e4d499df2504ba10189213054fcba7
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-04-26 02:59:04 +0000

    git-svn-id: https://scalaz.googlecode.com/svn/trunk@523 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scala/scalaz/BKTree.scala
+++ src/main/scala/scalaz/BKTree.scala
@@ -5,9 +5,16 @@ import collection.immutable.IntMap
 // todo broken
 // http://hackage.haskell.org/packages/archive/bktrees/0.2.1/doc/html/src/Data-Set-BKTree.html
 
-sealed trait BKTree[+A]
-private final case class BKTreeNode[+A](value: A, size: Int, children: IntMap[BKTree[A]]) extends BKTree[A]
-private final case object Empty extends BKTree[Nothing]
+sealed trait BKTree[+A] {
+  def isEmpty = this == BKTreeEmpty
+
+  def size = this match {
+    case BKTreeEmpty => 0
+    case BKTreeNode(_, s, _) => s
+  }
+}
+private final case class BKTreeNode[+A](value: A, sz: Int, children: IntMap[BKTree[A]]) extends BKTree[A]
+private final case object BKTreeEmpty extends BKTree[Nothing]
 
 object BKTree {
   
