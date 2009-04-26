97fd15938657a365ac8b6b1c98e36b8a81603641
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-04-26 02:56:42 +0000

    .
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@522 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scala/scalaz/BKTree.scala
+++ src/main/scala/scalaz/BKTree.scala
@@ -3,48 +3,12 @@ package scalaz
 import collection.immutable.IntMap
 
 // todo broken
+// http://hackage.haskell.org/packages/archive/bktrees/0.2.1/doc/html/src/Data-Set-BKTree.html
 
-sealed trait BKTree[+A] {
-  val value: A
-  val children: Map[Int, BKTree[A]]
-
-  import S._
-
-  def +[AA >: A](a: AA)(implicit m: MetricSpace[AA]): BKTree[AA] = {
-    val d = (value: AA) <===> a
-
-    val j = children + ((d, children get d match {
-      case None => a.bktree
-      case Some(cs) => cs + a
-    }))
-
-    BKTree.bktree(a, j)
-  }
-
-  import Stream.cons
-
-  def select(keys: List[Int]): List[BKTree[A]] = keys.foldRight(Nil: List[BKTree[A]])((k, x) => children get k match {
-    case None => x
-    case Some(a) => a :: x
-  })
-
-  def query[AA >: A](a: AA, n: Int)(implicit m: MetricSpace[AA]): List[(Int, AA)] = {
-    val d = (value: AA) <===> a
-
-    val c = select((d - n) to (d + n + 1) toList) flatMap(_.query(a, n))
-
-    if(d <= n)
-      (d, value) :: c
-    else
-      c
-  }
-}
+sealed trait BKTree[+A]
+private final case class BKTreeNode[+A](value: A, size: Int, children: IntMap[BKTree[A]]) extends BKTree[A]
+private final case object Empty extends BKTree[Nothing]
 
 object BKTree {
-  def bktree[A](v: A): BKTree[A] = bktree(v, IntMap.empty)
-
-  private def bktree[A](v: A, c: Map[Int, BKTree[A]]): BKTree[A] = new BKTree[A] {
-    val value = v
-    val children = c
-  }
+  
 }
