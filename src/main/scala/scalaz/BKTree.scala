df28574022d6d7c8d6549955931fe972d32f25e7
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-04-26 05:54:34 +0000

    .
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@524 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scala/scalaz/BKTree.scala
+++ src/main/scala/scalaz/BKTree.scala
@@ -2,20 +2,42 @@ package scalaz
 
 import collection.immutable.IntMap
 
-// todo broken
-// http://hackage.haskell.org/packages/archive/bktrees/0.2.1/doc/html/src/Data-Set-BKTree.html
-
 sealed trait BKTree[+A] {
+  import S._
+  
   def isEmpty = this == BKTreeEmpty
 
   def size = this match {
     case BKTreeEmpty => 0
     case BKTreeNode(_, s, _) => s
   }
+
+  def +[AA >: A](a: AA)(implicit m: MetricSpace[AA]): BKTree[AA] = this match {
+    case BKTreeEmpty => BKTreeNode(a, 1, IntMap.empty)
+    case BKTreeNode(v, s, c) => {
+      val d = (v: AA) <===> a
+      BKTreeNode(v, s + 1, c + ((d, c get d match {
+        case None => BKTreeNode(a, 1, IntMap.empty)
+        case Some(w) => w + a
+      })))
+    }
+  }
 }
-private final case class BKTreeNode[+A](value: A, sz: Int, children: IntMap[BKTree[A]]) extends BKTree[A]
+private final case class BKTreeNode[+A](value: A, sz: Int, children: Map[Int, BKTree[A]]) extends BKTree[A]
 private final case object BKTreeEmpty extends BKTree[Nothing]
 
 object BKTree {
-  
+  def empty[A]: BKTree[A] = BKTreeEmpty 
 }
+
+/*
+object T {
+  def main(args: Array[String]) {
+    import BKTree._
+    val k = (empty[String] /: args)((a, b) => a + b)
+    println(k)
+    println(args.length == k.size)
+  }
+}
+
+*/
