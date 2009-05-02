31dd19c21297c43c3dda36200ad4e103fe6f5b33
runarorama <runarorama@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-05-02 00:49:08 +0000

    git-svn-id: https://scalaz.googlecode.com/svn/trunk@587 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scala/scalaz/CC.scala
+++ src/main/scala/scalaz/CC.scala
@@ -16,12 +16,44 @@ object CC {
 
   import MA._
 
-  def gpure[A, B](a: A) = cc((k: A => B) => k(a))  
-
   def shift[A, B, C, D, E](f: (C => CC[E, E, A]) => CC[D, B, D]): CC[A, B, C] =
-    cc((k: C => A) => f((c: C) => gpure[A, E](k(c))).apply(identity(_)))
+    cc((k: C => A) => f((c: C) => CCMonadish.gpure[A, E](k(c))).apply(identity(_)))
 
   def reset[A, B, C](c: CC[B, C, B]): CC[A, A, C] = CC.cc((k: C => A) => k(c.apply(identity(_))))
 
   def run[A, B](c: CC[A, B, A]): B = c.apply(identity(_))
+
+  trait Monadish[M[_, _, _]] {
+    def gpure[A, B](a: A): M[B, B, A]
+
+    def gbind[A, B, C, D, E](s: M[B, C, D], f: D => M[A, B, E]): M[A, C, E]
+  }
+
+  trait MW[M[_], P, Q, A] {
+    val unMW: M[A]
+  }
+
+  def mw[M[_], P, Q, A](ma: M[A]) = new MW[M, P, Q, A] {
+    val unMW = ma
+  }
+
+  trait MWM[M[_]] {
+    type T[A, B, C] = MW[M, A, B, C]
+  }
+
+  def MWMonadish[M[_]](implicit m: Monad[M]) = {
+    new Monadish[MWM[M]#T] {
+      def gpure[A, B](a: A) = mw[M, B, B, A](m.pure(a))
+
+      def gbind[A, B, C, D, E](x: MWM[M]#T[B, C, D], f: D => MWM[M]#T[A, B, E]) =
+        mw[M, A, C, E](m.bind(x.unMW, ((d: D) => f(d).unMW)))
+    }
+  }
+
+  implicit def CCMonadish = new Monadish[CC] {
+    def gpure[A, B](a: A) = cc((k: A => B) => k(a))
+
+    def gbind[A, B, C, D, E](f: CC[B, C, D], h: D => CC[A, B, E]) = cc((k: E => A) => f((s: D) => h(s).apply(k)))
+  }
+
 }
