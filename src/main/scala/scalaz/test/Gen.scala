562cab7243796a353fbed5ac88f00681e67bc869
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-05-08 00:39:45 +0000

    git-svn-id: https://scalaz.googlecode.com/svn/trunk@624 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scala/scalaz/test/Gen.scala
+++ src/main/scala/scalaz/test/Gen.scala
@@ -4,6 +4,32 @@ sealed trait Gen[+A] {
   val f: (Int, Rand) => A
 
   def apply(sz: Int)(implicit r: Rand) = f(sz, r)
+
+  import S._
+  
+  // todo arbitrary
+
+  def filter(f: A => Boolean): Gen[A] = Gen.gen((sz, rd) => {
+    var a: A = this(sz)(rd)
+
+    while(!f(a)) {
+      a = this(sz)(rd)
+    }
+    
+    a
+  })
+
+  def resize(sz: Int) = Gen.gen((_, r) => apply(sz)(r))
+  
+  def |||[AA >: A](g: => Gen[AA]): Gen[AA] =
+    (0 >--> 1) >>= (n => (n == 0) ? g | this)
+
+  def replicate[M[_], AA >: A](n: Int)(implicit p: Pure[M], m: Monoid[M[AA]]): Gen[M[AA]] =
+    for(k <- this) yield (k: AA).replicate[M](n)
+
+  def many[M[_], AA >: A](implicit p: Pure[M], m: Monoid[M[AA]]): Gen[M[AA]] = Gen.sized(sz => for(n <- 0 >--> sz; c <- replicate[M, AA](n)) yield c)
+
+  def many1[M[_], AA >: A](implicit p: Pure[M], m: Monoid[M[AA]]): Gen[M[AA]] = Gen.sized(sz => for(n <- 1 >--> sz; c <- replicate[M, AA](n)) yield c)
 }
 
 object Gen {
@@ -25,7 +51,21 @@ object Gen {
 
   def randomised[A](f: Rand => Gen[A]): Gen[A] = parameterised((_, r) => f(r))
 
+  def frequency[A](k: (Int, Gen[A])*): Gen[A] = {
+    def pick(n: Int, l: List[(Int, Gen[A])]): Gen[A] = l match {
+      case Nil => fail
+      case (a, b) :: k => if(n <= a) b else pick(n - a, k)
+    }
 
+    for(n <- 1 >--> (k.map(_._1) :\ 0)(_ + _);
+        z <- pick(n, k.toList)) yield z
+  }
+
+  def freq[A](k: (Int, A)*): Gen[A] = frequency(k map (_ :-> (_.gen)): _*)
+
+  def oneOf[A](k: Gen[A]*) = if(k.isEmpty) fail else for(i <- 0 >--> k.length - 1;
+                                                           x <- k(i)) yield x
+   
   implicit val GenFunctor: Functor[Gen] = new Functor[Gen] {
     def fmap[A, B](r: Gen[A], f: A => B) = gen((sz, rd) => f(r(sz)(rd)))
   }
