2d75095449c26bc4df72e040ed4b417e62e01444
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-05-10 01:28:04 +0000

    git-svn-id: https://scalaz.googlecode.com/svn/trunk@635 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

---
+++ src/main/scala/scalaz/test/Result.scala
@@ -0,0 +1,21 @@
+package scalaz.test
+
+sealed trait Result {
+  val status: Either[Throwable, Status]
+  val succeeded: Int
+  val discarded: Int
+}
+
+object Result {
+  def genExceptionResult(t: Throwable, s: Int, d: Int) = new Result {
+    val status = Left(t)
+    val succeeded = s
+    val discarded = d
+  }
+
+  def result(s: Status, su: Int, d: Int) = new Result {
+    val status = Right(s)
+    val succeeded = su
+    val discarded = d
+  }
+}
