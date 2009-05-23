00e7aecabbc7a5fd46beb9929f0f69b8291d3199
runarorama <runarorama@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-05-23 20:13:40 +0000

    Actors
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@648 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scala/scalaz/concurrent/strategies.scala
+++ src/main/scala/scalaz/concurrent/strategies.scala
@@ -38,4 +38,16 @@ object strategies {
     }
   }
 
+  /**
+   * A strategy that evaluates its argument in the current thread.
+   */
+  object Sequential {
+    implicit def strategy[A] = new Strategy[A] {
+      def apply(a: () => A) = {
+        val v = a()
+        () => v
+      }
+    }
+  }
+
 }
