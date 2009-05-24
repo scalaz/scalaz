34c141aa5ccdfebcad6670c1e69bc4986a54ca9a
runarorama <runarorama@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-05-24 04:33:33 +0000

    Promise
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@656 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

---
+++ src/main/scala/scalaz/concurrent/Promise.scala
@@ -0,0 +1,47 @@
+package scalaz.concurrent
+
+import java.util.ArrayDeque
+import java.util.concurrent.CountDownLatch
+
+sealed trait Promise[A] {
+  private val latch = new CountDownLatch(1)
+  private val waiting = new ArrayDeque[Actor[A]]
+  private var v: Option[A] = None
+  protected val actor: Actor[(Either[() => A, Actor[A]], Promise[A])]
+  val strategy: Strategy[Unit]
+
+  def get = {
+    latch.await
+    v.get
+  }
+}
+
+import Actor._
+
+object Promise {
+  private def mkPromise[A](implicit s: Strategy[Unit]) = {
+    val q = actor[(Either[() => A, Actor[A]], Promise[A])]((p) => {
+      val fst = p._1
+      val snd = p._2
+      val as = snd.waiting
+      if (fst.isLeft) {
+        val a = fst.left.get()
+        snd.v = Some(a)
+        snd.latch.countDown
+        while (!as.isEmpty) as.removeFirst ! a
+      }
+      else if (snd.v.isEmpty) as.offerLast(p._1.right.get)
+      else p._1.right.get ! snd.v.get
+    })
+    new Promise[A] {
+      val strategy = s
+      val actor = q
+    }
+  }
+
+  def promise[A](a: => A)(implicit s: Strategy[Unit]) = {
+    val p = mkPromise[A]
+    p.actor ! ((Left(() => a), p))
+    p
+  }
+}
