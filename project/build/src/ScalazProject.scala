6c57f9b0717b02f66b7ad5c2901f9891bea8610c
tomjadams <tomjadams@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-06-09 17:21:43 +0000

    build options for lots of stuff.
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@689 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- project/build/src/ScalazProject.scala
+++ project/build/src/ScalazProject.scala
@@ -1,4 +1,13 @@
 import sbt._
+import java.util.jar.Attributes.Name._
 
-class ScalazProject(info: ProjectInfo) extends DefaultProject(info) {
+final class ScalazProject(info: ProjectInfo) extends DefaultProject(info) {
+  override def compileOptions = target(Target.Java1_5) :: Unchecked :: Optimize :: super.compileOptions.toList
+
+  override def packageOptions = ManifestAttributes((IMPLEMENTATION_TITLE, "Scalaz"), (IMPLEMENTATION_URL, "http://code.google.com/p/scalaz"),
+    (IMPLEMENTATION_VENDOR, "The Scalaz Project"), (SEALED, "true")) :: Nil
+
+  override def documentOptions = documentTitle("Scalaz " + projectVersion + " API Specification") :: windowTitle("Scalaz " + projectVersion) ::
+          super.documentOptions.toList
+
 }
