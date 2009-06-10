f0ec709691b94cadd1f997431a858e9941606618
jzaugg <jzaugg@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-06-10 09:09:25 +0000

    Disable compiler optimization -- it is really slow when compiling scalacheck.
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@690 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- project/build/src/ScalazProject.scala
+++ project/build/src/ScalazProject.scala
@@ -2,7 +2,7 @@ import sbt._
 import java.util.jar.Attributes.Name._
 
 final class ScalazProject(info: ProjectInfo) extends DefaultProject(info) {
-  override def compileOptions = target(Target.Java1_5) :: Unchecked :: Optimize :: super.compileOptions.toList
+  override def compileOptions = target(Target.Java1_5) :: Unchecked :: super.compileOptions.toList
 
   override def packageOptions = ManifestAttributes((IMPLEMENTATION_TITLE, "Scalaz"), (IMPLEMENTATION_URL, "http://code.google.com/p/scalaz"),
     (IMPLEMENTATION_VENDOR, "The Scalaz Project"), (SEALED, "true")) :: Nil
