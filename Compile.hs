c959aab5dcaa49ce90cdc0992f6425735db30351
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-06-11 02:54:56 +0000

    git-svn-id: https://scalaz.googlecode.com/svn/trunk@693 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- Compile.hs
+++ Compile.hs
@@ -2,7 +2,7 @@
 This belongs in its own library
 -}
 
-module Compile where
+module Compile(recurse) where
 
 import System.Exit
 import System.FilePath.Find -- -- http://hackage.haskell.org/packages/archive/FileManip/latest/doc/html/
