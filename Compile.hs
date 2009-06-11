3c062659f423f59bea67e3251929133a2783d564
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-06-11 06:06:07 +0000

    git-svn-id: https://scalaz.googlecode.com/svn/trunk@700 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- Compile.hs
+++ Compile.hs
@@ -2,10 +2,12 @@
 This belongs in its own library
 -}
 
-module Compile(recurse, Compile, (!!!)) where
+module Compile(recurse, Compile, (!!!), filterRecent) where
 
+import Control.Monad
 import System.Exit
 import System.FilePath.Find hiding (directory) -- http://hackage.haskell.org/packages/archive/FileManip/latest/doc/html/
+import System.Directory
 
 class Compile c where
   (!!!) :: c -> [FilePath] -> IO ExitCode
@@ -13,3 +15,7 @@ class Compile c where
 recurse :: (Compile c) => String -> c -> [FilePath] -> IO ExitCode
 recurse e c p = do k <- find always (extension ==? e) `mapM` p
                    c !!! concat k
+
+-- p must not non-empty, all filepaths must exist with the ability to get the modification-time
+filterRecent :: [FilePath] -> [FilePath] -> IO [FilePath]
+filterRecent p q = let r = fmap maximum $ mapM getModificationTime p in filterM (\z -> liftM2 (>) (getModificationTime z) r) q
