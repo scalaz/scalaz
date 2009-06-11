45028ec88b28301d26d898f7954908c97967983f
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-06-11 22:49:15 +0000

    git-svn-id: https://scalaz.googlecode.com/svn/trunk@712 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- Compile.hs
+++ Compile.hs
@@ -2,9 +2,10 @@
 This belongs in its own library
 -}
 
-module Compile(recurse, Compile, (!!!), filterRecent) where
+module Compile(recurse, Compile, (!!!), filterRecent, foldIf, systems) where
 
 import Control.Monad
+import System.Cmd
 import System.Exit
 import System.FilePath.Find hiding (directory) -- http://hackage.haskell.org/packages/archive/FileManip/latest/doc/html/
 import System.Directory
@@ -20,3 +21,9 @@ filterRecent :: [FilePath] -> [FilePath] -> IO [FilePath]
 filterRecent d s = if null d
                      then return s
                      else let r = fmap maximum $ mapM getModificationTime d in filterM (\z -> liftM2 (>) (getModificationTime z) r) s
+
+foldIf :: (Monad m) => (a -> Bool) -> (t -> m a) -> a -> [t] -> m a
+foldIf _ _ e [] = return e; foldIf z k e (h:t) = k h >>= \c -> if z c then e `seq` foldIf z k e t else return c
+
+systems :: [String] -> IO ExitCode
+systems = foldIf (== ExitSuccess) system ExitSuccess
