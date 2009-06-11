ce7617418341422d8ebec2dd710f7499db32f468
tomjadams <tomjadams@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-06-11 21:42:17 +0000

    minor changes
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@710 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- project/build/src/ScalazProject.scala
+++ project/build/src/ScalazProject.scala
@@ -6,10 +6,8 @@ final class ScalazProject(info: ProjectInfo) extends DefaultProject(info) {
 
   override def compileOptions = target(Target.Java1_5) :: Unchecked :: super.compileOptions.toList
 
-  override def packageOptions = ManifestAttributes((IMPLEMENTATION_TITLE, "Scalaz"), (IMPLEMENTATION_URL, "http://code.google.com/p/scalaz"),
-    (IMPLEMENTATION_VENDOR, "The Scalaz Project"), (SEALED, "true")) :: Nil
+  override def packageOptions = ManifestAttributes((IMPLEMENTATION_TITLE, "Scalaz"), (IMPLEMENTATION_URL, "http://code.google.com/p/scalaz"), (IMPLEMENTATION_VENDOR, "The Scalaz Project"), (SEALED, "true")) :: Nil
 
   override def documentOptions = documentTitle("Scalaz " + projectVersion + " API Specification") :: windowTitle("Scalaz " + projectVersion) ::
-          super.documentOptions.toList
-
+      super.documentOptions.toList
 }
