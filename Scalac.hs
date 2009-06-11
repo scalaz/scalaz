01af6bde4c8a051c89d5ac90c19a8bc360666967
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-06-11 04:17:06 +0000

    git-svn-id: https://scalaz.googlecode.com/svn/trunk@698 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- Scalac.hs
+++ Scalac.hs
@@ -37,7 +37,17 @@ module Scalac(none,
               ScalacTarget,
               Scalac,
               Scalac',
-              (!*!)) where
+              (!*!),
+              fscalac,
+              reset,
+              shutdown,
+              server,
+              flags,
+              scalacfsc,
+              fsc,
+              Fsc,
+              Fsc',
+              fsc') where
 
 import Compile
 import System.Cmd
@@ -124,78 +134,132 @@ scalacd = scalac {
                  }
 
 -- not exported
-dscalac (Scalac debug
-                nowarn
-                verbose
-                deprecation
-                unchecked
-                classpath
-                sourcepath
-                bootclasspath
-                extdirs
-                directory
-                encoding
-                target
-                print
-                optimise
-                explaintypes
-                uniqid
-                version
-                help
-                script) = intercalate " " $ filter (not . null) ["scalac",
-                          c "g" show debug,
-                          b "nowarn" nowarn,
-                          b "verbose" verbose,
-                          b "deprecation" deprecation,
-                          b "unchecked" unchecked,
-                          p "classpath" classpath,
-                          p "sourcepath" sourcepath,
-                          p "bootclasspath" bootclasspath,
-                          p "extdirs" extdirs,
-                          s "d" id directory,
-                          s "encoding" id encoding,
-                          s "target" show target,
-                          b "print" print,
-                          b "optimise" optimise,
-                          b "explaintypes" explaintypes,
-                          b "uniqid" uniqid,
-                          b "version" version,
-                          b "help" help,
-                          m ((:) '@') script]
-                          where
-                          d = (:) '-'
-                          b :: String -> Bool -> String
-                          b = b' . d
-                          b' :: [a] -> Bool -> [a]
-                          b' s k = if k then s else []
-                          m :: (k -> [a]) -> Maybe k -> [a]
-                          m = maybe []
-                          c :: String -> (k -> String) -> Maybe k -> String
-                          c = v ':'
-                          s :: String -> (k -> String) -> Maybe k -> String
-                          s = v ' '
-                          v :: Char -> String -> (k -> String) -> Maybe k -> String
-                          v c k s = m (\z -> '-' : k ++ [c] ++ s z)
-                          p :: String -> Maybe [FilePath] -> String
-                          p s = m (\z -> d s ++ ' ' : if null z then "\"\"" else intercalate [searchPathSeparator] (fmap surround z))
+dscalac :: String -> [String] -> Scalac -> String
+dscalac com r (Scalac debug
+                      nowarn
+                      verbose
+                      deprecation
+                      unchecked
+                      classpath
+                      sourcepath
+                      bootclasspath
+                      extdirs
+                      directory
+                      encoding
+                      target
+                      print
+                      optimise
+                      explaintypes
+                      uniqid
+                      version
+                      help
+                      script) = intercalate " " $ filter (not . null) [com,
+                                c "g" show debug,
+                                b "nowarn" nowarn,
+                                b "verbose" verbose,
+                                b "deprecation" deprecation,
+                                b "unchecked" unchecked,
+                                p "classpath" classpath,
+                                p "sourcepath" sourcepath,
+                                p "bootclasspath" bootclasspath,
+                                p "extdirs" extdirs,
+                                s "d" id directory,
+                                s "encoding" id encoding,
+                                s "target" show target,
+                                b "print" print,
+                                b "optimise" optimise,
+                                b "explaintypes" explaintypes,
+                                b "uniqid" uniqid,
+                                b "version" version,
+                                b "help" help,
+                                m ((:) '@') script] ++ r
 
 -- not exported
+d :: String -> String
+d = (:) '-'
+
+-- not exported
+b :: String -> Bool -> String
+b = b' . d
+
+-- not exported
+b' :: [a] -> Bool -> [a]
+b' s k = if k then s else []
+
+-- not exported
+m :: (k -> [a]) -> Maybe k -> [a]
+m = maybe []
+
+-- not exported
+c :: String -> (k -> String) -> Maybe k -> String
+c = v ':'
+
+-- not exported
+s :: String -> (k -> String) -> Maybe k -> String
+s = v ' '
+
+-- not exported
+v :: Char -> String -> (k -> String) -> Maybe k -> String
+v c k s = m (\z -> '-' : k ++ [c] ++ s z)
+
+-- not exported
+p :: String -> Maybe [FilePath] -> String
+p s = m (\z -> d s ++ ' ' : if null z then "\"\"" else intercalate [searchPathSeparator] (fmap surround z))
+
+-- not exported
+surround :: String -> String
 surround s = '"' : s ++ ['"']
 
 -- not exported
-doscalac k s ps = let v = dscalac s ++ ' ' : intercalate " " (fmap surround ps) in do k v >> system v
+docompile :: String -> (String -> IO a) -> [String] -> IO ExitCode
+docompile z k ps = let v = z ++ ' ' : intercalate " " (fmap surround ps) in do k v >> system v
+
+-- not exported
+doscalac :: Scalac -> (String -> IO a) -> [String] -> IO ExitCode
+doscalac s = docompile (dscalac "scalac" [] s)
 
 instance Compile Scalac where
-  s !!! ps = doscalac (const $ return ()) s ps
+  s !!! ps = doscalac s (const $ return ()) ps
 
 newtype Scalac' = Scalac' Scalac
 
 instance Compile Scalac' where
-  Scalac' s !!! ps = doscalac (Prelude.print) s ps
+  Scalac' s !!! ps = doscalac s (Prelude.print) ps
 
+scalac' :: Scalac -> Scalac'
 scalac' = Scalac'
 
 (!*!) :: (Compile c) => c -> [FilePath] -> IO ExitCode
 (!*!) = recurse ".scala"
 
+scalacd' :: Scalac'
 scalacd' = scalac' scalacd
+
+data Fsc = Fsc {
+  fscalac :: Scalac,
+  reset :: Bool,
+  shutdown :: Bool,
+  server :: Maybe (String, String),
+  flags :: [String]
+}
+
+scalacfsc :: Scalac -> Fsc
+scalacfsc s = Fsc s False False Nothing []
+
+fsc :: Fsc
+fsc = scalacfsc scalac
+
+-- not exported
+dofsc :: Fsc -> (String -> IO a) -> [String] -> IO ExitCode
+dofsc (Fsc fscalac reset shutdown server flags) = docompile (dscalac "fsc" [b "reset" reset, b "shutdown" shutdown, m (uncurry (++)) server, intercalate " " (fmap ("-J" ++) flags)] fscalac)
+
+instance Compile Fsc where
+  s !!! ps = dofsc s (const $ return ()) ps
+
+newtype Fsc' = Fsc' Fsc
+
+instance Compile Fsc' where
+  Fsc' s !!! ps = dofsc s (Prelude.print) ps
+
+fsc' :: Fsc -> Fsc'
+fsc' = Fsc'
