589379d64839257ce16b744a09795c63dfbef1e4
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-06-12 02:38:50 +0000

    .
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@730 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scala/scalaz/Pure.scala
+++ src/main/scala/scalaz/Pure.scala
@@ -252,4 +252,15 @@ object Pure {
       k
     }
   }
+
+  import org.scalacheck.{Gen, Arbitrary}
+
+  implicit val GenPure: Pure[Gen] = new Pure[Gen] {
+    def pure[A](a: => A) = Gen.sized(_ => a)
+  }
+
+  implicit val ArbitraryPure: Pure[Arbitrary] = new Pure[Arbitrary] {
+    def pure[A](a: => A) = Arbitrary(Gen.sized(_ => a))
+  }
+
 }
