bb8b7a1665a26e6048696a67cb4bf9620659fd72
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-06-12 02:30:47 +0000

    .
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@728 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scala/scalaz/Functor.scala
+++ src/main/scala/scalaz/Functor.scala
@@ -249,4 +249,30 @@ object Functor {
       a
     }
   }
+
+  import org.scalacheck.{Gen, Arbitrary, Constraint}
+  import Constraint.{Pos, Neg, Alpha, Numeric, AlphaNum, Small, Large}
+
+  implicit val GenFunctor: Functor[Gen] = new Functor[Gen] {
+    def fmap[A, B](r: Gen[A], f: A => B) = r map f
+  }
+
+  implicit val ArbitraryFunctor: Functor[Arbitrary] = new Functor[Arbitrary] {
+    def fmap[A, B](r: Arbitrary[A], f: A => B) = r map f
+  }
+
+  implicit val ConstraintFunctor: Functor[Constraint] = new Functor[Constraint] {
+    def fmap[A, B](r: Constraint[A], f: A => B) = r match {
+      case Pos(t) => Pos(f(t))
+      case Neg(t) => Neg(f(t))
+      case Alpha(t) => Alpha(f(t))
+      case Numeric(t) => Numeric(f(t))
+      case AlphaNum(t) => AlphaNum(f(t))             
+      case Small(t) => Small(f(t))
+      case Large(t) => Large(f(t))
+      case c => new Constraint[B] {
+        def unbox = f(c.unbox)
+      }
+    }
+  }
 }
