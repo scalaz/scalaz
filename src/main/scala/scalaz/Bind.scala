f087e808a2f3d30d4b1f358b0cfb9c8db64353f4
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-06-12 03:03:47 +0000

    .
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@732 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scala/scalaz/Bind.scala
+++ src/main/scala/scalaz/Bind.scala
@@ -247,4 +247,14 @@ object Bind {
       a
     }
   }
+
+  import org.scalacheck.{Gen, Arbitrary}
+
+  implicit val GenBind: Bind[Gen] = new Bind[Gen] {
+    def bind[A, B](r: Gen[A], f: A => Gen[B]) = r flatMap f
+  }
+
+  implicit val ArbitraryBind: Bind[Arbitrary] = new Bind[Arbitrary] {
+    def bind[A, B](r: Arbitrary[A], f: A => Arbitrary[B]) = Arbitrary(r.arbitrary flatMap (f(_).arbitrary))
+  }
 }
