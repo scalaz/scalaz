139ec95279ab2fa6b152c5d39237858ef07417c4
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-06-19 09:36:22 +0000

    .
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@746 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scala/scalaz/StringW.scala
+++ src/main/scala/scalaz/StringW.scala
@@ -16,7 +16,7 @@ sealed trait StringW {
                        else s + "s"
 
   import xml._
-  import Identity._
+  import Scalaz._
 
   /**
    * Construct an XML node based on the given option value. If there is no value available, then an empty text node is returned,
@@ -61,6 +61,53 @@ sealed trait StringW {
   def nelErr(message: => String) = nel(error(message))
   
   def unsafeNel = nelErr("cannot turn empty string into NonEmptyList")
+
+  import java.io.FileInputStream
+
+  def readFile[X](x: X, f: (X, Byte) => X) = {
+    val in = new FileInputStream(s)
+
+    try {
+      in.elements.foldLeft(x)(f)
+    } finally {
+      in.close
+    }
+  }
+
+  def fileEach(f: Byte => Unit) {
+    val in = new FileInputStream(s)
+
+    try {
+      in.elements.foreach(f)
+    } finally {
+      in.close
+    }
+  }
+
+  def readLines[X, Y](x: X, f: (X, Char) => X, y: Y, g: (Y, X) => Y) {
+    val in = new FileInputStream(s)
+
+    try {
+      var t = x
+      var u = y
+      val i = in.elements
+      val lineSeparators = List('\r'.toByte, '\n'.toByte)
+
+      while(i.hasNext) {
+        val c = i.next.toChar
+
+        if(lineSeparators.contains(c)) {
+          u = g(u, t)
+          t = x
+        } else
+          t = f(t, c)
+      }
+
+      u
+    } finally {
+      in.close
+    }
+  }
 }
 
 object StringW {
