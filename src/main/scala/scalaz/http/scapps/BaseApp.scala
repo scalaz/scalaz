84dce3ad14e68abdde9f0e535b769927f51f921c
tomjadams <tomjadams@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-08-22 11:24:10 +0000

    added logging of requests
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@820 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scala/scalaz/http/scapps/BaseApp.scala
+++ src/main/scala/scalaz/http/scapps/BaseApp.scala
@@ -20,12 +20,12 @@ abstract class BaseApp extends StreamStreamServletApplication {
   def route(implicit request : Request[Stream], servletRequest: HttpServletRequest) : Option[Response[Stream]]
 
   def log(request: Request[Stream], response: Response[Stream]) {
-    //"0.0.0.0 - localhost [%s] \"%s %s HTTP/1.0\" %s 0000" format ("", request.method.asString, request.path.list.mkString, response.status.toInt.toString)
-    //request
+    println("0.0.0.0 - localhost [%s] \"%s %s HTTP/1.0\" %s 0000" format ("", request.method.asString, request.path.list.mkString, response.status.toInt.toString))
+    println("Request body: " + request.body.map(_.toChar).mkString)
     // TODO use apache log format
     // 127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326
   }
-  
+
   val application = new ServletApplication[Stream, Stream] {
     def application(implicit servlet: HttpServlet, servletRequest: HttpServletRequest, request: Request[Stream]) = {
       val response = route getOrElse resource(x => OK << Stream.fromIterator(x), NotFound.xhtml)
