d1831f2d80ead6637ea7c5453dc4dc0f2c1b0c99
tomjadams <tomjadams@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-10-07 23:07:03 +0000

    removed old src tree
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@852 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- src/main/scala/scalaz/PartialWrapMA.scala
+++
@@ -1,5 +0,0 @@
-package scalaz
-
-trait PartialWrapMA[M[_], V[_[_], _]] {
-  def apply[A](a: M[A]): V[M, A]
-}
--- src/main/scala/scalaz/http/servlet/StreamStreamServlet.scala
+++
@@ -1,7 +0,0 @@
-package scalaz.http.servlet
-
-import HttpServletRequest._
-import HttpServletResponse._
-
-final class StreamStreamServlet extends
-  ScalazServlet[Stream, Stream, StreamStreamServletApplication](classOf[StreamStreamServletApplication])
