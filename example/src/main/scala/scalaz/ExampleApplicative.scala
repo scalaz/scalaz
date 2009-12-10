428663362b1405f51203e07f49ce2b21d9a38d25
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-12-10 04:31:47 +0000

    git-svn-id: https://scalaz.googlecode.com/svn/trunk@1212 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- example/src/main/scala/scalaz/ExampleApplicative.scala
+++ example/src/main/scala/scalaz/ExampleApplicative.scala
@@ -6,19 +6,27 @@ object ExampleApplicative {
   import Scalaz._
 
   def run {
-    // Applicative functor apply
+    // Apply the List Applicative functor
     ((List(40, 50, 60) ⊛ (List(1, 2, 3) ∘ ((_: Int) * (_: Int)).curry))) assert_≟ List(40, 50, 60, 80, 100, 120, 120, 150, 180)
 
-    // Applicative functor lift
+    // Lift-2 the List Applicative functor
     List(1, 2, 3) <⊛> (List(40, 50, 60), (_: Int) * (_: Int)) assert_≟ List(40, 50, 60, 80, 100, 120, 120, 150, 180)
 
-    // Applicative functor lift to pair
+    // Lift-2 the List Applicative functor to a pair
     (List(1, 2, 3) <×> List(40, 50, 60)) assert_≟ List((1, 40), (1, 50), (1, 60), (2, 40), (2, 50), (2, 60), (3, 40), (3, 50), (3, 60))
 
-    // Applicative functor lift (anonymous right)
+    // Apply the List Applicative functor with an anonymous right
     (List(1, 2, 3) ⊛> List(40, 50, 60)) assert_≟ List(40, 50, 60, 40, 50, 60, 40, 50, 60)
 
-    // Applicative functor lift (anonymous left)
+    // Apply the List Applicative functor with an anonymous left
     (List(1, 2, 3) <⊛ List(40, 50, 60)) assert_≟ List(1, 1, 1, 2, 2, 2, 3, 3, 3)
+
+    {
+      // Apply the Function Applicative functor to produce a function that lifts conjunction
+      // i.e. x => if(x < 15 && x % 2 == 0) without repeating the application to x.
+      val z = ((_: Int) > 15) ⊛ (((_: Int) % 2 == 0) ∘ ((_: Boolean) ∧ (_: Boolean)).curry)
+      
+      List(7, 8, 14, 15, 16, 20, 21) ∘ z assert_≟ List(false,false,false,false,true,true,false)
+    }
   }
 }
