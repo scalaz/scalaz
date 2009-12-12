1b5402f2af8c9d2a3c215860a6173c31f7a918db
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-12-12 01:22:46 +0000

    git-svn-id: https://scalaz.googlecode.com/svn/trunk@1245 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- core/src/main/scala/scalaz/geo/Radians.scala
+++ core/src/main/scala/scalaz/geo/Radians.scala
@@ -3,18 +3,23 @@ package scalaz.geo
 sealed trait Radians[A] {
   val toRadians: A => Double
   val fromRadians: Double => A
+
+  def xmap[B](f: B => A, g: A => B) = new Radians[B] {
+    val toRadians = f andThen Radians.this.toRadians
+    val fromRadians = Radians.this.fromRadians andThen g
+  }
 }
 
 object Radians {
   import scalaz.Scalaz._
 
-  implicit val DoubleRadians: Radians[Double] = radians(x => x, x => x)
+  implicit val DoubleRadians: Radians[Double] = radians(_ * π / 180, _ * 180 / π)
 
-  implicit val LatitudeRadians: Radians[Latitude] = radians(_.value, latitude(_))
+  implicit val LatitudeRadians: Radians[Latitude] = DoubleRadians xmap (_.value, latitude(_))
 
-  implicit val LongitudeRadians: Radians[Longitude] = radians(_.value, longitude(_))
+  implicit val LongitudeRadians: Radians[Longitude] = DoubleRadians xmap (_.value, longitude(_))
 
-  implicit val BearingRadians: Radians[Bearing] = radians(_.value, bearing(_)) 
+  implicit val BearingRadians: Radians[Bearing] = DoubleRadians xmap (_.value, bearing(_)) 
 }
 
 trait Radianss {
