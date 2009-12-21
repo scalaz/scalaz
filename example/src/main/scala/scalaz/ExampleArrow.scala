f5b817a268eaf4d84bef811855a401df97136a58
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-12-21 06:49:43 +0000

    Demonstrating arrows
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@1285 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- example/src/main/scala/scalaz/ExampleArrow.scala
+++ example/src/main/scala/scalaz/ExampleArrow.scala
@@ -10,19 +10,28 @@ object ExampleArrow {
     val times2 = (_: Int) * 2
     val rev = (_: String) reverse
 
-    // Applying first on the Function1 arrow.
-    plus1.first apply (7, "abc") assert_≟ (8, "abc")
+    // Function1 arrow
+    {
+      // Applying first on the Function1 arrow.
+      plus1.first apply (7, "abc") assert_≟ (8, "abc")
 
-    // Applying second on the Function1 arrow.
-    plus1.second apply ("def", 14) assert_≟ ("def", 15)
+      // Applying second on the Function1 arrow.
+      plus1.second apply ("def", 14) assert_≟ ("def", 15)
 
-    // Combine plus1 and rev on the Function1 arrow to apply across both elements of a pair.
-    plus1 *** rev apply (7, "abc") assert_≟ (8, "cba")
+      // Combine plus1 and rev on the Function1 arrow to apply across both elements of a pair.
+      plus1 *** rev apply (7, "abc") assert_≟ (8, "cba")
 
-    // Perform both plus1 and times2 on a value using the Function1 arrow
-    plus1 &&& times2 apply 7 assert_≟ (8, 14)
+      // Perform both plus1 and times2 on a value using the Function1 arrow
+      plus1 &&& times2 apply 7 assert_≟ (8, 14)
 
-    // Perform plus1 on a pair using the Function1 arrow
-    plus1.product apply (9, 99) assert_≟ (10, 100)
+      // Perform plus1 on a pair using the Function1 arrow
+      plus1.product apply (9, 99) assert_≟ (10, 100)
+    }
+
+    // Kleisli arrow
+    {
+      // TODO Eschew the explicit call to mab
+      // mab[PartialApplyK[Kleisli, Option]#Apply, List[Int], String](☆((n: List[Int]) => if(n.isEmpty) None else Some(n.shows.reverse)))
+    }
   }
 }
