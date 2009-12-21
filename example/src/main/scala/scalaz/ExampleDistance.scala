418df0e7b50fcd33a682ff68697cd905630c9a71
tonymorris <tonymorris@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-12-21 01:40:55 +0000

    Added example for edit distance
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@1279 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- example/src/main/scala/scalaz/ExampleDistance.scala
+++ example/src/main/scala/scalaz/ExampleDistance.scala
@@ -10,6 +10,13 @@ object ExampleDistance {
     val j = "algorithm" toArray
     val k = "altruistic" toArray
 
+    // Compute the edit distance between "algorithm" and "altruistic"
     j <---> k assert_≟ 6
+
+    val x = 1638452297 digits
+    val y = 444488444 digits
+
+    // Compute the edit distance between two strings of digits
+    x <---> y assert_≟ 9
   }
 }
