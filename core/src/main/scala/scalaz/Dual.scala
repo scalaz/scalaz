7b501a8325a886c38308cbee5945f627e54c8018
jzaugg <jzaugg@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2009-12-27 08:59:14 +0000

    Dual is a NewType.
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@1312 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- core/src/main/scala/scalaz/Dual.scala
+++ core/src/main/scala/scalaz/Dual.scala
@@ -1,14 +1,13 @@
 package scalaz
 
-// todo extends NewType?
-trait Dual[A] {
-  val value : A
-}
+/**
+ * @see <a href="http://www.haskell.org/ghc/docs/6.10-latest/html/libraries/base/Data-Monoid.html#t%3ADual">Data-Monoid</a>
+ * @see Semigroup.DualSemigroup
+ */
+trait Dual[A] extends NewType[A]
 
 trait Duals {
   implicit def DualTo[A](a: A): Dual[A] = new Dual[A] {
     val value = a
   }
-
-  implicit def DualFrom[A](d: Dual[A]): A = d.value
 }
