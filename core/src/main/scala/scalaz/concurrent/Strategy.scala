77bbd05fc6131ef637ae9661fc646eae3aa60a8e
jzaugg <jzaugg@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2010-01-10 21:12:02 +0000

    doco
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@1373 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- core/src/main/scala/scalaz/concurrent/Strategy.scala
+++ core/src/main/scala/scalaz/concurrent/Strategy.scala
@@ -2,6 +2,12 @@ package scalaz
 package concurrent
 
 trait Strategy[A] {
+  /**
+   * Evaluate `a` in a manner specific to this strategy. A typical strategy will schedule asynchronous
+   * evaluation of `a` and return a function that, when itself evaluated, will block until the result is ready.
+   *
+   * @return A function that returns the value of A.
+   */
   def apply(a: () => A): () => A
 }
 
