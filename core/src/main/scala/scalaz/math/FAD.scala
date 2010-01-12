f7a3d17e82fd5f88b7f8cd10deb9e20c4cb9103a
jzaugg <jzaugg@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2010-01-12 22:30:21 +0000

    Flesh out forward auto diff.
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@1374 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- core/src/main/scala/scalaz/math/FAD.scala
+++ core/src/main/scala/scalaz/math/FAD.scala
@@ -5,7 +5,7 @@ object FAD {
   import Dual._
 
   /**
-   * Compute the derivative of `f` using Forward Automatic Differentiation.
+   * Compute the derivative of `f` with respect to `x` using Forward Automatic Differentiation.
    */
   def diff[T: Real](f: Dual[T] => Dual[T])(x: T): T = {
     val d = dual(x, implicitly[Real[T]].one)
@@ -15,15 +15,27 @@ object FAD {
 
 trait Real[T] extends Fractional[T] {
   implicit def fromDouble(x: Double): T
+
   def sin(x: T): T
+
   def cos(x: T): T
+
   def exp(x: T): T
-  // todo more ops
+
+  def power(x: T, y: T): T
+
+  def log(x: T): T
 
   class RealOps(lhs: T) extends FractionalOps(lhs) {
     def exp() = Real.this.exp(lhs)
+
     def sin() = Real.this.sin(lhs)
+
     def cos() = Real.this.cos(lhs)
+
+    def **(rhs: T) = Real.this.power(lhs, rhs)
+
+    def log = Real.this.log(lhs)
   }
   override implicit def mkNumericOps(lhs: T): RealOps = new RealOps(lhs)
 }
@@ -56,6 +68,10 @@ object Real {
     def sin(x: Double) = scala.math.sin(x)
 
     def compare(p1: Double, p2: Double) = if (p1 < p2) -1 else if (p2 > p1) 1 else 0
+
+    def power(x: Double, y: Double) = scala.math.pow(x, y)
+
+    def log(x: Double) = scala.math.log(x)
   }
 
   implicit def DualReal[T: Real] = new Real[Dual[T]] {
@@ -64,6 +80,7 @@ object Real {
     import t.mkNumericOps
 
     import Dual._
+    import Scalaz._
 
     implicit def fromDouble(x: Double) = dual(t.fromDouble(x))
 
@@ -97,5 +114,19 @@ object Real {
     def sin(x: Dual[T]) = dual(x.a.sin, x.ε * x.a.cos)
 
     def cos(x: Dual[T]) = dual(x.a.cos, -x.ε * x.a.sin)
+
+    override def abs(x: Dual[T]) = if (x.a equiv t.zero) ⊥ else dual(x.a.abs, x.ε * t.signum(x.a))
+
+    // TODO scala.math.Numeric#signum returns Int, not T. Perhaps we should redefine the Numeric
+    // type class rather than use the one from Scala.
+    override def signum(x: Dual[T]) = ⊥
+
+    def power(x: Dual[T], y: Dual[T]) =
+      if (!(y.ε equiv t.zero) || (x.a equiv t.zero))
+        ⊥
+      else
+        dual(x.a ** y.a, y.a * (x.a ** (y.a - 1.0)) * x.ε)
+
+    def log(x: Dual[T]) = if (x.a <= 0) ⊥ else dual(x.a.log, x.ε / x.a)
   }
 }
