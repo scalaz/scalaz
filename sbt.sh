fd6c6c031d1cdecfb443c4212fd7cbd672f45fe5
jzaugg <jzaugg@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2010-01-18 22:45:29 +0000

    Update SBT.
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@1377 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- sbt.sh
+++ sbt.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java $SBT_OPTS -Dfile.encoding=UTF-8 -Xmx512M -jar `dirname $0`/xsbt-launch-0.6.8.jar "$@"
+java $SBT_OPTS -Dfile.encoding=UTF-8 -Xmx1024M -XX:MaxPermSize=128M -XX:NewSize=128M -XX:NewRatio=3 -jar `dirname $0`/xsbt-launch-0.6.10.jar "$@"
