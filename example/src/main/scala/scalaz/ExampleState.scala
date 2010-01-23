42b231d8a8faffb59abc3b3d3ecc70e1d41ffde1
jzaugg <jzaugg@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2010-01-23 11:55:47 +0000

    Example of the State Monad, stolen from Tony's blog.
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@1389 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

---
+++ example/src/main/scala/scalaz/ExampleState.scala
@@ -0,0 +1,66 @@
+package scalaz
+
+object ExampleState {
+  def main(args: Array[String]) = run
+
+  import Scalaz._
+
+  def run {
+    treeLabel
+  }
+
+  /**
+   * See <a href="http://blog.tmorris.net/the-state-monad-for-scala-users">The State Monad for Scala users</a>
+   */
+  def treeLabel {
+    sealed abstract class Tree[+A] {
+      /**
+       * Label the Leaf nodes of a the tree with increasing integers, traversing
+       * left to right. The current value of the label is be explicitly threaded
+       * through the recursion. 
+       */
+      def number(seed: Int): (Tree[(A, Int)], Int) = this match {
+        case Leaf(x) => (Leaf(x, seed), seed + 1)
+        case Branch(left, right) => left number seed match {
+          case (l, ls) => {
+            right number ls match {
+              case (r, rs) => (Branch(l, r), rs)
+            }
+          }
+        }
+      }
+
+      /**
+       * Use the State Monad to implicitly thread the current value
+       * of the label through the recursion.
+       */
+      def numberSM: State[Int, Tree[(A, Int)]] = this match {
+        case Leaf(x) => for{s <- init[Int];
+                            _ <- modify((_: Int) + 1)} yield Leaf((x, s))
+        case Branch(left, right) => for{l <- left.numberSM
+                                        r <- right.numberSM} yield Branch(l, r)
+      }
+
+      /**
+       * As above, but using State as an Applicative Functor rather than as a Monad.
+       * This is possible as the generators in the for comprehension above are independent.
+       * Note the correspondence between `<⊛` and `_ <- modify`.
+       */
+      def numberSA: State[Int, Tree[(A, Int)]] = this match {
+        case Leaf(x) => (init[Int] <⊛ modify((_: Int) + 1)) ∘ {s: Int => Leaf((x, s))}
+        case Branch(left, right) => left.numberSA <⊛> (right.numberSA, Branch.apply[(A, Int)])
+      }
+    }
+
+    final case class Leaf[A](a: A) extends Tree[A]
+    final case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+    implicit val TreeShow = showA[Tree[_]]
+    implicit val TreeEqual = equalA[Tree[_]]
+
+    val tree = Branch(Leaf("one"), Branch(Leaf("two"), Leaf("three")))
+    tree.number(1)._1 assert_≟ Branch(Leaf(("one", 1)), Branch(Leaf(("two", 2)), Leaf(("three", 3))))
+    tree.numberSM ! 1 assert_≟ tree.number(1)._1
+    tree.numberSA ! 1 assert_≟ tree.number(1)._1
+  }
+}
