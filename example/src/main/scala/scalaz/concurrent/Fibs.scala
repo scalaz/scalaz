fa41edb4cdef9b446a25d53e93beca090f8fb45e
runarorama <runarorama@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2010-02-03 03:55:46 +0000

    New concurrency examples.
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@1412 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

---
+++ example/src/main/scala/scalaz/concurrent/Fibs.scala
@@ -0,0 +1,26 @@
+object Fibs {
+  import java.util.concurrent._
+  import scalaz.concurrent._
+  import scalaz.Scalaz._
+  val cutoff = 30;
+
+  def run {
+    implicit val pool = Executors.newFixedThreadPool(5)
+    implicit val s = strategy.Executor.strategy[Unit]
+
+    val out = actor((xs: Stream[Int]) => {
+      xs.zipWithIndex.foreach(p => println("n=" + (p._2 + 1) + " => " + p._1))
+      pool.shutdown
+    })
+
+    def seqFib(n: Int): Int = if (n < 2) n else seqFib(n-1) + seqFib(n-2)
+
+    def fib(n: Int): Promise[Int] =
+      if (n < cutoff)
+        promise(seqFib(n))
+      else
+        fib(n-1) <**> (fib(n-2), (x:Int, y:Int) => x + y)
+
+    Stream.range(1,41).traverse(fib) to out
+  }
+}
---
+++ example/src/main/scala/scalaz/concurrent/PingPong.scala
@@ -0,0 +1,42 @@
+import java.util.concurrent._
+import scalaz.concurrent._
+import scalaz.Scalaz._
+
+object PingPong {
+  val actors = 10000
+  val pings = 3
+  implicit val pool = Executors.newFixedThreadPool(5)
+  implicit val s = strategy.Executor.strategy[Unit]
+
+  class Pong
+   { val p: Actor[Ping] = actor((m: Ping) => m.p ! Pong.this) }
+
+  class Ping(pings: Int, pong: Pong, cb: Actor[Ping])
+   { var n = pings
+     val p: Actor[Pong] = actor((m: Pong) =>
+      { n = n - 1
+        if (n > 0) pong.p ! Ping.this else cb ! Ping.this })
+     def start = pong.p ! this }
+
+  @volatile var done = 0
+  val callback = actor((p: Ping) =>
+   { done = done + 1
+     if (done % (actors / 10) == 0) 
+       println(done + " actors done -- " + pings * done + " total pongs.")
+     else () 
+     if (done >= actors) {
+      println("All done.")
+      pool.shutdown
+    } else ()
+  })
+
+  def run {
+    (1 to actors).foreach(i => {
+      val pong = new Pong
+      (new Ping(pings, pong, callback)).start
+      if (i % (actors / 10) == 0)
+        println(i + " actors started.")
+      else ()
+    })
+  }
+}
