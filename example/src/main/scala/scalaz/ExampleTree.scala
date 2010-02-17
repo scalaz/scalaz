708d06f6de6f6083fd45385626f7dfe8826d9f7c
jzaugg <jzaugg@ea79b6a6-c355-11dd-8bad-cde060d8fd3c>
2010-02-17 07:20:56 +0000

    commentary on treeloc examples.
    git-svn-id: https://scalaz.googlecode.com/svn/trunk@1432 ea79b6a6-c355-11dd-8bad-cde060d8fd3c

--- example/src/main/scala/scalaz/ExampleTree.scala
+++ example/src/main/scala/scalaz/ExampleTree.scala
@@ -10,9 +10,21 @@ object ExampleTree {
 
   def run {
     val tree: Tree[Int] = node(1, Stream(leaf(2), node(3, Stream(leaf(4)))))
+
+    // A tree of TreeLocs (aka Zipper), rooted at `tree`. Each TreeLoc is focussed on a different node.
+    val allTreeLocs: Tree[TreeLoc[Int]] = tree.loc.cojoin.toTree
+    // Getting the label of the focussed node from each TreeLoc restores the original tree
+    allTreeLocs.map(_.getLabel) assert_≟ tree
+    // Alternatively, we can get the path to root from each node
+    allTreeLocs.map(_.path).drawTree.println
+
+    // And finally wrap this up as a function:
     leafPaths(tree).toList.map(_.toList.reverse)  assert_≟ List(List(1, 2), List(1, 3, 4))
   }
 
+  /**
+   * Returns the paths from each leaf node back to the root node.
+   */
   def leafPaths[T](tree: Tree[T]): Stream[Stream[T]]
       = tree.loc.cojoin.toTree.flatten.filter(_.isLeaf).map(_.path)
 }
