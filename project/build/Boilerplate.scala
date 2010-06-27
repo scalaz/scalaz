e64650a7ffb0b7196fc1702e8408c6f83f07fa54
retronym <jzaugg@gmail.com>
2010-06-27 10:28:46 +0200

    (1, 2, 3).toIndexedSeq

--- project/build/Boilerplate.scala
+++ project/build/Boilerplate.scala
@@ -20,16 +20,19 @@ trait Boilerplate {
         val tupleWSource: String = {
           val tparams = (0 until arity).map(n => ('A' + n).toChar).mkString(", ")
           val params = (1 to arity).map("_" + _).mkString(", ")
-          """|
+          val ztparams = (1 to arity).map(_ => "Z").mkString(", ")
+          val pimp = """|
           |trait Tuple%dW[%s] extends PimpedType[Tuple%d[%s]] {
           |  def fold[Z](f: => (%s) => Z): Z = {import value._; f(%s)}
-          |}
-          |
+          |  def toIndexedSeq[Z](implicit ev: value.type <:< Tuple%d[%s]): IndexedSeq[Z] = {val zs = ev(value); import zs._; IndexedSeq(%s)}
+          |}""".stripMargin.format(arity, tparams, arity, tparams, tparams, params, arity, ztparams, params)
+
+          val conv = """|
           |trait Tuple%dWs {
           |  implicit def ToTuple%dW[%s](t: (%s)): Tuple%dW[%s] = new { val value = t } with Tuple%dW[%s]
           |}
-          |""".stripMargin.format(arity, tparams, arity, tparams, tparams, params, arity, arity,
-            tparams, tparams, arity, tparams, arity, tparams)
+          |""".stripMargin.format(arity, arity, tparams, tparams, arity, tparams, arity, tparams)
+          pimp + "\n" + conv
         }
 
         val source = "package scalaz\n\n" + tupleWSource
