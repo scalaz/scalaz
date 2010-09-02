5b0f7d79e92e5b5c817c9cb65a38c20338427917
retronym <jzaugg@gmail.com>
2010-09-02 20:27:22 +0200

    ASCII alias

--- core/src/main/scala/scalaz/Identity.scala
+++ core/src/main/scala/scalaz/Identity.scala
@@ -42,10 +42,6 @@ sealed trait Identity[A] extends PimpedType[A] {
    */
   def ??(d: => A)(implicit ev: Null <:< A): A = Option(value) getOrElse d
 
-  // using the implicit parameter ev here gives better compiler error messages for mistyped expressions like  1 assert_≟ "".
-  // the simpler signature is def assert_≟(b: A)(implicit e: Equal[A], s: Show[A])
-
-
   /**
    * Alias for assert_===
    */
@@ -54,6 +50,8 @@ sealed trait Identity[A] extends PimpedType[A] {
   /**
    * Raises an error if `value ≠ b`, according to the given `Equal`. The message is formated with the given `Show`.
    */
+  // using the implicit parameter ev here gives better compiler error messages for mistyped expressions like  1 assert_≟ "".
+  // the simpler signature is def assert_≟(b: A)(implicit e: Equal[A], s: Show[A])
   def assert_===[B](b: B)(implicit e: Equal[A], s: Show[A], ev: B <:< A) = if (≠(b)) error(shows + " ≠ " + ev(b).shows)
 
   def ?|?(a: A)(implicit o: Order[A]): Ordering = o order (value, a)
