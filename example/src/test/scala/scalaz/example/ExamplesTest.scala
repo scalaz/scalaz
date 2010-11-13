0badb3c4a1821a167671b0c296e80d18c335bbf0
retronym <jzaugg@gmail.com>
2010-11-13 09:24:52 +0100

    Replace manually crafted Intellij project structure with one automatically generated by the 'sbt-idea-plugin'. This works best in combination with the 'idea-sbt-plugin', that lets you disable the built in Intellij make and trigger an SBT action instead.

--- example/src/test/scala/scalaz/example/ExamplesTest.scala
+++ example/src/test/scala/scalaz/example/ExamplesTest.scala
@@ -3,7 +3,7 @@ package scalaz.example
 
 import org.specs.{Sugar, Specification}
 
-class ExamplesTest extends Specification with Sugar {
+object ExamplesTest extends Specification with Sugar {
   "run" in {
     Example.run must_== () 
   }
