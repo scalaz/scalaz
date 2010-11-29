fb3829a05e3411683d1af5de267c7e511daf1d0b
Edward Kmett <ekmett@gmail.com>
2010-11-29 05:10:54 -0500

    added unwrapping implicits. bug fix

--- core/src/main/scala/scalaz/Recursion.scala
+++ core/src/main/scala/scalaz/Recursion.scala
@@ -11,6 +11,7 @@ trait Nu_[F[_]] extends Immutable {
 object Nu_ { 
   def apply[F[_]](v: => F[Nu_[F]]) : Nu_[F] = new Nu_[F] { def out = v }
   def unapply[F[_]](v: Nu_[F]) = Some(v.out)
+  implicit def unwrap[F[_]](v: Nu_[F]) = v.out
 }
 
 /** 
@@ -24,6 +25,7 @@ trait Mu_[F[_]] extends Immutable {
 object Mu_ { 
   def apply[F[_]](v: F[Mu_[F]]) : Mu_[F] = new Mu_[F] { val out = v }
   def unapply[F[_]](v: Mu_[F]) = Some(v.out)
+  implicit def unwrap[F[_]](v: Mu_[F]) = v.out
 } 
 
 /** Positive corecursion.  */
@@ -36,9 +38,10 @@ trait Nu[F[+_]] extends Nu_[F] {
 object Nu { 
   def apply[F[+_]](v: => F[Nu[F]]) : Nu[F] = new Nu[F] { def out = v }
   def unapply[F[+_]](v : Nu[F]) = Some(v.out)
-  implicit def project[F[+_],G[+_]](v : Nu[F])(
+  implicit def rewrap[F[+_],G[+_]](v : Nu[F])(
     implicit lt: F[Nu[F]] <~: G[Nu[F]]
   ) : Nu[G] = v.asInstanceOf[Nu[G]]
+  implicit def unwrap[F[_]](v: Mu_[F]) = v.out
 }
 
 /** Positive recursion */
@@ -51,9 +54,10 @@ trait Mu[F[+_]] extends Nu[F] with Mu_[F] {
 object Mu {
   def apply[F[+_]](v: F[Mu[F]]) : Mu[F] = new Mu[F] { val out = v } 
   def unapply[F[+_]](v: Mu[F]) = Some(v.out)
-  implicit def project[F[+_],G[+_]](v : Mu[F])(
+  implicit def rewrap[F[+_],G[+_]](v : Mu[F])(
     implicit lt: F[Mu[F]] <~: G[Mu[F]]
   ) : Mu[G] = v.asInstanceOf[Mu[G]]
+  implicit def unwrap[F[+_]](v: Mu[F]) = v.out
 }
 
 /** Cofree corecursion */
@@ -71,6 +75,7 @@ object Cofree_ {
     def out = v
   }
   def unapply[F[_],A](v: Cofree_[F,A]) = Some((v.extract, v.out))
+  implicit def unwrap[F[_],A](v: Cofree_[F,A]) = (v.extract, v.out)
 }
 
 /** Positive cofree corecursion */
@@ -79,11 +84,6 @@ object Cofree_ {
 trait Cofree[F[+_],A] extends Nu[F] with Cofree_[F,A] {
   val extract: A 
   def out: F[Cofree[F,A]]
-  implicit def project[F[+_],G[+_],A,B >: A](
-    v : Cofree[F,A]
-  )(
-    implicit lt: F[Cofree[F,A]] <~: G[Cofree[F,A]]
-  ) : Cofree[G,B] = v.asInstanceOf[Cofree[G,B]]
 }
 object Cofree { 
   def apply[A,F[+_]](
@@ -95,6 +95,10 @@ object Cofree {
     def out = v
   }
   def unapply[F[+_],A](v: Cofree[F,A]) = Some((v.extract, v.out))
+  implicit def rewrap[F[+_],G[+_],A,B >: A](v : Cofree[F,A])(
+    implicit lt: F[Cofree[F,A]] <~: G[Cofree[F,A]]
+  ) : Cofree[G,B] = v.asInstanceOf[Cofree[G,B]]
+  implicit def unwrap[F[+_],A](v: Cofree[F,A]) = (v.extract, v.out)
 }
 
 /** Cofree recursion */
@@ -112,6 +116,7 @@ object CofreeRec_ {
     val out = v
   }
   def unapply[F[_],A](v: CofreeRec_[F,A]) = Some((v.extract, v.out))
+  implicit def unwrap[F[_],A](v: CofreeRec_[F,A]) = (v.extract, v.out)
 }
 
 /** Positive cofree recursion */
@@ -132,7 +137,8 @@ object CofreeRec {
     val out = v
   }
   def unapply[F[+_],A](v: CofreeRec[F,A]) = Some((v.extract, v.out))
-  implicit def project[F[+_],G[+_],A,B >: A](v : CofreeRec[F,A])(
+  implicit def rewrap[F[+_],G[+_],A,B >: A](v : CofreeRec[F,A])(
     implicit lt: F[CofreeRec[F,A]] <~: G[CofreeRec[F,A]]
   ) : CofreeRec[G,B] = v.asInstanceOf[CofreeRec[G,A]]
+  implicit def unwrap[F[+_],A](v: CofreeRec[F,A]) = (v.extract, v.out)
 }
