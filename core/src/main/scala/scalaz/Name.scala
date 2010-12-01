e070ac70c294221762861368f6f546a53c3a9611
retronym <jzaugg@gmail.com>
2010-12-01 08:11:31 +0100

    fix compile in scalacheck-binding. Use type lambas in Name. The examples module is not currently compiling.

--- core/src/main/scala/scalaz/Name.scala
+++ core/src/main/scala/scalaz/Name.scala
@@ -47,14 +47,14 @@ trait Names {
   implicit def nameToEphemeralStream[A] = pureName[EphemeralStream, A] _
   implicit def nameToFirstOption[A] = pureName[FirstOption,A] _
   implicit def nameToFunction0[A] = pureName[Function0,A] _
-  implicit def nameToFunction1[A,R] = pureName[PartialApply1Of2[Function1, R]#Apply,A] _
-  implicit def nameToFunction2[A,R, S] = pureName[PartialApply2Of3[Function2, R, S]#Apply,A] _
-  implicit def nameToFunction3[A,R, S, T] = pureName[PartialApply3Of4[Function3, R, S, T]#Apply,A] _
-  implicit def nameToFunction4[A,R, S, T, U] = pureName[PartialApply4Of5[Function4, R, S, T, U]#Apply,A] _
-  implicit def nameToFunction5[A,R, S, T, U, V] = pureName[PartialApply5Of6[Function5, R, S, T, U, V]#Apply,A] _
-  implicit def nameToFunction6[A,R, S, T, U, V, W] = pureName[PartialApply6Of7[Function6, R, S, T, U, V, W]#Apply,A] _
+  implicit def nameToFunction1[A,R] = pureName[({type λ[α]=R => α})#λ,A] _
+  implicit def nameToFunction2[A,R, S] = pureName[({type λ[α]=(R, S) => α})#λ,A] _
+  implicit def nameToFunction3[A,R, S, T] = pureName[({type λ[α]=(R, S, T) => α})#λ,A] _
+  implicit def nameToFunction4[A,R, S, T, U] = pureName[({type λ[α]=(R, S, T, U) => α})#λ,A] _
+  implicit def nameToFunction5[A,R, S, T, U, V] = pureName[({type λ[α]=(R, S, T, U, V) => α})#λ,A] _
+  implicit def nameToFunction6[A,R, S, T, U, V, W] = pureName[({type λ[α]=(R, S, T, U, V, W) => α})#λ,A] _
   implicit def nameToIdentity[A] = pureName[Identity,A] _
-  implicit def nameToIterV[A,E]  = pureName[PartialApply1Of2[IterV, E]#Apply,A] _
+  implicit def nameToIterV[A,E]  = pureName[({type λ[α]=IterV[E, α]})#λ,A] _
   implicit def nameToJavaArrayBlockingQueue[A] = pureName[ArrayBlockingQueue,A] _
   implicit def nameToJavaArrayList[A] = pureName[ArrayList,A] _
   implicit def nameToJavaConcurrentLinkedQueue[A] = pureName[ConcurrentLinkedQueue,A] _
@@ -71,26 +71,26 @@ trait Names {
   implicit def nameToJavaVector[A] = pureName[Vector,A] _
   implicit def nameToLastOption[A] = pureName[LastOption,A] _
   implicit def nameToList[A] = nameToTraversable[A, scala.List] _
-  implicit def nameToMapEntry[A,X: Zero] = pureName[PartialApply1Of2[Entry, X]#Apply,A] _
+  implicit def nameToMapEntry[A,X: Zero] = pureName[({type λ[α]=Entry[X, α]})#λ,A] _
   implicit def nameToNonEmptyList[A] = pureName[NonEmptyList,A] _
   implicit def nameToOption[A] = pureName[Option,A] _
   implicit def nameToPromise[A](implicit s: Strategy) = pureName[Promise,A] _
   implicit def nameToResponder[A] = pureName[Responder,A] _
   //implicit def nameToSet[A] = nameToTraversable[A, Set] _
-  implicit def nameToStateT[M[_]:Pure, S, A](a: Name[A]): StateT[M, S, A] = pureName[PartialApplyKA[StateT, M, S]#Apply,A](a)
-  implicit def nameToState[A,S] = pureName[PartialApply1Of2[State, S]#Apply,A] _
+  implicit def nameToStateT[M[_]:Pure, S, A](a: Name[A]): StateT[M, S, A] = pureName[({type λ[α]=StateT[M, S, α]})#λ,A](a)
+  implicit def nameToState[A,S] = pureName[({type λ[α]=State[S, α]})#λ,A] _
   implicit def nameToStream[A] = nameToTraversable[A, Stream] _
   implicit def nameToTreeLoc[A] = pureName[TreeLoc,A] _
   implicit def nameToTree[A] = pureName[Tree,A] _
   implicit def nameToTuple1[A]  = pureName[Tuple1,A] _
-  implicit def nameToTuple2[A,R: Zero] = pureName[PartialApply1Of2[Tuple2, R]#Apply,A] _
-  implicit def nameToTuple3[A,R: Zero, S: Zero] = pureName[PartialApply2Of3[Tuple3, R, S]#Apply,A] _
-  implicit def nameToTuple4[A,R: Zero, S: Zero, T: Zero] = pureName[PartialApply3Of4[Tuple4, R, S, T]#Apply,A] _
-  implicit def nameToTuple5[A,R: Zero, S: Zero, T: Zero, U: Zero] = pureName[PartialApply4Of5[Tuple5, R, S, T, U]#Apply,A] _
-  implicit def nameToTuple6[A,R: Zero, S: Zero, T: Zero, U: Zero, V: Zero] = pureName[PartialApply5Of6[Tuple6, R, S, T, U, V]#Apply,A] _
-  implicit def nameToTuple7[A,R: Zero, S: Zero, T: Zero, U: Zero, V: Zero, W: Zero] = pureName[PartialApply6Of7[Tuple7, R, S, T, U, V, W]#Apply,A] _
-  implicit def nameToValidationFailure[A,X] = pureName[PartialApply1Of2[FailProjection, X]#Flip,A] _
-  implicit def nameToValidation[A,X] = pureName[PartialApply1Of2[Validation, X]#Apply,A] _
+  implicit def nameToTuple2[A,R: Zero] = pureName[({type λ[α]=(R, α)})#λ,A] _
+  implicit def nameToTuple3[A,R: Zero, S: Zero] = pureName[({type λ[α]=(R, S, α)})#λ,A] _
+  implicit def nameToTuple4[A,R: Zero, S: Zero, T: Zero] = pureName[({type λ[α]=(R, S, T, α)})#λ,A] _
+  implicit def nameToTuple5[A,R: Zero, S: Zero, T: Zero, U: Zero] = pureName[({type λ[α]=(R, S, T, U, α)})#λ,A] _
+  implicit def nameToTuple6[A,R: Zero, S: Zero, T: Zero, U: Zero, V: Zero] = pureName[({type λ[α]=(R, S, T, U, V, α)})#λ,A] _
+  implicit def nameToTuple7[A,R: Zero, S: Zero, T: Zero, U: Zero, V: Zero, W: Zero] = pureName[({type λ[α]=(R, S, T, U, V, W, α)})#λ,A] _
+  implicit def nameToValidationFailure[A,X] = pureName[({type λ[α]=FailProjection[α, X]})#λ,A] _
+  implicit def nameToValidation[A,X] = pureName[({type λ[α]=Validation[X, α]})#λ,A] _
   implicit def nameToZipStream[A] = pureName[ZipStream,A] _
   implicit def nameToZipper[A]: Name[A] => Zipper[A]  = pureName[Zipper,A] _
 }
