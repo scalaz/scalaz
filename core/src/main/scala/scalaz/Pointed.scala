f58c5572372167954e69841b0ebaabc78cb0571d
retronym <jzaugg@gmail.com>
2010-12-02 18:05:11 +0100

    type annotate pointed.

--- core/src/main/scala/scalaz/Pointed.scala
+++ core/src/main/scala/scalaz/Pointed.scala
@@ -3,7 +3,7 @@ package scalaz
 trait Pointed[P[_]] extends Functor[P] with Pure[P]
 
 object Pointed {
-  implicit def pointed[P[_]](implicit t: Functor[P], p: Pure[P]) = new Pointed[P] {
+  implicit def pointed[P[_]](implicit t: Functor[P], p: Pure[P]): Pointed[P] = new Pointed[P] {
     def fmap[A, B](a: P[A], f: A => B) = t.fmap(a, f)
 
     def pure[A](a: => A): P[A] = p.pure(a)
