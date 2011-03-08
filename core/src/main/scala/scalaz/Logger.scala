e8ca57a0014fa2b886939114e39a1b394e7a9c74
Tony Morris <tmorris@tmorris.net>
2011-03-08 19:26:33 +1000

    implementing writer monad specialised to use IndSeq

--- core/src/main/scala/scalaz/Logger.scala
+++ core/src/main/scala/scalaz/Logger.scala
@@ -63,6 +63,67 @@ import Scalaz._
 
 object Logger {
   type LOGC[C] = IndSeq[C]
+
+  implicit def LoggerFunctor[L]: Functor[({type λ[α]=Logger[L, α]})#λ] = new Functor[({type λ[α]=Logger[L, α]})#λ] {
+    def fmap[A, B](r: Logger[L, A], f: A => B) =
+      r map f
+  }
+
+  implicit def LoggerPure[L]: Pure[({type λ[α]=Logger[L, α]})#λ] = new Pure[({type λ[α]=Logger[L, α]})#λ] {
+    def pure[A](a: => A) =
+      logger[L](a)
+  }
+
+  implicit def LoggerApply[L]: Apply[({type λ[α]=Logger[L, α]})#λ] = new Apply[({type λ[α]=Logger[L, α]})#λ] {
+    def apply[A, B](f: Logger[L, A => B], a: Logger[L, A]) =
+      for {
+        ff <- f
+        aa <- a
+      } yield ff(aa)
+  }
+
+  implicit def LoggerBind[L]: Bind[({type λ[α]=Logger[L, α]})#λ] = new Bind[({type λ[α]=Logger[L, α]})#λ] {
+    def bind[A, B](a: Logger[L, A], f: A => Logger[L, B]) =
+      a flatMap f
+  }
+
+  implicit def LoggerEach[L]: Each[({type λ[α]=Logger[L, α]})#λ] = new Each[({type λ[α]=Logger[L, α]})#λ] {
+    def each[A](a: Logger[L, A], f: A => Unit) =
+      a foreach f
+  }
+
+  implicit def LoggerIndex[L]: Index[({type λ[α]=Logger[L, α]})#λ] = new Index[({type λ[α]=Logger[L, α]})#λ] {
+    def index[A](a: Logger[L, A], n: Int) =
+      if(n== 0) Some(a.value) else None
+  }
+
+  implicit def LoggerFoldable[L]: Foldable[({type λ[α]=Logger[L, α]})#λ] = new Foldable[({type λ[α]=Logger[L, α]})#λ] {
+    override def foldRight[A, B](t: Logger[L, A], b: => B, f: (A, => B) => B) =
+      f(t.value, b)
+  }
+
+  implicit def LoggerTraverse[L]: Traverse[({type λ[α]=Logger[L, α]})#λ] = new Traverse[({type λ[α]=Logger[L, α]})#λ] {
+    def traverse[F[_] : Applicative, A, B](f: A => F[B], t: Logger[L, A]) = {
+      f(t.value) ∘ (b => logger(b) setLog t.log)
+    }
+  }
+
+  implicit def LoggerShow[L, A : Show]: Show[Logger[L, A]] = new Show[Logger[L, A]] {
+    def show(a: Logger[L, A]) =
+      ("logger(" + a.value + ")").toList
+  }
+
+  implicit def LoggerEqual[L, A: Equal]: Equal[Logger[L, A]] = new Equal[Logger[L, A]] {
+    def equal(a1: Logger[L, A], a2: Logger[L, A]) =
+      a1.value === a2.value
+  }
+
+  implicit def LoggerOrder[L, A: Order]: Order[Logger[L, A]] = new Order[Logger[L, A]] {
+    def order(a1: Logger[L, A], a2: Logger[L, A]) =
+      a1.value ?|? a2.value
+  }
+
+  implicit def LoggerZero[L, A: Zero]: Zero[Logger[L, A]] = zero(logger[L](∅[A]))
 }
 
 trait Loggers {
