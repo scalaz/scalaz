7e22c46151c24a7b291354091d3b9dc442e3352f
runarorama <runarorama@gmail.com>
2011-03-21 02:30:55 -0400

    Tweaked examples.

--- example/src/main/scala/scalaz/example/ExampleST.scala
+++ example/src/main/scala/scalaz/example/ExampleST.scala
@@ -17,16 +17,16 @@ object ExampleST {
   def e2[A] = e1[A].flatMap(_.read)
 
   // Run e2, returning the final value of the mutable reference.
-  def test = Forall[({type o[S] = ST[S, Int]})#o](
-    (k : (ST[S, Int] => Nothing) forSome { type S }) => k(e2))
+  def test = Forall[({type λ[S] = ST[S, Int]})#λ](
+    (k : DNE[({type λ[S] = ST[S, Int]})#λ]) => k(e2))
 
   // Will not compile because it exposes a mutable variable.
-  // def test2 = Forall[({type o[S] = ST[S, STRef[S, Int]]})#o](
-  //   (k: (ST[S, STRef[S, Int]] => Nothing) forSome {type S}) => k(e1))
+  // def test2 = Forall[({type λ[S] = ST[S, STRef[S, Int]]})#λ](
+  //   (k: DNE[({type λ[S] = ST[S, STRef[S, Int]]})#λ]) => k(e1))
 
   // Bin-sort a list into an immutable array.
   // Uses a non-observable mutable array in the background.
   def binSort[A: Manifest](size: Int, key: A => Int, as: List[A]): ImmutableArray[List[A]] =   
     accumArray(size, (vs: List[A], v: A) => v :: vs, List(), for { a <- as } yield (key(a), a))
-  
 }
+
