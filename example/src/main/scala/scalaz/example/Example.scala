a62d879f3413aca264b45099e04bf315d8552f63
Tony Morris <tmorris@tmorris.net>
2011-05-31 11:20:23 +1000

    Example composite compiles

--- example/src/main/scala/scalaz/example/Example.scala
+++ example/src/main/scala/scalaz/example/Example.scala
@@ -6,31 +6,21 @@ object Example {
     ExampleApplicative.run
     ExampleArrow.run
     ExampleBifunctor.run
-    ExampleBoolean.run
-    ExampleBKTree.run
     ExampleCategory.run
     ExampleContravariant.run
     ExampleComp.run
     ExampleDistance.run
-    ExampleEndo.run
     ExampleFunctor.run
     ExampleFold.run
     ExampleEqual.run
-    ExampleKleisli.run
-    ExampleIdentity.run
-    ExampleList.run
     ExampleMonad.run
     ExampleMonoid.run
     ExamplePlus.run
-    ExampleState.run
-    ExampleTree.run
     ExampleTraverse.run
-    ExampleValidation.run
     geo.ExampleVincenty.run
     concurrent.ExampleActor.run
     concurrent.HammerTime.run
     WordCount.wordCount
-    ExampleIteratee.run
   }
 
   def main(args: Array[String]) = run
