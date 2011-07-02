ad72a2a9d07df454f572a64b8076bb0d156f345a
retronym <jzaugg@gmail.com>
2011-07-02 20:10:21 +0200

    Re-yak-shaving.

--- project/ScalazBuild.scala
+++ project/ScalazBuild.scala
@@ -5,13 +5,16 @@ import java.util.jar.Attributes.Name._
 
 
 object ScalazBuild extends Build {
-  lazy val scalaz = Project("scalaz",
-    file("."),
-    settings = standardSettings
-  ) aggregate (core, http, geo, example, scalacheckBinding, scalacheckGeo, tests, full)
+  lazy val scalaz = Project(
+    id = "scalaz",
+    base = file("."),
+    settings = standardSettings,
+    aggregate = Seq(core, http, geo, example, scalacheckBinding, scalacheckGeo, tests, full)
+  )
 
-  lazy val core = Project("scalaz-core",
-    file("core"),
+  lazy val core = Project(
+    id = "scalaz-core",
+    base = file("core"),
     settings = standardSettings ++ Seq(
       (sourceGenerators in Compile) <+= (sourceManaged in Compile) map {
         dir => Seq(Boilerplate.generateTupleW(dir))
@@ -19,43 +22,55 @@ object ScalazBuild extends Build {
     )
   )
 
-  lazy val geo = Project("scalaz-geo",
-    file("geo"),
+  lazy val geo = Project(
+    id = "scalaz-geo",
+    base = file("geo"),
+    dependencies = Seq(core),
     settings = standardSettings
-  ) dependsOn (core)
+  )
 
-  lazy val http = Project("scalaz-http",
-    file("http"),
+  lazy val http = Project(
+    id = "scalaz-http",
+    base = file("http"),
+    dependencies = Seq(core),
     settings = standardSettings ++ Seq(
       libraryDependencies ++= Seq(Dependency.ServletApi)
     )
-  ) dependsOn (core)
+  )
 
-  lazy val scalacheckBinding = Project("scalaz-scalacheck-binding",
-    file("scalacheck-binding"),
+  lazy val scalacheckBinding = Project(
+    id = "scalaz-scalacheck-binding",
+    base = file("scalacheck-binding"),
+    dependencies = Seq(core),
     settings = standardSettings ++ Seq(
       libraryDependencies ++= Seq(Dependency.ScalaCheck)
     )
-  ) dependsOn (core)
+  )
 
-  lazy val scalacheckGeo = Project("scalaz-geo-scalacheck",
-    file("geo-scalacheck"),
+  lazy val scalacheckGeo = Project(
+    id = "scalaz-geo-scalacheck",
+    base = file("geo-scalacheck"),
+    dependencies = Seq(core),
     settings = standardSettings
-  ) dependsOn (geo, scalacheckBinding)
+  )
 
-  lazy val example = Project("scalaz-example",
-    file("example"),
+  lazy val example = Project(
+    id = "scalaz-example",
+    base = file("example"),
+    dependencies = Seq(core, geo, http),
     settings = standardSettings ++ Seq(
       libraryDependencies ++= Seq(Dependency.Specs, Dependency.ServletApi)
     )
-  ) dependsOn (core, geo, http)
+  )
 
-  lazy val tests = Project("scalaz-test-suite",
-    file("tests"),
+  lazy val tests = Project(
+    id = "scalaz-test-suite",
+    base = file("tests"),
+    dependencies = Seq(core, geo, scalacheckBinding, scalacheckGeo),
     settings = standardSettings ++ Seq(
       libraryDependencies ++= Seq(Dependency.Specs)
     )
-  ) dependsOn (core, geo, scalacheckBinding, scalacheckGeo)
+  )
 
   lazy val full = {
     // The projects that are packaged in the full distribution.
@@ -66,8 +81,32 @@ object ScalazBuild extends Build {
     val allSources = TaskKey[Seq[Seq[File]]]("all-sources")
     val allSourceDirectories = SettingKey[Seq[Seq[File]]]("all-source-directories")
 
-    Project("scalaz-full",
-      file("full"),
+    def artifactMappings(rootBaseDir: File, baseDir: File, scalaVersion: String, version: String,
+                         fullDocDir: File, artifacts: Seq[Map[Artifact, File]]): Seq[(File, String)] = {
+      val sxrDocDirectory = new File(fullDocDir.getAbsolutePath + ".sxr")
+
+      // Include a root folder in the generated archive.
+      val newBase = "scalaz_%s-%s".format(scalaVersion, version)
+
+      val jarsAndPomMappings = artifacts.flatMap(_.values) x flatRebase(newBase)
+      val etcMappings = ((rootBaseDir / "etc" ** "*") +++ Seq(rootBaseDir / "README")) x rebase(rootBaseDir, newBase)
+      val fullDocMappings = (fullDocDir ** "*") x rebase(fullDocDir.getParentFile, newBase)
+      val sxrDocMappings = (sxrDocDirectory ** "*") x rebase(sxrDocDirectory.getParentFile, newBase)
+      jarsAndPomMappings ++ etcMappings ++ fullDocMappings ++ sxrDocMappings
+    }
+
+    /** Scalac options for SXR */
+    def sxrOptions(baseDir: File, sourceDirs: Seq[Seq[File]]): Seq[String] = {
+      val xplugin = "-Xplugin:" + (baseDir / "lib" / "sxr_2.8.0.RC2-0.2.4-SNAPSHOT.jar").asFile.getAbsolutePath
+      val baseDirs = sourceDirs.flatten
+      val sxrBaseDir = "-P:sxr:base-directory:" + baseDirs.mkString(":")
+      Seq(xplugin, sxrBaseDir)
+    }
+
+    Project(
+      id = "scalaz-full",
+      base = file("full"),
+      dependencies = Seq(core, scalacheckBinding, http, example, tests),
       settings = standardSettings ++ Seq(
         allSources <<= projects.map(sources in Compile in _).join, // join: Seq[Task[A]] => Task[Seq[A]]
         allSourceDirectories <<= projects.map(sourceDirectories in Compile in _).join,
@@ -80,33 +119,15 @@ object ScalazBuild extends Build {
         (compile in Compile) := inc.Analysis.Empty,
 
         // Include SXR in the Scaladoc Build to generated HTML annotated sources.
-        (scaladocOptions in Compile) <++= (baseDirectory, allSourceDirectories) map {
-          (bd, ds) =>
-            val xplugin = "-Xplugin:" + (bd / "lib" / "sxr_2.8.0.RC2-0.2.4-SNAPSHOT.jar").asFile.getAbsolutePath
-            val baseDirs = ds.flatten
-            val sxrBaseDir = "-P:sxr:base-directory:" + baseDirs.mkString(":")
-            Seq(xplugin, sxrBaseDir)
-        },
+        (scaladocOptions in Compile) <++= (baseDirectory, allSourceDirectories) map sxrOptions,
 
         // Package an archive containing all artifacts, readme, licence, and documentation.
         // Use `LocalProject("scalaz")` rather than `scalaz` to avoid a circular reference.
         (mappings in packageBin in Compile) <<= (
                 baseDirectory in LocalProject("scalaz"), baseDirectory, scalaVersion, version,
-                docDirectory in Compile, allPackagedArtifacts) map {
-          (rootDir, bd, sv, v, fullDocDir, artifacts) =>
-            val sxrDocDirectory = new File(fullDocDir.getAbsolutePath + ".sxr")
-
-            // Include a root folder in the generated archive.
-            val newBase = "scalaz_%s-%s".format(sv, v)
-
-            val jarsAndPomMappings = artifacts.flatMap(_.values) x flatRebase(newBase)
-            val etcMappings = ((rootDir / "etc" ** "*") +++ Seq(rootDir / "README")) x rebase(rootDir, newBase)
-            val fullDocMappings = (fullDocDir ** "*") x rebase(fullDocDir.getParentFile, newBase)
-            val sxrDocMappings = (sxrDocDirectory ** "*") x rebase(sxrDocDirectory.getParentFile, newBase)
-            jarsAndPomMappings ++ etcMappings ++ fullDocMappings ++ sxrDocMappings
-        }
+                docDirectory in Compile, allPackagedArtifacts) map artifactMappings
       )
-    ) dependsOn (core, scalacheckBinding, http, example, tests)
+    )
   }
 
   object Dependency {
