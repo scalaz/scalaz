cd696e8a8c1a300458d3dd0f756951474c718012
retronym <jzaugg@gmail.com>
2011-07-03 09:47:15 +0200

    Fix host for nexus credentials.

--- project/ScalazBuild.scala
+++ project/ScalazBuild.scala
@@ -156,7 +156,7 @@ object ScalazBuild extends Build {
       (SEALED, "true"))
     )
   )
-
+  
   lazy val publishSetting = publishTo <<= (version) {
     version: String =>
       def repo(name: String) = name at "http://nexus-direct.scala-tools.org/content/repositories/" + name
@@ -168,7 +168,8 @@ object ScalazBuild extends Build {
   lazy val credentialsSetting = credentials += {
     val creds = Seq("build.publish.user", "build.publish.password").map(k => Option(System.getProperty(k))) match {
       case Seq(Some(user), Some(pass)) =>
-        Credentials("Sonatype Nexus Repository Manager", "nexus.scala-tools.org", user, pass)
+        println("user: [%s] pass: [%s]".format(user, pass))
+        Credentials("Sonatype Nexus Repository Manager", "nexus-direct.scala-tools.org", user, pass)
       case _ =>
         Credentials(Path.userHome / ".ivy2" / ".credentials")
     }
