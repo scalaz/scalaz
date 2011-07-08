ff3a0e3de77f3a371c1d45f683d666010a8410d1
Tony Morris <tmorris@tmorris.net>
2011-07-08 16:05:05 +1000

    SqlValue

--- sql/src/main/scala/scalaz/sql/SqlValueT.scala
+++ sql/src/main/scala/scalaz/sql/SqlValueT.scala
@@ -43,6 +43,48 @@ trait SqlValueTs {
   def sqlError[A]: SqlException => SqlValue[A] =
     e => sqlErrorT(Identity.id(e))
 
+  implicit def SqlValueTFunctor[F[_]: Functor]: Functor[({type λ[α] = SqlValueT[F, α]})#λ] = new Functor[({type λ[α] = SqlValueT[F, α]})#λ] {
+    def fmap[A, B](f: A => B) =
+      _ map f
+  }
+
+  implicit def SqlValueTApplic[F[_]: ApplicFunctor]: Applic[({type λ[α] = SqlValueT[F, α]})#λ] = new Applic[({type λ[α] = SqlValueT[F, α]})#λ] {
+    def applic[A, B](f: SqlValueT[F, A => B]) =
+      a => eitherSqlValueT(implicitly[Applic[({type λ[α] = EitherT[SqlException, F, α]})#λ]].applic(f.value)(a.value))
+  }
+
+  implicit def SqlValueTBind[F[_]: Monad]: Bind[({type λ[α] = SqlValueT[F, α]})#λ] = new Bind[({type λ[α] = SqlValueT[F, α]})#λ] {
+    def bind[A, B](f: A => SqlValueT[F, B]) =
+      _ flatMap f
+  }
+
+  implicit def SqlValueTPointed[F[_]: Pointed]: Pointed[({type λ[α] = SqlValueT[F, α]})#λ] = new Pointed[({type λ[α] = SqlValueT[F, α]})#λ] {
+    def point[A](a: => A) =
+      eitherSqlValueT(implicitly[Pointed[({type λ[α] = EitherT[SqlException, F, α]})#λ]].point(a))
+  }
+
+  implicit def SqlValueTApplicFunctor[F[_]: ApplicFunctor]: ApplicFunctor[({type λ[α] = SqlValueT[F, α]})#λ] = {
+    implicit val ftr = implicitly[ApplicFunctor[F]].functor
+    ApplicFunctor.applicFunctor[({type λ[α] = SqlValueT[F, α]})#λ]
+  }
+
+  implicit def SqlValueTApplicative[F[_]: Applicative]: Applicative[({type λ[α] = SqlValueT[F, α]})#λ] = {
+    implicit val ftr = implicitly[Applicative[F]].pointedFunctor
+    implicit val ap = implicitly[Applicative[F]].applicFunctor
+    Applicative.applicative[({type λ[α] = SqlValueT[F, α]})#λ]
+  }
+
+  implicit def SqlValueTBindFunctor[F[_]: Monad]: BindFunctor[({type λ[α] = SqlValueT[F, α]})#λ] = {
+    implicit val ftr = implicitly[Monad[F]].functor
+    BindFunctor.bindFunctor[({type λ[α] = SqlValueT[F, α]})#λ]
+  }
+
+  implicit def SqlValueTPointedFunctor[F[_]: PointedFunctor]: PointedFunctor[({type λ[α] = SqlValueT[F, α]})#λ] = {
+    implicit val p = implicitly[PointedFunctor[F]].pointed
+    implicit val ftr = implicitly[PointedFunctor[F]].functor
+    PointedFunctor.pointedFunctor[({type λ[α] = SqlValueT[F, α]})#λ]
+  }
+
   implicit def SqlValueTFoldr[F[_]: Foldr]: Foldr[({type λ[α] = SqlValueT[F, α]})#λ] = new Foldr[({type λ[α] = SqlValueT[F, α]})#λ] {
     def foldr[A, B] = k => b => s =>
       implicitly[Foldr[({type λ[α] = EitherT[SqlException, F, α]})#λ]].foldr(k)(b)(s.value)
