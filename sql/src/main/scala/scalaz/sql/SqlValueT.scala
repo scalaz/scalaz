2cf06fb2b54c969c0f37243a75dec7246646e043
Tony Morris <tmorris@tmorris.net>
2011-07-08 13:34:15 +1000

    SqlValue

--- sql/src/main/scala/scalaz/sql/SqlValueT.scala
+++ sql/src/main/scala/scalaz/sql/SqlValueT.scala
@@ -15,4 +15,26 @@ trait SqlValueTs {
 
   type SqlValue[A] =
   SqlValueT[Identity, A]
+
+  def sqlValueT[F[_], A](a: F[A])(implicit ftr: Functor[F]): SqlValueT[F, A] = new SqlValueT[F, A] {
+    val value = EitherT.eitherT(ftr.fmap((a: A) => Right(a): Either[SqlException, A])(a))
+  }
+
+  def sqlErrorT[F[_], A](a: F[SqlException])(implicit ftr: Functor[F]): SqlValueT[F, A] = new SqlValueT[F, A] {
+    val value = EitherT.eitherT(ftr.fmap((e: SqlException) => Left(e): Either[SqlException, A])(a))
+  }
+
+  def sqlValue[A]: A => SqlValue[A] =
+    a => sqlValueT(Identity.id(a))
+
+  def sqlError[A]: SqlException => SqlValue[A] =
+    e => sqlErrorT(Identity.id(e))
+
+  implicit val SqlValueTMonadTrans: MonadTrans[SqlValueT] = new MonadTrans[SqlValueT] {
+    def lift[G[_] : Monad, A](a: G[A]): SqlValueT[G, A] =
+      new SqlValueT[G, A] {
+        val value = implicitly[MonadTrans[({type λ[α[_], β] = EitherT[SqlException, α, β]})#λ]].lift(a)
+      }
+  }
+
 }
