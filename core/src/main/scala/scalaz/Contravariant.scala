8c6bad01ffb2f20b8e9250f2d171157887b6f11a
Tony Morris <tmorris@tmorris.net>
2011-07-08 13:05:59 +1000

    Kleisli contra

--- core/src/main/scala/scalaz/Contravariant.scala
+++ core/src/main/scala/scalaz/Contravariant.scala
@@ -16,41 +16,46 @@ trait Contravariants {
 
   import java.util.Comparator
 
-  implicit def ComparatorContravariant: Contravariant[Comparator] = new Contravariant[Comparator] {
+  implicit val ComparatorContravariant: Contravariant[Comparator] = new Contravariant[Comparator] {
     def contramap[A, B](f: B => A) =
       r => new Comparator[B] {
         def compare(b1: B, b2: B) = r.compare(f(b1), f(b2))
       }
   }
 
-  implicit def ComparableContravariant: Contravariant[Comparable] = new Contravariant[Comparable] {
+  implicit val ComparableContravariant: Contravariant[Comparable] = new Contravariant[Comparable] {
     def contramap[A, B](f: B => A) =
       r => new Comparable[B] {
         def compareTo(b: B) = r.compareTo(f(b))
       }
   }
 
-  implicit def MetricSpaceContravariant: Contravariant[MetricSpace] = new Contravariant[MetricSpace] {
+  implicit def KleisliContravariant[F[_], X]: Contravariant[({type λ[α] = Kleisli[α, F, X]})#λ] = new Contravariant[({type λ[α] = Kleisli[α, F, X]})#λ] {
+    def contramap[A, B](f: B => A) =
+      _ contramapRead f
+  }
+
+  implicit val MetricSpaceContravariant: Contravariant[MetricSpace] = new Contravariant[MetricSpace] {
     def contramap[A, B](f: B => A) =
       _ contramap f
   }
 
-  implicit def EqualContravariant: Contravariant[Equal] = new Contravariant[Equal] {
+  implicit val EqualContravariant: Contravariant[Equal] = new Contravariant[Equal] {
     def contramap[A, B](f: B => A) =
       _ contramap f
   }
 
-  implicit def OrderContravariant: Contravariant[Order] = new Contravariant[Order] {
+  implicit val OrderContravariant: Contravariant[Order] = new Contravariant[Order] {
     def contramap[A, B](f: B => A) =
       _ contramap f
   }
 
-  implicit def ShowContravariant: Contravariant[Show] = new Contravariant[Show] {
+  implicit val ShowContravariant: Contravariant[Show] = new Contravariant[Show] {
     def contramap[A, B](f: B => A) =
       _ contramap f
   }
 
-  implicit def ResourceContravariant: Contravariant[Resource] = new Contravariant[Resource] {
+  implicit val ResourceContravariant: Contravariant[Resource] = new Contravariant[Resource] {
     def contramap[A, B](f: B => A) =
       _ contramap f
   }
