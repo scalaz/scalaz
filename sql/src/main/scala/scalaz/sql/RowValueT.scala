ae33c131d67a8c671f4c1b91b86272dc53fa1358
Tony Morris <tmorris@tmorris.net>
2011-07-11 16:04:05 +1000

    Row value

--- sql/src/main/scala/scalaz/sql/RowValueT.scala
+++ sql/src/main/scala/scalaz/sql/RowValueT.scala
@@ -2,31 +2,35 @@ package scalaz
 package sql
 
 import SqlValueT._
+import RowValueT._
 
 sealed trait RowValueT[F[_], A] {
-  val value: EitherT[Option[String], ({type λ[α] = SqlValueT[F, α]})#λ, A]
+  val value: EitherT[Option[NullMsg], ({type λ[α] = SqlValueT[F, α]})#λ, A]
 
-  def switch(implicit ftr: Functor[F]): OptionT[({type λ[α] = EitherT[String, ({type λ[α] = SqlValueT[F, α]})#λ, α]})#λ, A] =
-    OptionT[({type λ[α] = EitherT[String, ({type λ[α] = SqlValueT[F, α]})#λ, α]})#λ, A](
-      EitherT[String, ({type λ[α] = SqlValueT[F, α]})#λ, Option[A]](
+  def switch(implicit ftr: Functor[F]): OptionT[({type λ[α] = EitherT[NullMsg, ({type λ[α] = SqlValueT[F, α]})#λ, α]})#λ, A] =
+    OptionT[({type λ[α] = EitherT[NullMsg, ({type λ[α] = SqlValueT[F, α]})#λ, α]})#λ, A](
+      EitherT[NullMsg, ({type λ[α] = SqlValueT[F, α]})#λ, Option[A]](
         SqlValueT(
           EitherT(
-            ftr.fmap((e: Either[Err, Either[Option[String], A]]) =>
+            ftr.fmap((e: Either[Err, Either[Option[NullMsg], A]]) =>
               (e.right map {
                 case Left(Some(s)) => Left(s)
                 case Left(None) => Right(None)
                 case Right(a) => Right(Some(a))
-              }): Either[Err, Either[String, Option[A]]]
+              }): Either[Err, Either[NullMsg, Option[A]]]
             )(value.runT.value.runT)))))
 }
 
-object RowValue extends RowValues {
-  def apply[F[_], A](x: EitherT[Option[String], ({type λ[α] = SqlValueT[F, α]})#λ, A]): RowValueT[F, A] = new RowValueT[F, A] {
+object RowValueT extends RowValueTs {
+  def apply[F[_], A](x: EitherT[Option[NullMsg], ({type λ[α] = SqlValueT[F, α]})#λ, A]): RowValueT[F, A] = new RowValueT[F, A] {
     val value = x
   }
 }
 
-trait RowValues {
+trait RowValueTs {
   type RowValue[A] =
   RowValueT[Identity, A]
+
+  type NullMsg =
+  String
 }
