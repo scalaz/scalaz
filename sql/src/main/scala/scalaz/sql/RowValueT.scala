0fd47fca1dbc164a22f0564adf49adc6349b5969
Tony Morris <tmorris@tmorris.net>
2011-07-11 16:01:46 +1000

    Row value

--- sql/src/main/scala/scalaz/sql/RowValueT.scala
+++ sql/src/main/scala/scalaz/sql/RowValueT.scala
@@ -6,26 +6,18 @@ import SqlValueT._
 sealed trait RowValueT[F[_], A] {
   val value: EitherT[Option[String], ({type λ[α] = SqlValueT[F, α]})#λ, A]
 
-  def switch(implicit ftr: Functor[F]): OptionT[({type λ[α] = EitherT[String, ({type λ[α] = SqlValueT[F, α]})#λ, α]})#λ, A] = {
+  def switch(implicit ftr: Functor[F]): OptionT[({type λ[α] = EitherT[String, ({type λ[α] = SqlValueT[F, α]})#λ, α]})#λ, A] =
     OptionT[({type λ[α] = EitherT[String, ({type λ[α] = SqlValueT[F, α]})#λ, α]})#λ, A](
       EitherT[String, ({type λ[α] = SqlValueT[F, α]})#λ, Option[A]](
         SqlValueT(
           EitherT(
             ftr.fmap((e: Either[Err, Either[Option[String], A]]) =>
-              (e match {
-                case Left(x) => Left(x)
-                case Right(y) => Right(y match {
-                  case Left(Some(s)) => Left(s)
-                  case Left(None) => Right(None)
-                  case Right(a) => Right(Some(a))
-                })
+              (e.right map {
+                case Left(Some(s)) => Left(s)
+                case Left(None) => Right(None)
+                case Right(a) => Right(Some(a))
               }): Either[Err, Either[String, Option[A]]]
-            )(value.runT.value.runT)
-          )
-        )
-      )
-    )
-  }
+            )(value.runT.value.runT)))))
 }
 
 object RowValue extends RowValues {
