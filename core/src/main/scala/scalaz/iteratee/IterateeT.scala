fd234f083c484d872572002e00f59ca735310b6b
Tony Morris <tmorris@tmorris.net>
2011-07-21 17:33:21 +1000

    use type aliases instead of wrapper for Enumerat*

--- core/src/main/scala/scalaz/iteratee/IterateeT.scala
+++ core/src/main/scala/scalaz/iteratee/IterateeT.scala
@@ -91,8 +91,14 @@ trait IterateeTs {
   type EnumerateeT[X, O, I, F[_], A] =
   StepT[X, I, F, A] => IterateeT[X, O, F, StepT[X, I, F, A]]
 
+  type Enumeratee[X, O, I, A] =
+  Step[X, I, A] => Iteratee[X, O, Step[X, I, A]]
+
   type EnumeratorT[X, E, F[_], A] =
   StepT[X, E, F, A] => IterateeT[X, E, F, A]
+
+  type Enumerator[X, E, A] =
+  StepT[X, E, A] => Iteratee[X, E, A]
 
   def iterateeT[X, E, F[_], A](s: F[StepT[X, E, F, A]]): IterateeT[X, E, F, A] = new IterateeT[X, E, F, A] {
     val value = s
