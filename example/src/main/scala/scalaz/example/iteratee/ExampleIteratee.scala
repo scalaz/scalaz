1dcfb237d8a454cc02f072f2dc53ed484c89d38d
Richard Wallace <rwallace@thewallacepack.net>
2011-07-24 22:18:03 -0700

    implement takeWhile enumeratee, joinI, an operator for combining an iteratee and an enumeratee in a single step, and fix the iteratee examples

--- example/src/main/scala/scalaz/example/iteratee/ExampleIteratee.scala
+++ example/src/main/scala/scalaz/example/iteratee/ExampleIteratee.scala
@@ -9,37 +9,33 @@ object ExampleIteratee {
 
 
   def run {
-    /*
-    (head[Int] enumerate Stream(1, 2, 3) run) assert_=== Some(1)
-    (length[Int] enumerate Stream(10, 20, 30) run) assert_=== 3
-    (peek[Int] enumerate Stream(1, 2, 3) run) assert_=== Some(1)
-    (head[Int] enumerate Stream() run) assert_=== None
+    ((head[Int, Identity] >>== Stream(1, 2, 3)) run(_ => none)) assert_=== Some(1)
+    ((length[Int, Identity] >>== Stream(10, 20, 30)) run(_ => -1)) assert_=== 3
+    ((peek[Int, Identity] >>== Stream(1, 2, 3)) run(_ => none)) assert_=== Some(1)
+    ((head[Int, Identity] >>== Stream()) run(_ => some(0))) assert_=== None
 
-    (head[Int] enumerateUp Iterator(1, 2, 3) flatMap (_.runT) unsafePerformIO) assert_=== Some(1)
-    (length[Int] enumerateUp Iterator(10, 20, 30) flatMap (_.runT) unsafePerformIO) assert_=== 3
-    (peek[Int] enumerateUp Iterator(1, 2, 3) flatMap (_.runT) unsafePerformIO) assert_=== Some(1)
-    (head[Int] enumerateUp Iterator() flatMap (_.runT) unsafePerformIO) assert_=== None
+    ((head[Int, IO] >>== Iterator(1, 2, 3)) runT(_ => IO(none)) unsafePerformIO) assert_=== Some(1)
+    ((length[Int, IO] >>== Iterator(10, 20, 30)) runT(_ => IO(-1)) unsafePerformIO) assert_=== 3
+    ((peek[Int, IO] >>== Iterator(1, 2, 3)) runT(_ => IO(none)) unsafePerformIO) assert_=== Some(1)
+    ((head[Int, IO] >>== Iterator()) runT(_ => IO(Some(-1))) unsafePerformIO) assert_=== None
 
     import java.io._
 
     def r = new StringReader("file contents")
 
-
-    (head[IoExceptionOr[Char]] enumerateUp r map (_ map (_ flatMap (_.toOption))) flatMap (_.runT) unsafePerformIO) assert_=== Some('f')
-    (length[IoExceptionOr[Char]] enumerateUp r flatMap (_.runT) unsafePerformIO) assert_=== 13
-    (peek[IoExceptionOr[Char]] enumerateUp r map (_ map (_ flatMap (_.toOption))) flatMap (_.runT) unsafePerformIO) assert_=== Some('f')
-    (head[IoExceptionOr[Char]] enumerateUp (new StringReader("")) map (_ map (_ flatMap (_.toOption))) flatMap (_.runT) unsafePerformIO) assert_=== None
+    ((head[IoExceptionOr[Char], IO] >>== r) map (_ flatMap (_.toOption)) runT(_ => IO(none)) unsafePerformIO) assert_=== Some('f')
+    ((length[IoExceptionOr[Char], IO] >>== r) runT(_ => IO(-1)) unsafePerformIO) assert_=== 13
+    ((peek[IoExceptionOr[Char], IO] >>== r) map (_ flatMap (_.toOption)) runT(_ => IO(none)) unsafePerformIO) assert_=== Some('f')
+    ((head[IoExceptionOr[Char], IO] >>== new StringReader("")) map (_ flatMap (_.toOption)) runT(_ => IO(Some('z'))) unsafePerformIO) assert_=== None
 
     // As a monad
-    val m1 = head[Int] >>= ((b:Option[Int]) => head[Int] map (b2 => (b <|*|> b2)))
-    (m1 enumerate Stream(1,2,3) run) assert_=== Some(1 -> 2)
+    val m1 = head[Int, Identity] flatMap ((b:Option[Int]) => head[Int, Identity] map (b2 => (b <|*|> b2)))
+    ((m1 >>== Stream(1,2,3)) run(_ => none)) assert_=== Some(1 -> 2)
 
-    val coli = takeWhile[Int, Identity, List[Int]](_ <= 5) apply collect[Int, List]
-    ((coli enumerate (1 to 10).toStream run) run) assert_=== (1 to 5).toList
-    (coli flatMap { i: Iteratee[Int, List[Int]] => length[Int] map (l => i.run :+ l) } enumerate (1 to 17).toStream run) assert_=== (1 to 5).toList :+ 12
+    val coli = collect[Int, List] <~ takeWhile(_ <= 5) 
+    ((coli >>== (1 to 10).toStream) run(_ => List())) assert_=== (1 to 5).toList
 
-    val colc = takeWhile[IoExceptionOr[Char], IO, List[IoExceptionOr[Char]]](_.fold(_ => false, _ != ' ')) apply collect[IoExceptionOr[Char], List].up[IO]
-    (colc enumerateT r map { _ map { _ map { _.map(_.toOption).sequence } } } flatMap { _.runT } flatMap { _.runT } unsafePerformIO) assert_=== Some(List('f', 'i', 'l', 'e'))
-    */
+    val colc = collect[IoExceptionOr[Char], List].up[IO] <~ takeWhile(_.fold(_ => false, _ != ' '))
+    ((colc >>== r) map(_ flatMap (_.toOption)) runT(_ => IO(List())) unsafePerformIO) assert_=== List('f', 'i', 'l', 'e')
   }
 }
