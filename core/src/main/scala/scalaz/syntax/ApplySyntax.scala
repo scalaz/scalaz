12e51ff73f3bb193ea9048e29457eed3e2d8cebf
Paul Chiusano <paul.chiusano@gmail.com>
2011-09-11 14:11:53 -0400

    Added special implicit conversion for monad transformer with Id as the monad

--- core/src/main/scala/scalaz/syntax/ApplySyntax.scala
+++ core/src/main/scala/scalaz/syntax/ApplySyntax.scala
@@ -1,17 +1,21 @@
 package scalaz
 package syntax
 
+import Id.Id
 
 trait ApplyV[F[_], A] extends SyntaxV[F[A]] {
   def <*>[B](f: F[A => B])(implicit F: Apply[F]) = F.ap(self)(f)
 }
 
 trait ToApplySyntax extends ToFunctorSyntax {
-  implicit def apply[F[_], A](v: F[A]) = (new ApplySyntax[F] {}).applyV(v)
-
-  implicit def applyBin[F[_, _], X, A](v: F[X, A]) = (new ApplySyntax[({type f[a] = F[X, a]})#f] {}).applyV(v)
-
-  implicit def applyBinT[F[_, _[_], _], G[_], X, A](v: F[X, G, A]) = (new ApplySyntax[({type f[a] = F[X, G, a]})#f] {}).applyV(v)
+  implicit def apply[F[_], A](v: F[A]) = 
+    (new ApplySyntax[F] {}).applyV(v)
+  implicit def applyBin[F[_, _], X, A](v: F[X, A]) = 
+    (new ApplySyntax[({type f[a] = F[X, a]})#f] {}).applyV(v)
+  implicit def applyBinT[F[_, _[_], _], G[_], X, A](v: F[X, G, A]) = 
+    (new ApplySyntax[({type f[a] = F[X, G, a]})#f] {}).applyV(v)
+  implicit def applyBinTId[F[_, _[_], _], X, A](v: F[X, Id, A]) = 
+    (new ApplySyntax[({type f[a] = F[X, Id, a]})#f] {}).applyV(v)
 }
 
 trait ApplySyntax[F[_]] extends FunctorSyntax[F] {
