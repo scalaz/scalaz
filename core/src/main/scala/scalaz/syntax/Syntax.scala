9e5a87ebf835b50a1844d5ee2cfdd72f4e7d3c8a
retronym <jzaugg@gmail.com>
2011-09-15 18:15:44 +0200

    import syntax.Syntax.all._ for the kitchen sink of conversions to the syntax wrappers.

--- core/src/main/scala/scalaz/syntax/Syntax.scala
+++ core/src/main/scala/scalaz/syntax/Syntax.scala
@@ -9,7 +9,11 @@ trait Syntaxes {
   object bind extends ToBindSyntax
   object monad extends ToMonadSyntax
   object traverse extends ToTraverseSyntax
+  object all extends ToAllSyntax
 }
+
+trait ToAllSyntax
+  extends ToFunctorSyntax with ToPointedSyntax with ToApplySyntax with ToApplicativeSyntax with ToBindSyntax with ToMonadSyntax with ToTraverseSyntax
 
 trait SyntaxV[A] { def self: A }
 
