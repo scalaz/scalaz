84adb4aa20b37b5fb167fcbe68facb7a47b2af1c
Jason Zaugg <jzaugg@gmail.com>
2011-09-17 22:26:12 +0200

    Semigroup and Monoid instance for Option.
    The duplication between the two is concerning.

--- core/src/main/scala/scalaz/instance/Option.scala
+++ core/src/main/scala/scalaz/instance/Option.scala
@@ -11,6 +11,27 @@ trait Options {
     def empty[A]: Option[A] = None
     def plus[A](a: Option[A], b: => Option[A]) = a orElse b
   }
+
+  implicit def optionSemigroup[A: Semigroup] = new Semigroup[Option[A]] {
+    def append(f1: Option[A], f2: => Option[A]): Option[A] = (f1, f2) match {
+      case (Some(a1), Some(a2)) => Some(Semigroup[A].append(a1, a2))
+      case (Some(a1), None) => f1
+      case (None, Some(a2)) => f2
+      case (None, None) => None
+    }
+  }
+
+  // TODO duplication with optionSemigroup
+  implicit def optionMonoid[A: Monoid] = new Monoid[Option[A]] {
+    def append(f1: Option[A], f2: => Option[A]): Option[A] = (f1, f2) match {
+      case (Some(a1), Some(a2)) => Some(Monoid[A].append(a1, a2))
+      case (Some(a1), None) => f1
+      case (None, Some(a2)) => f2
+      case (None, None) => None
+    }
+
+    def zero: Option[A] = Some(Monoid[A].zero)
+  }
 }
 
 object Option extends Options
