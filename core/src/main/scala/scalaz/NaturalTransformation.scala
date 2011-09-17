53f24df6c29e02144f9a1b34760ca0aa6d387a5b
Jason Zaugg <jzaugg@gmail.com>
2011-09-17 09:26:55 +0200

    Rename ~> to NaturalTransformation; provide ~> as a type alias.

--- core/src/main/scala/scalaz/NaturalTransformation.scala
+++ core/src/main/scala/scalaz/NaturalTransformation.scala
@@ -1,6 +1,6 @@
 package scalaz
 
-trait ~>[F[_],G[_]] { self =>  
+trait NaturalTransformation[F[_],G[_]] { self =>
   def apply[A](fa: F[A]): G[A]
   def compose[E[_]](f: E ~> F): E ~> G = new (E ~> G) {
     def apply[A](ea: E[A]) = self(f(ea))
--- core/src/main/scala/scalaz/package.scala
+++ core/src/main/scala/scalaz/package.scala
@@ -1,3 +1,5 @@
 package object scalaz {
   type Id[X] = X
+
+  type ~>[F[_],G[_]] = NaturalTransformation[F, G]
 }
