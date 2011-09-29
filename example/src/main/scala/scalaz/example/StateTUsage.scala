a6686b03f9747913e5890f0cd207bf595a2145a1
Jason Zaugg <jzaugg@gmail.com>
2011-09-29 22:07:01 +0200

    Cleanup demo of StateT instances.

--- example/src/main/scala/scalaz/example/StateTUsage.scala
+++ example/src/main/scala/scalaz/example/StateTUsage.scala
@@ -5,35 +5,18 @@ import scalaz._
 object StateTUsage extends App {
   import StateT._
 
-  val stateT: StateT[Int, Option, Int] = StateT((t: Int) => Some(0, t + 1))
-  import std.Option._
-
-  {
-    // Use State.stateTPointed to lift Pointed[Option] to Pointed[[a]StateT[Int, Option, a]]
-    Pointed[({type l[a] = StateT[Int, Option, a]})#l]
-
-    // Use State.stateTPointed to lift Applicative[Option] to Applicative[[a]StateT[Int, Option, a]]
-    Applicative[({type l[a] = StateT[Int, Option, a]})#l]
+  def f[M[_]: Functor] {
+    Functor[({type l[a] = StateT[Int, M, a]})#l]
   }
 
-//  implicitly[Monad[({type l[a] = StateT[Int, Option, a]})#l]]
-  stateTMonadState[Int,  Option] : Monad[({type l[a] = StateT[Int, Option, a]})#l]
-  
-  trait M[X]
-  
-  locally {
-    implicit val F: Functor[M] = null
-    implicitly[Functor[({type l[a] = StateT[Int, M, a]})#l]]
+  def p[M[_]: Pointed] {
+    Functor[({type l[a] = StateT[Int, M, a]})#l]
+    Pointed[({type l[a] = StateT[Int, M, a]})#l]
   }
 
-  locally {
-    implicit val F: Pointed[M] = null
-    implicitly[Pointed[({type l[a] = StateT[Int, M, a]})#l]]
-  }
-
-  locally {
-    implicit val F: Monad[M] = null
-    implicitly[Applicative[({type l[a] = StateT[Int, M, a]})#l]]
-    implicitly[Monad[({type l[a] = StateT[Int, M, a]})#l]]
+  def m[M[_]: Monad] {
+    Applicative[({type l[a] = StateT[Int, M, a]})#l]
+    Monad[({type l[a] = StateT[Int, M, a]})#l]
+    MonadState[({type f[s, a] = StateT[s, M, a]})#f, Int]
   }
 }
