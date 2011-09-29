327c59c489320c9341aafc5aa172c1153c583806
Jason Zaugg <jzaugg@gmail.com>
2011-09-29 11:14:50 +0200

    Ooops, I meant Cotnravariant.

--- core/src/main/scala/scalaz/NewType.scala
+++ core/src/main/scala/scalaz/NewType.scala
@@ -9,5 +9,5 @@ object NewType {
 
   def contraSubst[A, F[_], T](fa: F[A ## T])(implicit F: Contravariant[F]): F[A] = fa.asInstanceOf[F[A]]
 
-  def contraUnsubst[A, F[_], T](fa: F[A])(implicit F: Functor[F]): F[A ## T] = fa.asInstanceOf[F[A ## T]]
+  def contraUnsubst[A, F[_], T](fa: F[A])(implicit F: Contravariant[F]): F[A ## T] = fa.asInstanceOf[F[A ## T]]
 }
