b1aff7431a669938975ef3a2076a5482973c264f
Gerolf Seitz <gerolf.seitz@gmail.com>
2011-10-05 23:03:57 +0300

    Missing method 'gets' for the MonadState type class.

--- core/src/main/scala/scalaz/MonadState.scala
+++ core/src/main/scala/scalaz/MonadState.scala
@@ -4,6 +4,7 @@ trait MonadState[F[_,_],S] extends Monad[({type f[x]=F[S,x]})#f] {
   def init: F[S,S]
   def put(s: S): F[S,S]
   def modify(f: S => S): F[S,S] = bind(init)(s => put(f(s)))
+  def gets[A](f: S => A): F[S,A] = bind(init)(s => pure(f(s)))
 }
 
 object MonadState {
