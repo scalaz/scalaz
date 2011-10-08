df4642bc880353230c10b88e02aaf6792f8bbec1
Jason Zaugg <jzaugg@gmail.com>
2011-10-09 00:58:32 +0200

    UndoT monad transformer, contributed by Gerolf Seitz.

--- core/src/main/scala/scalaz/MonadState.scala
+++ core/src/main/scala/scalaz/MonadState.scala
@@ -2,9 +2,9 @@ package scalaz
 
 trait MonadState[F[_,_],S] extends Monad[({type f[x]=F[S,x]})#f] {
   def init: F[S,S]
-  def put(s: S): F[S,S]
-  def modify(f: S => S): F[S,S] = bind(init)(s => put(f(s)))
-  def gets[A](f: S => A): F[S,A] = bind(init)(s => pure(f(s)))
+  def put(s: S): F[S,Unit]
+  def modify(f: S => S): F[S, Unit] = bind(init)(s => put(f(s)))
+  def gets[A](f: S => A): F[S, A] = bind(init)(s => pure(f(s)))
 }
 
 object MonadState {
---
+++ example/src/main/scala/scalaz/example/UndoTUsage.scala
@@ -0,0 +1,25 @@
+package scalaz.example
+
+import scalaz.undo.Undo._
+import scalaz.syntax.Syntax.monad._
+import scalaz.std.Option._
+import scalaz.undo.UndoT
+
+object UndoTUsage extends App {
+
+  val result: UndoT[Int, Option, _] =
+    for {
+      one           <- hput(1)
+      two           <- hput(2)
+      three         <- hput(3)
+      twoAgain      <- undo[Int, Option]
+      four          <- hput(4)
+      twoAgainAgain <- undo[Int, Option]
+      fourAgain     <- redo[Int, Option]
+    } yield ()
+
+  // This should print 'Some(4)'
+  assert(execUndoT(result, 1) == Some(4))
+  println("success")
+
+}
