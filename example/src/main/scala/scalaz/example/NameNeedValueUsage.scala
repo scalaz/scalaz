c158f2ea562931fc4867959ca73e74ffc1fda795
Jason Zaugg <jzaugg@gmail.com>
2011-10-09 14:33:06 +0200

    Name/Need/Value
    Deviated from the scalaz6 implementation which doesn't look right.

---
+++ example/src/main/scala/scalaz/example/NameNeedValueUsage.scala
@@ -0,0 +1,54 @@
+package scalaz.example
+
+object NameNeedValueUsage extends App {
+  import scalaz._
+  
+  def undefined = sys.error("!!")
+  
+  class Inc {
+    var i = 0
+
+    def apply(): Int = {
+      i += 1
+      i
+    }
+  }
+
+  Need(undefined)
+
+  Name(undefined)
+
+  {
+    val inc = new Inc
+    val n = Name(inc.apply())
+    assert(n.value == 1)
+    assert(n.value == 2)
+  }
+
+  {
+    val inc = new Inc
+    val n = Need(inc.apply())
+    assert(n.value == 1)
+    assert(n.value == 1)
+  }
+
+  {
+    val inc = new Inc
+    val n = Value(inc.apply())
+    assert(n.value == 1)
+    assert(n.value == 1)
+  }
+
+  {
+    import Name.name._
+    val inc = new Inc
+    assert(bind(Name(inc.apply()))(x => Name(x)).value == 1)
+  }
+
+  {
+    import Need.need._
+    val inc = new Inc
+    assert(bind(Need(inc.apply()))(x => Need(x)).value == 1)
+  }
+
+}
