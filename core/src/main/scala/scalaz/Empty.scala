a0e9ba247843f980465663f6d5da3e8a8b238490
Jason Zaugg <jzaugg@gmail.com>
2011-10-09 15:41:44 +0200

    Add type class Empty above Plus.
    Add instance for Plus[[a]ListT[F, a]]

---
+++ core/src/main/scala/scalaz/Empty.scala
@@ -0,0 +1,20 @@
+package scalaz
+
+trait Empty[F[_]]  { self =>
+  def empty[A]: F[A]
+  ////
+
+  // derived functions
+
+  ////
+  val emptySyntax = new scalaz.syntax.EmptySyntax[F] {}
+}
+
+object Empty {
+  def apply[F[_]](implicit F: Empty[F]): Empty[F] = F
+
+  ////
+
+  ////
+}
+
