fd696047dbbc7825fdce131d0aaa3c085cc9426e
Jason Zaugg <jzaugg@gmail.com>
2011-10-09 22:21:42 +0200

    Placeholder for Arr syntax.
    Hand crafted, as the SBT generator doesn't handle (* *) -> *.

--- core/src/main/scala/scalaz/Arr.scala
+++ core/src/main/scala/scalaz/Arr.scala
@@ -2,6 +2,15 @@ package scalaz
 
 trait Arr[F[_, _]] {
   def arr[A, B](f: A => B): F[A, B]
+
+  // derived functions
+
+  val emptySyntax = new scalaz.syntax.ArrSyntax[F] {}
 }
 
-// TODO CoKleisliArr instance
+
+object Arr {
+  def apply[F[_, _]](implicit F: Arr[F]): Arr[F] = F
+
+  // TODO CoKleisliArr instance
+}
