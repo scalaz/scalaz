10ad0e54f330ab51afcbd573b3379f80df391dac
Jason Zaugg <jzaugg@gmail.com>
2011-10-12 00:25:05 +0200

    traverse, sequence, and overrides for efficiency in the Id monad.

--- core/src/main/scala/scalaz/Ident.scala
+++ core/src/main/scala/scalaz/Ident.scala
@@ -1,9 +1,33 @@
 package scalaz
 
 trait Idents {
-  val id = new Monad[Id] {
+  implicit val id = new Monad[Id] {
     def pure[A](a: => A): A = a
     def bind[A,B](a: A)(f: A => B): B = f(a)
+
+    // Overrides for efficiency.
+    override def map[A, B](fa: Id[A])(f: (A) => B) = f(fa)
+
+    override def map2[A, B, C](fa: Id[A], fb: Id[B])(f: (A, B) => C): Id[C] = f(fa, fb)
+
+    override def apply[A, B](f: (A) => B): Id[A] => Id[B] = f
+
+    override def ap[A, B](fa: Id[A])(f: Id[A => B]): Id[B] = f(fa)
+
+    override def ap2[A, B, C](fa: Id[A], fb: Id[B])(f: Id[(A, B) => C]): Id[C] = f(fa, fb)
+
+    override def lift2[A, B, C](f: (A, B) => C): (Id[A], Id[B]) => Id[C] = f
+
+    // `ffa: Id[Id[A]]`, gives, "cyclic aliasing or subtyping involving type Id", but `fff: A` is identical.
+    override def join[A](ffa: A) = ffa
+
+    override def traverse[A, G[_], B](value: G[A])(f: A => Id[B])(implicit A: Applicative[Id], G: Traverse[G]): Id[G[B]] =
+      G.map(value)(f)
+
+    override def sequence[A, G[_]: Traverse](as: G[Id[A]]): Id[G[A]] = as
+
+    // TODO more
+
   }
 }
 
