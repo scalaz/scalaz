73225c688167f95fb4d9f6493bfe3fa1395e645b
Jason Zaugg <jzaugg@gmail.com>
2011-10-16 22:22:59 +0200

    Comment with an convenient, albeit type-system-subversive, version of parserMonad.

--- core/src/main/scala/scalaz/std/util/parsing/combinator/Parser.scala
+++ core/src/main/scala/scalaz/std/util/parsing/combinator/Parser.scala
@@ -21,6 +21,25 @@ trait Parsers {
   //
   // This way seems to work without -Ydependent-method-types, yay!
   def parserMonad[P <: combinator.Parsers](p: P) = new ParsersW[P](p).instance
+
+
+  /* Alternative that works fully implicitly at the expense of a cast.
+  class ParsersW[P <: combinator.Parsers with Singleton] {
+      type Parser[A] = P#Parser[A]
+      object dummyParser extends combinator.Parsers
+      def instance: Monad[Parser] = new Monad[Parser] {
+        def pure[A](a: => A): Parser[A] = dummyParser.success(a).asInstanceOf[Parser[A]] // please look the other way!
+        def bind[A, B](fa: Parser[A])(f: (A) => Parser[B]): Parser[B] = fa flatMap f
+      }
+    }
+
+    // A few type gymnastics are required to target the path-dependent type
+    //
+    // The return type is Monad[p.type#Parser]
+    //
+    // This way seems to work without -Ydependent-method-types, yay!
+    implicit def parserMonad[P <: combinator.Parsers with Singleton] = new ParsersW[P].instance
+   */
 }
 
 object Parser extends Parsers
