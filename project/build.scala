13565566a22e579da96313478e06fdffe6bd92e1
Arjan Blokzijl <arjanblokzijl@gmail.com>
2011-11-02 05:38:19 +0100

    somehow scaladoc publishing does not seem to work locally, so disabling that for the moment to get on

--- project/build.scala
+++ project/build.scala
@@ -10,6 +10,7 @@ object build extends Build {
     organization := "org.scalaz",
     version := "7.1-SNAPSHOT",
     scalaVersion := "2.9.1",
+    (publishArtifact in Docs) := false,
 
     genTypeClasses <<= (scalaSource in Compile, streams, typeClasses) map {
       (scalaSource, streams, typeClasses) =>
@@ -26,6 +27,7 @@ object build extends Build {
         val all = "object all extends " + tcs.map(tc => "To%sSyntax".format(tc.name)).mkString(" with ")
         objects + "\n\n" + all
     }
+
   )
 
   lazy val scalaz = Project(
