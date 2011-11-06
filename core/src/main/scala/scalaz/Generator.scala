2923bf8e800fd14787267890ad5a65a6da799a6c
Jason Zaugg <jzaugg@gmail.com>
2011-11-06 10:36:07 +0100

    Generator

---
+++ core/src/main/scala/scalaz/Generator.scala
@@ -0,0 +1,29 @@
+package scalaz
+
+/**
+ * A Generator[C] is a container of elements, and which knows how to efficiently apply a Reducer
+ * to extract an answer by combining elements. A Reducer may supply efficient left-to-right and
+ * right-to-left reduction strategies that a Generator may avail itself of.
+ */
+trait Generator[C[_]] {
+  def reduce[E, M](r: Reducer[E, M], c: C[E]): M = to(r, r.zero, c)
+
+  def to[E, M](r: Reducer[E, M], m: M, c: C[E]): M = r.append(m, reduce(r, c))
+
+  def from[E, M](r: Reducer[E, M], c: C[E], m: M): M = r.append(reduce(r, c), m)
+}
+
+object Generator extends Generators
+
+trait Generators {
+
+  def FoldrGenerator[F[_] : Traverse]: Generator[F] = new Generator[F] {
+    override def reduce[E, M](r: Reducer[E, M], c: F[E]): M =
+      Traverse[F].foldR(c, r.zero)(a => b => r.cons(a, b))
+  }
+
+  def FoldlGenerator[F[_] : Traverse]: Generator[F] = new Generator[F] {
+    override def reduce[E, M](r: Reducer[E, M], c: F[E]): M =
+      Traverse[F].foldL(c, r.zero)((b, a) => r.snoc(b, a))
+  }
+}
