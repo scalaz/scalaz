c7ac2d983a5d2c548226b9f5ee298913d4cd7dba
Gerolf Seitz <gerolf.seitz@gmail.com>
2011-11-15 02:17:37 +0100

    Port MonoidTest.

---
+++ tests/src/test/scala/scalaz/MonoidTest.scala
@@ -0,0 +1,73 @@
+package scalaz
+
+import org.scalacheck.Arbitrary
+import java.math.BigInteger
+import scalacheck.{ScalazProperties, ScalazArbitrary, ScalaCheckBinding}
+import org.specs2.mutable.Specification
+import org.specs2.ScalaCheck
+
+
+class MonoidTest extends Specification with ScalaCheck {
+  addArguments(fullStackTrace)
+  import Scalaz._
+  import ScalaCheckBinding._
+  import ScalazArbitrary._
+  import Tags._
+  import std.boolean._
+  implicit val booleanMonoid = std.anyVal.booleanInstance.conjunction
+  import std.java.math.bigInteger._
+  import std.option._
+  import std.either._
+
+  "function monoid" in {
+    val f: (Int) => Int = implicitly[Semigroup[Int => Int]].append(_ + 1, _ * 2)
+    f(1) must_== 4
+  }
+
+  "monoid laws" in {
+    type A = Int
+    type B = Int
+
+    checkMonoidLaws[Boolean @@ Conjunction]
+    checkMonoidLaws[Boolean]
+    // todo
+    // checkMonoidLaws[Digit]
+    checkMonoidLaws[Int]
+    checkMonoidLaws[String]
+    checkMonoidLaws[List[Int]]
+    checkMonoidLaws[BigInt]
+    checkMonoidLaws[BigInteger]
+    checkMonoidLaws[BigInt @@ Multiplication]
+    checkMonoidLaws[BigInteger @@ Multiplication]
+    checkMonoidLaws[Short]
+    checkMonoidLaws[Short @@ Multiplication]
+    checkMonoidLaws[Byte]
+    checkMonoidLaws[Byte @@ Multiplication]
+    checkMonoidLaws[Long]
+    checkMonoidLaws[Long @@ Multiplication]
+//    checkMonoidLaws[ZipStream[A]]
+    checkMonoidLaws[List[A]]
+    checkMonoidLaws[Option[A]]
+    checkMonoidLaws[Option[A] @@ First]
+    checkMonoidLaws[Option[A] @@ Last]
+//    checkMonoidLaws[ArraySeq[A]]
+    checkMonoidLaws[Either.LeftProjection[A, B]]
+    checkMonoidLaws[Either.LeftProjection[A, B] @@ First]
+    checkMonoidLaws[Either.LeftProjection[A, B] @@ Last]
+    checkMonoidLaws[Either.RightProjection[B, A]]
+    checkMonoidLaws[Either.RightProjection[B, A] @@ First]
+    checkMonoidLaws[Either.RightProjection[B, A] @@ Last]
+    // todo more types
+//    checkMonoidLaws[Map[A, B]]
+  }
+
+  def checkMonoidLaws[A: Monoid : Equal : Manifest : Arbitrary] = {
+    val typeName = manifest[A].toString
+    typeName in {
+      import ScalazProperties.Semigroup._
+      import ScalazProperties.Monoid._
+      check(associative[A])
+      check(identity[A])
+    }
+  }
+}
