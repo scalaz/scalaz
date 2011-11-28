d9e6689fba7ae78f839e9aeeb5bcfdf9b652ecef
Arjan Blokzijl <arjanblokzijl@gmail.com>
2011-11-28 21:48:03 +0100

    added tests for traverseTree

--- tests/src/test/scala/scalaz/FingerTreeTest.scala
+++ tests/src/test/scala/scalaz/FingerTreeTest.scala
@@ -8,10 +8,14 @@ import scalacheck.ScalazArbitrary._
 import std.anyVal._
 import syntax.equal._
 import std.stream._
+import WriterT._
 
 class FingerTreeTest extends Specification with ScalaCheck {
   type SequenceTree[A] = FingerTree[Int, A]
   implicit def SizeReducer[A]: Reducer[A, Int] = UnitReducer(x => 1)
+
+  val intStream = Stream.from(1)
+
   def streamToTree[A](stream: Stream[A]): SequenceTree[A] = stream.foldLeft(FingerTree.empty(SizeReducer[A])) {
     case (t, x) => (t :+ x)
   }
@@ -60,6 +64,33 @@ class FingerTreeTest extends Specification with ScalaCheck {
 
   "foldLeft cons is reverse" ! check {(tree: SequenceTree[Int]) => tree.foldLeft(FingerTree.empty(SizeReducer[Int]))((x, y) => y +: x).toStream ?= tree.toStream.reverse}
 
+  import std.option._
+  import syntax.applicative._
+
+  "Fingertree" should {
+
+//    "apply effects in order" in {
+//      val s: Writer[Int, FingerTree[Int, Int]] = streamToTree(intStream.take(5)).traverseTree[({type λ[α] = Writer[String, α]})#λ, Int, Int](x => Writer(x.toString, x))
+//      s.runT must be_===(2346, streamToTree(intStream.take(5)))
+//    }
+
+    "traverseTree through the option effect yielding result" in {
+      val tree = streamToTree(intStream.take(20)).traverseTree[Option, Int, Int](i => Some(i * 2))
+      tree.map(_ toStream) getOrElse(Stream.empty) must be_===(streamToTree(intStream.take(20).map(_ * 2)).toStream)
+    }
+    "traverseTree through the option effect yielding none" in {
+      val tree = streamToTree(intStream.take(20)).traverseTree[Option, Int, Int](i => if (i < 10) Some(i * 2) else None)
+      tree must be_===(None)
+    }
+    import std.AllInstances._
+    import std.AllFunctions._
+
+    "not blow the stack" in {
+      val tree: Option[FingerTree[Int, Int]] = streamToTree(intStream.take(32 * 1024)).traverseTree[Option, Int, Int](x => Some(x))
+      tree.map(_.toStream.take(100)) getOrElse Stream.empty must be_===(intStream.take(100))
+    }
+
+  }
   // TODO reinstate
 //  "viewl works correctly" ! check {(tree: SequenceTree[Int]) =>
 //    val asStream = tree.toStream
