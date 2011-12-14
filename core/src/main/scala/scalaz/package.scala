e180f375c75ed9031019f017c413cdce0e92499d
Lars Hupel <lars.hupel@mytum.de>
2011-12-14 11:33:35 +0100

    make NaturalTransformation contra-/covariant

--- core/src/main/scala/scalaz/package.scala
+++ core/src/main/scala/scalaz/package.scala
@@ -105,9 +105,9 @@ package object scalaz {
    */
   type @@[T, Tag] = T with Tagged[Tag]
 
-  type ~>[F[_], G[_]] = NaturalTransformation[F, G]
-  type <~[F[_], G[_]] = NaturalTransformation[G, F]
-  type ~~>[F[_,_], G[_,_]] = BiNaturalTransformation[F, G]
+  type ~>[-F[_], +G[_]] = NaturalTransformation[F, G]
+  type <~[+F[_], -G[_]] = NaturalTransformation[G, F]
+  type ~~>[-F[_,_], +G[_,_]] = BiNaturalTransformation[F, G]
 
   type ⊥ = Nothing
   type ⊤ = Any
