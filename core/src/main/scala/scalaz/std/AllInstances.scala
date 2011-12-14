4ec64f057f32f7896a4b6214e29c333c2f3c1bfd
Jason Zaugg <jzaugg@gmail.com>
2011-12-14 18:55:43 +1000

    Add missing instances / syntax into the uber-imports.

--- core/src/main/scala/scalaz/std/AllInstances.scala
+++ core/src/main/scala/scalaz/std/AllInstances.scala
@@ -1,12 +1,13 @@
 package scalaz.std
 
 trait AllInstances
-  extends AnyValInstances with FunctionInstances with ListInstances
+  extends AnyValInstances with FunctionInstances with ListInstances with MapInstances
   with OptionInstances with StringInstances with StreamInstances with TupleInstances
   with EitherInstances with PartialFunctionInstances with TypeConstraintInstances
   with scalaz.std.math.BigDecimalInstances with scalaz.std.math.BigInts
   with scalaz.std.math.OrderingInstances
   with scalaz.std.util.parsing.combinator.Parsers
   with scalaz.std.java.util.MapInstances
+  // Intentionally omitted: IterableInstances
 
 object AllInstances extends AllInstances
