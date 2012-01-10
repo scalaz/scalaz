a18ad738dcfa24b2bbad5e7753d76eee183778af
Jason Zaugg <jzaugg@gmail.com>
2012-01-10 23:26:48 +0100

    Manual merge of Actor fix from master.

--- concurrent/src/main/scala/scalaz/concurrent/Actor.scala
+++ concurrent/src/main/scala/scalaz/concurrent/Actor.scala
@@ -4,47 +4,40 @@ package concurrent
 import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.ConcurrentLinkedQueue
 
-sealed case class Actor[A](e: A => Unit, onError: Throwable => Unit = throw (_))(implicit val strategy: Strategy) {
+final case class Actor[A](e: A => Unit, onError: Throwable => Unit = throw(_))(implicit val strategy: Strategy) {
   private val suspended = new AtomicBoolean(true)
   private val mbox = new ConcurrentLinkedQueue[A]
 
-  private def work = {
-    val mt = mbox.isEmpty
-    if (mt) () => ()
-    else if (suspended.compareAndSet(!mt, false)) act ! (())
-    else () => ()
+  val toEffect: Run[A] = Run.run[A]((a) => this ! a)
+
+  /** Alias for `apply` */
+  def !(a: A) {
+    mbox offer a
+    work
   }
 
-  val toRun: Run[A] = Run.run[A]((a) => this ! a)
-
-  def !(a: A) = if (mbox offer a) work else toRun ! a
-
+  /** Pass the message `a` to the mailbox of this actor */
   def apply(a: A) = this ! a
 
-  import Actor._
-
   def contramap[B](f: B => A): Actor[B] =
-    actor[B]((b: B) => (this ! f(b))(), onError)(strategy)
+    Actor[B]((b: B) => (this ! f(b)), onError)(strategy)
 
-  private val act: Run[Unit] = Run.run((u: Unit) => {
-    var go = true
+  private def work =
+    if (!mbox.isEmpty && suspended.compareAndSet(true, false)) act(())
+
+  private val act: Run[Unit] = Run.run{(u: Unit) =>
     var i = 0
-    while (go && i < 1000) {
+    while (i < 1000) {
       val m = mbox.poll
       if (m != null) try {
         e(m)
         i = i + 1
-      } catch {
-        case e => onError(e)
-      }
-      else {
-        suspended.set(true)
-        work
-        go = false
-      }
+      } catch { case e => onError(e) }
+      else i = 1000
     }
-    if (mbox.peek != null) act ! u else ()
-  })
+    suspended.set(true)
+    work
+  }
 }
 
 object Actor extends ActorFunctions with ActorInstances
