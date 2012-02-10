bfdb5ba2e7d3647b76ad20ef026f7da7f590ab55
Jason Zaugg <jzaugg@gmail.com>
2012-02-10 08:12:55 +0100

    Fix up delimiter blocks in Category, Compose

--- core/src/main/scala/scalaz/Category.scala
+++ core/src/main/scala/scalaz/Category.scala
@@ -6,7 +6,6 @@ package scalaz
  */
 ////
 trait Category[=>:[_, _]] extends ArrId[=>:] with Compose[=>:] { self =>
-
   ////
   // TODO GeneralizedCategory, GeneralizedFunctor, et al, from Scalaz6 ?
 
@@ -36,5 +35,8 @@ trait Category[=>:[_, _]] extends ArrId[=>:] with Compose[=>:] { self =>
 
 object Category {
   @inline def apply[F[_, _]](implicit F: Category[F]): Category[F] = F
+
+  ////
+  ////
 }
 
