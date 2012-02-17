59bd0ca19dfd59e5b1fdd6a0221a2e22a117a250
Lars Hupel <lars.hupel@mytum.de>
2012-02-17 19:51:53 +0100

    update the example accordingly and get rid of superfluous `coerce`

--- example/src/main/scala/scalaz/example/TypelevelUsage.scala
+++ example/src/main/scala/scalaz/example/TypelevelUsage.scala
@@ -95,13 +95,17 @@ object TypelevelUsage extends App {
 
     import KLists._
 
-    val aplist = klist1.coerce[Option]
+    val aplist = klist1
     val func: (Nothing, Int, String) => Double = { (x, y, z) => 2.0 }
 
-    val afunc = Option(func.curried)
+    val ares1 = aplist(Option(func.curried))
+    val ares2 = aplist.applyP(func.curried)
 
-    assert(klist1(afunc) === None)
-    assert(aplist(afunc) === None)
+    typed[Option[Double]](ares1)
+    typed[Option[Double]](ares2)
+
+    assert(ares1 === None)
+    assert(ares2 === None)
 
   }
 
@@ -111,7 +115,7 @@ object TypelevelUsage extends App {
 
     val downed = aplist.down
 
-    typed[Option[Nothing] :: Option[Int] :: Option[String] :: HNil](downed)
+    typed[Option[Nothing] :: Option[Int] :: Some[String] :: HNil](downed)
 
     assert(downed == aplist)
 
