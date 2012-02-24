1cd36f7a0e6058f6e4b17aea9e3f2a41a541dcec
Tony Morris <tmorris@tmorris.net>
2012-02-24 15:23:54 +1000

    examples

--- example/src/main/scala/scalaz/example/EnumUsage.scala
+++ example/src/main/scala/scalaz/example/EnumUsage.scala
@@ -16,5 +16,6 @@ object EnumUsage extends App {
 
   assert(((_4: Digit) |-> _9) === List(_4, _5, _6, _7, _8, _9))
   assert(('m' |=> 'u').toList === List('m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u'))
+  assert(('m' |--> (2, 'u')).toList === List('m', 'o', 'q', 's', 'u'))
 
 }
