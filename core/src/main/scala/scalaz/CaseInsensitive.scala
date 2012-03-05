b74821aa912ec3ac7871a6e1f106176038b402b4
Richard Wallace <rwallace@thewallacepack.net>
2012-03-04 21:47:20 -0700

    added CaseInsensitive with tests

---
+++ core/src/main/scala/scalaz/CaseInsensitive.scala
@@ -0,0 +1,61 @@
+package scalaz
+
+sealed trait CaseInsensitive[A] {
+  val original: A
+  def foldedCase: A
+  
+  final def map[B: FoldCase](f: A => B): CaseInsensitive[B] = CaseInsensitive(f(original))
+  
+  final override def equals(other: Any): Boolean = other match {
+    case that: CaseInsensitive[_] => foldedCase == that.foldedCase
+    case _ => false
+  }
+    
+  final override lazy val hashCode: Int = foldedCase.hashCode
+}
+
+object CaseInsensitive extends CaseInsensitiveInstances {
+  def apply[A](a: A)(implicit A: FoldCase[A]): CaseInsensitive[A] = mk(a, A.foldCase(a))
+  
+  private[scalaz] def mk[A](a: A, fc: => A): CaseInsensitive[A] = new CaseInsensitive[A] {
+    val original = a
+    lazy val foldedCase = fc
+  }
+}
+
+trait CaseInsensitiveInstances {
+  implicit def CaseInsensitiveMonoid[A: FoldCase : Monoid]: Monoid[CaseInsensitive[A]] =
+    new Monoid[CaseInsensitive[A]] {
+      def zero = CaseInsensitive.mk(Monoid[A].zero, Monoid[A].zero)
+      def append(a: CaseInsensitive[A], b: => CaseInsensitive[A]) = 
+        CaseInsensitive.mk(Semigroup[A].append(a.original, b.original), Semigroup[A].append(a.foldedCase, b.foldedCase))
+    }
+  
+  implicit def CaseInsensitiveEqual[A: Equal]: Equal[CaseInsensitive[A]] = 
+    new Equal[CaseInsensitive[A]] {
+      def equal(a: CaseInsensitive[A], b: CaseInsensitive[A]) = 
+        Equal[A].equal(a.foldedCase, b.foldedCase)
+    }
+  
+  implicit def CaseInsensitiveOrder[A: Order]: Order[CaseInsensitive[A]] = 
+    new Order[CaseInsensitive[A]] {
+      def order(a: CaseInsensitive[A], b: CaseInsensitive[A]) = Order[A].order(a.foldedCase, b.foldedCase)
+    }
+  
+  implicit def CaseInsensitiveShow[A: Show]: Show[CaseInsensitive[A]] =
+    new Show[CaseInsensitive[A]] {
+      def show(a: CaseInsensitive[A]) = Show[A].show(a.original)
+    }
+}
+
+trait FoldCase[A] {
+  def foldCase(a: A): A
+}
+
+object FoldCase extends FoldCaseInstances
+
+trait FoldCaseInstances {
+  implicit def StringFoldCase: FoldCase[String] = new FoldCase[String] {
+    def foldCase(s: String) = s.toLowerCase
+  }
+}
