2f65e562f267ce0532a69d7b144c393b0502a2b4
Tony Morris <tmorris@tmorris.net>
2012-04-14 18:01:56 +1000

    some and many

--- core/src/main/scala/scalaz/ApplicativePlus.scala
+++ core/src/main/scala/scalaz/ApplicativePlus.scala
@@ -15,6 +15,18 @@ trait ApplicativePlus[F[_]] extends Applicative[F] with PlusEmpty[F] { self =>
     implicit def G = G0
   }
 
+  def some[A](a: F[A]): F[List[A]] = {
+    lazy val y: Free.Trampoline[F[List[A]]] = z map (plus(_, point(Nil)))
+    lazy val z: Free.Trampoline[F[List[A]]] = y map (map2(a, _)(_ :: _))
+    z.run
+  }
+
+  def many[A](a: F[A]): F[List[A]] = {
+    lazy val y: Free.Trampoline[F[List[A]]] = z map (plus(_, point(Nil)))
+    lazy val z: Free.Trampoline[F[List[A]]] = y map (map2(a, _)(_ :: _))
+    y.run
+  }
+
   ////
   val applicativePlusSyntax = new scalaz.syntax.ApplicativePlusSyntax[F] {}
 }
