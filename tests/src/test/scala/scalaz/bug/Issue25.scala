dba98972bd42eca93e2f8fd5d5e87fc655a24061
Lars Hupel <lars.hupel@mytum.de>
2012-07-05 00:20:50 +0200

    Issue #25 seems to be fixed, so disable `pendingUntilFixed` in the tests

--- tests/src/test/scala/scalaz/bug/Issue25.scala
+++ tests/src/test/scala/scalaz/bug/Issue25.scala
@@ -3,10 +3,9 @@ package scalaz.bug
 import org.specs.{Specification, Sugar, ScalaCheck}
 import scalaz._
 import Scalaz._
-import org.specs.specification.PendingUntilFixed
 
 /** https://github.com/scalaz/scalaz/issues/25 */
-object Issue25 extends Specification with Sugar with ScalaCheck with PendingUntilFixed {
+object Issue25 extends Specification with Sugar with ScalaCheck {
 
   val f = 1 + (_: Int)
 
@@ -32,9 +31,7 @@ object Issue25 extends Specification with Sugar with ScalaCheck with PendingUnti
 
   "function1 semigroup 4" in {
     // TODO Something is awry with ListFoldable#foldRight
-    pendingUntilFixed {
-      val f2 = List(f, f).sumr
-      f2(7) must be_==(16)
-    }
+    val f2 = List(f, f).sumr
+    f2(7) must be_==(16)
   }
 }
