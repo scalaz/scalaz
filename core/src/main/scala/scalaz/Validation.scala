51b38ee9d8f7c66453c402766b7115e48c82afa9
Tony Morris <tmorris@tmorris.net>
2012-08-01 15:53:19 +1000

    private constructors for Validation

--- core/src/main/scala/scalaz/Validation.scala
+++ core/src/main/scala/scalaz/Validation.scala
@@ -284,9 +284,8 @@ sealed trait Validation[+E, +A] {
 
 }
 
-final case class Success[E, A](a: A) extends Validation[E, A]
-
-final case class Failure[E, A](e: E) extends Validation[E, A]
+private final case class Success[E, A](a: A) extends Validation[E, A]
+private final case class Failure[E, A](e: E) extends Validation[E, A]
 
 sealed trait FailProjection[+E, +A] {
 
