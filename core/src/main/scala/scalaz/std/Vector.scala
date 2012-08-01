9e54549bfadbce1d4676b7e38e79a4a7db604d47
Stephen Compall <scompall@spcapitaliq.com>
2012-08-01 15:44:31 -0400

    Replace vector instances with specialized indexedseq, except equal.

--- core/src/main/scala/scalaz/std/Vector.scala
+++ core/src/main/scala/scalaz/std/Vector.scala
@@ -1,7 +1,12 @@
 package scalaz
 package std
 
-import annotation.tailrec
+trait IndexedSeqSubVector extends IndexedSeqSub {
+  type IxSq[+A] = Vector[A]
+  protected final def buildIxSq[A, B] = implicitly
+  protected final def monad = vector.vectorInstance
+  protected final def empty[A] = Vector()
+}
 
 trait VectorInstances0 {
   implicit def vectorEqual[A](implicit A0: Equal[A]) = new IndexedSeqEqual[A, Vector[A]] {
@@ -10,88 +15,17 @@ trait VectorInstances0 {
 }
 
 trait VectorInstances extends VectorInstances0 {
-  implicit val vectorInstance = new Traverse[Vector] with MonadPlus[Vector] with Each[Vector] with Index[Vector] with Length[Vector] with ApplicativePlus[Vector] with Zip[Vector] with Unzip[Vector] {
-    def each[A](fa: Vector[A])(f: (A) => Unit) = fa foreach f
-    def index[A](fa: Vector[A], i: Int) = if (fa.size > i) Some(fa(i)) else None
-    def length[A](fa: Vector[A]) = fa.length
-    def point[A](a: => A) = scala.Vector(a)
-    def bind[A, B](fa: Vector[A])(f: A => Vector[B]) = fa flatMap f
-    def empty[A] = scala.Vector()
-    def plus[A](a: Vector[A], b: => Vector[A]) = a ++ b
-    override def map[A, B](v: Vector[A])(f: A => B) = v map f
+  object generic extends IndexedSeqSubVector with IndexedSeqSubInstances
 
-    def zip[A, B](a: => Vector[A], b: => Vector[B]) = a zip b
-    def unzip[A, B](a: Vector[(A, B)]) = a.unzip
+  implicit val vectorInstance = generic.ixSqInstance
 
-    def traverseImpl[F[_], A, B](v: Vector[A])(f: A => F[B])(implicit F: Applicative[F]) = {
-      DList.fromList(v.toList).foldr(F.point(Vector[B]())) {
-         (a, fbs) => F.map2(f(a), fbs)(_ +: _)
-      }
-    }
-    
-    override def traverseS[S,A,B](v: Vector[A])(f: A => State[S,B]): State[S,Vector[B]] =
-      State((s: S) => 
-        v.foldLeft((s, Vector[B]()))((acc, a) => {
-          val bs = f(a)(acc._1)
-          (bs._1, acc._2 :+ bs._2)
-        }))
+  implicit def vectorMonoid[A]: Monoid[Vector[A]] = generic.ixSqMonoid
 
-    override def foldRight[A, B](fa: Vector[A], z: => B)(f: (A, => B) => B) = {
-      import scala.collection.mutable.ArrayStack
-      val s = new ArrayStack[A]
-      fa.foreach(a => s += a)
-      var r = z
-      while (!s.isEmpty) {
-        // force and copy the value of r to ensure correctness
-        val w = r
-        r = f(s.pop, w)
-      }
-      r
-    }
+  implicit def vectorShow[A: Show]: Show[Vector[A]] = generic.ixSqShow
 
-  }
-
-  implicit def vectorMonoid[A]: Monoid[Vector[A]] = new Monoid[Vector[A]] {
-    def append(f1: Vector[A], f2: => Vector[A]) = f1 ++ f2
-    def zero: Vector[A] = Vector()
-  }
-
-  implicit def vectorShow[A: Show]: Show[Vector[A]] = new Show[Vector[A]] {
-    def show(as: Vector[A]) =
-      (List('[') +: (vector.intersperse(as.map(Show[A].show(_)), List(',')) :+ List(']'))).flatten.toList
-  }
-
-  implicit def vectorOrder[A](implicit A0: Order[A]): Order[Vector[A]] = new VectorOrder[A] {
-    implicit def A = A0
-  }
-
+  implicit def vectorOrder[A](implicit A0: Order[A]): Order[Vector[A]] = generic.ixSqOrder
 }
 
-trait VectorFunctions extends IndexedSeqSubFunctions {
-  type IxSq[+A] = Vector[A]
-  protected def buildIxSq[A, B] = implicitly
-  protected def monad = vector.vectorInstance
-  protected def empty[A] = Vector()
-}
-
-object vector extends VectorInstances with VectorFunctions {
+object vector extends IndexedSeqSubVector with VectorInstances with IndexedSeqSubFunctions {
   object vectorSyntax extends scalaz.syntax.std.ToVectorOps
-}
-
-trait VectorOrder[A] extends Order[Vector[A]] with IndexedSeqEqual[A, Vector[A]] {
-  implicit def A: Order[A]
-
-  import Ordering._
-
-  def order(a1: Vector[A], a2: Vector[A]) =
-    (a1, a2) match {
-      case (Vector(), Vector()) => EQ
-      case (Vector(), y)        => LT
-      case (x, Vector())        => GT
-      case (as, bs) => Order[A].order(as.head, bs.head) match {
-        case EQ => order(as.tail, bs.tail)
-        case x  => x
-      }
-    }
-
 }
