c4e352df1cadcd09e7be03e31b9005540e2fe037
Stephen Compall <scompall@spcapitaliq.com>
2012-08-01 16:21:27 -0400

    map.mapKeys

--- core/src/main/scala/scalaz/syntax/std/MapOps.scala
+++ core/src/main/scala/scalaz/syntax/std/MapOps.scala
@@ -7,6 +7,7 @@ import scalaz.std.{ map => dict }
 trait MapOps[K, A] extends Ops[Map[K, A]] {
   final def intersectWithKey[B, C](m: Map[K, B])(f: (K, A, B) => C): Map[K, C] = dict.intersectWithKey(self, m)(f)
   final def intersectWith[B, C](m: Map[K, B])(f: (A, B) => C): Map[K, C] = dict.intersectWith(self, m)(f)
+  final def mapKeys[K2](f: K => K2) = dict.mapKeys(self)(f)
   final def unionWithKey(m: Map[K, A])(f: (K, A, A) => A): Map[K, A] = dict.unionWithKey(self, m)(f)
   final def unionWith(m: Map[K, A])(f: (A, A) => A): Map[K, A] = dict.unionWith(self, m)(f)
 }
