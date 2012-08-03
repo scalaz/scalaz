78d6db1075c14adaa7f46421751dade688abdd1a
runarorama <runarorama@gmail.com>
2012-08-03 16:13:11 -0400

    Removed incoming files that we're not tracking

--- sbt.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-set SCRIPT_DIR=%~dp0
-set ANT_OPTS=-Dsbt.ivy.home=C:/Users/%USERNAME%/.ivy2/
-
-chcp 1253 && java %SBT_OPTS% -Xmx1024m -Dsbt.ivy.home=C:/Users/%USERNAME%/.ivy2/ -Ddb.dev.user=clarifidev -Ddb.dev.password=sti11f!y -Dfile.encoding=Cp1253 -Xms512M -Xmx1536M -Xss5M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=384M -jar "%SCRIPT_DIR%sbt-launch.jar" %* && chcp 437
