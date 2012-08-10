cc94678bf7369ed0e833a7bef15640218c7a6185
Kris Nuttycombe <kris@reportgrid.com>
2012-08-10 17:40:22 +0000

    Reverted privatization of Success and Failure because it breaks a shitton of code.

--- core/src/main/scala/scalaz/Validation.scala
+++ core/src/main/scala/scalaz/Validation.scala
@@ -301,8 +301,8 @@ sealed trait Validation[+E, +A] {
 
 }
 
-private final case class Success[E, A](a: A) extends Validation[E, A]
-private final case class Failure[E, A](e: E) extends Validation[E, A]
+final case class Success[E, A](a: A) extends Validation[E, A]
+final case class Failure[E, A](e: E) extends Validation[E, A]
 
 object Validation extends ValidationFunctions with ValidationInstances {
 
