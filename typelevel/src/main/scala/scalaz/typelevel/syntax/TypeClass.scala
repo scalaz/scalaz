30d06b794c24150ee53dd5d794f40fd4a738a2c9
e.e d3si9n <eed3si9n@gmail.com>
2012-10-02 06:27:05 -0400

    resurrected typelevel composition syntax

--- typelevel/src/main/scala/scalaz/typelevel/syntax/TypeClass.scala
+++ typelevel/src/main/scala/scalaz/typelevel/syntax/TypeClass.scala
@@ -29,11 +29,16 @@ trait TypeClasses extends TypeClasses0 {
   implicit def unpackKProduct[C[_[_]], T <: TCList](wrapper: WrappedProduct[C, T]) =
     wrapper.instance
 
+  implicit def unpackKComposition[C[_[_]], T <: TCList](wrapper: WrappedComposition[C, T]) =
+    wrapper.instance
+
   // Instance syntax
 
   implicit def wrapKProduct[C[_[_]] : KTypeClass, F[_]](instance: C[F]) =
     instance *: KTypeClass[C].emptyProduct
 
+  implicit def wrapKComposition[C[_[_]] : KTypeClass, F[_]](instance: C[F]) =
+    instance <<: KTypeClass[C].idCompose
 }
 
 // vim: expandtab:ts=2:sw=2
