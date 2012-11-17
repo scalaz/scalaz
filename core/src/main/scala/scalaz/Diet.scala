7776358a2bfd1b9c4e4d96860e6e4b9ab2f83e92
sean <git@futurenotfound.com>
2012-11-17 12:17:55 +0000

    Feature complete implementation of the Discrete Interval Encoding Tree type, except it's a Vector underneath. Tweak to how the trampolined calls are made within Enum which prevents stack overflow when constructing a large List (millions of items for example).

--- core/src/main/scala/scalaz/Diet.scala
+++ core/src/main/scala/scalaz/Diet.scala
@@ -3,127 +3,171 @@ package scalaz
 import scala.annotation.tailrec
 import Scalaz._
 import Free._
+import scala.math
 
-sealed trait Diet[+A] {
-  val depth: Int
+sealed trait Diev[+A] {
+  def +[B >: A](cakeInterval: (B, B))(implicit E: Enum[B]): Diev[B]
+
+  def +[B >: A](value: B)(implicit E: Enum[B]): Diev[B]
+
+  def -[B >: A](interval: (B, B))(implicit E: Enum[B]): Diev[B]
+
+  def -[B >: A](value: B)(implicit E: Enum[B]): Diev[B]
+
   def contains[B >: A](value: B)(implicit E: Enum[B]): Boolean
-  def +[B >: A](value: B)(implicit E: Enum[B]): Diet[B]
-  def freeAdd[B >: A, S[+_]](value: B)(implicit E: Enum[B], S: Pointed[S]): Free[S, Diet[B]]
-  def -[B >: A](value: B)(implicit E: Enum[B]): Diet[B]
-  def freeSubtract[B >: A, S[+_]](value: B)(implicit E: Enum[B], S: Pointed[S]): Free[S, Diet[B]]
-  def merge[B >: A](other: Diet[B])(implicit E: Enum[B]): Diet[B]
-  def foldLeft[B](b: B)(f: (B, (A, A)) => B): B = freeFoldLeft[B, Function0](b)(f).go(_.copoint)
-  def freeFoldLeft[B, S[+_]](b: B)(f: (B, (A, A)) => B)(implicit S: Pointed[S]): Free[S, B]
-  override def toString(): String = foldLeft(new StringBuilder)(_.append(_)).toString
+
+  def contains[B >: A](interval: (B, B))(implicit E: Enum[B]): Boolean
+
+  def foldLeft[B >: A, C](z: C)(f: (C, B) => C)(implicit E: Enum[B]): C
+
+  def toSet[B >: A](implicit E: Enum[B]): Set[B]
 }
 
-final case object DietEmpty extends Diet[Nothing] {
-  val depth: Int = 0
 
-  def contains[B >: Nothing](value: B)(implicit E: Enum[B]): Boolean = false
-
-  def +[B >: Nothing](value: B)(implicit E: Enum[B]): Diet[B] = DietNode(value, value, DietEmpty, DietEmpty)
-
-  def freeAdd[B >: Nothing, S[+_]](value: B)(implicit E: Enum[B], S: Pointed[S]): Free[S, Diet[B]] = return_(this.+(value))
-
-  def -[B >: Nothing](value: B)(implicit E: Enum[B]): Diet[B] = DietEmpty
-
-  def freeSubtract[B >: Nothing, S[+_]](value: B)(implicit E: Enum[B], S: Pointed[S]): Free[S, Diet[B]] = return_(this.-(value))
-
-  def merge[B >: Nothing](other: Diet[B])(implicit E: Enum[B]): Diet[B] = other
-
-  def freeFoldLeft[B, S[+_]](b: B)(f: (B, (Nothing, Nothing)) => B)(implicit S: Pointed[S]): Free[S, B] = return_(b)
-}
-
-sealed case class DietNode[+A](start: A, end: A, left: Diet[A], right: Diet[A]) extends Diet[A] {
-  val depth = left.depth.max(right.depth) + 1
-
-  def contains[B >: A](value: B)(implicit E: Enum[B]): Boolean = {
-    (start <= value && value <= end) || left.contains(value) || right.contains(value)
+object Diev {
+  def subtractInterval[B](minuend: (B, B), subtraend: (B, B))(implicit E: Enum[B]): Vector[(B, B)] = {
+    val startOverlap = if(subtraend._1 > minuend._1) Vector((minuend._1, subtraend._1.pred)) else Vector()
+    //println("startOverlap = " + startOverlap)
+    val endOverlap = if(subtraend._2 < minuend._2) Vector((subtraend._2.succ, minuend._2)) else Vector()
+    //println("endOverlap = " + endOverlap)
+    startOverlap ++ endOverlap
   }
 
-  def +[B >: A](value: B)(implicit E: Enum[B]): Diet[B] = freeAdd[B, Function0](value).go(_.copoint)
+  def fixIntervalOrder[B](interval: (B, B))(implicit E: Enum[B]): (B, B) = if (interval._2 < interval._1) (interval._2, interval._1) else interval
 
-  def freeAdd[B >: A, S[+_]](value: B)(implicit E: Enum[B], S: Pointed[S]): Free[S, Diet[B]] = {
-    if (value < start) {
-      if (value.succ == start) DietNode(value, end, left, right).joinLeft
-      else for {
-        addedLeft <- suspend(left.freeAdd[B, S](value))
-      } yield DietNode(start, end, addedLeft, right)
-    } else if (value > end) {
-      if (value.pred == end) DietNode(start, value, left, right).joinRight
-      else for {
-        addedRight <- suspend(right.freeAdd[B, S](value))
-      } yield DietNode(start, end, left, addedRight)
-    } else return_(this)
-  }
+  def empty[A]: Diev[A] = DieVector()
 
-  def -[B >: A](value: B)(implicit E: Enum[B]): Diet[B] = freeSubtract[B, Function0](value).go(_.copoint)
+  private[this] case class DieVector[+A](intervals: Vector[(A, A)] = Vector()) extends Diev[A] {
+    val liftedIntervals = intervals.lift
 
-  def freeSubtract[B >: A, S[+_]](value: B)(implicit E: Enum[B], S: Pointed[S]): Free[S, Diet[B]] = {
-    if (value < start) for {
-      subtractedLeft <- left.freeSubtract[B, S](value)
-    } yield DietNode(start, end, subtractedLeft, right)
-    else if (value > end) for {
-      subtractedRight <- right.freeSubtract[B, S](value)
-    } yield DietNode(start, end, left, subtractedRight)
-    else if (value == start) {
-      if (start == end) return_(left.merge[B](right))
-      else return_(DietNode((start: B).succ, end, left, right))
-    } else if (value == end) return_(DietNode(start, (end: B).pred, left, right))
-    else return_(DietNode(start, value.pred, left, DietNode(value.succ, end, DietEmpty, right)))
-  }
-
-  private[scalaz] def splitMax[B >: A, S[+_]](implicit E: Enum[B], S: Pointed[S]): Free[S, (Diet[B], (B, B))] = {
-    right match {
-      case DietEmpty => return_((left, (start, end)))
-      case rightAsNode@ DietNode(_, _, _, _) => suspend(for {
-        splitMaxResult <- rightAsNode.splitMax[B, S]
-      } yield (DietNode(start, end, left, splitMaxResult._1), splitMaxResult._2))
+    private[this] sealed abstract class SearchResult
+    private[this] sealed case class Coincidence(position: Int) extends SearchResult
+    private[this] sealed case class Between(before: Option[Int], after: Option[Int]) extends SearchResult {
+      def adjacentBefore[B >: A](interval: (B, B))(implicit E: Enum[B]): Option[Int] = before.filter{pos => (intervals(pos)._2: B).succ === interval._1}
+      def adjacentAfter[B >: A](interval: (B, B))(implicit E: Enum[B]): Option[Int] = after.filter{pos => (intervals(pos)._1: B).pred === interval._2}
     }
-  }
 
-  private[scalaz] def splitMin[B >: A, S[+_]](implicit E: Enum[B], S: Pointed[S]): Free[S, (Diet[B], (B, B))] = {
-    left match {
-      case DietEmpty => return_((right, (start, end)))
-      case leftAsNode@ DietNode(_, _, _, _) => suspend(for {
-        splitMinResult <- leftAsNode.splitMin[B, S]
-      } yield (DietNode(start, end, splitMinResult._1, right), splitMinResult._2))
+    private def construct[B >: A](prefixCount: Int, middle: Vector[(B, B)], suffixStart: Int): Diev[B] = {
+      DieVector(intervals.take(prefixCount) ++ middle ++ intervals.drop(suffixStart))
     }
-  }
 
-  private[scalaz] def joinLeft[B >: A, S[+_]](implicit E: Enum[B], S: Pointed[S]): Free[S, Diet[B]] = {
-    left match {
-      case DietEmpty => return_(this)
-      case leftAsNode@ DietNode(_, _, _, _) => suspend(for {
-        splitMaxResult <- leftAsNode.splitMax[B, S]
-      } yield if ((splitMaxResult._2._2: B).succ == start) DietNode(splitMaxResult._2._1, end, splitMaxResult._1, right) else this)
+    private[this] def binarySearch[B >: A](value: B)(implicit E: Enum[B]): SearchResult = {
+      @tailrec
+      def innerSearch(min: Int = 0, max: Int = intervals.size): SearchResult = {
+        //println("innerSearch(min = %s, max = %s)".format(min, max))
+        if (max <= min) {
+          val adjustedPosition = 0.max(min.min(max).min(intervals.size - 1))
+          //println("adjustedPosition = " + adjustedPosition)
+          liftedIntervals(adjustedPosition) match {
+            case Some((start, end)) => {
+              if (start <= value && value <= end) Coincidence(adjustedPosition)
+              else {
+                if (value < start) Between(liftedIntervals(adjustedPosition - 1).map(_ => adjustedPosition - 1), adjustedPosition.some)
+                else Between(adjustedPosition.some, liftedIntervals(adjustedPosition + 1).map(_ => adjustedPosition + 1))
+              }
+            }
+            case _ => Between(None, None)
+          }
+        } else {
+          val mid = min + ((max - min) / 2)
+          
+          intervals(mid) match {
+            case (start, end) => {
+              if (start <= value && value <= end) Coincidence(mid)
+              else {
+                if (value < start) innerSearch(min, mid - 1)
+                else innerSearch(mid + 1, max)
+              }
+            }
+          }
+        }
+      }
+
+      val resultOfSearch = innerSearch()
+      //println("resultOfSearch = " + resultOfSearch)
+      resultOfSearch
     }
-  }
-  private[scalaz] def joinRight[B >: A, S[+_]](implicit E: Enum[B], S: Pointed[S]): Free[S, Diet[B]]  = {
-    right match {
-      case DietEmpty => return_(this)
-      case rightAsNode@ DietNode(_, _, _, _) => suspend(for {
-        splitMinResult <- rightAsNode.splitMin[B, S]
-      } yield if ((end: B).succ == splitMinResult._2._1) DietNode(start, splitMinResult._2._2, left, splitMinResult._1) else this)
+
+    def subtractInterval[B](minuend: (B, B), subtraend: (B, B))(implicit E: Enum[B]): Vector[(B, B)] = {
+      val startOverlap = if(subtraend._1 > minuend._1) Vector((minuend._1, subtraend._1.pred)) else Vector()
+      //println("startOverlap = " + startOverlap)
+      val endOverlap = if(subtraend._2 < minuend._2) Vector((subtraend._2.succ, minuend._2)) else Vector()
+      //println("endOverlap = " + endOverlap)
+      startOverlap ++ endOverlap
     }
-  }
-  
-  def merge[B >: A](other: Diet[B])(implicit E: Enum[B]): Diet[B] = {
-    other match {
-      case DietEmpty => this
-      case otherAsNode@ DietNode(_, _, _, _) => {
-        val (otherLeft, (otherStart, otherEnd)) = otherAsNode.splitMax[B, Function0].go(_.copoint)
-        DietNode(start, end, otherLeft, right)
+
+    def fixIntervalOrder[B >: A](interval: (B, B))(implicit E: Enum[B]): (B, B) = if (interval._2 < interval._1) (interval._2, interval._1) else interval
+
+    def +[B >: A](interval: (B, B))(implicit E: Enum[B]): Diev[B] = {
+      (binarySearch(interval._1), binarySearch(interval._2)) match {
+        case (Coincidence(startPosition), Coincidence(endPosition)) => {
+          construct(startPosition, Vector((intervals(startPosition)._1.min(interval._1), intervals(endPosition)._2.max(interval._2))), endPosition + 1)
+        }
+        case (Coincidence(startPosition), between@Between(_, after)) => {
+          val adjustedAfter = between.adjacentAfter(interval).orElse(after)
+          construct(startPosition, Vector((intervals(startPosition)._1.min(interval._1), adjustedAfter.map(intervals(_)._2).getOrElse(interval._2))), adjustedAfter.getOrElse(intervals.size))
+        }
+        case (earlyBound@ Between(before, after), Coincidence(endPosition)) => {
+          val adjustedBefore = earlyBound.adjacentBefore(interval).orElse(before)
+          construct(adjustedBefore.getOrElse(0), Vector((adjustedBefore.map(intervals(_)._1).getOrElse(interval._1), intervals(endPosition)._2.max(interval._2))), endPosition + 1)
+        }
+        case (earlyBound@ Between(before, after), lateBound@Between(_, otherAfter)) => {
+          val adjacentBeforeResult = earlyBound.adjacentBefore(interval)
+          val adjacentAfterResult = lateBound.adjacentAfter(interval)
+          construct(
+            adjacentBeforeResult.orElse(before.map(_ + 1)).getOrElse(0),
+            Vector((adjacentBeforeResult.map(intervals(_)._1).getOrElse(interval._1), adjacentAfterResult.map(intervals(_)._2).getOrElse(interval._2))),
+            adjacentAfterResult.map(_ + 1).orElse(after).getOrElse(intervals.size)
+          )
+        }
       }
     }
-  }
 
-  def freeFoldLeft[B, S[+_]](b: B)(f: (B, (A, A)) => B)(implicit S: Pointed[S]): Free[S, B] = {
-    for {
-      fromLeft <- left.freeFoldLeft[B, S](b)(f)
-      fromThis <- return_[S, B](f(fromLeft, (start, end)))
-      fromRight <- right.freeFoldLeft[B, S](fromThis)(f)
-    } yield fromRight
+    def +[B >: A](value: B)(implicit E: Enum[B]): Diev[B] = this + (value, value)
+
+    def -[B >: A](interval: (B, B))(implicit E: Enum[B]): Diev[B] = {
+      val orderedInterval = fixIntervalOrder(interval)
+      (binarySearch(orderedInterval._1), binarySearch(orderedInterval._2)) match {
+        case (Coincidence(startPosition), Coincidence(endPosition)) => {
+          val middle = if (startPosition == endPosition) subtractInterval(intervals(startPosition), interval)
+          else subtractInterval(intervals(startPosition), interval) ++ subtractInterval(intervals(endPosition), interval)
+          construct(startPosition, middle, endPosition + 1)
+        }
+        case (Coincidence(startPosition), Between(_, endAfter)) => {
+          val middle = subtractInterval(intervals(startPosition), interval)
+          construct(startPosition, middle, endAfter.map(endAfterPos => endAfterPos + 1).getOrElse(intervals.size))
+        }
+        case (Between(startBefore, _), Coincidence(endPosition)) => {
+          val middle = subtractInterval(intervals(endPosition), interval)
+          construct(startBefore.map(startBeforePos => startBeforePos + 1).orZero, middle, endPosition + 1)
+        }
+        case (Between(startBefore, _), Between(_, endAfter)) => {
+          construct(startBefore.map(startBeforePos => startBeforePos + 1).orZero, Vector.empty, endAfter.map(endAfterPos => endAfterPos + 1).orZero)
+        }
+      }
+    }
+
+    def -[B >: A](value: B)(implicit E: Enum[B]): Diev[B] = this - (value, value)
+
+    def contains[B >: A](value: B)(implicit E: Enum[B]): Boolean = binarySearch(value) match {
+      case Coincidence(_) => true
+      case _ => false
+    }
+
+    def contains[B >: A](interval: (B, B))(implicit E: Enum[B]): Boolean = binarySearch(interval._1) match {
+      case Coincidence(position) if (intervals(position)._2 >= interval._2) => true
+      case _ => false
+    }
+
+    def foldLeft[B >: A, C](z: C)(f: (C, B) => C)(implicit E: Enum[B]): C = {
+      intervals.foldLeft(z){(z1, interval) =>
+        val range = (interval._1: B) |-> (interval._2: B)
+        range.foldLeft(z1)(f)
+      }
+    }
+
+    def toSet[B >: A](implicit E: Enum[B]): Set[B] = foldLeft[B, Set[B]](Set[B]())(_ + _)
+
+    override def toString(): String = intervals.view.map(interval => "(%s -> %s)".format(interval._1, interval._2)).mkString(", ")
   }
 }
