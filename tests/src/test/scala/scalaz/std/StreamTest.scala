6f5a02e4112ca277f6f4b27888136edb659a736d
Stephen Compall <scompall@nocandysw.com>
2012-11-27 21:41:41 -0500

    Test analogy of fold* family with fold*/reduce* family from scala.collection.

--- tests/src/test/scala/scalaz/std/StreamTest.scala
+++ tests/src/test/scala/scalaz/std/StreamTest.scala
@@ -38,4 +38,29 @@ class StreamTest extends Spec {
     }
     (a: Stream[Int], b: Int) => (a.intersperse(b) must be_===(intersperse(a, b)))
   }
+
+
+  "foldl1 is reduceLeft" ! check {(hd: List[Int], rngel: Stream[List[Int]]) =>
+    val rnge = hd #:: rngel
+    val F = Foldable[Stream]
+    Some(rnge.reduceLeft(_++_)) must be_===(F.foldl1(rnge)(_++_))
+  }
+
+  "foldl is foldLeft" ! check {(rnge: Stream[List[Int]]) =>
+    val F = Foldable[Stream]
+    (rnge.foldLeft(List[Int]())(_++_)
+      must be_===(F.foldLeft(rnge, List[Int]())(_++_)))
+  }
+
+  "foldr1 is reduceRight" ! check {(hd: List[Int], rngel: Stream[List[Int]]) =>
+    val rnge = hd #:: rngel
+    val F = Foldable[Stream]
+    Some(rnge.reduceRight(_++_)) must be_===(F.foldr1(rnge)(_++_))
+  }
+
+  "foldr is foldRight" ! check {(rnge: Stream[List[Int]]) =>
+    val F = Foldable[Stream]
+    (rnge.foldRight(List[Int]())(_++_)
+      must be_===(F.foldRight(rnge, List[Int]())(_++_)))
+  }
 }
