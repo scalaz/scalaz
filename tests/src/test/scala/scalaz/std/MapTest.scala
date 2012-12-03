14ec5a5ac8a8902a604ba552d60038751d78dd8a
Paul Chiusano <paul.chiusano@gmail.com>
2012-12-03 14:46:17 -0500

    added test for previously busted Map ordering

---
+++ tests/src/test/scala/scalaz/std/MapTest.scala
@@ -0,0 +1,20 @@
+package scalaz
+package std
+
+import std.AllInstances._
+import scalaz.scalacheck.ScalazProperties._
+import org.scalacheck.Prop.forAll
+import scala.math.{Ordering => SOrdering}
+
+class MapTest extends Spec {
+  "map ordering" ! prop {
+    val O = implicitly[Order[Map[String,Int]]]
+    val O2 = SOrdering.Iterable(implicitly[SOrdering[(String,Int)]])
+    (kvs: List[(String,Int)], kvs2: List[(String,Int)]) => {
+      val (m1, m2) = (kvs.toMap, kvs2.toMap)
+      val l: Boolean = O.lessThan(m1, m2)
+      val r: Boolean = O2.lt(kvs.sortBy(_._1), kvs2.sortBy(_._1))
+      l == r
+    }
+  }
+}
