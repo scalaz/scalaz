76f9e0bf96c21a108d69d84852e343f1318e62ec
xuwei-k <6b656e6a69@gmail.com>
2012-12-03 18:31:44 +0900

    fix history instance type

--- core/src/main/scala/scalaz/undo/History.scala
+++ core/src/main/scala/scalaz/undo/History.scala
@@ -4,7 +4,7 @@ package undo
 case class History[S](current: S, undos: List[S] = Nil, redos: List[S] = Nil)
 
 trait HistoryInstances {
-  implicit def history: Functor[History] = new Pointed[History] {
+  implicit def history: Pointed[History] = new Pointed[History] {
     def map[A, B](fa: History[A])(f: (A) => B): History[B] = History(f(fa.current), fa.undos.map(f), fa.redos.map(f))
 
     def point[A](a: => A): History[A] = History(a)
