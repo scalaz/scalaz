2b8fbfa795e29a98bec72aa027e329bc54370c00
Lars Hupel <lars.hupel@mytum.de>
2012-12-07 13:26:04 +0100

    change 'if' in test to use conditional ScalaCheck property

--- tests/src/test/scala/scalaz/std/MapTest.scala
+++ tests/src/test/scala/scalaz/std/MapTest.scala
@@ -3,7 +3,6 @@ package std
 
 import std.AllInstances._
 import scalaz.scalacheck.ScalazProperties._
-import org.scalacheck.Prop.forAll
 import scala.math.{Ordering => SOrdering}
 
 class MapTest extends Spec {
@@ -12,11 +11,11 @@ class MapTest extends Spec {
     val O2 = SOrdering.Iterable(implicitly[SOrdering[(String,Int)]])
     (kvs: List[(String,Int)], kvs2: List[(String,Int)]) => {
       val (m1, m2) = (kvs.toMap, kvs2.toMap)
-      if((m1.size == kvs.size) && (m2.size == kvs2.size)){
+      ((m1.size == kvs.size) && (m2.size == kvs2.size)) ==> {
         val l: Boolean = O.lessThan(m1, m2)
         val r: Boolean = O2.lt(kvs.sortBy(_._1), kvs2.sortBy(_._1))
         l == r
-      } else true
+      }
     }
   }
 }
