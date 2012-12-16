1e4431b5dd8f3102b93c008fd134aa9425815332
Stephen Compall <scompall@nocandysw.com>
2012-12-16 17:47:08 -0500

    Less-strict Function0 bind.

--- core/src/main/scala/scalaz/std/Function.scala
+++ core/src/main/scala/scalaz/std/Function.scala
@@ -22,7 +22,7 @@ trait FunctionInstances extends FunctionInstances0 {
 
     def copoint[A](p: () => A) = p()
 
-    def bind[A, B](fa: () => A)(f: (A) => () => B) = f(fa())
+    def bind[A, B](fa: () => A)(f: (A) => () => B) = () => f(fa())()
 
     def traverseImpl[G[_]: Applicative, A, B](fa: () => A)(f: (A) => G[B]) =
       Applicative[G].map(f(fa()))((b: B) => () => b)
