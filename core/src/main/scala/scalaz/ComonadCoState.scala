58866611bd700abdbbd39ae011a5db6070a3b468
Alexander Altman <alexanderaltman@me.com>
2012-12-19 21:24:56 -0500

    Removed some unnecessarily restrictive variances

--- core/src/main/scala/scalaz/ComonadCoState.scala
+++ core/src/main/scala/scalaz/ComonadCoState.scala
@@ -1,6 +1,6 @@
 package scalaz
 
-trait ComonadStore[F[_, +_], S] extends Comonad[({type f[+x]=F[S,x]})#f] {
+trait ComonadStore[F[_, _], S] extends Comonad[({type f[x]=F[S,x]})#f] {
   def pos[A](w: F[S, A]): S
   def peek[A](s: S, w: F[S, A]): A
 
@@ -13,10 +13,10 @@ trait ComonadStore[F[_, +_], S] extends Comonad[({type f[+x]=F[S,x]})#f] {
   def seeks[A](s: S => S, w: F[S, A]): F[S, A] =
     peeks(s, cojoin(w))
 
-  def experiment[G[+_], A](s: S => G[S], w: F[S, A])(implicit FG: Functor[G]): G[A] =
+  def experiment[G[_], A](s: S => G[S], w: F[S, A])(implicit FG: Functor[G]): G[A] =
     FG.map(s(pos(w)))(peek(_, w))
 }
 
 object ComonadStore {
-  def apply[F[_, +_],S](implicit F: ComonadStore[F, S]): ComonadStore[F, S] = F
+  def apply[F[_, _],S](implicit F: ComonadStore[F, S]): ComonadStore[F, S] = F
 }
