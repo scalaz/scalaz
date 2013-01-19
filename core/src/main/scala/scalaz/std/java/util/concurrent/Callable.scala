3b565e23acd3c8d3e7ea9c898eb8775f1de4bb24
Alexander Altman <alexanderaltman@me.com>
2013-01-19 12:33:50 -0800

    Fixed Callable changes
    [none]

--- core/src/main/scala/scalaz/std/java/util/concurrent/Callable.scala
+++ core/src/main/scala/scalaz/std/java/util/concurrent/Callable.scala
@@ -5,12 +5,12 @@ import java.util.concurrent.Callable
 
 
 trait CallableInstances {
-  implicit def callableEqual[A: Order] = new Order[Callable[A]] {
+  implicit def callableOrder[A: Order] = new Order[Callable[A]] {
     def order(f1: Callable[A], f2: Callable[A]) = Order[A].order(f1.call, f2.call)
   }
 
   implicit def callableMonad: Monad[Callable] = new Monad[Callable] {
-    def map[A, B](fa: Callable[A])(f: (A) => B) = new Callable[B] {
+    override def map[A, B](fa: Callable[A])(f: (A) => B) = new Callable[B] {
       def call() = f(fa.call)
     }
     def bind[A, B](fa: Callable[A])(f: (A) => Callable[B]) = new Callable[B] {
