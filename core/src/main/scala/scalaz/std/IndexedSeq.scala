b8e3bcbfa6efc8b94531d62a1bc0ba4a7c3d7e0e
Stephen Compall <scompall@nocandysw.com>
2013-01-22 21:24:30 -0500

    Cheaper IndexedSeq `spanM` without `as` aliasing.  Fixes #266.

--- core/src/main/scala/scalaz/std/IndexedSeq.scala
+++ core/src/main/scala/scalaz/std/IndexedSeq.scala
@@ -170,10 +170,7 @@ trait IndexedSeqSubFunctions extends IndexedSeqSub {
   /** A pair of the longest prefix of passing `as` against `p`, and
     * the remainder. */
   final def spanM[A, M[_] : Monad](as: IxSq[A])(p: A => M[Boolean]): M[(IxSq[A], IxSq[A])] =
-    if (as.isEmpty) Monad[M].point(empty, empty) else
-          Monad[M].bind(p(as.head))(b =>
-            if (b) Monad[M].map(spanM(as.tail)(p))((k: (IxSq[A], IxSq[A])) => (as.head +: k._1, k._2))
-            else Monad[M].point(empty, as))
+    Monad[M].map(takeWhileM(as)(p))(ys => (ys, as drop (ys.length)))
 
   /** `spanM` with `p`'s complement. */
   final def breakM[A, M[_] : Monad](as: IxSq[A])(p: A => M[Boolean]): M[(IxSq[A], IxSq[A])] =
