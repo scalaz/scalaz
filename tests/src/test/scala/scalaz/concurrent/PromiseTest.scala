be680ac31eed915295e7d77c6d30c51fccb6755c
Runar Bjarnason <runarorama@gmail.com>
2013-03-07 21:30:50 -0500

    Added tests for promise

--- tests/src/test/scala/scalaz/concurrent/PromiseTest.scala
+++ tests/src/test/scala/scalaz/concurrent/PromiseTest.scala
@@ -4,6 +4,7 @@ package concurrent
 import scalaz.scalacheck.ScalazProperties._
 import scalaz.scalacheck.ScalazArbitrary._
 import std.AllInstances._
+import org.scalacheck.Prop._
 
 class PromiseTest extends Spec {
   implicit def promiseEqual[A: Equal] = Equal[A].contramap((_: Promise[A]).get)
@@ -11,4 +12,9 @@ class PromiseTest extends Spec {
   checkAll(monad.laws[Promise])
   checkAll(traverse.laws[Promise])
   checkAll(comonad.laws[Promise])
+
+  check(throws(Promise({throw new Error("x"); 1}).flatMap(_ => Promise(2)).get, classOf[Error]))
+  check(throws(Promise(0).filter(_ != 0).get, classOf[Promise.BrokenException]))
+  check(forAll((x: Int) => Promise(x).filter(_ => true).get === x))
+
 }
