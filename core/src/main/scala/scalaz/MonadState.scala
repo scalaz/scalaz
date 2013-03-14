627ce242d635641ecfa0ae2fa0a003eaad1e9687
Gary Pamparà <gpampara@gmail.com>
2013-03-14 10:35:15 +0200

    Added missing 'get' for MonadState instances.
    Signed-off-by: Gary Pamparà <gpampara@gmail.com>

--- core/src/main/scala/scalaz/MonadState.scala
+++ core/src/main/scala/scalaz/MonadState.scala
@@ -5,6 +5,7 @@ package scalaz
   */
 trait MonadState[F[_,_],S] extends Monad[({type f[x]=F[S,x]})#f] {
   def init: F[S,S]
+  def get: F[S, S]
   def put(s: S): F[S,Unit]
   def modify(f: S => S): F[S, Unit] = bind(init)(s => put(f(s)))
   def gets[A](f: S => A): F[S, A] = bind(init)(s => point(f(s)))
