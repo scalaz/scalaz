60154b0e1136c27334be797c5905a77118fe7d26
Sanshiro Yoshida <halcat0x15a@gmail.com>
2013-03-16 16:01:59 +0900

    add reader and writer instance

--- effect/src/main/scala/scalaz/std/effect/AllEffectInstances.scala
+++ effect/src/main/scala/scalaz/std/effect/AllEffectInstances.scala
@@ -3,6 +3,8 @@ package scalaz.std.effect
 trait AllEffectInstances 
     extends InputStreamInstances
     with OutputStreamInstances
+    with ReaderInstances
+    with WriterInstances
     with sql.ConnectionInstances
     with sql.StatementInstances
     with sql.PreparedStatementInstances
