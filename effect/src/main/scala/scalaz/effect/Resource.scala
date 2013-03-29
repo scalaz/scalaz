117d0158aa84000d28e34892a4d7fa255a7d9cc6
Lars Hupel <lars.hupel@mytum.de>
2013-03-29 14:51:47 +0100

    remove redundant (and commented) instances

--- effect/src/main/scala/scalaz/effect/Resource.scala
+++ effect/src/main/scala/scalaz/effect/Resource.scala
@@ -24,33 +24,5 @@ object Resource {
   @inline def apply[F](implicit F: Resource[F]): Resource[F] = F
 
   ////
-  /*
-    TODO move to scalaz.effect.std._. Cumbersome, I know.
-    import java.io.{OutputStream, InputStream}
-    import java.sql.{PreparedStatement, ResultSet, Statement, Connection}
-
-    implicit val InputStreamResource: Resource[InputStream] = new Resource[InputStream] {
-      val close = (r: InputStream) => IO(r.close)
-    }
-
-    implicit val OutputStreamResource: Resource[OutputStream] = new Resource[OutputStream] {
-      val close = (r: OutputStream) => IO(r.close)
-    }
-
-    implicit val SQLConnectionResource: Resource[Connection] = new Resource[Connection] {
-      val close = (r: Connection) => IO(r.close)
-    }
-
-    implicit val SQLStatementResource: Resource[Statement] = new Resource[Statement] {
-      val close = (r: Statement) => IO(r.close)
-    }
-
-    implicit val SQLPreparedStatementResource: Resource[PreparedStatement] = new Resource[PreparedStatement] {
-      val close = (r: PreparedStatement) => IO(r.close)
-    }
-
-    implicit val SQLResultSetResource: Resource[ResultSet] = new Resource[ResultSet] {
-      val close = (r: ResultSet) => IO(r.close)
-    }*/
   ////
 }
