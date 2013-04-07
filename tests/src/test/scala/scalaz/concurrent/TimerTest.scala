e0350c74362a6da9a732371afac32e8bdfe52dce
Sean Parsons <github@futurenotfound.com>
2013-04-07 10:28:58 +0100

    Early version of Timer for performing timeouts using Future.

---
+++ tests/src/test/scala/scalaz/concurrent/TimerTest.scala
@@ -0,0 +1,56 @@
+package scalaz
+package concurrent
+
+import scalaz.scalacheck.ScalazProperties._
+import scalaz.scalacheck.ScalazArbitrary._
+import std.AllInstances._
+import org.specs2.execute.{Failure, Result}
+import ConcurrentTest._
+import scalaz._,Scalaz._
+
+class TimerTest extends Spec {
+  def withTimer[T](expression: Timer => T): T = {
+    val timer = new Timer(10)
+    try {
+      expression(timer)
+    } finally {
+      timer.stop()
+    }
+  }
+  "Timer" should {
+    "stop normally" in {
+      withTimer(timer => ())
+    }
+    "handle stop being called repeatedly" in {
+      withTimer{timer =>
+        timer.stop()
+      }
+    }
+    "valueWait produces a value after the specified timeout" in {
+      withTimer{timer =>
+        val start = System.currentTimeMillis
+        withTimeout(200){
+          val future = timer.valueWait("Test", 100)
+          future.run must_== "Test"
+          (System.currentTimeMillis - start) >= 100
+        }
+      }
+    }
+    "withTimeout(Future...) produces a Timeout if the timeout is exceeded" in {
+      withTimer{timer =>
+        val future = timer.withTimeout(Future{Thread.sleep(500); "Test"}, 100)
+        withTimeout(1000){
+          future.run must_== Timeout.left
+        }
+      }
+    }
+    "produces the result of the Future if the timeout is not exceeded" in {
+      withTimer{timer =>
+        val future = timer.withTimeout(Future{Thread.sleep(50); "Test"}, 200)
+        withTimeout(1000){
+          future.run must_== "Test".right
+        }
+      }
+    }
+  }
+}
