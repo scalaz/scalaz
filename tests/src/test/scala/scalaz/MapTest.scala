b2e787e52744b8a4ea6263c6b2d93f23c9d5bb5f
markhibberd <mark@hibberd.id.au>
2013-04-23 08:33:19 +1000

    Fix Equal[Map[K, V]] instance.
    Fixes #341.
    
    Checks corrects keys set.
    Swapped to using total 'get' function on map. Map.apply, urghhh!
    
    Cherry-picking fceb867

---
+++ tests/src/test/scala/scalaz/MapTest.scala
@@ -0,0 +1,27 @@
+package scalaz
+
+import org.scalacheck.Arbitrary, Arbitrary.arbitrary
+import scalaz.scalacheck.ScalazProperties._
+import std.AllInstances._
+
+
+class MapTest extends Spec {
+  "Map functions" should {
+    "satisfy monoid laws" ! monoid.laws[Map[Int, String]]
+    "satisfy order laws" ! order.laws[Map[Int, String]]
+    "satisfy traverse laws" ! order.laws[Map[Int, String]]
+    "satisfy equals laws" ! equal.laws[Map[Int, String]]
+    "satisfy equals laws when not natural" ! equal.laws[Map[NotNatural, String]]
+  }
+
+  case class NotNatural(id: Int)
+  implicit def NotNaturalArbitrary: Arbitrary[NotNatural] =
+    Arbitrary(arbitrary[Int] map (NotNatural.apply))
+
+  implicit def NotNaturalOrder: Order[NotNatural] =
+    Order.orderBy[NotNatural, Int](_.id)
+
+  implicit def NotNaturalEqual: Equal[NotNatural] = new Equal[NotNatural] {
+    def equal(a1: NotNatural, a2: NotNatural): Boolean = a1.id == a2.id
+  }
+}
