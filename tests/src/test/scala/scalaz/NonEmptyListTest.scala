d9049f0ea3364edf164bcd6d628a0d7be9386795
Stephen Compall <scompall@nocandysw.com>
2013-05-22 06:48:57 -0400

    Test traverse1 laws on Nel, Tree theoretically in future.

--- tests/src/test/scala/scalaz/NonEmptyListTest.scala
+++ tests/src/test/scala/scalaz/NonEmptyListTest.scala
@@ -10,7 +10,7 @@ class NonEmptyListTest extends Spec {
   checkAll("NonEmptyList", semigroup.laws[NonEmptyList[Int]])
   checkAll("NonEmptyList", equal.laws[NonEmptyList[Int]])
   checkAll("NonEmptyList", order.laws[NonEmptyList[Int]])
-  checkAll("NonEmptyList", traverse.laws[NonEmptyList])
+  checkAll("NonEmptyList", traverse1.laws[NonEmptyList])
   checkAll("NonEmptyList", comonad.laws[NonEmptyList])
 
   "NonEmptyList size is correct" ! prop { xs:NonEmptyList[Int] =>
