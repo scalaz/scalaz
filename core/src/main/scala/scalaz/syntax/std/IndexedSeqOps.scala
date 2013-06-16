cdd41097c2efa6e21ebe69e1ce98388fb67401f7
Lars Hupel <lars.hupel@mytum.de>
2013-06-16 15:14:03 +0200

    deprecate `groupByM` syntax on `IndexedSeq`

--- core/src/main/scala/scalaz/syntax/std/IndexedSeqOps.scala
+++ core/src/main/scala/scalaz/syntax/std/IndexedSeqOps.scala
@@ -35,7 +35,9 @@ trait IndexedSeqOps[IS[+_], A] extends Ops[IS[A]] {
 
   final def breakM[M[_] : Monad](p: A => M[Boolean]): M[(IS[A], IS[A])] = v.breakM(self)(p)
 
-  final def groupByM[M[_] : Monad](p: (A, A) => M[Boolean]): M[IS[IS[A]]] = v.groupByM(self)(p)
+  @deprecated("use groupWhenM", "7.1")
+  final def groupByM[M[_] : Monad](p: (A, A) => M[Boolean]): M[IS[IS[A]]] = v.groupWhenM(self)(p)
+  final def groupWhenM[M[_] : Monad](p: (A, A) => M[Boolean]): M[IS[IS[A]]] = v.groupWhenM(self)(p)
 
   final def groupWhen(p: (A, A) => Boolean): IS[IS[A]] = v.groupWhen(self)(p)
 
