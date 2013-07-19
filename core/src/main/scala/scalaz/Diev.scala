836c5e4e21310394fb440e9c8ae93ebdc3379114
xuwei-k <6b656e6a69@gmail.com>
2013-07-19 13:38:54 +0900

    use scaladoc wiki syntax

--- core/src/main/scala/scalaz/Diev.scala
+++ core/src/main/scala/scalaz/Diev.scala
@@ -7,7 +7,7 @@ import scala.math
 import scala.collection.mutable.ListBuffer
 
 /**
- * Implementation of a Discrete Interval Encoding Tree (http://web.engr.oregonstate.edu/~erwig/diet/) that
+ * Implementation of a Discrete Interval Encoding Tree [[http://web.engr.oregonstate.edu/~erwig/diet/]] that
  * is actually implemented using a Vector and is balanced at all times as a result.
  */
 sealed trait Diev[A] {
