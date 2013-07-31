14f0ef654c5cfadd685691961a6f2878c6699f14
xuwei-k <6b656e6a69@gmail.com>
2013-08-01 06:18:43 +0900

    separate GenerateTupleW

---
+++ project/GenerateTupleW.scala
@@ -0,0 +1,70 @@
+import sbt._
+
+object GenerateTupleW {
+
+  def apply(outputDir: File) = {
+    val arities = 2 to 12
+
+    def writeFileScalazPackage(fileName: String, source: String): File = {
+      val file = (outputDir / "scalaz" / "syntax" / "std" / fileName).asFile
+      IO.write(file, source)
+      file
+    }
+
+    def double(s: String) = s + s
+
+    val tuples: IndexedSeq[(String, String)] = for (arity: Int <- arities) yield {
+      case class N(n: Int) {
+        val alpha: String = ('A' + (n - 1)).toChar.toString
+        val alpha2: String = alpha + alpha
+        val element: String = "_" + n
+      }
+      val ns = (1 to arity) map N.apply
+      def mapMkString(f: N => String): String = ns.map(f).mkString(", ")
+
+      val tparams = mapMkString {
+        n => n.alpha
+      }
+      val params = mapMkString {
+        n => n.element
+      }
+
+      val ztparams = mapMkString {
+        _ => "Z"
+      }
+
+      val mapallTParams = mapMkString {
+        n => n.alpha2
+      }
+      val mapallParams = mapMkString {
+        n => "%s: (%s => %s) = identity[%s] _".format(n.element, n.alpha, n.alpha2, n.alpha)
+      }
+      val mapallApply = mapMkString {
+        n => "%s(value.%s)".format(n.element, n.element)
+      }
+
+      val pimp = """|
+          |trait Tuple%dOps[%s] extends Ops[Tuple%d[%s]] {
+          |  val value = self
+          |  def fold[Z](f: => (%s) => Z): Z = {import value._; f(%s)}
+          |  def toIndexedSeq[Z](implicit ev: value.type <:< Tuple%d[%s]): IndexedSeq[Z] = {val zs = ev(value); import zs._; IndexedSeq(%s)}
+          |  def mapElements[%s](%s): (%s) = (%s)
+          |}""".stripMargin.format(arity, tparams, arity, tparams, tparams, params, arity,
+        ztparams, params,
+        mapallTParams, mapallParams, mapallTParams, mapallApply
+      )
+
+      val conv = """implicit def ToTuple%dOps[%s](t: (%s)): Tuple%dOps[%s] = new { val self = t } with Tuple%dOps[%s]
+          |""".stripMargin.format(arity, tparams, tparams, arity, tparams, arity, tparams)
+      (pimp, conv)
+    }
+
+    val source = "package scalaz\npackage syntax\npackage std\n\nimport collection.immutable.IndexedSeq\n\n" +
+      tuples.map(_._1).mkString("\n") +
+      "\n\ntrait ToTupleOps {\n" +
+         tuples.map("  " + _._2).mkString("\n") +
+      "}"
+    writeFileScalazPackage("TupleOps.scala", source)
+  }
+
+}
