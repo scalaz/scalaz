66de42bd01ca74c401a63b08a9e5d6b142402432
xuwei-k <6b656e6a69@gmail.com>
2013-08-30 11:47:02 +0900

    add TheseOps#this TheseOps#that

--- core/src/main/scala/scalaz/syntax/TheseOps.scala
+++ core/src/main/scala/scalaz/syntax/TheseOps.scala
@@ -5,7 +5,13 @@ final class TheseOps[A](self: A) {
   final def wrapThis[B]: A \&/ B =
     \&/.This(self)
 
+  final def `this`[B]: A \&/ B =
+    \&/.This(self)
+
   final def wrapThat[B]: B \&/ A =
+    \&/.That(self)
+
+  final def that[B]: B \&/ A =
     \&/.That(self)
 }
 
