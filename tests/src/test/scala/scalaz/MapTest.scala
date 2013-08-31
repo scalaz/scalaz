c7b6393f346441fd137b4d2b26ceb00a5085c376
Stephen Compall <scompall@nocandysw.com>
2013-08-30 23:00:40 -0400

    Promote arbitrary ==>> to scalacheck-binding.

--- tests/src/test/scala/scalaz/MapTest.scala
+++ tests/src/test/scala/scalaz/MapTest.scala
@@ -452,9 +452,6 @@ class MapTest extends Spec {
 
   import scalaz.scalacheck.ScalaCheckBinding._
 
-  implicit def mapArb[A, B](implicit o: Order[A], a: Arbitrary[List[(A, B)]]): Arbitrary[A ==>> B] =
-    Functor[Arbitrary].map(a)(as => fromList(as))
-
   checkAll(equal.laws[Int ==>> Int])
   checkAll(order.laws[Int ==>> Int])
 
