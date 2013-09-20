eff7740d6766184c81dde2c637ec1e96ba5b111c
xuwei-k <6b656e6a69@gmail.com>
2013-09-20 21:53:25 +0900

    add ToAlignOps

--- core/src/main/scala/scalaz/syntax/Syntax.scala
+++ core/src/main/scala/scalaz/syntax/Syntax.scala
@@ -159,4 +159,4 @@ trait ToTypeClassOps
   with ToPlusOps with ToApplicativePlusOps with ToMonadPlusOps with ToTraverseOps with ToBifunctorOps
   with ToBitraverseOps with ToComposeOps with ToCategoryOps
   with ToArrowOps with ToFoldableOps with ToChoiceOps with ToSplitOps with ToZipOps with ToUnzipOps with ToMonadTellOps with ToMonadListenOps
-  with ToFoldable1Ops with ToTraverse1Ops with ToOptionalOps
+  with ToFoldable1Ops with ToTraverse1Ops with ToOptionalOps with ToAlignOps
