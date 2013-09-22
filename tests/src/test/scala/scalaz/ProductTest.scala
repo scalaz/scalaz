f5613156a04c9e1d88875015f4bdce96b61d8c5e
xuwei-k <6b656e6a69@gmail.com>
2013-09-22 12:33:58 +0900

    add ProductTest

---
+++ tests/src/test/scala/scalaz/ProductTest.scala
@@ -0,0 +1,23 @@
+package scalaz
+
+import std.AllInstances._
+import scalaz.scalacheck.ScalazProperties._
+import scalaz.scalacheck.ScalazArbitrary._
+
+class ProductTest extends Spec {
+  type OptionList[α] = (Option[α], List[α])
+  type OneAndOption[α] = OneAnd[Option, α]
+  type OneAndOptionPair[α] = (OneAndOption[α], OneAndOption[α])
+
+  implicit val optionListApplicativePlus = ApplicativePlus[Option].product[List]
+  implicit val optionListZip = Zip[Option].product[List]
+  implicit val oneAndOptionPairTraverse1 = Traverse1[OneAndOption].product[OneAndOption]
+
+  checkAll(applicative.laws[OptionList])
+  checkAll(plusEmpty.laws[OptionList])
+  checkAll(zip.laws[OptionList])
+  checkAll(traverse1.laws[OneAndOptionPair])
+
+  implicit val eitherTuple2 = Bitraverse[Either].product[Tuple2]
+  checkAll(bitraverse.laws[({type λ[α, β] = (Either[α, β], (α, β))})#λ])
+}
