37a1192e930756c4e8babb39ddbf967a016e9975
xuwei-k <6b656e6a69@gmail.com>
2013-10-05 16:04:52 +0900

    add EndomorphicTest

---
+++ tests/src/test/scala/scalaz/EndomorphicTest.scala
@@ -0,0 +1,25 @@
+package scalaz
+
+class EndomorphicTest extends Spec {
+
+  object instances{
+    def semigroup[F[_, _]: Compose, A] = Semigroup[Endomorphic[F, A]]
+    def monoid[F[_, _]: Category, A] = Monoid[Endomorphic[F, A]]
+
+    def semigroup[F[_, _]: Category, A] = Semigroup[Endomorphic[F, A]]
+
+    object kleisli {
+      def semigroup[F[_]: Bind, A] = Semigroup[Endomorphic[({type λ[α, β] = Kleisli[F, α, β]})#λ, A]]
+      def monoid[F[_]: Monad, A] = Monoid[Endomorphic[({type λ[α, β] = Kleisli[F, α, β]})#λ, A]]
+
+      def semigroup[F[_]: Monad, A] = Semigroup[Endomorphic[({type λ[α, β] = Kleisli[F, α, β]})#λ, A]]
+    }
+
+    object cokleisli {
+      def semigroup[F[_]: Cobind, A] = Semigroup[Endomorphic[({type λ[α, β] = Cokleisli[F, α, β]})#λ, A]]
+      def monoid[F[_]: Comonad, A] = Monoid[Endomorphic[({type λ[α, β] = Cokleisli[F, α, β]})#λ, A]]
+
+      def semigroup[F[_]: Comonad, A] = Semigroup[Endomorphic[({type λ[α, β] = Cokleisli[F, α, β]})#λ, A]]
+    }
+  }
+}
