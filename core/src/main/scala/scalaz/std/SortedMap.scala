918f642c7178ee63e5409dd7792a1220a35d8cac
Stephen Compall <scompall@nocandysw.com>
2013-10-31 22:16:49 -0400

    Fix infinite recursion in SortedMap CanBuildFrom derivation.

--- core/src/main/scala/scalaz/std/SortedMap.scala
+++ core/src/main/scala/scalaz/std/SortedMap.scala
@@ -8,7 +8,7 @@ sealed trait MapSubSortedMap {
   type XMap[K, V] = SortedMap[K, V]
   type BuildKeyConstraint[K] = Order[K]
 
-  protected final implicit def buildXMap[K, V, K2: BuildKeyConstraint, V2]
+  protected final def buildXMap[K, V, K2: BuildKeyConstraint, V2]
       : CanBuildFrom[XMap[K, V], (K2, V2), XMap[K2, V2]] = {
     implicit val so = Order[K2].toScalaOrdering
     implicitly
