ec0f958a7638b563d17cec7eee63f9aa04d42b97
Stephen Compall <scompall@nocandysw.com>
2013-11-16 01:57:57 -0500

    Crush OptionT Apply and Applicative into Monad; make its ap sane.

--- tests/src/test/scala/scalaz/OptionTTest.scala
+++ tests/src/test/scala/scalaz/OptionTTest.scala
@@ -15,8 +15,7 @@ class OptionTTest extends Spec {
 
   object instances {
     def functor[F[_] : Functor] = Functor[({type λ[α] = OptionT[F, α]})#λ]
-    def apply[F[_] : Apply] = Apply[({type λ[α] = OptionT[F, α]})#λ]
-    def monad[F[_] : Monad] = Monad[({type λ[α] = OptionT[F, α]})#λ]
+    def monad[F[_] : Monad] = MonadPlus[({type λ[α] = OptionT[F, α]})#λ]
     def foldable[F[_] : Foldable] = Foldable[({type λ[α] = OptionT[F, α]})#λ]
     def traverse[F[_] : Traverse] = Traverse[({type λ[α] = OptionT[F, α]})#λ]
 
