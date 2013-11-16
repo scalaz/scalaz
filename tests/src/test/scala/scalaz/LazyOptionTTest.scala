53fc0954ec8c40fc7cb25d5d45af4b32087628f1
Stephen Compall <scompall@nocandysw.com>
2013-11-16 02:24:09 -0500

    Crush LazyOptionT Apply and Applicative into Monad; make its ap sane.

--- tests/src/test/scala/scalaz/LazyOptionTTest.scala
+++ tests/src/test/scala/scalaz/LazyOptionTTest.scala
@@ -13,7 +13,6 @@ class LazyOptionTTest extends Spec {
 
   object instances {
     def functor[F[_] : Functor] = Functor[({type λ[α] = LazyOptionT[F, α]})#λ]
-    def apply[F[_] : Apply] = Apply[({type λ[α] = LazyOptionT[F, α]})#λ]
     def monad[F[_] : Monad] = Monad[({type λ[α] = LazyOptionT[F, α]})#λ]
 
     // checking absence of ambiguity
