b1d7c81de8a1b463170d87178c351f8552db957c
xuwei-k <6b656e6a69@gmail.com>
2013-12-13 02:12:54 +0900

    \/.fromTryCatchThrowable test
    (cherry picked from commit d39657fe0615132b9390228882678a36fa544355)

--- tests/src/test/scala/scalaz/DisjunctionTest.scala
+++ tests/src/test/scala/scalaz/DisjunctionTest.scala
@@ -14,4 +14,20 @@ object DisjunctionTest extends SpecLite {
   checkAll(traverse.laws[({type λ[α] = Int \/ α})#λ])
   checkAll(bitraverse.laws[\/])
 
+  "fromTryCatchThrowable" in {
+    class Foo extends Throwable
+    final class Bar extends Foo
+    val foo = new Foo
+    val bar = new Bar
+
+    implicit val equalFoo = Equal.equalA[Foo]
+    implicit val showFoo = Show.showA[Foo]
+    implicit val equalBar = Equal.equalA[Bar]
+    implicit val showBar = Show.showA[Bar]
+
+    \/.fromTryCatchThrowable[Int, Foo](1) must_=== \/.right(1)
+    \/.fromTryCatchThrowable[Int, Foo](throw foo) must_=== \/.left(foo)
+    \/.fromTryCatchThrowable[Int, Foo](throw bar) must_=== \/.left(bar)
+    \/.fromTryCatchThrowable[Int, Bar](throw foo).mustThrowA[Foo]
+  }
 }
