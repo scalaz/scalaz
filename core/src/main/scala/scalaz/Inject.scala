31f9ebeb6b2ede54a748b805d32c52f909ca43cd
Stephen Compall <scompall@nocandysw.com>
2014-01-06 22:40:25 -0500

    Move Functor[S] constraint on Free out of structure, onto functions.

--- core/src/main/scala/scalaz/Inject.scala
+++ core/src/main/scala/scalaz/Inject.scala
@@ -38,7 +38,7 @@ trait InjectFunctions {
   def inject[F[_], G[_], A](ga: G[Free[F, A]])(implicit F: Functor[F], I: Inject[G, F]): Free[F, A] =
     Suspend[F, A](I.inj(ga))
 
-  def match_[F[_], G[_], A](fa: Free[F, A])(implicit I: Inject[G, F]): Option[G[Free[F, A]]] =
+  def match_[F[_], G[_], A](fa: Free[F, A])(implicit F: Functor[F], I: Inject[G, F]): Option[G[Free[F, A]]] =
     fa.resume.fold(I.prj(_), _ => none)
 }
 
