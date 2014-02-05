d8a80af646507030ff195446220cc36315129240
Stephen Compall <scompall@nocandysw.com>
2014-02-04 20:24:30 -0500

    Leibniz#apply

--- core/src/main/scala/scalaz/Leibniz.scala
+++ core/src/main/scala/scalaz/Leibniz.scala
@@ -18,6 +18,7 @@ import Id._
  * The more refined types are useful if you need to be able to substitute into restricted contexts.
  */
 sealed abstract class Leibniz[-L, +H >: L, A >: L <: H, B >: L <: H] {
+  def apply(a: A): B = subst[Id](a)
   def subst[F[_ >: L <: H]](p: F[A]): F[B]
   def compose[L2 <: L, H2 >: H, C >: L2 <: H2](that: Leibniz[L2, H2, C, A]): Leibniz[L2, H2, C, B] =
     Leibniz.trans[L2, H2, C, A, B](this, that)
