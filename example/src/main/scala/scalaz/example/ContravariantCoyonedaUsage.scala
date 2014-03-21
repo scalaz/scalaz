c238bc57849eae65be28b4bf56e24c64de614a2f
Stephen Compall <scompall@nocandysw.com>
2014-03-20 23:39:21 -0400

    Finish contravariant coyoneda example.

--- example/src/main/scala/scalaz/example/ContravariantCoyonedaUsage.scala
+++ example/src/main/scala/scalaz/example/ContravariantCoyonedaUsage.scala
@@ -1,16 +1,18 @@
 package scalaz.example
 
-import scalaz.{\/, ContravariantCoyoneda => CtCoyo, Order}
+import scalaz.{\/, ContravariantCoyoneda => CtCoyo, Monoid, Order}
 import scalaz.std.anyVal._
 import scalaz.std.function._
 import scalaz.std.list._
 import scalaz.std.option._
 import scalaz.std.string._
 import scalaz.std.tuple._
+import scalaz.std.vector._
 import scalaz.syntax.arrow._
 import scalaz.syntax.id._
 import scalaz.syntax.monoid._
 import scalaz.syntax.order._
+import scalaz.syntax.show._
 import scalaz.syntax.traverse._
 import scalaz.syntax.std.option._
 import scalaz.syntax.std.string._
@@ -157,7 +159,11 @@ object ContravariantCoyonedaUsage extends App {
   } yield unstructuredData.sortBy(v => v(i))(ord.toScalaOrdering)
 
   // We can no longer use `schwartzian', though, because there is no
-  // way to separate the sort key from the underlying Order.
+  // way to separate the sort key from the underlying Order.  We could
+  // combine them all into a single ADT, but that would be closed, it
+  // would be inconvenient to build a correct Order instance, and the
+  // complication turns out to be unnecessary.  Contravariant coyoneda
+  // is here to simplify our lives.
   //
   // Enter Contravariant Coyoneda
   // ----------------------------
@@ -301,5 +307,252 @@ object ContravariantCoyonedaUsage extends App {
     CCOrder(l.k &&& r.k)
   }
 
+  // Now we have a base case, and an induction step.  I think we're
+  // ready.
+
+  def sortSpecOrd(s: SortSpec): CtCoyo[Order, Vector[String]] =
+    s.foldRight(unitOrd: CtCoyo[Order, Vector[String]]){(sti, acc) =>
+      val (st, i) = sti
+      ordFanout(recItemOrd(i, sortTypeOrd(st)), acc)
+    }
+
+  def sortDataBy(xs: List[Vector[String]], o: SortSpec)
+      : List[Vector[String]] = {
+    val coyo = sortSpecOrd(o)
+    schwartzian(xs)(coyo.k)(coyo.fi)
+  }
+
+  val sortedBySpec: List[Vector[String]] =
+    sortDataBy(unstructuredData, mainLtoRsort)
+
+  println("sortedBySpec: " |+| sortedBySpec.shows)
+
+  val sortedByNonCity: List[Vector[String]] =
+    sortDataBy(unstructuredData, mainLtoRsort.tail)
+
+  println("sortedByNonCity: " |+| sortedByNonCity.shows)
+
+  // Hold on.  The types are changing again.  We started with an I of
+  // `Unit', then had an I = `(someI, Unit)', then an I = `(anotherI,
+  // (someI, Unit))'.  How come this works at all?
+  //
+  // Induction steps
+  // ---------------
+  //
+  // The step function passed to the above fold is a logical induction
+  // step.  It calls no functions that actually care about what any
+  // `I' is; `ordFanout' only cares that it gets two functions and two
+  // Orders, and each function's return type matches up with its
+  // associative order!  `ContravariantCoyoneda' acts as a bit of
+  // scaffolding to maintain the proof as we perform each inductive
+  // step.  The only place we know the type is in the `sortTypeOrd'
+  // function body, and we throw it away before returning.
+  //
+  // So we end up using an arbitrary nesting of tuples in the ultimate
+  // `I' that results, but it doesn't matter, because each step of the
+  // code that results knows exactly enough type information for this
+  // to be sound.  If you're wondering, here's the type that got
+  // erased for `mainLtoRsort':
+  //
+  // (String, ((Int, Option[(Int, Int)]) \/ String,
+  //           (Long \/ String, Unit)))
+  //
+  // Let's check out the Is via the unsafe `toString' method for what
+  // must have been used for `sortedBySpec' and `sortedByNonCity'.
+
+  val mainLtoRcoyo: CtCoyo[Order, Vector[String]] =
+    sortSpecOrd(mainLtoRsort)
+
+  val mainLtoRtailcoyo: CtCoyo[Order, Vector[String]] =
+    sortSpecOrd(mainLtoRsort.tail)
+
+  println("list of mainLtoRcoyo.I: " |+|
+            unstructuredData.map(r => mainLtoRcoyo.k(r).toString).shows)
+  println("list of mainLtoRtailcoyo.I: " |+|
+            unstructuredData.map(r => mainLtoRtailcoyo.k(r).toString).shows)
+
+  // Digression: the monoid
+  // ----------------------
+  //
+  // Something interesting about `sortSpecOrd': it's completely
+  // coincidental that I wrote it with a right fold.  It works just as
+  // well with a left fold.
+
+  def sortSpecOrdL(s: SortSpec): CtCoyo[Order, Vector[String]] =
+    s.foldLeft(unitOrd: CtCoyo[Order, Vector[String]]){(acc, sti) =>
+      val (st, i) = sti
+      ordFanout(acc, recItemOrd(i, sortTypeOrd(st)))
+    }
+
+  // What does `I' look like then?  Let's use the unsafe `toString'
+  // again to see.
+
+  val mainLtoRcoyoL: CtCoyo[Order, Vector[String]] =
+    sortSpecOrdL(mainLtoRsort)
+
+  println("list of mainLtoRcoyoL.I: " |+|
+            unstructuredData.map(r => mainLtoRcoyoL.k(r).toString).shows)
+
+  // Now the type looks like this:
+  //
+  // (((Unit, String),
+  //   (Int, Option[(Int, Int)]) \/ String),
+  //  Long \/ String)
+  //
+  // Yet, the resulting order is the same.  Compare to
+  // `sortedByNonCity', which used the right-fold.
+
+  val sortedByNonCityL: List[Vector[String]] = {
+    val coyo = sortSpecOrdL(mainLtoRsort.tail)
+    schwartzian(unstructuredData)(coyo.k)(coyo.fi)
+  }
+
+  println("sortedByNonCityL: " |+| sortedByNonCity.shows)
+
+  // Our three properties of Order listed under "Products" now come
+  // into play.  That `unitOrd' doesn't change behavior of the sort,
+  // combined with the free theorems of `ordFanout', means that we can
+  // build a lawful monoid from those two functions.
+
+  implicit def ctCoyoOrdMonoid[A]: Monoid[CtCoyo[Order, A]] =
+    Monoid instance (ordFanout(_, _), unitOrd)
+
+  // And now we can build one more version of `sortSpecOrd' that has
+  // cleaned up quite nicely.
+
+  def sortSpecOrdF(s: SortSpec): CtCoyo[Order, Vector[String]] =
+    s.foldMap{case (st, i) => recItemOrd(i, sortTypeOrd(st))}
+
+  // "But how can this follow the monoid laws; it isn't associative
+  // because `I' changes depending on the order of the fold!"  Well,
+  // you're not allowed to care about that under the rules of
+  // parametricity [2], just like you're not allowed to test stack
+  // depth in a function and claim that the changing results means the
+  // functor identity law is violated for `Function1'.  It's *some*
+  // `I', and that's all you get.  Nothing that actually knows how to
+  // work with `I' in this result cares about the grouping of
+  // combination, so it's a monoid.
+  //
+  // How well does this generalize to `F's other than `Order'?  I
+  // think the presence of a universally-quantified product with a
+  // single left and right identity, and parametricity, gives you a
+  // fanout-style `Monoid', no problem.
+  //
+  // Knowing more about `I'
+  // ----------------------
+  //
+  // Contravariant Coyoneda isn't *for* Order.  That's just the
+  // example I've shown here.  `F' is abstract for a reason.
+  //
+  // Let's consider some kind of distributed arrangement for sorting:
+  // we cut data into slices, deliver them – and the sort spec — to
+  // each node, *they* do the transition to `I' and sort their
+  // components, and we get it all back and merge the results.
+  //
+  // There are various type-safe binary format libraries, such as
+  // scodec [3] and f0 [4].  Let's simulate one of those.  Never mind
+  // that this is phantom; assume that serialization and
+  // deserialization methods are defined:
+
+  trait Binfmt[A] {
+    def describe: String
+  }
+
+  object Binfmt {
+    def apply[A](s: String): Binfmt[A] = new Binfmt[A] {val describe = s}
+
+    implicit val descLong: Binfmt[Long] = Binfmt("<Long>")
+    implicit val descInt: Binfmt[Int] = Binfmt("<Int>")
+    implicit val descString: Binfmt[String] = Binfmt("<String>")
+    implicit val descUnit: Binfmt[Unit] = Binfmt("")
+    implicit def descOption[A](implicit a: Binfmt[A])
+        : Binfmt[Option[A]] = Binfmt("?<" |+| a.describe |+| ">")
+    implicit def desc_\/[A, B](implicit a: Binfmt[A], b: Binfmt[B])
+        : Binfmt[A \/ B] = Binfmt("<" |+| a.describe |+| "\\/"
+                                      |+| b.describe |+| ">")
+    implicit def desc2Tuple[A, B](implicit a: Binfmt[A], b: Binfmt[B])
+        : Binfmt[(A, B)] = Binfmt(a.describe |+| b.describe)
+  }
+
+  // A bidirectional formatter, unlike `Order', does not have a
+  // `Contravariant' instance.  But that doesn't matter; contravariant
+  // coyoneda does all the work.
+  //
+  // Previously, we proved at every step that we had a function and an
+  // Order that lined up.  Now, we need a function, Order, *and*
+  // Binfmt that line up.  We didn't make the previous functions
+  // general enough, so let's revisit them to include `Binfmt' as
+  // simply as possible.  Using the `F' abstraction, you can make the
+  // idea of which Binfmt typeclass abstract in your own designs; this
+  // works well for an "open world" style `SortSpec', though our
+  // example is closed.
+
+  type BinOrd[A] = (Binfmt[A], Order[A])
+
+  def CCBinOrd[A, B](f: A => B)(implicit b: Binfmt[B], o: Order[B])
+      : CtCoyo.Aux[BinOrd, A, B] =
+    CtCoyo[BinOrd, A, B]((b, o))(f)
+
+  def sortTypeBinOrd(s: SortType): CtCoyo[BinOrd, String] = s match {
+    case SortType.CI => CCBinOrd(caseInsensitively)
+    case SortType.Dateish => CCBinOrd(parseDate)
+    case SortType.Num => CCBinOrd(parseCommaNum)
+  }
+
+  // It turns out that recItemOrd is completely abstract:
+
+  def recItem[F[_]](i: Int, o: CtCoyo[F, String])
+      : CtCoyo[F, Vector[String]] =
+    o contramap (v => v(i))
+
+  // And, again, with a general notion of the underlying product
+  // function and identity you could probably produce the monoid
+  // generically, but for now:
+
+  def unitBinOrd[A]: CtCoyo.Aux[BinOrd, A, Unit] = CCBinOrd(a => ())
+
+  def binOrdFanout[A](l: CtCoyo[BinOrd, A], r: CtCoyo[BinOrd, A])
+      : CtCoyo.Aux[BinOrd, A, (l.I, r.I)] = {
+    implicit val (lfb, lfo) = l.fi
+    implicit val (rfb, rfo) = r.fi
+    CCBinOrd(l.k &&& r.k)
+  }
+
+  // I've chosen the `Binfmt' instances with a bit of care to preserve
+  // their monoid-hood.  It's not a requirement for you to do this
+  // kind of abstracting, though.
+
+  implicit def ctCoyoBinOrdMonoid[A]: Monoid[CtCoyo[BinOrd, A]] =
+    Monoid instance (binOrdFanout(_, _), unitBinOrd)
+
+  def sortSpecBinOrdF(s: SortSpec): CtCoyo[BinOrd, Vector[String]] =
+    s.foldMap{case (st, i) => recItem[BinOrd](i, sortTypeBinOrd(st))}
+
+  // The drawback here is that I can't just build a separate stack
+  // willynilly for `Binfmt'.  I have to prove at each step that *the
+  // same* `I' is used for the Binfmt and the Order for this to be
+  // useful.  Once again, an idea of a fold step that can be fused
+  // with the `Order' construction safely can be abstracted out here.
+
+  val (binfmtdesc, finalsort) = {
+    val bo = sortSpecBinOrdF(mainLtoRsort)
+    (bo.fi._1.describe,
+     schwartzian(unstructuredData)(bo.k)(bo.fi._2))
+  }
+
+  // For your edification, the binfmtdesc is:
+  //
+  //   <String><<Int>?<<Int><Int>>\/<String>><<Long>\/<String>>
+  //
+  // But you should just sbt "example/runMain
+  // scalaz.example.ContravariantCoyonedaUsage" to see this and the
+  // other prints in action.
+
+  println("binfmtdesc: " |+| binfmtdesc)
+  println("finalsort: " |+| finalsort.shows)
+
   // [1] https://en.wikipedia.org/wiki/Schwartzian_transform
+  // [2] http://failex.blogspot.com/2013/06/fake-theorems-for-free.html
+  // [3] https://github.com/scodec/scodec
+  // [4] https://github.com/joshcough/f0
 }
