5192b02031af32e83aad41455ad43e5df346dfd4
julien-truffaut <truffaut.julien@gmail.com>
2014-04-27 17:42:25 +0100

    Add Apply instance of Const and rearrange instances in an inheritance hierarchy

--- core/src/main/scala/scalaz/Const.scala
+++ core/src/main/scala/scalaz/Const.scala
@@ -1,35 +1,56 @@
 package scalaz
 
-case class Const[A, B](getConst: A) {
-  def ===(x: Const[A, B])(implicit A: Equal[A]): Boolean =
-    A.equal(getConst, x.getConst)
+final case class Const[A, B](getConst: A)
 
-  def map[C](f: B => C): Const[A, C] = Const(getConst)
+private sealed trait ConstFunctor[C] extends Functor[({type λ[α] = Const[C, α]})#λ]{
+  override def map[A, B](fa: Const[C, A])(f: A => B): Const[C, B] = Const(fa.getConst)
 }
+
+private sealed trait ConstApply[C] extends Apply[({type λ[α] = Const[C, α]})#λ] with ConstFunctor[C]{
+  def C: Monoid[C]
+
+  override def ap[A, B](fa: => Const[C, A])(f: => Const[C, A => B]): Const[C, B] =
+    Const(C.append(fa.getConst, f.getConst))
+}
+
+private sealed trait ConstApplicative[C] extends Applicative[({type λ[α] = Const[C, α]})#λ] with ConstApply[C]{
+  override def point[A](a: => A): Const[C, A] = Const(C.zero)
+}
+
+private sealed trait ConstEqual[A, B] extends Equal[Const[A, B]] {
+  def OA: Equal[A]
+
+  override def equal(a1: Const[A, B], a2: Const[A, B]): Boolean =
+    OA.equal(a1.getConst, a2.getConst)
+}
+
+sealed trait ConstInstances3 {
+  implicit def constFunctor[C]: Functor[({type λ[α] = Const[C, α]})#λ] = new ConstFunctor[C]{}
+}
+
+sealed trait ConstInstances2 extends ConstInstances3 {
+  implicit def constApply[C: Monoid]: Apply[({type λ[α] = Const[C, α]})#λ] = new ConstApply[C] {
+    val C: Monoid[C] = implicitly
+  }
+}
+
+
+sealed trait ConstInstances1 extends ConstInstances2 {
+  implicit def constApplicative[C: Monoid]: Applicative[({type λ[α] = Const[C, α]})#λ] = new ConstApplicative[C] {
+    val C: Monoid[C] = implicitly
+  }
+}
+
+sealed abstract class ConstInstances0 extends ConstInstances1 {
+  implicit def constEqual[A : Equal, B]: Equal[Const[A, B]] = new ConstEqual[A, B]{
+    val OA: Equal[A] = implicitly
+  }
+}
+
+sealed abstract class ConstInstances extends ConstInstances0
 
 object Const extends ConstInstances with ConstFunctions
 
-sealed abstract class ConstInstances {
-  /** The constant functor that maps every type to `A` */
-  implicit def constFunctor[C]: Functor[({type l[a] = Const[C, a]})#l] =
-    new Functor[({type l[a] = Const[C, a]})#l] {
-      override def map[A, B](fa: Const[C, A])(f: A => B): Const[C, B] =
-        fa.map(f)
-    }
-
-  implicit def constApplicative[F: Monoid] = new Applicative[({ type l[a] = Const[F, a] })#l] {
-    def point[A](a: => A): Const[F, A] = Const(Monoid[F].zero)
-
-    def ap[A, B](fa: => Const[F, A])(f: => Const[F, A => B]): Const[F, B] =
-      Const(Monoid[F].append(fa.getConst, f.getConst))
-  }
-
-  implicit def constEqual[A : Equal, B]: Equal[Const[A, B]] =
-    new Equal[Const[A, B]] {
-      override def equal(a1: Const[A, B], a2: Const[A, B]): Boolean =
-        a1 === a2
-    }
-}
 
 sealed trait ConstFunctions {
   /** A properly universally quantified constant function. */
