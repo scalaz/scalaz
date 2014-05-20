88a571e89338f2a1452b293c994f6fc36ec5c3ce
Gerolf Seitz <gerolf.seitz@gmail.com>
2014-05-20 08:43:27 +0200

    Use ASCII arrows

--- effect/src/main/scala/scalaz/effect/KleisliEffect.scala
+++ effect/src/main/scala/scalaz/effect/KleisliEffect.scala
@@ -38,6 +38,6 @@ private trait KleisliLiftIO[M[_], R] extends LiftIO[({type λ[α] = Kleisli[M, R
 private trait KleisliCatchIO[M[_], R] extends MonadCatchIO[({type λ[α] = Kleisli[M, R, α]})#λ] with KleisliLiftIO[M, R] with KleisliMonadReader[M, R] {
   implicit def F: MonadCatchIO[M]
 
-  def except[A](k: Kleisli[M, R, A])(h: Throwable ⇒ Kleisli[M, R, A]) = 
-    Kleisli(r ⇒ F.except(k.run(r))(t ⇒ h(t).run(r)))
+  def except[A](k: Kleisli[M, R, A])(h: Throwable => Kleisli[M, R, A]) = 
+    Kleisli(r => F.except(k.run(r))(t => h(t).run(r)))
 }
