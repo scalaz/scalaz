5b6efe8cfd338cb16753bb1a726d59c0bb75f892
Mike O'Connor <stew@vireo.org>
2014-06-12 13:53:53 -0700

    Add Traverse and Zip instances for DList
    (cherry picked from commit 5cee9f1d19e84e2b2fd55a6e8265ebf9b239ade0)

--- core/src/main/scala/scalaz/DList.scala
+++ core/src/main/scala/scalaz/DList.scala
@@ -62,6 +62,14 @@ trait DList[A] {
 
 object DList extends DListFunctions with DListInstances {
   def apply[A](xs: A*) = fromList(xs.toList)
+
+  implicit val dlistInstance: Traverse[DList] with Zip[DList] = new Traverse[DList] with Zip[DList]  {
+    override def map[A, B](fa: DList[A])(f: A => B) = fa map f
+    override def zip[A,B](a: => DList[A], b: => DList[B]): DList[(A, B)] =
+      a.uncons(DList(), (h, t) => b.uncons(DList(), (h2, t2) => (h -> h2) +: zip(t, t2)))
+    override def traverseImpl[F[_], A, B](fa: DList[A])(f: A => F[B])(implicit F: Applicative[F]): F[DList[B]] =
+      fa.foldr(F.point(DList[B]()))((a, fbs) => F.apply2(f(a), fbs)(_ +: _))
+  }
 }
 
 trait DListInstances {
