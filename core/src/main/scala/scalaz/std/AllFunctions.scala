e2025f4c9e355f7ecfe37f10c997f3415153a06a
Lars Hupel <lars.hupel@mytum.de>
2014-06-28 14:45:49 +0200

    Fix #704: `AllFunctions` should extend `BooleanFunctions`

--- core/src/main/scala/scalaz/std/AllFunctions.scala
+++ core/src/main/scala/scalaz/std/AllFunctions.scala
@@ -5,6 +5,7 @@ trait AllFunctions
   extends ListFunctions
   with OptionFunctions
   with StreamFunctions
+  with BooleanFunctions
   with math.OrderingFunctions
   with StringFunctions
   with FunctionFunctions
