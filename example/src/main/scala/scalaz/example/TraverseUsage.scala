scalaz/scalaz

commit c5980d619dd624288180383125a5dc09ed6b997e

fix spurious compiler warning wrt to new tagging
AnyRef{type Tag = scalaz.Tags.Disjunction; type Self = Boolean} and Boolean are
unrelated: they will most likely never compare equal
   assert(res1.foldMap(Tags.Disjunction(_)) == false)
                                            ^

Turns out *they* do compare equal, because tags are still unboxed.

example/src/main/scala/scalaz/example/TraverseUsage.scala
@@ -97,8 +97,8 @@ object TraverseUsage extends App {
   val res1: List[Boolean] = nonRepeating.traverseS(checkForRepeats).eval(None)
   val res2: List[Boolean] = repeating.traverseS(checkForRepeats).eval(None)
 
-  assert(res1.foldMap(Tags.Disjunction(_)) == false)
-  assert(res2.foldMap(Tags.Disjunction(_)) == true)
+  assert(Tag.unwrap(res1.foldMap(Tags.Disjunction(_))) == false)
+  assert(Tag.unwrap(res2.foldMap(Tags.Disjunction(_))) == true)
 
   // Here's a variation of above which might be a bit of a head
   // scratcher, but this works because a Monoid gives rise to an
@@ -106,6 +106,6 @@ object TraverseUsage extends App {
   // constructor, we need traverseU instead of traverse to find the
   // Applicative.
   import scalaz.Applicative.monoidApplicative
-  assert(res1.traverseU(Tags.Disjunction(_)) == false)
-  assert(res2.traverseU(Tags.Disjunction(_)) == true)
+  assert(Tag.unwrap(res1.traverseU(Tags.Disjunction(_))) == false)
+  assert(Tag.unwrap(res2.traverseU(Tags.Disjunction(_))) == true)
 }

commit d03778e50d60bf5d3ef1e7291e1558df14b4fb45

Use typesafe equality in an example
This prevents the sort of breakage in
c5980d619dd624288180383125a5dc09ed6b997e (which fixed a (spurious)
warning which should have been an error).

example/src/main/scala/scalaz/example/TraverseUsage.scala
@@ -97,8 +97,8 @@ object TraverseUsage extends App {
   val res1: List[Boolean] = nonRepeating.traverseS(checkForRepeats).eval(None)
   val res2: List[Boolean] = repeating.traverseS(checkForRepeats).eval(None)
 
-  assert(Tag.unwrap(res1.foldMap(Tags.Disjunction(_))) == false)
-  assert(Tag.unwrap(res2.foldMap(Tags.Disjunction(_))) == true)
+  assert(Tag.unwrap(res1.foldMap(Tags.Disjunction(_))) === false)
+  assert(Tag.unwrap(res2.foldMap(Tags.Disjunction(_))) === true)
 
   // Here's a variation of above which might be a bit of a head
   // scratcher, but this works because a Monoid gives rise to an
@@ -106,6 +106,6 @@ object TraverseUsage extends App {
   // constructor, we need traverseU instead of traverse to find the
   // Applicative.
   import scalaz.Applicative.monoidApplicative
-  assert(Tag.unwrap(res1.traverseU(Tags.Disjunction(_))) == false)
-  assert(Tag.unwrap(res2.traverseU(Tags.Disjunction(_))) == true)
+  assert(Tag.unwrap(res1.traverseU(Tags.Disjunction(_))) === false)
+  assert(Tag.unwrap(res2.traverseU(Tags.Disjunction(_))) === true)
 }