52a168352250a7856df9995bb9b13d82babbc285
Mike O'Connor <stew@vireo.org>
2014-08-05 20:37:20 -0400

    Adds a double ended queue
    Uses the "Bankers double ended queue" as described by C. Okasaki
    
    (cherry picked from commit 6e8ddf08d1b4712741089135c257d45ec748f0f1)

--- scalacheck-binding/src/main/scala/scalaz/scalacheck/ScalazArbitrary.scala
+++ scalacheck-binding/src/main/scala/scalaz/scalacheck/ScalazArbitrary.scala
@@ -265,6 +265,8 @@ object ScalazArbitrary {
 
   implicit def ilistArbitrary[A](implicit A: Arbitrary[List[A]]) = Functor[Arbitrary].map(A)(IList.fromList)
 
+  implicit def dequeueArbitrary[A](implicit A: Arbitrary[List[A]]) = Functor[Arbitrary].map(A)(list => Dequeue(list: _*))
+
   implicit def lazyTuple2Arbitrary[A, B](implicit A: Arbitrary[A], B: Arbitrary[B]): Arbitrary[LazyTuple2[A, B]] =
     Applicative[Arbitrary].apply2(A, B)(LazyTuple2(_, _))
 
