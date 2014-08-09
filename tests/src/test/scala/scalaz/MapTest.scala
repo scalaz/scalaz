b5e1798036513e781fd57435aef92bcd0341f37e
xuwei-k <6b656e6a69@gmail.com>
2014-08-09 18:00:34 +0900

    remove unused import
    (cherry picked from commit 450c90c9fe95765b262073224b9f60e0a63cb94a)

--- tests/src/test/scala/scalaz/MapTest.scala
+++ tests/src/test/scala/scalaz/MapTest.scala
@@ -5,7 +5,6 @@ import org.scalacheck.Prop.forAll
 
 object MapTest extends SpecLite {
   import org.scalacheck.Arbitrary
-  import scalaz.scalacheck.ScalaCheckBinding._
   import scalaz.scalacheck.ScalazProperties._
   import scalaz.scalacheck.ScalazArbitrary._
   import std.anyVal._
