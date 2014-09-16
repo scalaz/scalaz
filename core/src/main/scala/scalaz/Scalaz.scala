9c246bd5cfeed81bb9bc6d0f1e586f321a946388
Tom Switzer <thomas.switzer@gmail.com>
2014-09-16 13:14:43 -0400

    MonadError[Future] and Catchable[Future]

--- core/src/main/scala/scalaz/Scalaz.scala
+++ core/src/main/scala/scalaz/Scalaz.scala
@@ -1,5 +1,7 @@
 package scalaz
 
+import scala.concurrent.Future
+
 object Scalaz
   extends StateFunctions        // Functions related to the state monad
   with syntax.ToTypeClassOps    // syntax associated with type classes
@@ -7,4 +9,8 @@ object Scalaz
   with std.AllInstances         // Type class instances for the standard library types
   with std.AllFunctions         // Functions related to standard library types
   with syntax.std.ToAllStdOps   // syntax associated with standard library types
-  with IdInstances              // Identity type and instances
+  with IdInstances {            // Identity type and instances
+
+  implicit def newFutureInstance(implicit ec: scala.concurrent.ExecutionContext): Cobind[Future] with MonadError[({type λ[α,β] = Future[β]})#λ, Throwable] with Catchable[Future] =
+    new scalaz.std.FutureInstance
+}
