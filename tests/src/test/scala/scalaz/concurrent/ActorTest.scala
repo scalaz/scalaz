6f036f3e3f02f9f691d0d01981edf82fe7a77c85
Andriy Plokhotnyuk <a.plokhotnyuk@thenewmotion.com>
2014-11-20 17:36:25 +0200

    Revise actor tests to check suite over different strategies

--- tests/src/test/scala/scalaz/concurrent/ActorTest.scala
+++ tests/src/test/scala/scalaz/concurrent/ActorTest.scala
@@ -1,18 +1,58 @@
 package scalaz
 package concurrent
 
-import collection.mutable
 import java.util.concurrent._
 import ConcurrentTest._
 
 object ActorTest extends SpecLite {
-  val NumOfMessages = 1000
+  val NumOfMessages = 100000
   val NumOfThreads = 4
-  val NumOfMessagesPerThread = NumOfMessages / NumOfThreads
-  val executor = Executors.newFixedThreadPool(NumOfThreads)
-  implicit val strategy = Actor.strategy(executor)
 
-  "actor" should {
+  "actor with sequential strategy" should {
+    actorTests(NumOfMessages)(Strategy.Sequential)
+  }
+
+  "actor with default strategy" should {
+    actorTests(NumOfMessages)(Strategy.DefaultStrategy)
+  }
+
+  "actor with executor strategy backed by Scala fork-join pool" should {
+    actorTests(NumOfMessages)(Strategy.Executor(new scala.concurrent.forkjoin.ForkJoinPool()))
+  }
+
+  "actor with executor strategy backed by Java fork-join pool" should {
+    actorTests(NumOfMessages)(Strategy.Executor(new ForkJoinPool()))
+  }
+
+  "actor with executor strategy backed by fixed thread pool" should {
+    actorTests(NumOfMessages)(Strategy.Executor(Executors.newFixedThreadPool(NumOfThreads, Strategy.DefaultDaemonThreadFactory)))
+  }
+
+  "actor with naive strategy" should {
+    actorTests(NumOfMessages / 1000)(Strategy.Naive)
+  }
+
+  "actor with Swing worker strategy" should {
+    actorTests(NumOfMessages)(Strategy.SwingWorker)
+  }
+
+  "actor with Swing invoke later strategy" should {
+    actorTests(NumOfMessages)(Strategy.SwingInvokeLater)
+  }
+
+  "actor with actor strategy backed by Scala fork-join pool" should {
+    actorTests(NumOfMessages)(Actor.strategy(new scala.concurrent.forkjoin.ForkJoinPool()))
+  }
+
+  "actor with actor strategy backed by Java fork-join pool" should {
+    actorTests(NumOfMessages)(Actor.strategy(new ForkJoinPool()))
+  }
+
+  "actor with actor strategy backed by fixed thread pool" should {
+    actorTests(NumOfMessages)(Actor.strategy(Executors.newFixedThreadPool(NumOfThreads, Strategy.DefaultDaemonThreadFactory)))
+  }
+
+  def actorTests(n: Int)(implicit s: Strategy) = {
     "execute code async" in {
       val latch = new CountDownLatch(1)
       val actor = Actor[Int]((i: Int) => latch.countDown())
@@ -22,13 +62,13 @@ object ActorTest extends SpecLite {
 
     "catch code errors that can be handled" in {
       val latch = new CountDownLatch(1)
-      val actor = Actor[Int]((i: Int) => 100 / i, (ex: Throwable) => latch.countDown())
-      actor ! 0
+      val actor = Actor[Int]((i: Int) => 1 / 0, (ex: Throwable) => latch.countDown())
+      actor ! 1
       assertCountDown(latch, "Should catch an exception")
     }
 
     "exchange messages with another actor without loss" in {
-      val latch = new CountDownLatch(NumOfMessages)
+      val latch = new CountDownLatch(n)
       var actor1: Actor[Int] = null
       val actor2 = Actor[Int]((i: Int) => actor1 ! i - 1)
       actor1 = Actor[Int] {
@@ -39,80 +79,52 @@ object ActorTest extends SpecLite {
             latch.countDown()
           }
       }
-      actor1 ! NumOfMessages
-      assertCountDown(latch, "Should exchange " + NumOfMessages + " messages")
+      actor1 ! n
+      assertCountDown(latch, "Should exchange " + n + " messages")
+    }
+
+    "send messages to itself and process them" in {
+      val latch = new CountDownLatch(1)
+      var actor: Actor[Int] = null
+      actor = Actor[Int] {
+        (i: Int) =>
+          if (i > 0) actor ! i - 1
+          else latch.countDown()
+      }
+      actor ! n
+      assertCountDown(latch, "Should send & process " + n + " messages")
     }
 
     "handle messages in order of sending by each thread" in {
-      val latch = new CountDownLatch(NumOfMessages)
+      val latch = new CountDownLatch(n)
       val actor = countingDownActor(latch)
       for (j <- 1 to NumOfThreads) fork {
-        for (i <- 1 to NumOfMessagesPerThread) {
+        for (i <- 1 to n / NumOfThreads) {
           actor !(j, i)
         }
       }
-      assertCountDown(latch, "Should process " + NumOfMessages + " messages")
-    }
-  }
-
-  "actor strategy" should {
-    "execute code async for different pools" in {
-      val l = new CountDownLatch(2/*3*/)
-      val f = (_: Int) => l.countDown()
-      Actor(f) {
-        import scala.concurrent.forkjoin._
-        Actor.strategy(new ForkJoinPool(NumOfThreads))
-      } ! 1
-// TODO uncomment if Java 6 support will be discarded or add dependency on JSR166 classes compiled for Java 6
-/*
-      Actor(f) {
-        Actor.strategy(new ForkJoinPool(NumOfThreads))
-      } ! 1
-*/
-      Actor(f) {
-        Actor.strategy(Executors.newFixedThreadPool(NumOfThreads))
-      } ! 1
-      assertCountDown(l, "Should execute on all pools")
+      assertCountDown(latch, "Should process " + n + " messages")
     }
 
-    "redirect unhandled errors to thread's uncaught exception handler of different pools" in {
-      val l = new CountDownLatch(2/*3*/)
-      val f = (_: Int) => throw new RuntimeException()
-      val h = new java.lang.Thread.UncaughtExceptionHandler() {
-        override def uncaughtException(t: Thread, e: Throwable): Unit = l.countDown()
-      }
-      Actor(f) {
-        import scala.concurrent.forkjoin._
-        Actor.strategy(new ForkJoinPool(NumOfThreads, new ForkJoinPool.ForkJoinWorkerThreadFactory() {
-          override def newThread(pool: ForkJoinPool): ForkJoinWorkerThread = new ForkJoinWorkerThread(pool) {
-            setUncaughtExceptionHandler(h)
-          }
-        }, null, true))
-      } ! 1
-// TODO uncomment if Java 6 support will be discarded or add dependency on JSR166 classes compiled for Java 6
-/*
-      Actor(f) {
-        Actor.strategy(new ForkJoinPool(NumOfThreads, new ForkJoinPool.ForkJoinWorkerThreadFactory() {
-          override def newThread(pool: ForkJoinPool): ForkJoinWorkerThread = new ForkJoinWorkerThread(pool) {
-            setUncaughtExceptionHandler(h)
-          }
-        }, null, true))
-      } ! 1
-*/
-      Actor(f) {
-        Actor.strategy(new ThreadPoolExecutor(NumOfThreads, NumOfThreads, 0L, TimeUnit.MILLISECONDS,
-          new LinkedBlockingQueue[Runnable], new ThreadFactory {
-            override def newThread(r: Runnable): Thread = new Thread(r) {
-              setUncaughtExceptionHandler(h)
-            }
-          }))
-      } ! 1
-      assertCountDown(l, "Should handle all uncaught exceptions")
+    "redirect unhandled errors to uncaught exception handler of thread" in {
+      val l = new CountDownLatch(1)
+      val err = System.err
+      try {
+        System.setErr(new java.io.PrintStream(new java.io.OutputStream {
+          override def write(b: Int): Unit = l.countDown()
+        }))
+        Actor((_: Int) => 1 / 0) ! 1
+        assertCountDown(l, "Should print to System.err uncaught exception")
+      } catch {
+        case e: ArithmeticException if e.getMessage == "/ by zero" =>
+          l.countDown() // for a sequential strategy
+          assertCountDown(l, "Should print to System.err uncaught exception")
+      } finally System.setErr(err)
     }
   }
 
   def countingDownActor(latch: CountDownLatch): Actor[(Int, Int)] = Actor[(Int, Int)] {
-    val ms = mutable.Map[Int, Int]()
+    val ms = collection.mutable.Map[Int, Int]()
 
     (m: (Int, Int)) =>
       val (j, i) = m
