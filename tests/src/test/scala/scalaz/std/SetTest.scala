d36484e087bba3b39b75c5c59675ed29a780b365
Mark Farrell <m4farrel@uwaterloo.ca>
2014-12-28 10:13:10 +0000

    Added test: foldLeftM should not stack overflow on Set
    Signed-off-by: Mark Farrell <m4farrel@uwaterloo.ca>
    (cherry picked from commit a49b8263ade6b25dda99b9e43a486c31ff704c6f)

--- tests/src/test/scala/scalaz/std/SetTest.scala
+++ tests/src/test/scala/scalaz/std/SetTest.scala
@@ -1,12 +1,29 @@
 package scalaz
 package std
 
+import syntax.foldable._
 import std.AllInstances._
 import scalaz.scalacheck.ScalazProperties._
 import org.scalacheck.Prop.forAll
 
 object SetTest extends SpecLite {
+
   checkAll(order.laws[Set[Int]])
   checkAll(monoid.laws[Set[Int]])
   checkAll(isEmpty.laws[Set])
+
+  "foldLeftM" should {
+
+    // as reported in <https://github.com/scalaz/scalaz/issues/866>
+    // fixed in b0b80be
+    "not stack overflow" in {
+
+      def sum = Set(1,2,3).foldLeftM[Option,Int](0) { case (x,y) => Some(x+y) }
+
+      sum must_== Some(6)
+
+    }
+
+  }
+
 }
