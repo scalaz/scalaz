3073bd87525be42c4dd16baa4a7c4586f87bf8f7
Brian McKenna <brian@simpleenergy.com>
2015-01-08 08:37:57 -0700

    Add Associativity laws
    (cherry picked from commit c0bfe74129b43a46ea75e857a3bf45de10971652)

--- core/src/main/scala/scalaz/Associative.scala
+++ core/src/main/scala/scalaz/Associative.scala
@@ -12,6 +12,17 @@ trait Associative[=>:[_, _]]  { self =>
 
   def reassociateRight[A, B, C](f: (A =>: B) =>: C): A =>: (B =>: C)
 
+  trait AssociativeLaw {
+    /** Reassociating left and then right is a no-op. */
+    def leftRight[A, B, C](fa: A =>: (B =>: C))(implicit FR: Equal[A =>: (B =>: C)]): Boolean =
+      FR.equal(reassociateRight(reassociateLeft(fa)), fa)
+
+    /** Reassociating right and then left is a no-op. */
+    def rightLeft[A, B, C](fa: (A =>: B) =>: C)(implicit FL: Equal[(A =>: B) =>: C]): Boolean =
+      FL.equal(reassociateLeft(reassociateRight(fa)), fa)
+  }
+
+  def associativeLaw = new AssociativeLaw {}
   ////
   val associativeSyntax = new scalaz.syntax.AssociativeSyntax[=>:] { def F = Associative.this }
 }
