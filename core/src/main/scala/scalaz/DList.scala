5191748bf95b0e14a86d9b386732acf556c163aa
xuwei-k <6b656e6a69@gmail.com>
2015-02-10 19:11:41 +0900

    add IsEmpty[DList]
    (cherry picked from commit 67ad7a977e019942da626f3c6f8bc96d5a67df2c)

--- core/src/main/scala/scalaz/DList.scala
+++ core/src/main/scala/scalaz/DList.scala
@@ -83,11 +83,12 @@ sealed abstract class DListInstances {
     val zero = DList[A]()
     def append(a: DList[A], b: => DList[A]) = a ++ b
   }
-  implicit val dlistMonadPlus: MonadPlus[DList] with Traverse[DList] with Zip[DList] = new MonadPlus[DList] with Traverse[DList] with Zip[DList]  {
+  implicit val dlistMonadPlus: MonadPlus[DList] with Traverse[DList] with Zip[DList] with IsEmpty[DList] = new MonadPlus[DList] with Traverse[DList] with Zip[DList] with IsEmpty[DList] {
     def point[A](a: => A) = DList(a)
     def bind[A, B](as: DList[A])(f: A => DList[B]) = as flatMap f
     def plus[A](a: DList[A], b: => DList[A]) = a ++ b
     def empty[A] = DList()
+    def isEmpty[A](fa: DList[A]) = fa.isEmpty
     def zip[A,B](a: => DList[A], b: => DList[B]): DList[(A, B)] = a zip b
     def traverseImpl[F[_], A, B](fa: DList[A])(f: A => F[B])(implicit F: Applicative[F]): F[DList[B]] =
       fa.foldr(F.point(DList[B]()))((a, fbs) => F.apply2(f(a), fbs)(_ +: _))
