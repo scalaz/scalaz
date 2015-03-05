392fb1282c3dbef1dcac4aa9ae792eb21876b0f4
xuwei-k <6b656e6a69@gmail.com>
2015-03-05 12:37:02 +0900

    Scala 2.10.5
    (cherry picked from commit 1b945e30db6c165fdadea16cf02f476b859dff79)

--- project/build.scala
+++ project/build.scala
@@ -61,8 +61,8 @@ object build extends Build {
   lazy val standardSettings: Seq[Sett] = Defaults.defaultSettings ++ sbtrelease.ReleasePlugin.releaseSettings ++ Seq[Sett](
     organization := "org.scalaz",
 
-    scalaVersion := "2.10.4",
-    crossScalaVersions := Seq("2.9.3", "2.10.4", "2.11.6"),
+    scalaVersion := "2.10.5",
+    crossScalaVersions := Seq("2.9.3", "2.10.5", "2.11.6"),
     resolvers ++= (if (scalaVersion.value.endsWith("-SNAPSHOT")) List(Opts.resolver.sonatypeSnapshots) else Nil),
     scalacOptions <++= (scalaVersion) map { sv =>
       val versionDepOpts =
