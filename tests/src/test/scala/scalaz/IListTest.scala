22570bdaa7946311e2580df3b063b4903f201fd0
xuwei-k <6b656e6a69@gmail.com>
2015-03-10 15:25:11 +0900

    add IList#interleave
    (cherry picked from commit 45be2b9e475ed7bf957ef768c1532a24c1cbd269)

--- tests/src/test/scala/scalaz/IListTest.scala
+++ tests/src/test/scala/scalaz/IListTest.scala
@@ -233,6 +233,24 @@ object IListTest extends SpecLite {
     ns.inits.map(_.toList).toList must_=== ns.toList.inits.toList
   }
 
+  "interleave" ! forAll { (xs: IList[Int], ys: IList[Int]) =>
+    val a = xs interleave ys
+    (xs.length + ys.length) must_=== a.length
+    val min = math.min(xs.length, ys.length)
+
+    Foldable[IList].all(xs.zipWithIndex){ case (x, i) =>
+      val index = if(i <= min) i * 2 else (min * 2) + i - min
+      a.index(index) == Some(x)
+    } must_=== true
+
+    Foldable[IList].all(ys.zipWithIndex){ case (y, i) =>
+      val index = if(i < min) (i * 2) + 1 else (min * 2) + i - min
+      a.index(index) == Some(y)
+    } must_=== true
+
+    xs.interleave(ys).toStream must_=== std.stream.interleave(xs.toStream, ys.toStream)
+  }
+
   // intersperse is tested above
   // isEmpty is tested by empty laws
 
