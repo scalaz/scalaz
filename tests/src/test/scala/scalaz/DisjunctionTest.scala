232bef2758197925ad695a98a3dd041461c7ec81
Peter Le Bek <peteralebek@gmail.com>
2015-04-07 18:14:36 -0400

    Use case objects in test to avoid type casing

--- tests/src/test/scala/scalaz/DisjunctionTest.scala
+++ tests/src/test/scala/scalaz/DisjunctionTest.scala
@@ -35,38 +35,35 @@ object DisjunctionTest extends SpecLite {
 
   "recover" in {
     sealed trait Foo
-    final class Bar extends Foo
-    final class Baz extends Foo
-    val bar = new Bar
+    case object Bar extends Foo
+    case object Baz extends Foo
 
     implicit val equalFoo = Equal.equalA[Foo]
     implicit val showFoo = Show.showA[Foo]
 
-    -\/[Foo](bar).recover({ case _: Bar => 1 }) must_=== \/-(1)
-    -\/[Foo](bar).recover({ case _: Baz => 1 }) must_=== -\/(bar)
-    \/.right[Foo, Int](1).recover({ case _: Foo => 4 }) must_=== \/-(1)
+    -\/[Foo](Bar).recover({ case Bar => 1 }) must_=== \/-(1)
+    -\/[Foo](Bar).recover({ case Baz => 1 }) must_=== -\/(Bar)
+    \/.right[Foo, Int](1).recover({ case Bar => 4 }) must_=== \/-(1)
   }
 
   "recoverWith" in {
     sealed trait Foo
-    final class Bar extends Foo
-    final class Baz extends Foo
-    val bar = new Bar
-    val baz = new Baz
+    case object Bar extends Foo
+    case object Baz extends Foo
 
     implicit val equalFoo = Equal.equalA[Foo]
     implicit val showFoo = Show.showA[Foo]
 
     val barToBaz: PartialFunction[Foo, \/[Foo, Int]] = {
-      case _: Bar => -\/(baz)
+      case Bar => -\/(Baz)
     }
 
     val bazToInt: PartialFunction[Foo, \/[Foo, Int]] = {
-      case _: Baz => \/-(1)
+      case Baz => \/-(1)
     }
 
-    -\/[Foo](bar).recoverWith(barToBaz) must_=== -\/(baz)
-    -\/[Foo](bar).recoverWith(bazToInt) must_=== -\/(bar)
+    -\/[Foo](Bar).recoverWith(barToBaz) must_=== -\/(Baz)
+    -\/[Foo](Bar).recoverWith(bazToInt) must_=== -\/(Bar)
     \/.right[Foo, Int](1).recoverWith(barToBaz) must_=== \/-(1)
   }
 }
