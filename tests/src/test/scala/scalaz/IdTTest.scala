0f21748f22d9cff829a402c76dfd96811d9b9d25
xuwei-k <6b656e6a69@gmail.com>
2015-06-08 04:12:43 +0900

    add Equal and Order instance of IdT
    (cherry picked from commit 5079a45d5d817ac44272b8b47735de0bfae206f9)

--- tests/src/test/scala/scalaz/IdTTest.scala
+++ tests/src/test/scala/scalaz/IdTTest.scala
@@ -5,6 +5,8 @@ import org.scalacheck.Prop.forAll
 object IdTTest extends SpecLite {
 
   object instances {
+    def equal[F[_], A](implicit F: Equal[F[A]]) = Equal[IdT[F, A]]
+    def order[F[_], A](implicit F: Order[F[A]]) = Order[IdT[F, A]]
     def functor[F[_] : Functor] = Functor[({type λ[α] = IdT[F, α]})#λ]
     def apply[F[_] : Apply] = Apply[({type λ[α] = IdT[F, α]})#λ]
     def monad[F[_] : Monad] = Monad[({type λ[α] = IdT[F, α]})#λ]
@@ -12,6 +14,7 @@ object IdTTest extends SpecLite {
     def traverse[F[_] : Traverse] = Traverse[({type λ[α] = IdT[F, α]})#λ]
 
     // checking absence of ambiguity
+    def equal[F[_], A](implicit F: Order[F[A]]) = Equal[IdT[F, A]]
     def functor[F[_] : Monad] = Functor[({type λ[α] = IdT[F, α]})#λ]
     def functor[F[_] : Monad : Traverse] = Functor[({type λ[α] = IdT[F, α]})#λ]
     def apply[F[_] : Monad] = Apply[({type λ[α] = IdT[F, α]})#λ]
