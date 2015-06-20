35c3036eebec6bf13f30069108d204c68d967af1
xuwei-k <6b656e6a69@gmail.com>
2015-06-20 13:46:53 +0900

    add TheseOrder
    (cherry picked from commit e9ee485d488b0b20f78d366a9ff47dabbec44ac3)

--- tests/src/test/scala/scalaz/TheseTest.scala
+++ tests/src/test/scala/scalaz/TheseTest.scala
@@ -13,7 +13,7 @@ object TheseTest extends SpecLite {
   checkAll(monad.laws[TheseInt])
   checkAll(cobind.laws[TheseInt])
   checkAll(traverse.laws[TheseInt])
-  checkAll(equal.laws[Int \&/ Int])
+  checkAll(order.laws[Int \&/ Int])
   checkAll(semigroup.laws[Int \&/ Int])
   checkAll(bitraverse.laws[\&/])
 
@@ -39,6 +39,8 @@ object TheseTest extends SpecLite {
   }
 
   object instances {
+    def equal[A: Equal, B: Equal] = Equal[A \&/ B]
+    def order[A: Order, B: Order] = Order[A \&/ B]
     def functor[L] = Functor[({type λ[α] = L \&/ α})#λ]
     def apply[L: Semigroup] = Apply[({type λ[α] = L \&/ α})#λ]
     def applicative[L: Semigroup] = Applicative[({type λ[α] = L \&/ α})#λ]
@@ -53,5 +55,6 @@ object TheseTest extends SpecLite {
 
     // checking absence of ambiguity
     def functor[L: Semigroup] = Functor[({type λ[α] = L \&/ α})#λ]
+    def equal[A: Order, B: Order] = Equal[A \&/ B]
   }
 }
