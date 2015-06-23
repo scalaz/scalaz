b1e86fd483283e80601b0556ce94f52e1dbb47b8
Runar Bjarnason <runar.bjarnason@oncue.com>
2015-06-23 15:49:36 -0400

    `Forall` is an endofunctor in an endofunctor category

--- core/src/main/scala/scalaz/Forall.scala
+++ core/src/main/scala/scalaz/Forall.scala
@@ -1,8 +1,13 @@
 package scalaz
 
 /** A universally quantified value */
-trait Forall[P[_]] {
+trait Forall[P[_]] { self =>
   def apply[A]: P[A]
+
+  /** `Forall` is an endofunctor in an endofunctor category */
+  def map[Q[_]](f: P ~> Q) = new Forall[Q] {
+    def apply[A]: Q[A] = f(self.apply)
+  }
 }
 
 object Forall extends Foralls
