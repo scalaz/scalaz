94cfba67ec31479f8c0b30a4b2add0123b70a8a2
kenji yoshida <6b656e6a69@gmail.com>
2015-06-27 21:43:36 +0900

    s/Copointed/Comonad

--- tests/src/test/scala/scalaz/std/FutureTest.scala
+++ tests/src/test/scala/scalaz/std/FutureTest.scala
@@ -42,8 +42,8 @@ class FutureTest extends SpecLite {
   // For some reason ArbitraryThrowable isn't being chosen by scalac, so we give it explicitly.
   checkAll(monadError.laws[λ[(α, β) => Future[β]], Throwable](implicitly, implicitly, implicitly, implicitly, ArbitraryThrowable))
 
-  // Scope these away from the rest as Copointed[Future] is a little evil.
-  // Should fail to compile by default: implicitly[Copointed[Future]]
+  // Scope these away from the rest as Comonad[Future] is a little evil.
+  // Should fail to compile by default: implicitly[Comonad[Future]]
   {
     implicit val cm: Comonad[Future] = futureComonad(duration)
     checkAll(comonad.laws[Future])
