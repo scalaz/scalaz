afa6a8cd96c6598f54fb3ac9ac58f472738fb24a
Adelbert Chang <adelbertc@gmail.com>
2015-07-01 13:42:17 -0700

    Add ReaderT value alias
    (cherry picked from commit 6db9bf77d30ac10c68b946aa81336b1626a52fcc)

--- core/src/main/scala/scalaz/package.scala
+++ core/src/main/scala/scalaz/package.scala
@@ -116,6 +116,7 @@ package object scalaz {
   type |>=|[G[_], F[_]] = MonadPartialOrder[G, F]
 
   type ReaderT[F[_], E, A] = Kleisli[F, E, A]
+  val ReaderT = Kleisli
   type =?>[E, A] = Kleisli[Option, E, A]
   type Reader[E, A] = ReaderT[Id, E, A]
 
