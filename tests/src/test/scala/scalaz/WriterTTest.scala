1798c3c82a0cd539c55026c062369fb86c383c2f
xuwei-k <6b656e6a69@gmail.com>
2015-07-04 13:34:38 +0900

    add more tests for WriterT
    (cherry picked from commit a39987d7aefaa3218c1ec1f8a574230fe0199cc0)

--- tests/src/test/scala/scalaz/WriterTTest.scala
+++ tests/src/test/scala/scalaz/WriterTTest.scala
@@ -34,18 +34,25 @@ object WriterTTest extends SpecLite {
   object instances {
     def functor[F[_]: Functor, W] = Functor[({type λ[α]=WriterT[F, W, α]})#λ]
     def apply[F[_]: Apply, W: Semigroup] = Apply[({type λ[α]=WriterT[F, W, α]})#λ]
+    def applicative[F[_]: Applicative, W: Monoid] = Applicative[({type λ[α]=WriterT[F, W, α]})#λ]
     def monad[F[_]: Monad, W: Monoid] = Monad[({type λ[α]=WriterT[F, W, α]})#λ]
     def foldable[F[_]: Foldable, W] = Foldable[({type λ[α]=WriterT[F, W, α]})#λ]
     def traverse[F[_]: Traverse, W] = Traverse[({type λ[α]=WriterT[F, W, α]})#λ]
 
     def functor[F[_]: Monad, W: Monoid] = Functor[({type λ[α]=WriterT[F, W, α]})#λ]
     def apply[F[_]: Monad, W: Monoid] = Apply[({type λ[α]=WriterT[F, W, α]})#λ]
+    def apply[F[_]: Monad, W: Semigroup] = Apply[({type λ[α]=WriterT[F, W, α]})#λ]
+    def apply[F[_]: Bind, W: Monoid] = Apply[({type λ[α]=WriterT[F, W, α]})#λ]
+    def apply[F[_]: Bind, W: Semigroup] = Apply[({type λ[α]=WriterT[F, W, α]})#λ]
+    def apply[F[_]: Apply, W: Monoid] = Apply[({type λ[α]=WriterT[F, W, α]})#λ]
+    def applicative[F[_]: Monad, W: Monoid] = Applicative[({type λ[α]=WriterT[F, W, α]})#λ]
     def functor[F[_]: Traverse, W: Monoid] = Functor[({type λ[α]=WriterT[F, W, α]})#λ]
     def foldable[F[_]: Traverse, W] = Foldable[({type λ[α]=WriterT[F, W, α]})#λ]
     
     object writer {
       def functor[W] = Functor[({type λ[α]=Writer[W, α]})#λ]
       def apply[W: Semigroup] = Apply[({type λ[α]=Writer[W, α]})#λ]
+      def applicative[W: Monoid] = Applicative[({type λ[α]=Writer[W, α]})#λ]
       def monad[W: Monoid] = Monad[({type λ[α]=Writer[W, α]})#λ]
       def foldable[W] = Foldable[({type λ[α]=Writer[W, α]})#λ](WriterT.writerTFoldable[Id, W])
       def traverse[W] = Traverse[({type λ[α]=Writer[W, α]})#λ]
