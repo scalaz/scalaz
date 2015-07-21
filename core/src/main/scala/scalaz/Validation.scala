d31acee5c9b5e74b57a4c02cee06dc84a6c81d07
xuwei-k <6b656e6a69@gmail.com>
2015-07-21 10:03:02 +0900

    use value class in ValidationFlatMap

--- core/src/main/scala/scalaz/Validation.scala
+++ core/src/main/scala/scalaz/Validation.scala
@@ -421,7 +421,7 @@ sealed abstract class ValidationInstances0 extends ValidationInstances1 {
   }
 }
 
-final class ValidationFlatMap[E, A] private[scalaz](val self: Validation[E, A]) {
+final class ValidationFlatMap[E, A] private[scalaz](val self: Validation[E, A]) extends AnyVal {
   /** Bind through the success of this validation. */
   def flatMap[EE >: E, B](f: A => Validation[EE, B]): Validation[EE, B] =
     self match {
