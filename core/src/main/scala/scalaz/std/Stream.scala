79a7002ae58f5b278c8c366059fc671de2061aa5
xuwei-k <6b656e6a69@gmail.com>
2015-09-13 04:52:15 +0900

    add `Order[Stream[A]]`
    (cherry picked from commit 88b167899f2229f51b083c2bbfccfa02de8a6897)

--- core/src/main/scala/scalaz/std/Stream.scala
+++ core/src/main/scala/scalaz/std/Stream.scala
@@ -102,15 +102,13 @@ trait StreamInstances {
     def zero: Stream[A] = scala.Stream.empty
   }
 
-  implicit def streamEqual[A](implicit A0: Equal[A]) = new Equal[Stream[A]] {
-    def equal(a1: Stream[A], a2: Stream[A]) = (a1 corresponds a2)(A0.equal)
-  }
+  implicit def streamEqual[A](implicit A0: Equal[A]): Equal[Stream[A]] =
+    new StreamEqual[A] { def A = A0 }
   implicit def streamShow[A](implicit A0: Show[A]) = new Show[Stream[A]] {
     override def show(as: Stream[A]) = "Stream(" +: stream.intersperse(as.map(A0.show), Cord(",")).foldLeft(Cord())(_ ++ _) :+ ")"
   }
 
 
-  // TODO order, ...
 }
 
 trait StreamFunctions {
@@ -200,4 +198,29 @@ trait StreamFunctions {
 
 object stream extends StreamInstances with StreamFunctions {
   object streamSyntax extends scalaz.syntax.std.ToStreamOps
+
+  implicit def streamOrder[A](implicit A0: Order[A]): Order[Stream[A]] =
+    new Order[Stream[A]] with StreamEqual[A] {
+      def A = A0
+      import Ordering._
+      @annotation.tailrec
+      override final def order(a: Stream[A], b: Stream[A]): Ordering =
+        if(a.isEmpty) {
+          if(b.isEmpty) EQ
+          else LT
+        } else {
+          if(b.isEmpty) GT
+          else {
+            A.order(a.head, b.head) match {
+              case EQ => order(a.tail, b.tail)
+              case x => x
+            }
+          }
+        }
+    }
+}
+
+private trait StreamEqual[A] extends Equal[Stream[A]] {
+  def A: Equal[A]
+  override final def equal(a1: Stream[A], a2: Stream[A]) = (a1 corresponds a2)(A.equal)
 }
