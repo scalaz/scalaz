c9e3b443f68559c3762a63123a63ea3c4849d214
xuwei-k <6b656e6a69@gmail.com>
2015-09-26 05:05:13 +0900

    remove unused type parameter

--- core/src/main/scala/scalaz/Foldable1.scala
+++ core/src/main/scala/scalaz/Foldable1.scala
@@ -122,7 +122,7 @@ trait Foldable1[F[_]] extends Foldable[F] { self =>
   def traverse1_[M[_], A, B](fa: F[A])(f: A => M[B])(implicit a: Apply[M], x: Semigroup[M[B]]): M[Unit] =
     a.map(foldMap1(fa)(f))(_ => ())
 
-  def sequence1_[M[_], A, B](fa: F[M[A]])(implicit a: Apply[M], x: Semigroup[M[A]]): M[Unit] =
+  def sequence1_[M[_], A](fa: F[M[A]])(implicit a: Apply[M], x: Semigroup[M[A]]): M[Unit] =
     traverse1_(fa)(x => x)
 
   /** always return `false` */
