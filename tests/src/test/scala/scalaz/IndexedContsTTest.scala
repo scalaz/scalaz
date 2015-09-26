122e5f9dfc3fbe9189d1f817e703c78047fbb3ef
xuwei-k <6b656e6a69@gmail.com>
2015-09-26 22:16:12 +0900

    add ContsTBind

--- tests/src/test/scala/scalaz/IndexedContsTTest.scala
+++ tests/src/test/scala/scalaz/IndexedContsTTest.scala
@@ -7,9 +7,12 @@ object IndexedContsTTest extends SpecLite {
     def functorLeft[W[_], M[_]: Functor, O, A] = Functor[IndexedContsT[W, M, ?, O, A]]
     def contravariant[W[_]: Functor, M[_]: Functor, R, A] = Contravariant[IndexedContsT[W, M, R, ?, A]]
     def bifunctor[W[_]: Functor, M[_]: Functor, O] = Bifunctor[IndexedContsT[W, M, ?, O, ?]]
+    def bind[W[_]: Cobind, M[_], R] = Bind[ContsT[W, M, R, ?]]
     def monad[W[_]: Comonad, M[_], R] = Monad[ContsT[W, M, R, ?]]
 
     // checking absence of ambiguity
     def functor[W[_]: Comonad, M[_], R] = Functor[ContsT[W, M, R, ?]]
+    def functor[W[_]: Cobind, M[_], R] = Functor[ContsT[W, M, R, ?]]
+    def bind[W[_]: Comonad, M[_], R] = Bind[ContsT[W, M, R, ?]]
   }
 }
