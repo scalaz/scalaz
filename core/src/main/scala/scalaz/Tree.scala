f5d2dfc84e53fde70af0f9455ac9f6979662ff88
Jeffrey Shaw <shawjef3@gmail.com>
2015-10-02 11:01:32 -0400

    Deprecate Tree.apply, Tree.leaf, and Tree.node in favor of Tree.Node and Tree.Leaf, which also allow pattern matching. Use the new objects everywhere.

--- core/src/main/scala/scalaz/Tree.scala
+++ core/src/main/scala/scalaz/Tree.scala
@@ -40,7 +40,7 @@ sealed abstract class Tree[A] {
    **/
   def scanr[B](g: (A, Stream[Tree[B]]) => B): Tree[B] = {
     lazy val c = subForest.map(_.scanr(g))
-    node(g(rootLabel, c), c)
+    Node(g(rootLabel, c), c)
   }
 
   /** A 2D String representation of this Tree, separated into lines. 
@@ -104,27 +104,27 @@ sealed abstract class Tree[A] {
     lazy val uz = subForest.map(_.unzip)
     lazy val fst = uz map (_._1)
     lazy val snd = uz map (_._2)
-    (node(rootLabel._1, fst), node(rootLabel._2, snd))
+    (Node(rootLabel._1, fst), Node(rootLabel._2, snd))
   }
 
   def foldNode[Z](f: A => Stream[Tree[A]] => Z): Z =
     f(rootLabel)(subForest)
 
   def map[B](f: A => B): Tree[B] =
-    node(f(rootLabel), subForest map (_ map f))
+    Node(f(rootLabel), subForest map (_ map f))
 
   def flatMap[B](f: A => Tree[B]): Tree[B] = {
     val r: Tree[B] = f(rootLabel)
-    Tree.node(r.rootLabel, r.subForest #::: subForest.map(_.flatMap(f)))
+    Node(r.rootLabel, r.subForest #::: subForest.map(_.flatMap(f)))
   }
 
   def traverse1[G[_] : Apply, B](f: A => G[B]): G[Tree[B]] = {
     val G = Apply[G]
     import Stream._
     subForest match {
-      case Empty => G.map(f(rootLabel))(Tree(_))
+      case Empty => G.map(f(rootLabel))(Leaf(_))
       case x #:: xs => G.apply2(f(rootLabel), NonEmptyList.nel(x, IList.fromFoldable(xs)).traverse1(_.traverse1(f))) {
-        case (h, t) => Tree.node(h, t.list.toStream)
+        case (h, t) => Node(h, t.list.toStream)
       }
     }
   }
@@ -132,7 +132,7 @@ sealed abstract class Tree[A] {
 
 sealed abstract class TreeInstances {
   implicit val treeInstance: Traverse1[Tree] with Monad[Tree] with Comonad[Tree] with Align[Tree] with Zip[Tree] = new Traverse1[Tree] with Monad[Tree] with Comonad[Tree] with Align[Tree] with Zip[Tree] {
-    def point[A](a: => A): Tree[A] = Tree.leaf(a)
+    def point[A](a: => A): Tree[A] = Tree.Leaf(a)
     def cobind[A, B](fa: Tree[A])(f: Tree[A] => B): Tree[B] = fa cobind f
     def copoint[A](p: Tree[A]): A = p.rootLabel
     override def map[A, B](fa: Tree[A])(f: A => B) = fa map f
@@ -150,7 +150,7 @@ sealed abstract class TreeInstances {
     override def foldMap[A, B](fa: Tree[A])(f: A => B)(implicit F: Monoid[B]): B = fa foldMap f
     def alignWith[A, B, C](f: (\&/[A, B]) ⇒ C) = { 
       def align(ta: Tree[A], tb: Tree[B]): Tree[C] =
-        Tree.node(f(\&/(ta.rootLabel, tb.rootLabel)), Align[Stream].alignWith[Tree[A], Tree[B], Tree[C]]({
+        Tree.Node(f(\&/(ta.rootLabel, tb.rootLabel)), Align[Stream].alignWith[Tree[A], Tree[B], Tree[C]]({
           case \&/.This(sta) ⇒ sta map {a ⇒ f(\&/.This(a))}
           case \&/.That(stb) ⇒ stb map {b ⇒ f(\&/.That(b))}
           case \&/(sta, stb) ⇒ align(sta, stb)
@@ -160,7 +160,7 @@ sealed abstract class TreeInstances {
     def zip[A, B](aa: => Tree[A], bb: => Tree[B]) = {
       val a = aa
       val b = bb
-      Tree.node(
+      Tree.Node(
         (a.rootLabel, b.rootLabel),
         Zip[Stream].zipWith(a.subForest, b.subForest)(zip(_, _))
       )
@@ -189,29 +189,61 @@ sealed abstract class TreeInstances {
 
 object Tree extends TreeInstances {
   /** Construct a tree node with no children. */
-  def apply[A](root: => A): Tree[A] = leaf(root)
+  @deprecated(message = "Use Leaf.apply or Node.apply instead.", since = "7.2.0")
+  def apply[A](root: => A): Tree[A] = Leaf(root)
 
+  /**
+   * Node represents a tree node that may have children.
+   *
+   * You can use Node for tree construction or pattern matching.
+   */
   object Node {
+    def apply[A](root: => A, forest: => Stream[Tree[A]]): Tree[A] = {
+      new Tree[A] {
+        lazy val rootLabel = root
+        lazy val subForest = forest
+
+        override def toString = "<tree>"
+      }
+    }
+
     def unapply[A](t: Tree[A]): Option[(A, Stream[Tree[A]])] = Some((t.rootLabel, t.subForest))
   }
 
-  /** Construct a new Tree node. */
-  def node[A](root: => A, forest: => Stream[Tree[A]]): Tree[A] = new Tree[A] {
-    lazy val rootLabel = root
-    lazy val subForest = forest
+  /**
+   *  Leaf represents a a tree node with no children.
+   *
+   *  You can use Leaf for tree construction or pattern matching.
+   */
+  object Leaf {
+    def apply[A](root: => A): Tree[A] = {
+      Node(root, Stream.empty)
+    }
 
-    override def toString = "<tree>"
+    def unapply[A](t: Tree[A]): Option[A] = {
+      t match {
+        case Node(root, Stream.Empty) =>
+          Some(root)
+        case _ =>
+          None
+      }
+    }
   }
 
+  /** Construct a new Tree node. */
+  @deprecated(message = "Use Node.apply instead.", since = "7.2.0")
+  def node[A](root: => A, forest: => Stream[Tree[A]]): Tree[A] = Node(root, forest)
+
   /** Construct a tree node with no children. */
-  def leaf[A](root: => A): Tree[A] = node(root, Stream.empty)
+  @deprecated(message = "Use Leaf.apply instead.", since = "7.2.0")
+  def leaf[A](root: => A): Tree[A] = Leaf(root)
 
   def unfoldForest[A, B](s: Stream[A])(f: A => (B, () => Stream[A])): Stream[Tree[B]] =
     s.map(unfoldTree(_)(f))
 
   def unfoldTree[A, B](v: A)(f: A => (B, () => Stream[A])): Tree[B] =
     f(v) match {
-      case (a, bs) => node(a, unfoldForest(bs.apply())(f))
+      case (a, bs) => Node(a, unfoldForest(bs.apply())(f))
     }
 }
 
