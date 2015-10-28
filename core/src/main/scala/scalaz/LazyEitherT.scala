c266794dc102903750e75fba9603b6139d6c7d0d
xuwei-k <6b656e6a69@gmail.com>
2015-10-28 10:34:45 +0900

    add `Hoist[λ[(a[_], b) => LazyEitherT[a, A, b]]]`

--- core/src/main/scala/scalaz/LazyEitherT.scala
+++ core/src/main/scala/scalaz/LazyEitherT.scala
@@ -269,6 +269,18 @@ sealed abstract class LazyEitherTInstances extends LazyEitherTInstances0 {
 
       def iso = LazyEitherT.lazyEitherTLeftProjectionEIso2[F, L]
     }
+
+  implicit def lazyEitherTHoist[A]: Hoist[λ[(a[_], b) => LazyEitherT[a, A, b]]] =
+    new Hoist[λ[(a[_], b) => LazyEitherT[a, A, b]]] {
+      override def hoist[M[_]: Monad, N[_]](f: M ~> N) =
+        new (LazyEitherT[M, A, ?] ~> LazyEitherT[N, A, ?]) {
+          def apply[B](mb: LazyEitherT[M, A, B]) = LazyEitherT(f(mb.run))
+        }
+      override def liftM[M[_], B](mb: M[B])(implicit M: Monad[M]) =
+        LazyEitherT(M.map(mb)(LazyEither.lazyRight[A].apply(_)))
+      override def apply[M[_]: Monad] =
+        LazyEitherT.lazyEitherTMonad[M, A]
+    }
 }
 
 
