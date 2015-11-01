5b4159db7752ca4da87424ba977e562c9b712650
xuwei-k <6b656e6a69@gmail.com>
2015-11-01 16:13:14 +0900

    add `MonadPlus[NullResult[X, ?]]`

--- core/src/main/scala/scalaz/NullResult.scala
+++ core/src/main/scala/scalaz/NullResult.scala
@@ -197,8 +197,8 @@ sealed abstract class NullResultInstances extends NullResultInstances0 {
         }
     }
 
-  implicit def nullResultMonad[X]: Monad[NullResult[X, ?]] =
-    new Monad[NullResult[X, ?]] {
+  implicit def nullResultMonadPlus[X]: MonadPlus[NullResult[X, ?]] =
+    new MonadPlus[NullResult[X, ?]] {
       override def map[A, B](a: NullResult[X, A])(f: A => B) =
         a map f
       override def ap[A, B](a: => NullResult[X, A])(f: => NullResult[X, A => B]) =
@@ -207,6 +207,10 @@ sealed abstract class NullResultInstances extends NullResultInstances0 {
         NullResult.always(a)
       override def bind[A, B](a: NullResult[X, A])(f: A => NullResult[X, B]) =
         a flatMap f
+      override def empty[A] =
+        NullResult.never[X, A]
+      override def plus[A](a: NullResult[X, A], b: => NullResult[X, A]) =
+        NullResult[X, A](x => a(x) orElse b(x))
     }
 
   implicit def nullResultContravariant[X]: Contravariant[NullResult[?, X]] =
