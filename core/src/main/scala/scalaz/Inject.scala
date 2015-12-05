6753ee6fdb86c4f753573f41108840a0832fd84b
Tomas Mikula <tomas.mikula@gmail.com>
2015-12-05 14:51:00 -0500

    Make Inject[F, G] extend (F ~> G).
    This makes Inject[F, G] directly usable as an argument to Free[F, A]#mapSuspension().
    
    (cherry picked from commit c26debc7080a18da59406238a34a4f7121577292)

--- core/src/main/scala/scalaz/Inject.scala
+++ core/src/main/scala/scalaz/Inject.scala
@@ -7,7 +7,8 @@ import std.option.{none, some}
  *
  * @see [[http://www.staff.science.uu.nl/~swier004/Publications/DataTypesALaCarte.pdf]]
  */
-sealed abstract class Inject[F[_], G[_]] {
+sealed abstract class Inject[F[_], G[_]] extends (F ~> G) {
+  def apply[A](fa: F[A]): G[A] = inj(fa)
   def inj[A](fa: F[A]): G[A]
   def prj[A](ga: G[A]): Option[F[A]]
 }
