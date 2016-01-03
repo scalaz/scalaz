29680269e7dfcdc104b549f3cd5bdf9a75995165
Matthew King <mking@users.noreply.github.com>
2016-01-03 01:23:14 -0800

    fix digit append
    (cherry picked from commit 99b4c17066a42d90da25a0e2e6944bffcee081e6)

--- tests/src/test/scala/scalaz/DigitTest.scala
+++ tests/src/test/scala/scalaz/DigitTest.scala
@@ -5,5 +5,6 @@ import scalaz.scalacheck.ScalazArbitrary._
 import org.scalacheck.Prop.forAll
 
 object DigitTest extends SpecLite {
-  checkAll(order.laws[Digit])
+  checkAll(enum.laws[Digit])
+  checkAll(monoid.laws[Digit])
 }
