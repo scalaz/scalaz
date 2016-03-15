e8d8cef40a7f456e68800895c8589b2db1082d21
Tomas Mikula <tomas.mikula@gmail.com>
2016-03-15 18:40:56 -0400

    Add StreamT#asStream: conversion of StreamT[Id, A] to a lazy Stream[A].
    (cherry picked from commit 3d5a7c7aacea1e5ae541664385e7db342089c7b8)

--- core/src/main/scala/scalaz/StreamT.scala
+++ core/src/main/scala/scalaz/StreamT.scala
@@ -109,7 +109,28 @@ sealed class StreamT[M[_], A](val step: M[StreamT.Step[A, StreamT[M, A]]]) {
        )
     }
   
+  /**
+   * **Warning:** Requires evaluation of the whole stream. Depending on
+   * the monad `M`, the evaluation will happen either immediately, or
+   * will be deferred until the resulting `Stream` is extracted from the
+   * returned `M`.
+   */
   def toStream(implicit M: Monad[M]): M[Stream[A]] = M.map(rev)(_.reverse)
+
+  /**
+   * Converts this `StreamT` to a lazy `Stream`, i.e. without forcing
+   * evaluation of all elements. Note, however, that at least one element
+   * of this stream will be evaluated, and depending on the structure of
+   * this stream, up to two elements might be evaluated.
+   */
+  def asStream(implicit ev: M[Step[A, StreamT[M, A]]] =:= Id[Step[A, StreamT[Id, A]]]): Stream[A] = {
+    def go(s: StreamT[Id, A]): Stream[A] = s.uncons match {
+      case None => Stream.empty[A]
+      case Some((a, s1)) => Stream.cons(a, go(s1))
+    }
+
+    go(StreamT(ev(step)))
+  }
     
   def foldRight[B](z: => B)(f: (=> A, => B) => B)(implicit M: Monad[M]): M[B] =
     M.map(rev) {
