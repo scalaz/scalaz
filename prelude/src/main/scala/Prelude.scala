5293600e0687270e41cbf6430320298b1a5f70fd
Alois Cochard <alois.cochard@gmail.com>
2016-05-28 21:21:59 +0200

    Initial `prelude` module.

---
+++ prelude/src/main/scala/Prelude.scala
@@ -0,0 +1,60 @@
+package scalaz
+
+import clazz._
+import data._
+
+import scala.language.implicitConversions
+
+trait Prelude extends data.DisjunctionFunctions {
+  // Core Class
+  // ==========
+  type Applicative[F[_]] = clazz.Applicative[F]
+  type Apply[F[_]] = clazz.Apply[F]
+  type Bind[M[_]] = clazz.Bind[M]
+  type Foldable[T[_]] = clazz.Foldable[T]
+  type Functor[F[_]] = clazz.Functor[F]
+  type Monad[M[_]] = clazz.Monad[M]
+  type Traversable[T[_]] = clazz.Traversable[T]
+
+  def Applicative[F[_]](implicit F: Applicative[F]): Applicative[F] = F
+  def Apply[F[_]](implicit F: Apply[F]): Apply[F] = F
+  def Bind[F[_]](implicit F: Bind[F]): Bind[F] = F
+  def Foldable[F[_]](implicit F: Foldable[F]): Foldable[F] = F
+  def Functor[F[_]](implicit F: Functor[F]): Functor[F] = F
+  def Monad[M[_]](implicit M: Monad[M]): Monad[M] = M
+  def Traversable[T[_]](implicit T: Traversable[T]): Traversable[T] = T
+
+  // ApplicativeSyntax
+  implicit def PapplicativeOpsA[A](a: A): ApplicativeSyntax.OpsA[A] = new ApplicativeSyntax.OpsA(a)
+
+  // ApplySyntax
+  implicit def PapplyOps[F[_], A](fa: F[A])(implicit F: Apply[F]): ApplySyntax.Ops[F, A] =
+    new ApplySyntax.Ops(fa)
+
+  // BindSyntax
+  implicit def PbindOps[M[_], A](ma: M[A])(implicit M: Bind[M]): BindSyntax.Ops[M, A] =
+    new BindSyntax.Ops(ma)
+
+  // FoldableSyntax
+  implicit def PfoldableOps[F[_], A](fa: F[A])(implicit F: Foldable[F]): FoldableSyntax.Ops[F, A] =
+    new FoldableSyntax.Ops(fa)
+
+  // FunctorSyntax
+  implicit def PfunctorOps[F[_], A](fa: F[A])(implicit F: Functor[F]): FunctorSyntax.Ops[F, A] =
+    new FunctorSyntax.Ops(fa)
+
+  // MaybeSyntax
+  implicit class POptionAsMaybe[A](oa: Option[A]) { def asMaybe: Maybe[A] = Maybe.fromOption(oa) }
+
+  // TraversableSyntax
+  implicit def PtraversableOps[T[_], A](ta: T[A])(implicit T: Traversable[T]): TraversableSyntax.Ops[T, A] =
+    new TraversableSyntax.Ops(ta)
+
+  // Core Data
+  // =========
+
+  type \/[L, R] = data.Disjunction.\/[L, R]
+  type Maybe[A] = data.Maybe[A]
+}
+
+object Prelude extends Prelude
