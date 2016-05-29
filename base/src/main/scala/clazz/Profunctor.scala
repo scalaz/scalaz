64c20d4aafbf91aee64b724bb7af316884fc71f4
Alois Cochard <alois.cochard@gmail.com>
2016-05-30 00:46:41 +0200

    Add `clazz.Profunctor`.

---
+++ base/src/main/scala/clazz/Profunctor.scala
@@ -0,0 +1,16 @@
+package scalaz
+package clazz
+
+import data.Disjunction._
+
+abstract class Profunctor[F[_, _]] {
+  def lmap[A, B, C](fab: F[A, B])(ca: C => A): F[C, B] = dimap[A, B, C, B](fab)(ca)(identity)
+  def rmap[A, B, C](fab: F[A, B])(bc: B => C): F[A, C] = dimap[A, B, A, C](fab)(identity)(bc)
+  def dimap[A, B, C, D](fab: F[A, B])(ca: C => A)(bd: B => D): F[C, D] = rmap(lmap(fab)(ca))(bd)
+}
+
+object Profunctor extends ProfunctorInstances {
+  def apply[F[_, _]](implicit F: Profunctor[F]): Profunctor[F] = F
+
+  object syntax extends ProfunctorSyntax
+}
---
+++ base/src/main/scala/clazz/ProfunctorInstances.scala
@@ -0,0 +1,10 @@
+package scalaz
+package clazz
+
+trait ProfunctorInstances { instances =>
+  implicit val function: Profunctor[Function] = new Profunctor[Function] {
+    override def lmap[A, B, C](ab: A => B)(ca: C => A): C => B = c => ab(ca(c))
+    override def rmap[A, B, C](ab: A => B)(bc: B => C): A => C = a => bc(ab(a))
+    override def dimap[A, B, C, D](ab: A => B)(ca: C => A)(bd: B => D): C => D = c => bd(ab(ca(c)))
+  }
+}
