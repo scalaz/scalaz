9ae7452945356be093100cabf6bf7a72f7ca7360
Stephen Compall <scompall@nocandysw.com>
2016-05-28 23:51:20 -0600

    CorecursiveList
    (cherry picked from commit 5fbbe39d7d70c9eb3bfde10b184d9163d86f0d61)

--- scalacheck-binding/src/main/scala/scalaz/scalacheck/ScalazArbitrary.scala
+++ scalacheck-binding/src/main/scala/scalaz/scalacheck/ScalazArbitrary.scala
@@ -31,6 +31,9 @@ object ScalazArbitrary {
   implicit def EphemeralStreamArbitrary[A : Arbitrary] =
     Functor[Arbitrary].map(arb[Stream[A]])(EphemeralStream.fromStream[A](_))
 
+  implicit def CorecursiveListArbitrary[A : Arbitrary]: Arbitrary[CorecursiveList[A]] =
+    Functor[Arbitrary].map(arb[Stream[A]])(CorecursiveList.fromStream)
+
   implicit def ImmutableArrayArbitrary[A : Arbitrary : ClassManifest] =
     Functor[Arbitrary].map(arb[Array[A]])(ImmutableArray.fromArray[A](_))
 
