fdecaf86e1fd5d52d2d8b2bf45d9b70dd001d090
Chris Marshall <chris.marshall@gsacapital.com>
2016-06-02 16:59:08 +0100

    Removed unnecessary implicit

--- tests/src/test/scala/scalaz/TheseTTest.scala
+++ tests/src/test/scala/scalaz/TheseTTest.scala
@@ -11,14 +11,6 @@ object TheseTTest extends SpecLite {
   type TheseTListInt[A] = TheseT[List, Int, A]
   type TheseTOptionInt[A] = TheseT[Option, Int, A]
 
-  implicit def TheseTListIntEqual[A: Equal]: Equal[TheseTListInt[A]] = new Equal[TheseTListInt[A]] {
-    ////
-    import scalaz.syntax.equal._
-    import scalaz.std.list._
-    override def equal(a1: TheseTListInt[A], a2: TheseTListInt[A]) =
-      a1.run === a2.run
-  }
-
   checkAll(monad.laws[TheseTListInt])
   checkAll(traverse.laws[TheseTListInt])
   checkAll(bitraverse.laws[TheseTList])
