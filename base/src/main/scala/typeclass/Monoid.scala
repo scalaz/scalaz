64f84d0061787f7046a6d467df5f55e3b4526e20
Julien Truffaut <truffaut.julien@gmail.com>
2016-06-07 14:29:28 +0100

    add Const, Semigroup and Monoid

---
+++ base/src/main/scala/typeclass/Monoid.scala
@@ -0,0 +1,12 @@
+package scalaz
+package typeclass
+
+trait Monoid[A] {
+  def semigroup: Semigroup[A]
+
+  def empty: A
+}
+
+object Monoid {
+  def apply[A](implicit A: Monoid[A]): Monoid[A] = A
+}
