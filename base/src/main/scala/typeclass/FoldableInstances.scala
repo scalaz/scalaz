cb262ee376505fadaa6ceaf14752c4c1dfab0cb3
Vincent Marquez <vincent.marquez@gmail.com>
2016-06-16 20:34:38 -0700

    Traversable consistency so it uses the TraversableClass.

--- base/src/main/scala/typeclass/FoldableInstances.scala
+++ base/src/main/scala/typeclass/FoldableInstances.scala
@@ -2,10 +2,5 @@ package scalaz
 package typeclass
 
 trait FoldableInstances {
-  implicit val list: Foldable[List] = new Foldable[List] {
-    override def foldLeft[A, B](fa: List[A], z: B)(f: (B, A) => B): B = fa.foldLeft(z)(f)
-    override def foldRight[A, B](fa: List[A], z: => B)(f: (A, => B) => B): B = fa.foldRight(z) { (a, b) => f(a, b) }
-    override def toList[A](xs: List[A]): List[A] = xs
-  }
-}
 
+}
--- base/src/main/scala/typeclass/TraversableInstances.scala
+++ base/src/main/scala/typeclass/TraversableInstances.scala
@@ -6,13 +6,20 @@ import Apply.syntax._
 import Applicative.syntax._
 
 trait TraversableInstances {
-  implicit val list: Traversable[List] = new Traversable[List] {
-    override val functor = Monad.list.applicative.apply.functor
-    override val foldable = Foldable.list
-
+  implicit val list: Traversable[List] = new TraversableClass[List] {
+    
     override def traverse[F[_], A, B](ta: List[A])(f: A => F[B])(implicit F: Applicative[F]): F[List[B]] =
       ta.foldLeft[F[List[B]]](List.empty[B].pure[F]) { (flb, a) => flb.ap(f(a).map(b => (xs: List[B]) => b::xs)) }
+    
     override def sequence[F[_], A](ta: List[F[A]])(implicit F: Applicative[F]): F[List[A]] =
       traverse[F, F[A], A](ta)(identity)
+  
+    override def foldLeft[A, B](fa: List[A], z: B)(f: (B, A) => B): B = fa.foldLeft(z)(f)
+    
+    override def foldRight[A, B](fa: List[A], z: => B)(f: (A, => B) => B): B = fa.foldRight(z) { (a, b) => f(a, b) }
+    
+    override def toList[A](xs: List[A]): List[A] = xs
+
+    override def map[A, B](fa: List[A])(f: A => B) = fa.map(f)
   }
 }
