da68599e579903ead0d687ee5bfd982b1c848a96
Alois Cochard <alois.cochard@gmail.com>
2016-06-20 09:41:17 +0200

    Remove variance annotation on `Disjunction`.

--- base/src/main/scala/data/DisjunctionFunctions.scala
+++ base/src/main/scala/data/DisjunctionFunctions.scala
@@ -4,11 +4,8 @@ package data
 import Disjunction.{\/, \/-, -\/}
 
 trait DisjunctionFunctions {
-  @inline def -\/[L](value: L): Disjunction[L, Nothing] = -\/(value)
-  @inline def \/-[R](value: R): Disjunction[Nothing, R] = \/-(value)
-
-  @inline def left[L](value: L): Disjunction[L, Nothing] = -\/(value)
-  @inline def right[R](value: R): Disjunction[Nothing, R] = \/-(value)
+  @inline def left[L, R](value: L): Disjunction[L, R] = -\/(value)
+  @inline def right[L, R](value: R): Disjunction[L, R] = \/-(value)
 
   def either[A, B, C](ac: A => C)(bc: B => C): A \/ B => C = _ match {
     case -\/(l)  => ac(l)
