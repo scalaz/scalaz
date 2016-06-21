49c9d90af865021163b1cfcabbd2a55f3fa65a81
Alois Cochard <alois.cochard@gmail.com>
2016-06-21 21:27:56 +0200

    Replace `Functor` syntax with macro based implementation.

--- base/src/main/scala/typeclass/FunctorSyntax.scala
+++ base/src/main/scala/typeclass/FunctorSyntax.scala
@@ -2,6 +2,7 @@ package scalaz
 package typeclass
 
 import scala.language.implicitConversions
+import scala.language.experimental.macros
 
 trait FunctorSyntax {
   def map[F[_], A, B](fa: F[A])(f: A => B)(implicit F: Functor[F]): F[B] = F.map(fa)(f)
@@ -12,7 +13,7 @@ trait FunctorSyntax {
 
 object FunctorSyntax {
   class Ops[F[_], A](self: F[A])(implicit F: Functor[F]) {
-    def map[B](f: A => B): F[B] = F.map[A, B](self)(f)
+    def map[B](f: A => B): F[B] = macro meta.Ops._f[A => B, F[B]]
     def void: F[Unit] = F.map[A, Unit](self)(_ => ())
   }
 }
