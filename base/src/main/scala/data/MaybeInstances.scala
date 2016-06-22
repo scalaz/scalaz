7dd15c666c92f3ebd0dacc22dfe54475ad6c12b4
Alois Cochard <alois.cochard@gmail.com>
2016-06-23 00:22:54 +0200

    Implement `TraversableClass` on `MaybeInstances`.

--- base/src/main/scala/data/MaybeInstances.scala
+++ base/src/main/scala/data/MaybeInstances.scala
@@ -2,21 +2,36 @@ package scalaz
 package data
 
 import typeclass._
-import Maybe.{Just, Empty}
+import Applicative.Class._
 
-trait MaybeInstances extends MonadClass[Maybe] {
+import Maybe.{just, empty}
+
+// TODO Rework lazyness
+trait MaybeInstances extends MonadClass[Maybe] with TraversableClass[Maybe] {
 
   implicit def monadInstance: Monad[Maybe] = this
 
   override def ap[A, B](ma: Maybe[A])(mf: Maybe[A => B]): Maybe[B] =
-    ma.fold(a => map[A => B, B](mf)(f => f(a)), Empty())
+    ma.fold(a => map[A => B, B](mf)(f => f(a)), empty)
 
   override def flatMap[A, B](ma: Maybe[A])(f: A => Maybe[B]): Maybe[B] =
-    ma.fold(a => f(a), Empty())
+    ma.fold(a => f(a), empty)
 
   override def map[A, B](ma: Maybe[A])(f: A => B): Maybe[B] =
-    ma.fold(a => Just(f(a)), Empty())
+    ma.fold(a => just(f(a)), empty)
 
   override def pure[A](a: A): Maybe[A] =
-    Just(a)
+    just(a)
+
+  override def traverse[F[_], A, B](ma: Maybe[A])(f: A => F[B])(implicit F: Applicative[F]): F[Maybe[B]] =
+    ma.fold(a => f(a).map(just(_)), empty.pure[F])
+
+  override def sequence[F[_], A](ma: Maybe[F[A]])(implicit F: Applicative[F]): F[Maybe[A]] =
+    ma.fold(fa => fa.map(just(_)), empty.pure[F])
+
+  override def foldLeft[A, B](ma: Maybe[A], b: B)(f: (B, A) => B): B = ma.fold(a => f(b, a), b)
+
+  override def foldRight[A, B](ma: Maybe[A], b: => B)(f: (A, => B) => B): B = ma.fold(a => f(a, b), b)
+
+  override def toList[A](ma: Maybe[A]): List[A] = ma.fold(List(_), Nil)
 }
