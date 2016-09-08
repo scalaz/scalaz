36e466587dd4b2c888754ec5690734af956ba8d8
xuwei-k <6b656e6a69@gmail.com>
2016-09-08 10:42:32 +0900

    remove unstable test

--- tests/src/test/scala/scalaz/NeedTest.scala
+++ tests/src/test/scala/scalaz/NeedTest.scala
@@ -26,24 +26,4 @@ object NeedTest extends SpecLite {
   checkAll("Need", zip.laws[Need])
   checkAll("Need", align.laws[Need])
 
-  "Need" should {
-    "clear the Function0 reference" in {
-      @volatile var flag = false
-      val method = Need.getClass.getMethod("apply", classOf[Function0[_]])
-      val need = method.invoke(
-        Need,
-        new runtime.AbstractFunction0[String]{
-          override def finalize = {flag = true}
-          override def apply = ""
-        }
-      ).asInstanceOf[Need[String]]
-
-      flag must_== false
-      print(need.value)
-      System.gc()
-      System.runFinalization()
-      flag must_== true
-    }
-  }
-
 }
