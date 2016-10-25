3ac3811d105c1c78756c2e0c05e93013b56eb861
Edmund Noble <edmundnoble@gmail.com>
2016-10-24 21:38:54 -0400

    Make CofreeBind sufficiently lazy

--- core/src/main/scala/scalaz/Cofree.scala
+++ core/src/main/scala/scalaz/Cofree.scala
@@ -222,7 +222,7 @@ private trait CofreeBind[F[_]] extends Bind[Cofree[F, ?]] with CofreeComonad[F]{
 
   def bind[A, B](fa: Cofree[F, A])(f: A => Cofree[F, B]): Cofree[F, B] = {
     val c = f(fa.head)
-    Cofree.applyT(c.head, c.t.map(ct => G.plus(c.tail, F.map(fa.tail)(bind(_)(f))) ) )
+    Cofree.applyT(c.head, c.t.map(ct => G.plus(ct, F.map(fa.tail)(bind(_)(f))) ) )
   }
 }
 
