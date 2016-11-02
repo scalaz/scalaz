3696799b6836c7ace6e8134f8ff2ff94cc6270c4
xuwei-k <6b656e6a69@gmail.com>
2016-11-02 14:37:53 +0900

    Scala 2.12.0

--- project/ScalazBuild.scala
+++ project/ScalazBuild.scala
@@ -4,7 +4,7 @@ import Keys._
 import pl.project13.scala.sbt.JmhPlugin
 
 object Scalaz extends Build {
-  val testDeps = Seq("org.scalacheck" %% "scalacheck" % "1.13.0" % "test")
+  val testDeps = Seq("org.scalacheck" %% "scalacheck" % "1.13.4" % "test")
 
   def module(prjName: String) = Project(
     id = prjName,
@@ -12,12 +12,28 @@ object Scalaz extends Build {
     name := s"scalaz-$prjName",
     scalaVersion := "2.11.8",
     scalacOptions ++= Seq("-feature","-deprecation", "-Xlint", "-language:higherKinds",
-                          "-Ybackend:GenBCode", "-Ydelambdafy:method", "-target:jvm-1.8"),
+                          "-Ydelambdafy:method", "-target:jvm-1.8"),
+    scalacOptions ++= {
+      CrossVersion.partialVersion(scalaVersion.value) match {
+        case Some((2, 11)) =>
+          Seq("-Ybackend:GenBCode")
+        case _ =>
+          Seq("-Ypartial-unification")
+      }
+    },
     libraryDependencies ++= testDeps ++ Seq(
-      "org.scala-lang.modules" %% "scala-java8-compat" % "0.7.0",
-      compilerPlugin("org.spire-math" %% "kind-projector" % "0.7.1"),
-      compilerPlugin("com.milessabin" % "si2712fix-plugin" % "1.1.0" cross CrossVersion.full)
-    )
+      compilerPlugin("org.spire-math" %% "kind-projector" % "0.9.3")
+    ) ++ {
+      CrossVersion.partialVersion(scalaVersion.value) match {
+        case Some((2, 11)) =>
+          Seq(
+            "org.scala-lang.modules" %% "scala-java8-compat" % "0.7.0",
+            compilerPlugin("com.milessabin" % "si2712fix-plugin" % "1.1.0" cross CrossVersion.full)
+          )
+        case _ =>
+          Nil
+      }
+    }
   )
 
   lazy val root = Project(
@@ -39,8 +55,7 @@ object Scalaz extends Build {
       libraryDependencies ++=
         Seq ( "org.scala-lang" % "scala-reflect" % scalaVersion.value
             , "org.scala-lang" % "scala-compiler" % scalaVersion.value % "provided"
-            , "org.scalaz" %% "scalaz-core" % "7.2.1"
-            , "org.typelevel" %% "cats" % "0.5.0" )
+            , "org.scalaz" %% "scalaz-core" % "7.2.7")
     )
 
   lazy val meta         = module("meta")
