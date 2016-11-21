a6a9f7b0d02fa8ade162dba8f86c4795c553feb5
xuwei-k <6b656e6a69@gmail.com>
2016-11-22 03:26:26 +0900

    use value class

--- core/src/main/scala/scalaz/syntax/std/BooleanOps.scala
+++ core/src/main/scala/scalaz/syntax/std/BooleanOps.scala
@@ -6,7 +6,7 @@ import scalaz.std.{boolean => b}
 import scalaz.Tags.{ Conjunction, Disjunction }
 
 
-final class BooleanOps(self: Boolean) {
+final class BooleanOps(val self: Boolean) extends AnyVal {
 
   final def conjunction: Boolean @@ Conjunction = Conjunction(self)
 
@@ -235,15 +235,6 @@ final class BooleanOps(self: Boolean) {
    */
   final def fold[A](t: => A, f: => A): A = b.fold(self, t, f)
 
-  final class Conditional[X](t: => X) {
-    def |(f: => X) = if (self) t else f
-  }
-
-  /**
-   * Conditional operator that returns the first argument if this is `true`, the second argument otherwise.
-   */
-  final def ?[X](t: => X): Conditional[X] = new Conditional(t)
-
   /**
    * Returns the given argument in `Some` if this is `true`, `None` otherwise.
    */
@@ -253,17 +244,6 @@ final class BooleanOps(self: Boolean) {
    * Returns the given argument in `LazySome` if this is `true`, `LazyNone` otherwise.
    */
   final def lazyOption[A](a: => A): LazyOption[A] = LazyOption.condLazyOption(self, a)
-
-  final class ConditionalEither[A](a: => A) {
-    def or[B](b: => B) =
-      if (self) \/-(a) else -\/(b)
-  }
-
-  /**
-   * Returns the first argument in `\/-` if this is `true`, otherwise the second argument in
-   * `-\/`.
-   */
-  final def either[A, B](a: => A): ConditionalEither[A] = new ConditionalEither(a)
 
   /**
    * Returns the given argument if this is `true`, otherwise, the zero element for the type of the given
@@ -276,6 +256,18 @@ final class BooleanOps(self: Boolean) {
    * argument.
    */
   final def !?[A](a: => A)(implicit z: Monoid[A]): A = b.zeroOrValue(self)(a)
+}
+
+final class BooleanOps2(self: Boolean) {
+
+  final class Conditional[X] private[BooleanOps2] (t: => X) {
+    def |(f: => X) = if (self) t else f
+  }
+
+  /**
+   * Conditional operator that returns the first argument if this is `true`, the second argument otherwise.
+   */
+  final def ?[X](t: => X): Conditional[X] = new Conditional(t)
 
   sealed abstract class GuardPrevent[M[_]] {
     def apply[A](a: => A)(implicit M: Applicative[M], M0: PlusEmpty[M]): M[A]
@@ -288,8 +280,20 @@ final class BooleanOps(self: Boolean) {
   final def prevent[M[_]] = new GuardPrevent[M] {
     def apply[A](a: => A)(implicit M: Applicative[M], M0: PlusEmpty[M]) = b.emptyOrPure[M, A](self)(a)
   }
+
+  final class ConditionalEither[A] private[BooleanOps2] (a: => A) {
+    def or[B](b: => B): B \/ A =
+      if (self) \/-(a) else -\/(b)
+  }
+
+  /**
+   * Returns the first argument in `\/-` if this is `true`, otherwise the second argument in
+   * `-\/`.
+   */
+  final def either[A, B](a: => A): ConditionalEither[A] = new ConditionalEither(a)
 }
 
 trait ToBooleanOps {
   implicit def ToBooleanOpsFromBoolean(a: Boolean): BooleanOps = new BooleanOps(a)
+  implicit def ToBooleanOps2FromBoolean(a: Boolean): BooleanOps2 = new BooleanOps2(a)
 }
