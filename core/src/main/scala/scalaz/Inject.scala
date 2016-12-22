146bf5e0914f1db2d9e177cc03b0bf616138b1e7
Greg Pfeil <greg@technomadic.org>
2016-12-22 14:32:59 -0500

    Add `Inject.unapply`.
    This makes it possible to pattern-match on unknown Coproducts, given
    appropriate `Inject` constraints.
    
    E.g.
    ```scala
    def foo[F[_], A](fa: F[A])(implicit In: SomeComponent :<: F) = fa match {
      case In(SomeComponent(_)) => true
      case _ => false
    }
    ```
    
    (cherry picked from commit 43f69e62c2ac7ee88c2dcba5be653b7fc9f4353f)

--- core/src/main/scala/scalaz/Inject.scala
+++ core/src/main/scala/scalaz/Inject.scala
@@ -11,6 +11,7 @@ import Free.Suspend
  */
 sealed abstract class Inject[F[_], G[_]] extends (F ~> G) {
   def apply[A](fa: F[A]): G[A] = inj(fa)
+  def unapply[A](ga: G[A]): Option[F[A]] = prj(ga)
   def inj[A](fa: F[A]): G[A]
   def prj[A](ga: G[A]): Option[F[A]]
 }
