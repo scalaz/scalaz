f9c7dffebb6a3b666160adb1aefcdcce435f9e5a
Stephen Compall <scompall@nocandysw.com>
2017-03-30 22:43:25 -0400

    Scala 2.12 has right-biased either; reference this in \/ doc.
    (cherry picked from commit 6989a93a7dd3239f937b2e3a6abdb0d462cea3d2)

--- core/src/main/scala-2.12+/scalaz/Foldable1Parent.scala
+++ core/src/main/scala-2.12+/scalaz/Foldable1Parent.scala
@@ -9,5 +9,8 @@ trait Foldable1Parent[F[_]] { self: Foldable1[F] =>
   def msuml1[G[_]: Plus, A](fa: F[G[A]]): G[A] =
     ToFoldable1Ops(fa)(self).msuml1
 
+  def toNel[A](fa: F[A]): NonEmptyList[A] =
+    foldMapRight1(fa)(NonEmptyList.nel(_, Nil))(_ <:: _)
+
   ////
 }
--- core/src/main/scala-2.12+/scalaz/Traverse1Parent.scala
+++ core/src/main/scala-2.12+/scalaz/Traverse1Parent.scala
@@ -8,5 +8,8 @@ trait Traverse1Parent[F[_]] { self: Traverse1[F] =>
   def sequence1U[GA](fga: F[GA])(implicit G: Unapply[Apply, GA]): G.M[F[G.A]] =
     sequence1(G.leibniz.subst(fga))(G.TC)
 
+  def traverse1U[A, GB](fa: F[A])(f: A => GB)(implicit G: Unapply[Apply, GB]): G.M[F[G.A]] =
+    traverse1(fa)(G.leibniz.onF(f))(G.TC)
+
   ////
 }
