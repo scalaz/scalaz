780fc0faaf7939296882ded243b93d5d8e8e6a28
xuwei-k <6b656e6a69@gmail.com>
2017-04-01 15:08:07 +0900

    fix scaladoc

--- core/src/main/scala/scalaz/syntax/std/BooleanOps.scala
+++ core/src/main/scala/scalaz/syntax/std/BooleanOps.scala
@@ -223,7 +223,7 @@ final class BooleanOps(val self: Boolean) extends AnyVal {
   final def unlessMU[MA](f: => MA)(implicit M: Unapply[Applicative, MA]): M.M[Unit] = b.unlessMU(self)(f)
 
   /**
-   * Returns the given argument if `cond` is true`, otherwise, unit lifted into M.
+   * Returns the given argument if `cond` is `true`, otherwise, unit lifted into M.
    */
   final def whenM[M[_]: Applicative, A](f: => M[A]): M[Unit] = b.whenM(self)(f)
 
