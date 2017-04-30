9ade5a477453d312eaba80eda60b22cbe00f78fa
Yang Bo <atryyang@thoughtworks.com>
2017-05-01 00:38:35 +0800

    Add more stack-safe type classes

--- tests/src/test/scala/scalaz/IndexedContsTTest.scala
+++ tests/src/test/scala/scalaz/IndexedContsTTest.scala
@@ -1,6 +1,8 @@
 package scalaz
 
-import org.scalacheck.Arbitrary
+import org.scalacheck.{Arbitrary, Cogen}
+
+import scalaz.Free.Trampoline
 import scalaz.scalacheck.ScalazArbitrary._
 import scalaz.scalacheck.ScalazProperties._
 import scalaz.std.anyVal._
@@ -26,11 +28,31 @@ object IndexedContsTTest extends SpecLite {
 
   checkAll(monadPlus.laws[ContTMaybeBoolean])
 
+  private[this] implicit def trampolineArbitrary[A](implicit arbitraryA: Arbitrary[A]): Arbitrary[Trampoline[A]] = {
+    Arbitrary(arbitraryA.arbitrary.map(Trampoline.done(_)))
+  }
+
+  private[this] implicit def cogenTrampoline[A](implicit cogenA: Cogen[A]): Cogen[Trampoline[A]] = {
+    cogenA.contramap(_.run)
+  }
+
+  private[this] implicit def trampolineEqual[A](implicit equalA: Equal[A]): Equal[Trampoline[A]] = Equal.equal { (a, b) =>
+    equalA.equal(a.run, b.run)
+  }
+
+  type ContTTrampolineBoolean[A] = ContT[Trampoline, Boolean, A]
+
+  checkAll(monad.laws[ContTTrampolineBoolean])
+
   object instances {
-    def functorRight[W[_]: Functor, M[_], R, O] = Functor[IndexedContsT[W, M, R, O, ?]]
-    def functorLeft[W[_], M[_]: Functor, O, A] = Functor[IndexedContsT[W, M, ?, O, A]]
-    def contravariant[W[_]: Functor, M[_]: Functor, R, A] = Contravariant[IndexedContsT[W, M, R, ?, A]]
-    def bifunctor[W[_]: Functor, M[_]: Functor, O] = Bifunctor[IndexedContsT[W, M, ?, O, ?]]
+    def functorRight[W[_]: Functor, M[_], R, O] =
+      Functor[IndexedContsT[W, M, R, O, ?]]
+    def functorLeft[W[_], M[_]: Functor, O, A] =
+      Functor[IndexedContsT[W, M, ?, O, A]]
+    def contravariant[W[_]: Functor, M[_]: Functor, R, A] =
+      Contravariant[IndexedContsT[W, M, R, ?, A]]
+    def bifunctor[W[_]: Functor, M[_]: Functor, O] =
+      Bifunctor[IndexedContsT[W, M, ?, O, ?]]
     def bind[W[_]: Cobind, M[_], R] = Bind[ContsT[W, M, R, ?]]
     def monad[W[_]: Comonad, M[_], R] = Monad[ContsT[W, M, R, ?]]
 
@@ -38,5 +60,19 @@ object IndexedContsTTest extends SpecLite {
     def functor[W[_]: Comonad, M[_], R] = Functor[ContsT[W, M, R, ?]]
     def functor[W[_]: Cobind, M[_], R] = Functor[ContsT[W, M, R, ?]]
     def bind[W[_]: Comonad, M[_], R] = Bind[ContsT[W, M, R, ?]]
+
+    // stack safe type classes
+    def stackSafeFunctor[W[_]: Functor, S[_], R] =
+      Functor[ContsT[W, Free[S, ?], R, ?]]
+    def stackSafeApplicative[W[_]: Comonad, S[_], R] =
+      Applicative[ContsT[W, Free[S, ?], R, ?]]
+    def stackSafeBind[W[_]: Cobind, S[_], R] =
+      Bind[ContsT[W, Free[S, ?], R, ?]]
+    def stackSafeBindRec[W[_]: Cobind, S[_], R] =
+      BindRec[ContsT[W, Free[S, ?], R, ?]]
+    def stackSafeBindRec[W[_]: Comonad, S[_], R] =
+      BindRec[ContsT[W, Free[S, ?], R, ?]]
+    def stackSafeMonad[W[_]: Comonad, S[_], R] =
+      Monad[ContsT[W, Free[S, ?], R, ?]]
   }
 }
