a6a1f0a2a9d4a7e4a157183cf5e6b6ea6d4433b0
kenji yoshida <6b656e6a69@gmail.com>
2017-05-14 20:25:33 +0900

    add some Band instances
    (cherry picked from commit 18c09e36c81332e3c54b5336f1149a990fce2727)

--- core/src/main/scala/scalaz/Semigroup.scala
+++ core/src/main/scala/scalaz/Semigroup.scala
@@ -96,30 +96,30 @@ object Semigroup {
     }
 
   /** A purely left-biased semigroup. */
-  def firstSemigroup[A] =
-    new Semigroup[A] {
+  def firstSemigroup[A]: Semigroup[A] =
+    new Band[A] {
       def append(f1: A, f2: => A): A = f1
     }
 
   @inline implicit def firstTaggedSemigroup[A]: Semigroup[A @@ Tags.FirstVal] = firstSemigroup[A @@ Tags.FirstVal]
 
   /** A purely right-biased semigroup. */
-  def lastSemigroup[A] =
-    new Semigroup[A] {
+  def lastSemigroup[A]: Semigroup[A] =
+    new Band[A] {
       def append(f1: A, f2: => A): A = f2
     }
 
   @inline implicit def lastTaggedSemigroup[A]: Semigroup[A @@ Tags.LastVal] = lastSemigroup[A @@ Tags.LastVal]
 
   def minSemigroup[A](implicit o: Order[A]): Semigroup[A @@ Tags.MinVal] =
-    new Semigroup[A @@ Tags.MinVal] {
+    new Band[A @@ Tags.MinVal] {
       def append(f1: A @@ Tags.MinVal, f2: => A @@ Tags.MinVal) = Tags.MinVal(o.min(Tag.unwrap(f1), Tag.unwrap(f2)))
     }
 
   @inline implicit def minTaggedSemigroup[A : Order]: Semigroup[A @@ Tags.MinVal] = minSemigroup[A]
 
   def maxSemigroup[A](implicit o: Order[A]): Semigroup[A @@ Tags.MaxVal] =
-    new Semigroup[A @@ Tags.MaxVal] {
+    new Band[A @@ Tags.MaxVal] {
       def append(f1: A @@ Tags.MaxVal, f2: => A @@ Tags.MaxVal) = Tags.MaxVal(o.max(Tag.unwrap(f1), Tag.unwrap(f2)))
     }
 
