759ac2f17eca7c335561520ae9c9841fa74cfff3
xuwei-k <6b656e6a69@gmail.com>
2017-05-31 01:22:56 +0900

    kind-projector 0.9.4
    (cherry picked from commit c66f4234a4d813c09fdcc2f62e1f188015c870c1)

--- project/build.scala
+++ project/build.scala
@@ -285,7 +285,7 @@ object build {
         Nil
     }),
     resolvers += Resolver.sonatypeRepo("releases"),
-    kindProjectorVersion := "0.9.3",
+    kindProjectorVersion := "0.9.4",
     libraryDependencies += compilerPlugin("org.spire-math" % "kind-projector" % kindProjectorVersion.value cross CrossVersion.binary)
   ) ++ osgiSettings ++ Seq[Sett](
     OsgiKeys.additionalHeaders := Map("-removeheaders" -> "Include-Resource,Private-Package")
