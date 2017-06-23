23ddf0ced80b53c14903718d1ac1fc8431a1563e
xuwei-k <6b656e6a69@gmail.com>
2017-06-23 10:08:22 +0900

    remove scala-native workaround

--- project/build.scala
+++ project/build.scala
@@ -265,31 +265,7 @@ object build {
     }
   )
 
-  // workaround for https://github.com/scala-native/scala-native/issues/562
-  private[this] def scalaNativeDiscoverOrDummy(binaryName: String, binaryVersions: Seq[(String, String)]): File = {
-    // https://github.com/scala-native/scala-native/blob/v0.1.0/sbt-scala-native/src/main/scala/scala/scalanative/sbtplugin/ScalaNativePluginInternal.scala#L59
-    // https://github.com/scala-native/scala-native/blob/v0.1.0/sbt-scala-native/src/main/scala/scala/scalanative/sbtplugin/ScalaNativePluginInternal.scala#L284-L289
-    try {
-      val clazz = scalanative.sbtplugin.ScalaNativePluginInternal.getClass
-      val instance = clazz.getField(scala.reflect.NameTransformer.MODULE_INSTANCE_NAME).get(null)
-      val method = clazz.getMethods.find(_.getName contains "discover").getOrElse(sys.error("could not found the discover method"))
-      method.invoke(instance, binaryName, binaryVersions).asInstanceOf[File]
-    } catch {
-      case e: Throwable =>
-        val e0 = e match {
-          case _: java.lang.reflect.InvocationTargetException if e.getCause != null =>
-            e.getCause
-          case _ =>
-            e
-        }
-        scala.Console.err.println(e0)
-        file("dummy")
-    }
-  }
-
   val nativeSettings = Seq(
-    nativeClang := scalaNativeDiscoverOrDummy("clang", Seq(("3", "8"), ("3", "7"))),
-    nativeClangPP := scalaNativeDiscoverOrDummy("clang++", Seq(("3", "8"), ("3", "7"))),
     scalacOptions --= Scala211_jvm_and_js_options,
     scalaVersion := Scala211,
     crossScalaVersions := Scala211 :: Nil
