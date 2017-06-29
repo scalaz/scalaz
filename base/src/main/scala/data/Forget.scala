b13fae89671477619409eb07810b7e3bed8ad524
vincent <vincent.marques@one.verizon.com>
2017-06-29 14:56:02 -0700

    removing files in the wrong place

--- base/src/main/scala/data/Forget.scala
+++
@@ -1,9 +0,0 @@
-package scalaz
-package data
-import typeclass._
-
-final case class Forget[A, B, C](forget: B => A){
-  def retag[D]: Forget[A, B, D] = this.asInstanceOf[Forget[A, B, D]]
-}
-
-object Forget extends ForgetInstances
