7d5a4f2f9a2682b853857ab10496e75424ba4bef
Brian McKenna <bmckenna@atlassian.com>
2017-07-24 19:12:44 +1000

    Fix compilation error in ApTest

--- tests/src/test/scala/scalaz/ApTest.scala
+++ tests/src/test/scala/scalaz/ApTest.scala
@@ -2,7 +2,7 @@ package scalaz
 
 object ApTest extends SpecLite {
   object instances {
-    def semigroup[A: Monoid] = Semigroup[Ap[List, A]]
+    def semigroup[A: Monoid] = Semigroup[Ap[IList, A]]
     def monoid[A: Monoid] = Monoid[Ap[Maybe, A]]
   }
 }
