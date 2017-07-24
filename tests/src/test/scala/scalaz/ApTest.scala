89b2050d891e0fc2bb7bb29739616a76292ed704
xuwei-k <6b656e6a69@gmail.com>
2017-07-24 23:48:44 +0900

    add more tests for scalaz.Ap
    (cherry picked from commit 3d8593ec764ee3912fa42bf0fedcac6badcb0ae8)

--- tests/src/test/scala/scalaz/ApTest.scala
+++ tests/src/test/scala/scalaz/ApTest.scala
@@ -1,6 +1,16 @@
 package scalaz
 
+import scalaz.scalacheck.ScalazArbitrary._
+import scalaz.scalacheck.ScalazProperties._
+import scalaz.std.AllInstances._
+
 object ApTest extends SpecLite {
+
+  checkAll(equal.laws[Ap[IList, Int]])
+
+  checkAll("Ap[IList, Int]", monoid.laws[Ap[IList, Int]])
+  checkAll("Ap[Maybe, Int]", monoid.laws[Ap[Maybe, Int]])
+
   object instances {
     def semigroup[A: Monoid] = Semigroup[Ap[IList, A]]
     def monoid[A: Monoid] = Monoid[Ap[Maybe, A]]
