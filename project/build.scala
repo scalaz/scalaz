bca6dc9cc48fa11387b6589b261d81af55f382a8
xuwei-k <6b656e6a69@gmail.com>
2017-08-13 12:42:30 +0900

    Scala 2.12.3

--- project/build.scala
+++ project/build.scala
@@ -109,6 +109,7 @@ object build {
   }
 
   private def Scala211 = "2.11.11"
+  private def Scala212 = "2.12.3"
 
   private val SetScala211 = releaseStepCommand("++" + Scala211)
 
@@ -126,8 +127,8 @@ object build {
     mappings in (Compile, packageSrc) ++= (managedSources in Compile).value.map{ f =>
       (f, f.relativeTo((sourceManaged in Compile).value).get.getPath)
     },
-    scalaVersion := "2.12.2",
-    crossScalaVersions := Seq("2.10.6", Scala211, "2.12.2", "2.13.0-M1"),
+    scalaVersion := Scala212,
+    crossScalaVersions := Seq("2.10.6", Scala211, Scala212, "2.13.0-M1"),
     resolvers ++= (if (scalaVersion.value.endsWith("-SNAPSHOT")) List(Opts.resolver.sonatypeSnapshots) else Nil),
     fullResolvers ~= {_.filterNot(_.name == "jcenter")}, // https://github.com/sbt/sbt/issues/2217
     scalaCheckVersion_1_12 := {
