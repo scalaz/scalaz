a381900972aa87326dabb94b754dda5ae386b383
杨博 (Yang Bo) <pop.atry@gmail.com>
2017-08-16 13:08:43 +0800

    Avoid conflicting symbols

--- core/src/main/scala/scalaz/IndexedContsT.scala
+++ core/src/main/scala/scalaz/IndexedContsT.scala
@@ -57,10 +57,7 @@ final case class IndexedContsT[W[_], M[_], R, O, A] private(_run: W[A => M[O]] =
     }
 }
 
-object IndexedContsT extends IndexedContsTInstances with IndexedContsTFunctions {
-  def apply[W[_], M[_], R, O, A](f: W[A => M[O]] => M[R]): IndexedContsT[W, M, R, O, A] =
-    new IndexedContsT[W, M, R, O, A](f)
-}
+object IndexedContsT extends IndexedContsTInstances with IndexedContsTFunctions
 
 trait IndexedContsTFunctions {
   def point[W[_], M[_], R, A](a: => A)(implicit W: Comonad[W]): ContsT[W, M, R, A] =
