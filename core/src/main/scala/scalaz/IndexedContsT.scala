e888fe46e9f86483578483df33170d6528260640
杨博 (Yang Bo) <pop.atry@gmail.com>
2017-08-17 00:03:32 -0500

    Make IndexedContsT a case class
    (cherry picked from commit 9e57d5c6ce4f966386f1a8aadf06cc8e038fa654)

--- core/src/main/scala/scalaz/IndexedContsT.scala
+++ core/src/main/scala/scalaz/IndexedContsT.scala
@@ -1,6 +1,6 @@
 package scalaz
 
-final class IndexedContsT[W[_], M[_], R, O, A] private(_run: W[A => M[O]] => M[R]) {
+final case class IndexedContsT[W[_], M[_], R, O, A] (_run: W[A => M[O]] => M[R]) {
   def run(wamo: W[A => M[O]]): M[R] = _run(wamo)
 
   def apply(wamo: W[A => M[O]]): M[R] = run(wamo)
@@ -43,7 +43,6 @@ final class IndexedContsT[W[_], M[_], R, O, A] private(_run: W[A => M[O]] => M[R
 }
 
 object IndexedContsT extends IndexedContsTInstances with IndexedContsTFunctions {
-  def apply[W[_], M[_], R, O, A](f: W[A => M[O]] => M[R]): IndexedContsT[W, M, R, O, A] = new IndexedContsT[W, M, R, O, A](f)
 }
 
 trait IndexedContsTFunctions {
