be8e6a8187c57d2761ebc82fb92423c2a615b6a3
xuwei-k <6b656e6a69@gmail.com>
2017-11-12 11:03:31 +0900

    fix "Adapting argument" warnings
    (cherry picked from commit f20a68eb5bf1cea83d51583cdaed7d523464f3f7)

--- tests/src/test/scala/scalaz/concurrent/MVarTest.scala
+++ tests/src/test/scala/scalaz/concurrent/MVarTest.scala
@@ -26,7 +26,7 @@ object MVarTest extends SpecLite {
         b <- in.take
       } yield (a, b)
 
-      run.unsafePerformIO must_== ("one", "two")
+      run.unsafePerformIO must_== ("one" -> "two")
     }
   }
 }
