cc3036d9962a7ff29ec176499aaaa2f9e3df334b
Harrison Houghton <hhoughton@learningobjects.com>
2017-11-30 09:06:30 -0500

    Add macro shape for OpsA-style syntax.
    This allows for such syntax as `f.pure[F]` and (eventually)
    `err.raiseError[M]`.

--- base/shared/src/main/scala/scalaz/typeclass/ApplicativeSyntax.scala
+++ base/shared/src/main/scala/scalaz/typeclass/ApplicativeSyntax.scala
@@ -1,14 +1,17 @@
 package scalaz
 package typeclass
 
-import scala.language.implicitConversions
+import com.github.ghik.silencer.silent
+
+import language.implicitConversions
+import language.experimental.macros
 
 trait ApplicativeSyntax {
   implicit def applicativeOpsA[A](a: A): ApplicativeSyntax.OpsA[A] = new ApplicativeSyntax.OpsA(a)
 }
 
 object ApplicativeSyntax {
-  class OpsA[A](a: A) {
-    def pure[F[_]](implicit F: Applicative[F]): F[A] = F.pure(a)
+  class OpsA[A](@silent a: A) {
+    def pure[F[_]: Applicative]: F[A] = macro meta.IdOps.id_1
   }
 }
