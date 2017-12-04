2d252bfb2d179e870476c31cab5458451ea1a91c
Merlin Hughes <merlin@merlin.org>
2017-12-04 09:39:23 -0500

    sunder minimal js enum evidence from jvm evidence because class shenanigans not supported

---
+++ tests/js/src/test/scala/scalaz/std/java/EnumTest.scala
@@ -0,0 +1,25 @@
+package scalaz
+package std.java
+
+
+object EnumTest extends SpecLite {
+
+  import syntax.equal._
+  import std.java.enum._
+  import java.util.concurrent.TimeUnit
+
+  //get all ordered 2-element subsets of TimeUnit
+  @annotation.tailrec def pairs[A](l: List[A], acc: List[(A, A)] = Nil): List[(A, A)] = l match {
+    case Nil | (_ :: Nil)      => acc
+    case x :: (xxs @ (_ :: _)) => pairs(xxs, (xxs map (x -> _)) ::: acc)
+  }
+
+  "equal" in {
+    TimeUnit.values forall (tu => tu === tu)
+  }
+
+  "not equal" in {
+    pairs(TimeUnit.values.toList) forall { case (tu1, tu2) => tu1 =/= tu2 }
+  }
+
+}
