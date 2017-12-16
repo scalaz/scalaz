030d4b942d48c595ca70d2692d06cf1585058d15
kenji yoshida <6b656e6a69@gmail.com>
2017-12-16 10:58:27 +0900

    improve Arrow[PartialFunction]#compose#isDefinedAt (#1574)

--- core/src/main/scala/scalaz/std/PartialFunction.scala
+++ core/src/main/scala/scalaz/std/PartialFunction.scala
@@ -6,9 +6,22 @@ trait PartialFunctionInstances {
     def arr[A, B](f: A => B) = {
       case a => f(a)
     }
+
+    // https://github.com/scala/scala/blob/v2.12.4/src/library/scala/PartialFunction.scala#L195-L218
+    private[this] val fallback_pf: PartialFunction[Any, Any] = { case _ => fallback_pf }
+    private[this] def checkFallback[B] = fallback_pf.asInstanceOf[PartialFunction[Any, B]]
+    private[this] def fallbackOccurred[B](x: B) = (fallback_pf eq x.asInstanceOf[AnyRef])
+
     def compose[A, B, C](f: PartialFunction[B, C], g: PartialFunction[A, B]) = new PartialFunction[A, C] {
       def apply(a: A): C = f(g(a))
-      def isDefinedAt(a: A): Boolean = g.isDefinedAt(a) && f.isDefinedAt(g(a))
+      def isDefinedAt(a: A): Boolean = {
+        val x: B = g.applyOrElse(a, checkFallback[B])
+        if (!fallbackOccurred(x)) {
+          f.isDefinedAt(x)
+        } else {
+          false
+        }
+      }
     }
 
     def id[A] = {
