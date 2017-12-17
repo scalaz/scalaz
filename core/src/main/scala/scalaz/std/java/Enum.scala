53fb85fdde922da83a195bfde702b2fe6ca4992f
xuwei-k <6b656e6a69@gmail.com>
2017-12-17 11:32:46 +0900

    make an enum for a jenum
    (cherry picked from commit 68ee32f5bbc25126935d98e62cbfb81144fe7f5b)

--- core/src/main/scala/scalaz/std/java/Enum.scala
+++ core/src/main/scala/scalaz/std/java/Enum.scala
@@ -5,4 +5,18 @@ trait EnumInstances {
   implicit def enumInstance[E <: java.lang.Enum[E]]: Equal[E] = Equal.equal[E](_ eq _)
 }
 
-object enum extends EnumInstances
+object enum extends EnumInstances {
+  implicit def enumInstance2[E <: java.lang.Enum[E]](implicit E: ClassManifest[E]): Enum[E] = new Enum[E] {
+    private[this] lazy val values: Array[E] = E.erasure.asInstanceOf[Class[E]].getEnumConstants
+
+    override def equal(x: E, y: E): Boolean = x eq y
+    override def equalIsNatural: Boolean = true
+
+    override def pred(e: E): E  = values((values.indexOf(e) + values.length - 1) % values.length)
+    override def succ(e: E): E  = values((values.indexOf(e) + 1) % values.length)
+    override def min: Option[E] = values.headOption
+    override def max: Option[E] = values.lastOption
+    override def order(x: E, y: E): Ordering =
+      Ordering.fromInt(values.indexOf(x) - values.indexOf(y))
+  }
+}
