350d58b4e0f384e8c19ab33bb428bc5d1c330df9
Brian McKenna <bmckenna@atlassian.com>
2018-01-05 11:57:41 +1100

    Add Tannen
    Composes a Functor on the outside of a Bifunctor.

---
+++ core/src/main/scala/scalaz/Tannen.scala
@@ -0,0 +1,23 @@
+package scalaz
+
+/** Composes a Functor on the outside of a Bifunctor.
+  *
+  * [[https://hackage.haskell.org/package/bifunctors-5.5/docs/Data-Bifunctor-Tannen.html#t:Tannen]]
+  */
+final case class Tannen[F[_], G[_, _], A, B](f: F[G[A, B]])
+
+object Tannen extends TannenInstances
+
+sealed abstract class TannenInstances {
+  implicit def TannenFunctor[F[_], G[_, _], E](implicit F: Functor[F], G: Functor[G[E, ?]]): Functor[Tannen[F, G, E, ?]] =
+    new Functor[Tannen[F, G, E, ?]] {
+      def map[A, B](fab: Tannen[F, G, E, A])(f: A => B): Tannen[F, G, E, B] =
+        Tannen(F.map(fab.f)(G.map(_)(f)))
+    }
+
+  implicit def TannenBifunctor[F[_], G[_, _]](implicit F: Functor[F], G: Bifunctor[G]): Bifunctor[Tannen[F, G, ?, ?]] =
+    new Bifunctor[Tannen[F, G, ?, ?]] {
+      def bimap[A, B, C, D](fab: Tannen[F, G, A, B])(f: A => C, g: B => D): Tannen[F, G, C, D] =
+        Tannen(F.map(fab.f)(G.bimap(_)(f, g)))
+    }
+}
