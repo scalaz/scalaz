a0a161721074c2ba5f59a94dfb23f32da944baed
Brian McKenna <bmckenna@atlassian.com>
2018-01-05 12:05:14 +1100

    Switch TannenFunctor to use Bifunctor
    This removes the flexible context and means it has a different constraint
    compared to Functor composition.

--- core/src/main/scala/scalaz/Tannen.scala
+++ core/src/main/scala/scalaz/Tannen.scala
@@ -9,10 +9,10 @@ final case class Tannen[F[_], G[_, _], A, B](f: F[G[A, B]])
 object Tannen extends TannenInstances
 
 sealed abstract class TannenInstances {
-  implicit def TannenFunctor[F[_], G[_, _], E](implicit F: Functor[F], G: Functor[G[E, ?]]): Functor[Tannen[F, G, E, ?]] =
+  implicit def TannenFunctor[F[_], G[_, _], E](implicit F: Functor[F], G: Bifunctor[G]): Functor[Tannen[F, G, E, ?]] =
     new Functor[Tannen[F, G, E, ?]] {
       def map[A, B](fab: Tannen[F, G, E, A])(f: A => B): Tannen[F, G, E, B] =
-        Tannen(F.map(fab.f)(G.map(_)(f)))
+        Tannen(F.map(fab.f)(G.rightMap(_)(f)))
     }
 
   implicit def TannenBifunctor[F[_], G[_, _]](implicit F: Functor[F], G: Bifunctor[G]): Bifunctor[Tannen[F, G, ?, ?]] =
