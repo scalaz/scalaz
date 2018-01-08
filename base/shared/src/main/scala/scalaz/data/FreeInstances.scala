5b1d3d4737185076d0afe372fe7ecf4af344adeb
Emily Pillmore <emilyp@cakesolutions.net>
2018-01-08 01:35:07 -0500

    finalize

--- base/shared/src/main/scala/scalaz/data/FreeInstances.scala
+++ base/shared/src/main/scala/scalaz/data/FreeInstances.scala
@@ -4,7 +4,7 @@ import scalaz.typeclass._
 
 trait FreeInstances {
 
-  def monad[F[_]]: MonadClass[Free[F, ?]] = new MonadClass.Template[Free[F, ?]] {
+  implicit final def monad[F[_]]: MonadClass[Free[F, ?]] = new MonadClass.Template[Free[F, ?]] {
     override def flatMap[A, B](ma: Free[F, A])(f: A => Free[F, B]): Free[F, B] = ma flatMap f
     override def ap[A, B](fa: Free[F, A])(f: Free[F, A => B]): Free[F, B] = f flatMap fa.map
     override def map[A, B](ma: Free[F, A])(f: A => B): Free[F, B] = ma map f
