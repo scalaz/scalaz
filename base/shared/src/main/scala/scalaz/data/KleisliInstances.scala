8d49189def1aafb6a7538afd1fa59a7842ba036c
Emily Pillmore <emilyp@cakesolutions.net>
2018-01-13 17:26:57 -0500

    updates to kleisli compose

--- base/shared/src/main/scala/scalaz/data/KleisliInstances.scala
+++ base/shared/src/main/scala/scalaz/data/KleisliInstances.scala
@@ -4,7 +4,7 @@ package data
 
 trait KleisliInstances {
 
-  implicit def kleisliCompose[M[_], A, B](implicit M: Monad[M]): Compose[Kleisli[M, ?, ?]] = new Compose[Kleisli[M, ?, ?]] {
+  implicit def kleisliCompose[M[_]](implicit M: Monad[M]): Compose[Kleisli[M, ?, ?]] = new Compose[Kleisli[M, ?, ?]] {
     override def compose[A, B, C](f: Kleisli[M, B, C], g: Kleisli[M, A, B]): Kleisli[M, A, C] =
       Kleisli.wrapKleisli(a => M.bind.flatMap(Kleisli.runKleisli(g)(a))(Kleisli.runKleisli(f)))
   }
