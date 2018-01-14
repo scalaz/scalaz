90ad9701d1928d8d20948e188888d4cc007199ae
Emily Pillmore <emilyp@cakesolutions.net>
2018-01-14 02:19:10 -0500

    fixes for Freer

--- base/shared/src/main/scala/scalaz/data/Freer.scala
+++ base/shared/src/main/scala/scalaz/data/Freer.scala
@@ -1,6 +1,8 @@
 package scalaz
 package data
 
+import scala.annotation.tailrec
+
 sealed abstract class Freer[F[_], A] {
 
   def map[B](f: A => B): Freer[F, B] =
@@ -13,33 +15,27 @@ sealed abstract class Freer[F[_], A] {
       case Freer.Impure(ff, rs) => Freer.runQuiver(ff)(rs :+ Kleisli.wrapKleisli(k))
     }
 
-//  final def hoist[G[_]](α: F ~> Freer[G, ?]): Freer[G, A] =
-//    foldMap(α)(Freer.monad)
-//
-//  final def foldFreer[B](la: F[Freer[F, A]] => B)(ra: A => B)(implicit F: Functor[F]): B =
-//    runFreer.fold(la)(ra)
-//
-//  final def foldMap[M[_]](α: F ~> M)(implicit M: Monad[M]): M[A] = ???
-//    this match {
-//      case Freer.Pure(a) => M.applicative.pure(a)
-//      case Freer.LiftF(fa) => Forall.specialize[λ[α => F[α] => M[α]], A](α).apply(fa)
-//      case Freer.Impure(f, q) =>
-//        M.bind.flatMap(f foldMap α)(a => Freer.runQuiver(f)(q).apply(a).foldMap(α))
-//    }
-//
-//  @tailrec
-//  def runFreer(implicit F: Functor[F]): F[Freer[F, A]] \/ A = ???
-//    this match {
-//      case Freer.Pure(a) => \/-(a)
-//      case Freer.LiftF(fa) => -\/(F.map(fa)(Freer.pure))
-//      case Freer.Impure(f, q) =>
-//        f match {
-//          case Freer.Pure(e) => Kleisli.runKleisli(Freer.runQuiver(f)(q)).apply(e).runFreer
-//          case Freer.LiftF(fe) => -\/(F.map(fe)(Kleisli.runKleisli(Freer.runQuiver(f)(q))))
-//          case Freer.Impure(ff, qq) => ???
-//            Freer.impure(ff, qq :+ Freer.runQuiver(f)(q)).runFreer
-//        }
-//    }
+  final def hoist[G[_]](α: F ~> Freer[G, ?]): Freer[G, A] =
+    foldMap(α)(Freer.monad)
+
+  final def foldFreer[B](la: F[Freer[F, A]] => B)(ra: A => B)(implicit F: Functor[F]): B =
+    runFreer.fold(la)(ra)
+
+  final def foldMap[M[_]](α: F ~> M)(implicit M: Monad[M]): M[A] =
+    this match {
+      case Freer.Pure(a) => M.applicative.pure(a)
+      case Freer.LiftF(fa) => Forall.specialize[λ[α => F[α] => M[α]], A](α).apply(fa)
+      case Freer.Impure(f, q) =>
+        M.bind.flatMap(f foldMap α)(a => Freer.runQuiver(f)(q).foldMap(α))
+    }
+
+  @tailrec
+  final def runFreer(implicit F: Functor[F]): F[Freer[F, A]] \/ A =
+    this match {
+      case Freer.Pure(a) => \/-(a)
+      case Freer.LiftF(fa) => -\/(F.map(fa)(Freer.pure))
+      case Freer.Impure(f, q) => Freer.runQuiver(f)(q).runFreer
+    }
 }
 
 object Freer extends FreerInstances {
@@ -62,7 +58,7 @@ object Freer extends FreerInstances {
   def singleF[F[_], A, B](f: A => Freer[F, B]): Quiver[F, A, B] = singleK(Kleisli.wrapKleisli[Freer[F, ?], A, B](f))
   def singleK[F[_], A, B](r: =>:[F, A, B]): Quiver[F, A, B] = ACatenable1.lift[=>:[F, ?, ?], A, B](r)
 
-  def runQuiver[F[_], A, B](fa: Freer[F, A])(q: Freer.Quiver[F, A, B]): Freer[F, B] =
+  private[Freer] def runQuiver[F[_], A, B](fa: Freer[F, A])(q: Freer.Quiver[F, A, B]): Freer[F, B] =
     q.foldLeft[Freer[F, ?]](fa)(
       ν[RightAction[Freer[F, ?], =>:[F, ?, ?]]][A, B](
         (f, k) => Freer.monad[F].flatMap(f)(Kleisli.runKleisli(k))
