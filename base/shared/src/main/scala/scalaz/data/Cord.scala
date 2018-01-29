ca5fa28f1c5c53a5a4e069c7ad125c0703a8b60e
Edmund Noble <edmundnoble@gmail.com>
2018-01-28 23:08:24 -0500

    Use an array for the stack

--- base/shared/src/main/scala/scalaz/data/Cord.scala
+++ base/shared/src/main/scala/scalaz/data/Cord.scala
@@ -24,15 +24,18 @@ object CordImpl extends CordModule {
 
   type Cord = CordRepr
 
-  final class CordRepr(val under: AnyRef, val length: Int)
+  final class CordRepr(val under: AnyRef, val length: Int, val depth: Int)
   final class Concat(val left: AnyRef, val right: AnyRef)
 
-  def apply(string: String): Cord = new CordRepr(string, string.length)
+  def apply(string: String): Cord = new CordRepr(string, string.length, 1)
 
-  def concat(left: Cord, right: Cord): Cord = new CordRepr(new Concat(left.under, right.under), left.length + right.length)
-  def cons(left: String, right: Cord): Cord = new CordRepr(new Concat(left, right.under), left.length + right.length)
-  def snoc(left: Cord, right: String): Cord = new CordRepr(new Concat(left.under, right), left.length + right.length)
-  val empty: Cord = new CordRepr("", 0)
+  def concat(left: Cord, right: Cord): Cord =
+    new CordRepr(new Concat(left.under, right.under), left.length + right.length, Math.max(left.depth + 1, right.depth))
+  def cons(left: String, right: Cord): Cord =
+    new CordRepr(new Concat(left, right.under), left.length + right.length, right.depth)
+  def snoc(left: Cord, right: String): Cord =
+    new CordRepr(new Concat(left.under, right), left.length + right.length, left.depth + 1)
+  val empty: Cord = new CordRepr("", 0, 1)
 
   def length(cord: Cord): Int = cord.length
 
@@ -42,24 +45,27 @@ object CordImpl extends CordModule {
     sb.toString()
   }
 
-  def unsafeAppendToH(rights: java.util.ArrayDeque[AnyRef], cord: Cord, builder: StringBuilder): Unit = {
+  def unsafeAppendToH(rights: Array[AnyRef], cord: Cord, builder: StringBuilder): Unit = {
     var current = cord.under
+    var stackPtr = 0
     while (current != null) current match {
       case s: String =>
         builder.append(s)
-        if (rights.size() > 0) {
-          current = rights.pop()
+        if (stackPtr > 0) {
+          stackPtr = stackPtr - 1
+          current = rights(stackPtr)
         } else {
           current = null
         }
       case c: Concat =>
         current = c.left
-        rights.push(c.right)
+        rights(stackPtr) = c.right
+        stackPtr = stackPtr + 1
     }
   }
 
   def unsafeAppendTo(cord: Cord, builder: StringBuilder): Unit = {
-    val rights = new java.util.ArrayDeque[AnyRef](64)
+    val rights = new Array[AnyRef](cord.depth)
     unsafeAppendToH(rights, cord, builder)
   }
 }
