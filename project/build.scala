e04bfde8a19e54109dfc5bbb63bf44bb19f3ea06
xuwei-k <6b656e6a69@gmail.com>
2018-02-25 18:15:06 +0900

    add Scala 2.13.0-M3 in crossScalaVersions

--- project/build.scala
+++ project/build.scala
@@ -143,7 +143,7 @@ object build {
       (f, path)
     },
     scalaVersion := Scala212,
-    crossScalaVersions := Seq("2.10.7", Scala211, Scala212),
+    crossScalaVersions := Seq("2.10.7", Scala211, Scala212, "2.13.0-M3"),
     resolvers ++= (if (scalaVersion.value.endsWith("-SNAPSHOT")) List(Opts.resolver.sonatypeSnapshots) else Nil),
     fullResolvers ~= {_.filterNot(_.name == "jcenter")}, // https://github.com/sbt/sbt/issues/2217
     scalaCheckVersion_1_12 := {
