8d6b689f19f9ac9cf875a0bb1744d24af6ca1a09
John A. De Goes <john@degoes.net>
2018-05-14 13:00:41 -0600

    address review comments

--- base/shared/src/main/scala/scalaz/typeclass/SemigroupSyntax.scala
+++ base/shared/src/main/scala/scalaz/typeclass/SemigroupSyntax.scala
@@ -6,7 +6,5 @@ import scala.language.experimental.macros
 trait SemigroupSyntax {
   implicit final class ToSemigroupOps[A: Semigroup](a: A) {
     def append(f: => A): A = macro meta.Ops.fa_1
-
-    def <>(f: => A): A = macro meta.Ops.fa_1
   }
 }
--- effect/shared/src/main/scala/scalaz/effect/FiberInstances.scala
+++ effect/shared/src/main/scala/scalaz/effect/FiberInstances.scala
@@ -7,7 +7,7 @@ import scalaz.typeclass.{ ApplicativeClass, MonoidClass }
 trait FiberInstances {
   implicit def fiberMonoid[E, A](implicit A: Monoid[A]): Monoid[Fiber[E, A]] =
     instanceOf(new MonoidClass[Fiber[E, A]] {
-      final def append(a1: Fiber[E, A], a2: => Fiber[E, A]) = a1.zipWith(a2)(A.append(_, _))
+      final def append(a1: Fiber[E, A], a2: => Fiber[E, A]) = a1 <> a2
       final def empty = new Fiber[E, A] {
         def join: IO[E, A]                            = IO.now(A.empty)
         def interrupt[E2](t: Throwable): IO[E2, Unit] = IO.unit[E2]
