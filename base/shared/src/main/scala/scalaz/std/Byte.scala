b9ec7b61d33aa0018603217abbaf185a72830a61
Tim Steinbach <tim@nequissimus.com>
2018-05-19 16:34:33 -0400

    WIP Ord for Byte

--- base/shared/src/main/scala/scalaz/std/Byte.scala
+++ base/shared/src/main/scala/scalaz/std/Byte.scala
@@ -6,4 +6,13 @@ import utils._
 trait ByteInstances {
   implicit val byteDebug: Debug[Byte] = toStringDebug[Byte]
   implicit val byteEq: Eq[Byte]       = universalEq[Byte]
+
+  /* https://github.com/scalaz/scalaz/pull/1792
+  implicit val byteOrd: Ord[Byte] = instanceOf(new OrdClass[Byte] {
+    def comp(a: Byte, b: Byte) = java.lang.Byte.compare(a, b) match {
+      case 0 => EQ
+      case x if x < 0 => LT
+      case _ => GT
+    }
+  })*/
 }
