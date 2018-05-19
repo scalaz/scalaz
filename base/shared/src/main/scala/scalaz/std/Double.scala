af971be8ed8ec6088f02d23462b38d0f5e64f27e
Tim Steinbach <tim@nequissimus.com>
2018-05-19 17:26:56 -0400

    WIP Ord for Double

--- base/shared/src/main/scala/scalaz/std/Double.scala
+++ base/shared/src/main/scala/scalaz/std/Double.scala
@@ -6,7 +6,25 @@ import scalaz.core.EqClass
 import utils._
 
 trait DoubleInstances {
+  private[this] final val NegZero: Double = -0.0d
+  private[this] final val PosZero: Double = 0.0d
+
   implicit val doubleDebug: Debug[Double] = toStringDebug[Double]
   implicit val doubleEq: Eq[Double] =
-    instanceOf[EqClass[Double]]((a, b) => doubleToRawLongBits(a) == doubleToRawLongBits(b))
+    instanceOf(new EqClass[Double] {
+      def equal(a: Double, b: Double) = (a, b) match {
+        case (NegZero, PosZero) => true
+        case (PosZero, NegZero) => true
+        case (x, y)             => doubleToRawLongBits(x) == doubleToRawLongBits(y)
+      }
+    })
+
+  /* https://github.com/scalaz/scalaz/pull/1792
+  implicit val doubleOrd: Ord[Double] = instanceOf(new OrdClass[Double] {
+    def comp(a: Double, b: Double): Double = (a, b) match {
+      case (a, b) if doubleEq.equal(a, b) => EQ
+      case (a, b) if doubleToRawLongBits(a) < doubleToRawLongBits(b) => LT
+      case _ => GT
+    }
+  })*/
 }
