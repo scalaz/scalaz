6af220a5aca200a798f2c105f4fcf3629532e41b
Tim Steinbach <tim@nequissimus.com>
2018-05-19 16:37:14 -0400

    WIP Ord for Int

--- base/shared/src/main/scala/scalaz/std/Int.scala
+++ base/shared/src/main/scala/scalaz/std/Int.scala
@@ -6,4 +6,13 @@ import utils._
 trait IntInstances {
   implicit val intDebug: Debug[Int] = toStringDebug[Int]
   implicit val intEq: Eq[Int]       = universalEq[Int]
+
+  /* https://github.com/scalaz/scalaz/pull/1792
+  implicit val intOrd: Ord[Int] = instanceOf(new OrdClass[Int] {
+    def comp(a: Int, b: Int) = java.lang.Integer.compare(a, b) match {
+      case 0 => EQ
+      case x if x < 0 => LT
+      case _ => GT
+    }
+  })*/
 }
