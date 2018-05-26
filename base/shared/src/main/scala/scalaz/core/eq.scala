8240c6d92334d3cbb52054a7969afd838bec450b
Filippo Mariotti <filippo.mariotti@gmail.com>
2018-05-26 19:56:59 +0100

    Refactored core

--- base/shared/src/main/scala/scalaz/core/eq.scala
+++ base/shared/src/main/scala/scalaz/core/eq.scala
@@ -3,6 +3,14 @@ package core
 
 import scala.language.experimental.macros
 
+trait EqClass[A] {
+  def equal(first: A, second: A): Boolean
+}
+
+trait EqInstances {
+  implicit final val voidEq: Eq[Void] = instanceOf[EqClass[Void]]((a, b) => a.absurd)
+}
+
 trait EqSyntax {
   implicit final class ToEqOps[A](a: A) {
     private[core] type equal
