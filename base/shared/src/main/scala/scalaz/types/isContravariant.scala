af6b0339af918b6f757c2e6aac63a6e0ec6920cb
Filippo Mariotti <filippo.mariotti@gmail.com>
2018-05-26 22:28:17 +0100

    Changed types

--- base/shared/src/main/scala/scalaz/types/isContravariant.scala
+++ base/shared/src/main/scala/scalaz/types/isContravariant.scala
@@ -61,3 +61,13 @@ object IsContravariantClass {
 
   trait Alt[D <: Alt[D]]
 }
+
+trait IsContravariantInstances {
+  implicit def scalaContravariant[F[- _]]: IsContravariant[F] =
+    instanceOf(new IsContravariantClass.SubstCv[F] {
+      def substCv[G[+ _], A, B](g: G[F[B]])(implicit ev: A <~< B): G[F[A]] = {
+        type H[-T] = G[F[T]]
+        ev.substCt[H](g)
+      }
+    })
+}
--- base/shared/src/main/scala/scalaz/types/isCovariant.scala
+++ base/shared/src/main/scala/scalaz/types/isCovariant.scala
@@ -71,3 +71,13 @@ object IsCovariantClass {
   trait Alt[D <: Alt[D]] { self: D =>
   }
 }
+
+trait IsCovariantInstances {
+  implicit def scalaCovariant[F[+ _]]: IsCovariant[F] =
+    instanceOf(new IsCovariantClass.SubstCv[F] {
+      def substCv[G[+ _], A, B](g: G[F[A]])(implicit ev: A <~< B): G[F[B]] = {
+        type H[+T] = G[F[T]]
+        ev.substCv[H](g)
+      }
+    })
+}
