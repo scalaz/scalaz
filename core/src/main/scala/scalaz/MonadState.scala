8ecafce61c03c6834372028f70561cff9e02f9b5
Sam Halliday <sam.halliday@gmail.com>
2018-05-31 00:14:00 +0100

    MonadState.liftF

--- core/src/main/scala/scalaz/MonadState.scala
+++ core/src/main/scala/scalaz/MonadState.scala
@@ -33,6 +33,23 @@ object MonadState {
     }
 
   ////
+  sealed abstract class Ast[S, A]
+  final case class Get[S]()     extends Ast[S, S]
+  final case class Put[S](s: S) extends Ast[S, Unit]
 
+  def liftF[F[_], S](
+    implicit I: Ast[S, ?] :<: F
+  ): MonadState[Free[F, ?], S] with BindRec[Free[F, ?]] =
+    new MonadState[Free[F, ?], S] with BindRec[Free[F, ?]] {
+      val delegate = Free.freeMonad[F]
+      def point[A](a: =>A): Free[F, A] = delegate.point(a)
+      def bind[A, B](fa: Free[F, A])(f: A => Free[F, B]) = delegate.bind(fa)(f)
+      override def map[A, B](fa: Free[F, A])(f: A => B) = delegate.map(fa)(f)
+      override def tailrecM[A, B](f: A => Free[F, A \/ B])(a: A) = delegate.tailrecM(f)(a)
+
+      def init: Free[F, S] = get
+      def get: Free[F, S] = Free.liftF(I.inj(Get[S]()))
+      def put(s: S): Free[F, Unit] = Free.liftF(I.inj(Put[S](s)))
+    }
   ////
 }
