67bc445496dea809fbdbbbc1f20c854f7b0337e8
Rutvik Patel <heyrutvik@gmail.com>
2018-06-26 09:24:37 +0530

    Void syntax removed

--- core/src/main/scala/scalaz/syntax/VoidSyntax.scala
+++
@@ -1,9 +0,0 @@
-package scalaz.syntax
-
-import scalaz._
-
-trait VoidOps {
-  implicit class VoidOps(v: Void) {
-    def absurd[A]: A = Void.absurd(v)
-  }
-}
--- tests/src/test/scala/scalaz/VoidTest.scala
+++ tests/src/test/scala/scalaz/VoidTest.scala
@@ -1,13 +1,11 @@
 package scalaz
 
-import scalaz.Scalaz._
-
 object VoidTest {
 
   def _void(v: Void): Unit = {
     v.absurd[Int]
     v.absurd[Nothing]
-    v.absurd : Int
-    v.absurd : Nothing
+    v.absurd: Int
+    v.absurd: Nothing
   }
 }
