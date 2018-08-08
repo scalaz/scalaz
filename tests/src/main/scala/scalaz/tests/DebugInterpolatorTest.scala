f721ebdaf1be2d14fd2bb2f74f3d2c030cba6d6f
Edmund Noble <edmundnoble@gmail.com>
2018-08-08 02:55:01 -0400

    Run DebugInterpolatorTest with testz

---
+++ tests/src/main/scala/scalaz/tests/DebugInterpolatorTest.scala
@@ -0,0 +1,29 @@
+package scalaz
+package tests
+
+import Predef._
+import data.{Cord, IList}
+import Scalaz._
+
+import testz.Harness
+import z._
+
+object DebugInterpolatorTest {
+
+  def tests[T](harness: Harness[T]): T = {
+    import harness._
+    section("debug instance resolution")(
+      test("int") { () =>
+        assertEqual(z"before ${1} after", Cord("before 1 after"))
+      },
+      test("string") { () =>
+        assertEqual(z"before ${"foo"} after", Cord("before foo after"))
+      },
+      test("ilist") { () =>
+        assertEqual(z"before ${IList(1, 2, 3)} after", Cord("before IList(1,2,3) after"))
+      }
+    )
+  }
+
+
+}
