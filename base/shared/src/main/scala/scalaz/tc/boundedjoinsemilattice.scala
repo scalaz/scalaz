461c7506579079aca52e1f92cf2910c0810d10c2
Emily Pillmore <emily@kadena.io>
2018-08-26 21:11:35 -0400

    add bounded * instances

--- base/shared/src/main/scala/scalaz/tc/boundedjoinsemilattice.scala
+++ base/shared/src/main/scala/scalaz/tc/boundedjoinsemilattice.scala
@@ -1,5 +1,9 @@
 package scalaz.tc
 
+import scalaz.data.{ Const, Identity }
+
+import scala.{ Boolean, Unit }
+
 /**
  * A Join Semi-lattice which has a unique bottom element, denoted ⊥ such that the
  * following laws hold:
@@ -13,4 +17,45 @@ package scalaz.tc
  */
 trait BoundedJoinSemiLatticeClass[A] extends JoinSemiLatticeClass[A] {
   def bottom: A
+}
+
+object BoundedJoinSemiLatticeClass {
+
+  implicit val bool: BoundedJoinSemiLatticeClass[Boolean] = instanceOf(new BoundedJoinSemiLatticeClass[Boolean] {
+    override def bottom: Boolean                         = false
+    override def join(a1: Boolean, a2: Boolean): Boolean = a1 || a2
+  })
+
+  implicit val unit: BoundedJoinSemiLatticeClass[Unit] = instanceOf(new BoundedJoinSemiLatticeClass[Unit] {
+    override def bottom: Unit                   = ()
+    override def join(a1: Unit, a2: Unit): Unit = ()
+  })
+
+  implicit def identity[A](implicit L: BoundedJoinSemiLattice[A]): BoundedJoinSemiLatticeClass[Identity[A]] =
+    instanceOf(new BoundedJoinSemiLatticeClass[Identity[A]] {
+      override def bottom: Identity[A] = Identity(L.bottom)
+      override def join(a1: Identity[A], a2: Identity[A]): Identity[A] =
+        Identity(L.join(Identity.run(a1), Identity.run(a2)))
+    })
+
+  implicit def const[A, B](implicit L: BoundedJoinSemiLattice[A]): BoundedJoinSemiLatticeClass[Const[A, B]] =
+    instanceOf(new BoundedJoinSemiLatticeClass[Const[A, B]] {
+      override def bottom: Const[A, B]                                 = Const(L.bottom)
+      override def join(a1: Const[A, B], a2: Const[A, B]): Const[A, B] = Const(L.join(Const.run(a1), Const.run(a2)))
+    })
+
+  implicit def writer[A, B](implicit L: BoundedJoinSemiLattice[B]): BoundedJoinSemiLatticeClass[A => B] =
+    instanceOf(new BoundedJoinSemiLatticeClass[A => B] {
+      override def bottom: A => B = _ => L.bottom
+      override def join(a1: A => B, a2: A => B): A => B =
+        a => L.join(a1(a), a2(a))
+    })
+
+  implicit def pair[A, B](implicit L1: BoundedJoinSemiLattice[A],
+                          L2: BoundedJoinSemiLattice[B]): BoundedJoinSemiLatticeClass[(A, B)] =
+    instanceOf(new BoundedJoinSemiLatticeClass[(A, B)] {
+      override def bottom: (A, B) = (L1.bottom, L2.bottom)
+      override def join(a1: (A, B), a2: (A, B)): (A, B) =
+        (L1.join(a1._1, a2._1), L2.join(a1._2, a2._2))
+    })
 }
--- base/shared/src/main/scala/scalaz/tc/boundedmeetsemilattice.scala
+++ base/shared/src/main/scala/scalaz/tc/boundedmeetsemilattice.scala
@@ -1,5 +1,8 @@
 package scalaz.tc
 
+import scalaz.data.{ Const, Identity }
+import scala.{ Boolean, Unit }
+
 /**
  * A Meet Semi-Lattice with a top element ⊤ such that the following laws hold:
  *
@@ -12,4 +15,44 @@ package scalaz.tc
  */
 trait BoundedMeetSemiLatticeClass[A] extends MeetSemiLatticeClass[A] {
   def top: A
+}
+
+object BoundedMeetSemiLatticeClass {
+  implicit val bool: BoundedMeetSemiLatticeClass[Boolean] = instanceOf(new BoundedMeetSemiLatticeClass[Boolean] {
+    override def top: Boolean                            = true
+    override def meet(a1: Boolean, a2: Boolean): Boolean = a1 && a2
+  })
+
+  implicit val unit: BoundedMeetSemiLatticeClass[Unit] = instanceOf(new BoundedMeetSemiLatticeClass[Unit] {
+    override def top: Unit                      = ()
+    override def meet(a1: Unit, a2: Unit): Unit = ()
+  })
+
+  implicit def identity[A](implicit L: BoundedMeetSemiLattice[A]): BoundedMeetSemiLatticeClass[Identity[A]] =
+    instanceOf(new BoundedMeetSemiLatticeClass[Identity[A]] {
+      override def top: Identity[A] = Identity(L.top)
+      override def meet(a1: Identity[A], a2: Identity[A]): Identity[A] =
+        Identity(L.meet(Identity.run(a1), Identity.run(a2)))
+    })
+
+  implicit def const[A, B](implicit L: BoundedMeetSemiLattice[A]): BoundedMeetSemiLatticeClass[Const[A, B]] =
+    instanceOf(new BoundedMeetSemiLatticeClass[Const[A, B]] {
+      override def top: Const[A, B]                                    = Const(L.top)
+      override def meet(a1: Const[A, B], a2: Const[A, B]): Const[A, B] = Const(L.meet(Const.run(a1), Const.run(a2)))
+    })
+
+  implicit def writer[A, B](implicit L: BoundedMeetSemiLattice[B]): BoundedMeetSemiLatticeClass[A => B] =
+    instanceOf(new BoundedMeetSemiLatticeClass[A => B] {
+      override def top: A => B = _ => L.top
+      override def meet(a1: A => B, a2: A => B): A => B =
+        a => L.meet(a1(a), a2(a))
+    })
+
+  implicit def pair[A, B](implicit L1: BoundedMeetSemiLattice[A],
+                          L2: BoundedMeetSemiLattice[B]): BoundedMeetSemiLatticeClass[(A, B)] =
+    instanceOf(new BoundedMeetSemiLatticeClass[(A, B)] {
+      override def top: (A, B) = (L1.top, L2.top)
+      override def meet(a1: (A, B), a2: (A, B)): (A, B) =
+        (L1.meet(a1._1, a2._1), L2.meet(a1._2, a2._2))
+    })
 }
