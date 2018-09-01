559c415ab88918d735f921e12fe61ff347bf23a9
Edmund Noble <edmundnoble@gmail.com>
2018-09-01 12:54:45 -0400

    Remove Point stuff

--- tests/src/main/scala/scalaz/tests/AMaybeTests.scala
+++ tests/src/main/scala/scalaz/tests/AMaybeTests.scala
@@ -10,18 +10,6 @@ import AMaybe.{ empty, just }
 import testz._
 
 object AMaybeTests {
-  trait Point[F[_]] {
-    def point[A](a: A): F[A]
-  }
-
-  trait FreePoint[A, B] {
-    def doIt[F[_]](point: Point[F])(a: A): F[A]
-  }
-
-  val listPoint = new Point[IList] {
-    def point[A](a: A): IList[A] = IList(a)
-  }
-
   def tests[T](harness: Harness[T], sequence: (T, T) => T): T = {
     import harness._
     sequence(
