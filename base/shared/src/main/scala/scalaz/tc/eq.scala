a37412eed03585668c28273da49d117fb392551d
Clinton Montague <clinton@slightlymore.co.uk>
2018-09-30 03:58:10 +0100

    Remove Eq.never (#2007)

--- base/shared/src/main/scala/scalaz/tc/eq.scala
+++ base/shared/src/main/scala/scalaz/tc/eq.scala
@@ -100,7 +100,6 @@ trait EqSyntax {
 
 object Eq {
   def always[A]: Eq[A]     = instanceOf[EqClass[A]]((_, _) => true)
-  def never[A]: Eq[A]      = instanceOf[EqClass[A]]((_, _) => false)
   def fromEquals[A]: Eq[A] = instanceOf[EqClass[A]](_ == _)
   def byReference[A]: Eq[A] = instanceOf[EqClass[A]](
     (a1, a2) => a1.asInstanceOf[scala.AnyRef] eq a2.asInstanceOf[scala.AnyRef]
