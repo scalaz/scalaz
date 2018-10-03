4b71412d3938922181f71c77e0a01932de7ae534
merlin <merlin@merlin.org>
2018-10-03 09:09:02 -0400

    Plus for Validation accumulates errors

--- core/src/main/scala/scalaz/Validation.scala
+++ core/src/main/scala/scalaz/Validation.scala
@@ -505,8 +505,8 @@ sealed abstract class ValidationInstances1 extends ValidationInstances2 {
 }
 
 sealed abstract class ValidationInstances2 extends ValidationInstances3 {
-  implicit def ValidationInstances1[L]: Traverse[Validation[L, ?]] with Cozip[Validation[L, ?]] with Plus[Validation[L, ?]] with Optional[Validation[L, ?]] =
-    new Traverse[Validation[L, ?]] with Cozip[Validation[L, ?]] with Plus[Validation[L, ?]] with Optional[Validation[L, ?]] {
+  implicit def ValidationInstances1[L]: Traverse[Validation[L, ?]] with Cozip[Validation[L, ?]] with Optional[Validation[L, ?]] =
+    new Traverse[Validation[L, ?]] with Cozip[Validation[L, ?]] with Optional[Validation[L, ?]] {
 
       override def map[A, B](fa: Validation[L, A])(f: A => B) =
         fa map f
@@ -526,13 +526,15 @@ sealed abstract class ValidationInstances2 extends ValidationInstances3 {
           }
         }
 
-      def plus[A](a: Validation[L, A], b: => Validation[L, A]) =
-        a orElse b
-
       def pextract[B, A](fa: Validation[L,A]): Validation[L,B] \/ A =
         fa.fold(l => -\/(Failure(l)), \/.right)
     }
+
+  implicit def ValidationPlus[E: Semigroup, A]: Plus[Validation[E, ?]] = new Plus[Validation[E, ?]] {
+    override def plus[A](a1: Validation[E, A], a2: => Validation[E, A]) =
+      a1 findSuccess a2
   }
+}
 
 sealed abstract class ValidationInstances3 {
   implicit val ValidationInstances0 : Bitraverse[Validation] =
