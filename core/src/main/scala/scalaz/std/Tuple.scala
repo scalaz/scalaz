3061cf5c775972bff921d8bd550e4b2cf7401ff6
Vincent Marquez <vincent.marquez@gmail.com>
2018-11-09 19:04:03 -0800

    Tuple's Band/Semigroup causes ambiguous implicits, need to swap (#1444)

--- core/src/main/scala/scalaz/std/Tuple.scala
+++ core/src/main/scala/scalaz/std/Tuple.scala
@@ -2,6 +2,61 @@ package scalaz
 package std
 
 sealed trait TupleInstances0 {
+  implicit def tuple1Semigroup[A1](implicit A1: Semigroup[A1]): Semigroup[Tuple1[A1]] = new Tuple1Semigroup[A1] {
+    implicit def _1: Semigroup[A1] = A1
+  }
+  implicit def tuple2Semigroup[A1, A2](implicit A1: Semigroup[A1], A2: Semigroup[A2]): Semigroup[(A1, A2)] = new Tuple2Semigroup[A1, A2] {
+    implicit def _1 = A1
+    implicit def _2 = A2
+  }
+  implicit def tuple3Semigroup[A1, A2, A3](implicit A1: Semigroup[A1], A2: Semigroup[A2], A3: Semigroup[A3]): Semigroup[(A1, A2, A3)] = new Tuple3Semigroup[A1, A2, A3] {
+    implicit def _1 = A1
+    implicit def _2 = A2
+    implicit def _3 = A3
+  }
+  implicit def tuple4Semigroup[A1, A2, A3, A4](implicit A1: Semigroup[A1], A2: Semigroup[A2], A3: Semigroup[A3], A4: Semigroup[A4]): Semigroup[(A1, A2, A3, A4)] = new Tuple4Semigroup[A1, A2, A3, A4] {
+    implicit def _1 = A1
+    implicit def _2 = A2
+    implicit def _3 = A3
+    implicit def _4 = A4
+  }
+  implicit def tuple5Semigroup[A1, A2, A3, A4, A5](implicit A1: Semigroup[A1], A2: Semigroup[A2], A3: Semigroup[A3], A4: Semigroup[A4], A5: Semigroup[A5]): Semigroup[(A1, A2, A3, A4, A5)] = new Tuple5Semigroup[A1, A2, A3, A4, A5] {
+    implicit def _1 = A1
+    implicit def _2 = A2
+    implicit def _3 = A3
+    implicit def _4 = A4
+    implicit def _5 = A5
+  }
+  implicit def tuple6Semigroup[A1, A2, A3, A4, A5, A6](implicit A1: Semigroup[A1], A2: Semigroup[A2], A3: Semigroup[A3], A4: Semigroup[A4], A5: Semigroup[A5], A6: Semigroup[A6]): Semigroup[(A1, A2, A3, A4, A5, A6)] = new Tuple6Semigroup[A1, A2, A3, A4, A5, A6] {
+    implicit def _1 = A1
+    implicit def _2 = A2
+    implicit def _3 = A3
+    implicit def _4 = A4
+    implicit def _5 = A5
+    implicit def _6 = A6
+  }
+  implicit def tuple7Semigroup[A1, A2, A3, A4, A5, A6, A7](implicit A1: Semigroup[A1], A2: Semigroup[A2], A3: Semigroup[A3], A4: Semigroup[A4], A5: Semigroup[A5], A6: Semigroup[A6], A7: Semigroup[A7]): Semigroup[(A1, A2, A3, A4, A5, A6, A7)] = new Tuple7Semigroup[A1, A2, A3, A4, A5, A6, A7] {
+    implicit def _1 = A1
+    implicit def _2 = A2
+    implicit def _3 = A3
+    implicit def _4 = A4
+    implicit def _5 = A5
+    implicit def _6 = A6
+    implicit def _7 = A7
+  }
+  implicit def tuple8Semigroup[A1, A2, A3, A4, A5, A6, A7, A8](implicit A1: Semigroup[A1], A2: Semigroup[A2], A3: Semigroup[A3], A4: Semigroup[A4], A5: Semigroup[A5], A6: Semigroup[A6], A7: Semigroup[A7], A8: Semigroup[A8]): Semigroup[(A1, A2, A3, A4, A5, A6, A7, A8)] = new Tuple8Semigroup[A1, A2, A3, A4, A5, A6, A7, A8] {
+    implicit def _1 = A1
+    implicit def _2 = A2
+    implicit def _3 = A3
+    implicit def _4 = A4
+    implicit def _5 = A5
+    implicit def _6 = A6
+    implicit def _7 = A7
+    implicit def _8 = A8
+  }
+}
+
+sealed trait TupleInstances1 extends TupleInstances0 {
   /** Product functor and comonad */
   implicit def tuple2Instance[A1]: Traverse[(A1, ?)] with Comonad[(A1, ?)] = new Tuple2Functor[A1] with Comonad[(A1, ?)] {
     override def cojoin[A](a: (A1, A)) = (a._1, a)
@@ -16,6 +71,81 @@ sealed trait TupleInstances0 {
   implicit def tuple7Functor[A1, A2, A3, A4, A5, A6]: Traverse[(A1, A2, A3, A4, A5, A6, ?)] = new Tuple7Functor[A1, A2, A3, A4, A5, A6] {}
   implicit def tuple8Functor[A1, A2, A3, A4, A5, A6, A7]: Traverse[(A1, A2, A3, A4, A5, A6, A7, ?)] = new Tuple8Functor[A1, A2, A3, A4, A5, A6, A7] {}
 
+  implicit def tuple1Monoid[A1](implicit A1: Monoid[A1]): Monoid[Tuple1[A1]] =
+    new Tuple1Monoid[A1] {
+      implicit def _1 = A1
+    }
+  implicit def tuple2Monoid[A1, A2](implicit A1: Monoid[A1], A2: Monoid[A2]): Monoid[(A1, A2)] =
+    new Tuple2Monoid[A1, A2] {
+      implicit def _1 = A1
+      implicit def _2 = A2
+    }
+  implicit def tuple3Monoid[A1, A2, A3](implicit A1: Monoid[A1], A2: Monoid[A2], A3: Monoid[A3]): Monoid[(A1, A2, A3)] =
+    new Tuple3Monoid[A1, A2, A3] {
+      implicit def _1 = A1
+      implicit def _2 = A2
+      implicit def _3 = A3
+    }
+  implicit def tuple4Monoid[A1, A2, A3, A4](implicit A1: Monoid[A1], A2: Monoid[A2], A3: Monoid[A3], A4: Monoid[A4]): Monoid[(A1, A2, A3, A4)] =
+    new Tuple4Monoid[A1, A2, A3, A4] {
+      implicit def _1 = A1
+      implicit def _2 = A2
+      implicit def _3 = A3
+      implicit def _4 = A4
+    }
+  implicit def tuple5Monoid[A1, A2, A3, A4, A5](implicit A1: Monoid[A1], A2: Monoid[A2], A3: Monoid[A3], A4: Monoid[A4], A5: Monoid[A5]): Monoid[(A1, A2, A3, A4, A5)] =
+    new Tuple5Monoid[A1, A2, A3, A4, A5] {
+      implicit def _1 = A1
+      implicit def _2 = A2
+      implicit def _3 = A3
+      implicit def _4 = A4
+      implicit def _5 = A5
+    }
+  implicit def tuple6Monoid[A1, A2, A3, A4, A5, A6](implicit A1: Monoid[A1], A2: Monoid[A2], A3: Monoid[A3], A4: Monoid[A4], A5: Monoid[A5], A6: Monoid[A6]): Monoid[(A1, A2, A3, A4, A5, A6)] =
+    new Tuple6Monoid[A1, A2, A3, A4, A5, A6] {
+      implicit def _1 = A1
+      implicit def _2 = A2
+      implicit def _3 = A3
+      implicit def _4 = A4
+      implicit def _5 = A5
+      implicit def _6 = A6
+    }
+  implicit def tuple7Monoid[A1, A2, A3, A4, A5, A6, A7](implicit A1: Monoid[A1], A2: Monoid[A2], A3: Monoid[A3], A4: Monoid[A4], A5: Monoid[A5], A6: Monoid[A6], A7: Monoid[A7]): Monoid[(A1, A2, A3, A4, A5, A6, A7)] =
+    new Tuple7Monoid[A1, A2, A3, A4, A5, A6, A7] {
+      implicit def _1 = A1
+      implicit def _2 = A2
+      implicit def _3 = A3
+      implicit def _4 = A4
+      implicit def _5 = A5
+      implicit def _6 = A6
+      implicit def _7 = A7
+    }
+  implicit def tuple8Monoid[A1, A2, A3, A4, A5, A6, A7, A8](implicit A1: Monoid[A1], A2: Monoid[A2], A3: Monoid[A3], A4: Monoid[A4], A5: Monoid[A5], A6: Monoid[A6], A7: Monoid[A7], A8: Monoid[A8]): Monoid[(A1, A2, A3, A4, A5, A6, A7, A8)] =
+    new Tuple8Monoid[A1, A2, A3, A4, A5, A6, A7, A8] {
+      implicit def _1 = A1
+      implicit def _2 = A2
+      implicit def _3 = A3
+      implicit def _4 = A4
+      implicit def _5 = A5
+      implicit def _6 = A6
+      implicit def _7 = A7
+      implicit def _8 = A8
+    }
+}
+
+sealed trait TupleInstances2 extends TupleInstances1 {
+  implicit val tuple2Bitraverse: Bitraverse[Tuple2] = new Bitraverse[Tuple2] {
+    override def bimap[A, B, C, D](fab: (A, B))(f: A => C, g: B => D) =
+      (f(fab._1), g(fab._2))
+    def bitraverseImpl[G[_]: Applicative, A, B, C, D](fab: (A, B))(f: A => G[C], g: B => G[D]) =
+      Applicative[G].tuple2(f(fab._1), g(fab._2))
+  }
+
+  implicit val tuple2Associative: Associative[Tuple2] = new Associative[Tuple2] {
+    def reassociateLeft[A, B, C](f: (A, (B, C))): ((A, B), C) = ((f._1, f._2._1), f._2._2)
+    def reassociateRight[A, B, C](f: ((A, B), C)): (A, (B, C)) = (f._1._1, (f._1._2, f._2))
+  }
+  
   implicit def tuple1Band[A1](implicit A1: Band[A1]): Band[Tuple1[A1]] =
     new Tuple1Semigroup[A1] with Band[Tuple1[A1]] {
       def _1 = A1
@@ -76,74 +206,6 @@ sealed trait TupleInstances0 {
       def _7 = A7
       def _8 = A8
     }
-
-}
-
-sealed trait TupleInstances1 extends TupleInstances0 {
-  implicit val tuple2Bitraverse: Bitraverse[Tuple2] = new Bitraverse[Tuple2] {
-    override def bimap[A, B, C, D](fab: (A, B))(f: A => C, g: B => D) =
-      (f(fab._1), g(fab._2))
-    def bitraverseImpl[G[_]: Applicative, A, B, C, D](fab: (A, B))(f: A => G[C], g: B => G[D]) =
-      Applicative[G].tuple2(f(fab._1), g(fab._2))
-  }
-
-  implicit val tuple2Associative: Associative[Tuple2] = new Associative[Tuple2] {
-    def reassociateLeft[A, B, C](f: (A, (B, C))): ((A, B), C) = ((f._1, f._2._1), f._2._2)
-    def reassociateRight[A, B, C](f: ((A, B), C)): (A, (B, C)) = (f._1._1, (f._1._2, f._2))
-  }
-
-  implicit def tuple1Semigroup[A1](implicit A1: Semigroup[A1]): Semigroup[Tuple1[A1]] = new Tuple1Semigroup[A1] {
-    implicit def _1: Semigroup[A1] = A1
-  }
-  implicit def tuple2Semigroup[A1, A2](implicit A1: Semigroup[A1], A2: Semigroup[A2]): Semigroup[(A1, A2)] = new Tuple2Semigroup[A1, A2] {
-    implicit def _1 = A1
-    implicit def _2 = A2
-  }
-  implicit def tuple3Semigroup[A1, A2, A3](implicit A1: Semigroup[A1], A2: Semigroup[A2], A3: Semigroup[A3]): Semigroup[(A1, A2, A3)] = new Tuple3Semigroup[A1, A2, A3] {
-    implicit def _1 = A1
-    implicit def _2 = A2
-    implicit def _3 = A3
-  }
-  implicit def tuple4Semigroup[A1, A2, A3, A4](implicit A1: Semigroup[A1], A2: Semigroup[A2], A3: Semigroup[A3], A4: Semigroup[A4]): Semigroup[(A1, A2, A3, A4)] = new Tuple4Semigroup[A1, A2, A3, A4] {
-    implicit def _1 = A1
-    implicit def _2 = A2
-    implicit def _3 = A3
-    implicit def _4 = A4
-  }
-  implicit def tuple5Semigroup[A1, A2, A3, A4, A5](implicit A1: Semigroup[A1], A2: Semigroup[A2], A3: Semigroup[A3], A4: Semigroup[A4], A5: Semigroup[A5]): Semigroup[(A1, A2, A3, A4, A5)] = new Tuple5Semigroup[A1, A2, A3, A4, A5] {
-    implicit def _1 = A1
-    implicit def _2 = A2
-    implicit def _3 = A3
-    implicit def _4 = A4
-    implicit def _5 = A5
-  }
-  implicit def tuple6Semigroup[A1, A2, A3, A4, A5, A6](implicit A1: Semigroup[A1], A2: Semigroup[A2], A3: Semigroup[A3], A4: Semigroup[A4], A5: Semigroup[A5], A6: Semigroup[A6]): Semigroup[(A1, A2, A3, A4, A5, A6)] = new Tuple6Semigroup[A1, A2, A3, A4, A5, A6] {
-    implicit def _1 = A1
-    implicit def _2 = A2
-    implicit def _3 = A3
-    implicit def _4 = A4
-    implicit def _5 = A5
-    implicit def _6 = A6
-  }
-  implicit def tuple7Semigroup[A1, A2, A3, A4, A5, A6, A7](implicit A1: Semigroup[A1], A2: Semigroup[A2], A3: Semigroup[A3], A4: Semigroup[A4], A5: Semigroup[A5], A6: Semigroup[A6], A7: Semigroup[A7]): Semigroup[(A1, A2, A3, A4, A5, A6, A7)] = new Tuple7Semigroup[A1, A2, A3, A4, A5, A6, A7] {
-    implicit def _1 = A1
-    implicit def _2 = A2
-    implicit def _3 = A3
-    implicit def _4 = A4
-    implicit def _5 = A5
-    implicit def _6 = A6
-    implicit def _7 = A7
-  }
-  implicit def tuple8Semigroup[A1, A2, A3, A4, A5, A6, A7, A8](implicit A1: Semigroup[A1], A2: Semigroup[A2], A3: Semigroup[A3], A4: Semigroup[A4], A5: Semigroup[A5], A6: Semigroup[A6], A7: Semigroup[A7], A8: Semigroup[A8]): Semigroup[(A1, A2, A3, A4, A5, A6, A7, A8)] = new Tuple8Semigroup[A1, A2, A3, A4, A5, A6, A7, A8] {
-    implicit def _1 = A1
-    implicit def _2 = A2
-    implicit def _3 = A3
-    implicit def _4 = A4
-    implicit def _5 = A5
-    implicit def _6 = A6
-    implicit def _7 = A7
-    implicit def _8 = A8
-  }
   /** `Tuple1[A]` is isomorphic to `Id[X]` */
   implicit val tuple1Instance: Traverse[Tuple1] with Monad[Tuple1] with Comonad[Tuple1] = new Tuple1Monad with Tuple1Functor with Comonad[Tuple1] {
     override def cojoin[A](a: Tuple1[A]) = Tuple1(a)
@@ -256,7 +318,7 @@ sealed trait TupleInstances1 extends TupleInstances0 {
       implicit def _8 = A8
     }
 }
-sealed trait TupleInstances2 extends TupleInstances1 {
+sealed trait TupleInstances3 extends TupleInstances2 {
 
   implicit def tuple1Show[A1](implicit A1: Show[A1]): Show[Tuple1[A1]] =
     new Tuple1Show[A1] {
@@ -379,66 +441,7 @@ sealed trait TupleInstances2 extends TupleInstances1 {
       implicit def _7 = A7
       implicit def _8 = A8
     }
-  implicit def tuple1Monoid[A1](implicit A1: Monoid[A1]): Monoid[Tuple1[A1]] =
-    new Tuple1Monoid[A1] {
-      implicit def _1 = A1
-    }
-  implicit def tuple2Monoid[A1, A2](implicit A1: Monoid[A1], A2: Monoid[A2]): Monoid[(A1, A2)] =
-    new Tuple2Monoid[A1, A2] {
-      implicit def _1 = A1
-      implicit def _2 = A2
-    }
-  implicit def tuple3Monoid[A1, A2, A3](implicit A1: Monoid[A1], A2: Monoid[A2], A3: Monoid[A3]): Monoid[(A1, A2, A3)] =
-    new Tuple3Monoid[A1, A2, A3] {
-      implicit def _1 = A1
-      implicit def _2 = A2
-      implicit def _3 = A3
-    }
-  implicit def tuple4Monoid[A1, A2, A3, A4](implicit A1: Monoid[A1], A2: Monoid[A2], A3: Monoid[A3], A4: Monoid[A4]): Monoid[(A1, A2, A3, A4)] =
-    new Tuple4Monoid[A1, A2, A3, A4] {
-      implicit def _1 = A1
-      implicit def _2 = A2
-      implicit def _3 = A3
-      implicit def _4 = A4
-    }
-  implicit def tuple5Monoid[A1, A2, A3, A4, A5](implicit A1: Monoid[A1], A2: Monoid[A2], A3: Monoid[A3], A4: Monoid[A4], A5: Monoid[A5]): Monoid[(A1, A2, A3, A4, A5)] =
-    new Tuple5Monoid[A1, A2, A3, A4, A5] {
-      implicit def _1 = A1
-      implicit def _2 = A2
-      implicit def _3 = A3
-      implicit def _4 = A4
-      implicit def _5 = A5
-    }
-  implicit def tuple6Monoid[A1, A2, A3, A4, A5, A6](implicit A1: Monoid[A1], A2: Monoid[A2], A3: Monoid[A3], A4: Monoid[A4], A5: Monoid[A5], A6: Monoid[A6]): Monoid[(A1, A2, A3, A4, A5, A6)] =
-    new Tuple6Monoid[A1, A2, A3, A4, A5, A6] {
-      implicit def _1 = A1
-      implicit def _2 = A2
-      implicit def _3 = A3
-      implicit def _4 = A4
-      implicit def _5 = A5
-      implicit def _6 = A6
-    }
-  implicit def tuple7Monoid[A1, A2, A3, A4, A5, A6, A7](implicit A1: Monoid[A1], A2: Monoid[A2], A3: Monoid[A3], A4: Monoid[A4], A5: Monoid[A5], A6: Monoid[A6], A7: Monoid[A7]): Monoid[(A1, A2, A3, A4, A5, A6, A7)] =
-    new Tuple7Monoid[A1, A2, A3, A4, A5, A6, A7] {
-      implicit def _1 = A1
-      implicit def _2 = A2
-      implicit def _3 = A3
-      implicit def _4 = A4
-      implicit def _5 = A5
-      implicit def _6 = A6
-      implicit def _7 = A7
-    }
-  implicit def tuple8Monoid[A1, A2, A3, A4, A5, A6, A7, A8](implicit A1: Monoid[A1], A2: Monoid[A2], A3: Monoid[A3], A4: Monoid[A4], A5: Monoid[A5], A6: Monoid[A6], A7: Monoid[A7], A8: Monoid[A8]): Monoid[(A1, A2, A3, A4, A5, A6, A7, A8)] =
-    new Tuple8Monoid[A1, A2, A3, A4, A5, A6, A7, A8] {
-      implicit def _1 = A1
-      implicit def _2 = A2
-      implicit def _3 = A3
-      implicit def _4 = A4
-      implicit def _5 = A5
-      implicit def _6 = A6
-      implicit def _7 = A7
-      implicit def _8 = A8
-    }
+  
 
   implicit val tuple1Cozip: Cozip[Tuple1] = new Tuple1Cozip {}
   implicit def tuple2Cozip[A1]: Cozip[(A1, ?)] = new Tuple2Cozip[A1] {}
@@ -569,7 +572,7 @@ sealed trait TupleInstances2 extends TupleInstances1 {
 
 }
 
-trait TupleInstances extends TupleInstances2
+trait TupleInstances extends TupleInstances3
 
 object tuple extends TupleInstances {
   object tupleSyntax extends scalaz.syntax.std.ToTupleOps
