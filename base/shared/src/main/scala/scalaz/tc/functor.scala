9d26351b73d6f480fd20b2075b5006197bfa92a5
enzief <ngocduy.pham@outlook.com>
2019-03-22 14:43:48 +0100

    move CompositionX to their respective X file

--- base/shared/src/main/scala/scalaz/tc/functor.scala
+++ base/shared/src/main/scala/scalaz/tc/functor.scala
@@ -41,3 +41,10 @@ trait FunctorSyntax {
     def void(implicit ev: Functor[F]): F[Unit]            = ev.map[A, Unit](self)(_ => ())
   }
 }
+
+class CompositionFunctorClass[F[_], G[_]](implicit F: FunctorClass[F], G: FunctorClass[G])
+    extends FunctorClass[λ[α => F[G[α]]]] {
+
+  def map[A, B](fga: F[G[A]])(f: A => B): F[G[B]] =
+    F.map(fga)(G.map(_)(f))
+}
