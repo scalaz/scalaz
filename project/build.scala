e8e2910c896beb0842aa3881e04a2c85a920cfad
xuwei-k <6b656e6a69@gmail.com>
2020-03-21 22:41:17 +0900

    Scala 2.12.11

--- project/build.scala
+++ project/build.scala
@@ -132,7 +132,7 @@ object build {
   )
 
   private def Scala211 = "2.11.12"
-  private def Scala212 = "2.12.10"
+  private def Scala212 = "2.12.11"
   private def Scala213 = "2.13.1"
 
   private val SetScala211 = releaseStepCommand("++" + Scala211)
