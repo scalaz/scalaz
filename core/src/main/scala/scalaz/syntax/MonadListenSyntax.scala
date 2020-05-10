322439d78de4235eafcd4bbeeef2ca6fc8dcae62
xuwei-k <6b656e6a69@gmail.com>
2020-05-10 12:49:09 +0900

    add explicit type annotations

--- core/src/main/scala/scalaz/syntax/MonadListenSyntax.scala
+++ core/src/main/scala/scalaz/syntax/MonadListenSyntax.scala
@@ -11,13 +11,13 @@ final class MonadListenOps[F[_], W, A] private[syntax](self: F[A])(implicit ML: 
 }
 
 trait ToMonadListenOps0[TC[F[_], W] <: MonadListen[F, W]] {
-  implicit def ToMonadListenOps[F[_], A, W](v: F[A])(implicit F0: TC[F, W]) =
+  implicit def ToMonadListenOps[F[_], A, W](v: F[A])(implicit F0: TC[F, W]): MonadListenOps[F, W, A] =
     new MonadListenOps[F, W, A](v)(F0)
 }
 
 trait ToMonadListenOps[TC[F[_], W] <: MonadListen[F, W]] extends ToMonadListenOps0[TC] with ToMonadTellOps[TC]
 
 trait MonadListenSyntax[F[_], W] extends MonadTellSyntax[F, W] {
-  implicit def ToMonadListenOps[A](v: F[A])(implicit F0: MonadListen[F, W]) =
+  implicit def ToMonadListenOps[A](v: F[A])(implicit F0: MonadListen[F, W]): MonadListenOps[F, W, A] =
     new MonadListenOps[F, W, A](v)(F0)
 }
