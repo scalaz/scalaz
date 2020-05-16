35d70acfff6b63913c4478c505899fbfc97ea4dd
kenji yoshida <6b656e6a69@gmail.com>
2020-05-16 16:00:16 +0900

    s/ISet.Tip[A]/ISet.Tip[A]()/
    prepare dotty
    
    ```
    [warn] 338 |    case Tip()        => ISet.Tip[A]
    [warn]     |                         ^^^^^^^^^^^
    [warn]     |                         method apply must be called with () argument
    ```

--- core/src/main/scala/scalaz/Map.scala
+++ core/src/main/scala/scalaz/Map.scala
@@ -335,7 +335,7 @@ sealed abstract class ==>>[A, B] {
     foldrWithKey(IList.empty[A])((x, _, xs) => x :: xs)
 
   def keySet: ISet[A] = this match {
-    case Tip()        => ISet.Tip[A]
+    case Tip()        => ISet.Tip[A]()
     case Bin(k,v,l,r) => ISet.Bin(k,l.keySet,r.keySet)
   }
 
