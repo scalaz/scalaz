8797b7de0ececbe354591f17e45581303d1eb944
kenji yoshida <6b656e6a69@gmail.com>
2020-05-19 09:03:57 +0900

    avoid `Array()`
    prepare dotty
    
    ```
    $ dotr
    Starting dotty REPL...
    scala> Array()                                                                                                                  
    1 |Array()
      |       ^
      |       No ClassTag available for Nothing
    ```

--- tests/src/test/scala/scalaz/ImmutableArrayTest.scala
+++ tests/src/test/scala/scalaz/ImmutableArrayTest.scala
@@ -13,7 +13,7 @@ object ImmutableArrayTest extends SpecLite {
   "Issue #812" in {
     val xs = ImmutableArray.fromArray(Array("test"))
     val t = xs.tail
-    t.toArray.toList must_==(Array().toList)
+    t.toArray.toList must_==(Nil)
   }
 
   checkAll(equal.laws[ImmutableArray[Int]])
