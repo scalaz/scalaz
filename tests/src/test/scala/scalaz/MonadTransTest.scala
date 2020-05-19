73fb1f03b9075abea4e8ec2052b47d8221a80538
kenji yoshida <6b656e6a69@gmail.com>
2020-05-19 12:52:02 +0900

    thanks partial-unification
    (cherry picked from commit fa308b37de399d1d9d61119e1fc2802af56b4d1a)

--- tests/src/test/scala/scalaz/MonadTransTest.scala
+++ tests/src/test/scala/scalaz/MonadTransTest.scala
@@ -12,8 +12,7 @@ object MonadTransTest extends SpecLite {
 
   "liftMU" in {
     val mt = IndexedStateT.StateMonadTrans[Int]
-    // Inference of M fails when using liftM with a higher kinded type
-    mt.liftM[String \/ *, Int](10.right[String]).run(1) must_===((1, 10).right[String])
+    mt.liftM(10.right[String]).run(1) must_===((1, 10).right[String])
     mt.liftMU(10.right[String]).run(1) must_===((1, 10).right[String])
   }
 }
