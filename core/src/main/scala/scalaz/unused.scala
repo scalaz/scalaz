7c20f29cc6e562f7a4f76a347601407fc524ecf1
kenji yoshida <6b656e6a69@gmail.com>
2020-05-23 22:29:28 +0900

    Delete unused.scala
    https://github.com/scala/scala/pull/7623

--- core/src/main/scala/scalaz/unused.scala
+++
@@ -1,8 +0,0 @@
-package scalaz
-
-/**
- * Mark an explicit or implicit parameter as known to be unused, as shorthand
- * for the longer form, useful in combination with
- * `-Ywarn-unused:explicits,implicits` compiler options.
- */
-class unused extends deprecated("unused", "")
