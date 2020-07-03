87d9140cda812ebacc435e49417ba7399cfe6445
xuwei-k <6b656e6a69@gmail.com>
2020-07-03 14:33:55 +0900

    add "-Xsource:3" option

--- project/build.scala
+++ project/build.scala
@@ -165,6 +165,7 @@ object build {
           )
         case _ =>
           Seq(
+            "-Xsource:3",
             "-Xlint:_,-type-parameter-shadow,-missing-interpolator",
             "-Ywarn-dead-code",
             "-Ywarn-numeric-widen",
