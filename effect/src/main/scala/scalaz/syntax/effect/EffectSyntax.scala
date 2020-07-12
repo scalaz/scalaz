efdcb9462003557570faaff1fd12e606755ed56d
xuwei-k <6b656e6a69@gmail.com>
2020-07-12 11:58:44 +0900

    fix warning. remove scalaz.effect.syntax package object
    ```
    [warn] /home/travis/build/scalaz/scalaz/effect/src/main/scala/scalaz/syntax/effect/package.scala:9:23: package object inheritance is deprecated (https://github.com/scala/scala-dev/issues/441);
    [warn] drop the `extends` clause or use a regular object instead
    [warn] package object effect extends EffectSyntaxes
    [warn]                       ^
    [warn] one warning found
    ```

--- effect/src/main/scala/scalaz/syntax/effect/EffectSyntax.scala
+++ effect/src/main/scala/scalaz/syntax/effect/EffectSyntax.scala
@@ -1,19 +1,12 @@
 package scalaz.syntax.effect
 
-trait EffectSyntaxes {
+object id extends ToIdOps
 
-  object id extends ToIdOps
+object resource extends ToResourceOps
 
-  object resource extends ToResourceOps
+object monadCatchIO extends ToMonadCatchIOOps
 
-  object monadCatchIO extends ToMonadCatchIOOps
-
-  object all extends ToAllEffectTypeClassOps
-
-}
+object all extends ToAllEffectTypeClassOps
 
 trait ToAllEffectTypeClassOps
     extends ToIdOps with ToResourceOps with ToMonadCatchIOOps
-
-/**The members of this object are also offered in the package object [[scalaz.syntax.effect]] */
-object EffectSyntax extends EffectSyntaxes
--- effect/src/main/scala/scalaz/syntax/effect/package.scala
+++
@@ -1,9 +0,0 @@
-package scalaz.syntax
-
-/**
- * Implicits to provide a convenient syntax to work with type classes
- * and functions.
- *
- * Non-trivial code should *not* be defined in this package; instead delegate.
- */
-package object effect extends EffectSyntaxes
