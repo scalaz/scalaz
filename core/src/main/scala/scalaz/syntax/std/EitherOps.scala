beeb9836e7c8d79ae6f5a79edb421a3b951764b5
xuwei-k <6b656e6a69@gmail.com>
2021-09-26 22:58:28 +0900

    add `coerceLeft` and `coerceRight`

--- core/src/main/scala/scalaz/syntax/std/EitherOps.scala
+++ core/src/main/scala/scalaz/syntax/std/EitherOps.scala
@@ -9,7 +9,21 @@ final class EitherOps[A, B](private val self: Either[A, B]) extends AnyVal {
   final def toValidation: Validation[A, B] = Validation fromEither self
 }
 
+final class RightOps[A, B](private val self: Right[A, B]) extends AnyVal {
+
+  def coerceLeft[C]: Either[C, B] = self.asInstanceOf[Either[C, B]]
+
+}
+
+final class LeftOps[A, B](private val self: Left[A, B]) extends AnyVal {
+
+  def coerceRight[C]: Either[A, C] = self.asInstanceOf[Either[A, C]]
+
+}
+
 trait ToEitherOps {
   implicit def ToEitherOpsFromEither[A, B](e: Either[A, B]): EitherOps[A, B] = new EitherOps(e)
+  implicit def ToRightOpsFromRight[A, B](e: Right[A, B]): RightOps[A, B] = new RightOps(e)
+  implicit def ToLeftOpsFromLeft[A, B](e: Left[A, B]): LeftOps[A, B] = new LeftOps(e)
 }
 
