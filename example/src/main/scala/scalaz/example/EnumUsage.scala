93ecc3f343774c84b861712796b2e188bbdde0e3
xuwei-k <6b656e6a69@gmail.com>
2022-02-08 13:33:16 +0900

    avoid deprecated `scala.App`
    (cherry picked from commit 88fc7de1c439d152d40fce6b20d90aea33cbb91b)

--- example/src/main/scala/scalaz/example/EnumUsage.scala
+++ example/src/main/scala/scalaz/example/EnumUsage.scala
@@ -1,21 +1,23 @@
 package scalaz.example
 
-object EnumUsage extends App {
-  import scalaz._, Scalaz._, Digit._
+object EnumUsage {
+  def main(args: Array[String]): Unit = {
+    import scalaz._, Scalaz._, Digit._
 
-  assert(!true.succ)
-  assert(false.succ)
+    assert(!true.succ)
+    assert(false.succ)
 
-  assert((_2: Digit) -+- 5 === _7)
-  assert((_8: Digit) --- 6 === _2)
+    assert((_2: Digit) -+- 5 === _7)
+    assert((_8: Digit) --- 6 === _2)
 
-  assert((7 |==> (2, 14)).toIList === IList(7, 9, 11, 13))
-  assert((7 |--> (2, 14)) === IList(7, 9, 11, 13))
-  assert((14 |==> (-2, 7)).toIList === IList(14, 12, 10, 8))
-  assert((14 |--> (-2, 7)) === IList(14, 12, 10, 8))
+    assert((7 |==> (2, 14)).toIList === IList(7, 9, 11, 13))
+    assert((7 |--> (2, 14)) === IList(7, 9, 11, 13))
+    assert((14 |==> (-2, 7)).toIList === IList(14, 12, 10, 8))
+    assert((14 |--> (-2, 7)) === IList(14, 12, 10, 8))
 
-  assert(((_4: Digit) |-> _9) === IList(_4, _5, _6, _7, _8, _9))
-  assert(('m' |=> 'u').toIList === IList('m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u'))
-  assert(('m' |--> (2, 'u')) === IList('m', 'o', 'q', 's', 'u'))
+    assert(((_4: Digit) |-> _9) === IList(_4, _5, _6, _7, _8, _9))
+    assert(('m' |=> 'u').toIList === IList('m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u'))
+    assert(('m' |--> (2, 'u')) === IList('m', 'o', 'q', 's', 'u'))
+  }
 
 }
--- example/src/main/scala/scalaz/example/StrongUsage.scala
+++ example/src/main/scala/scalaz/example/StrongUsage.scala
@@ -4,19 +4,21 @@ import scalaz._
 import Scalaz._
 import scalaz.Strong.uncurry
 
-object StrongUsage extends App {
+object StrongUsage {
 
   // Function1 Strong first
-  def len: String => Int = _.length
-  (len.first(("foo", 14))) assert_=== ((3, 14))
+  def main(args: Array[String]): Unit = {
+    def len: String => Int = _.length
+    (len.first(("foo", 14))) assert_=== ((3, 14))
 
-  // Function1 Strong uncurry
-  val multiplyStrN: String => (Int => String) =
-    (str: String) =>
-      (times: Int) =>
-        str.reverse * times
+    // Function1 Strong uncurry
+    val multiplyStrN: String => (Int => String) =
+      (str: String) =>
+        (times: Int) =>
+          str.reverse * times
 
-  val evil2: (String, Int) = ("evil", 2)
-  val uncurried: ((String,Int)) => String = uncurry(multiplyStrN)
-  uncurried(evil2) assert_=== "livelive"
+    val evil2: (String, Int) = ("evil", 2)
+    val uncurried: ((String,Int)) => String = uncurry(multiplyStrN)
+    uncurried(evil2) assert_=== "livelive"
+  }
 }
