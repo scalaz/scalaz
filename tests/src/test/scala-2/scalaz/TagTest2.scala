807d50d210391140df9ac5819196f5757266b803
xuwei-k <6b656e6a69@gmail.com>
2023-05-24 07:36:34 +0900

    prepare Scala 3.3

--- tests/src/test/scala-2/scalaz/TagTest2.scala
+++ tests/src/test/scala-2/scalaz/TagTest2.scala
@@ -4,6 +4,14 @@ import std.AllInstances._
 import Tags.{Multiplication => Mult}
 
 object TagTest2 extends SpecLite {
+  "k.@@" should {
+    "be abstract" in {
+      val r = (List[Int](42) ++ List[Int @@ Mult]()).toSet
+      r: Set[Any]
+      true
+    }
+  }
+
   "of.onCov" should {
     "choose covariant position" in {
       Tag.of[Mult].onCov((a:Int) => a): (Int => (Int @@ Mult))
