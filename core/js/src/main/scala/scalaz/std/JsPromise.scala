d3bcd21d8381fbcd8aa2bbd2502abbd61e2bffeb
xuwei-k <6b656e6a69@gmail.com>
2023-11-11 08:10:03 +0900

    avoid implicit object

--- core/js/src/main/scala/scalaz/std/JsPromise.scala
+++ core/js/src/main/scala/scalaz/std/JsPromise.scala
@@ -15,56 +15,57 @@ object jsPromise extends JSPromiseInstances {
   implicit def jsPromiseMonoid[A](implicit A: Monoid[A]): Monoid[js.Promise[A]] =
     Monoid.liftMonoid[js.Promise, A]
 
-  implicit object JSPromiseInstance extends Nondeterminism[js.Promise] with MonadError[js.Promise, Any] with Cobind[js.Promise] {
+  implicit val JSPromiseInstance: Nondeterminism[js.Promise] with MonadError[js.Promise, Any] with Cobind[js.Promise] =
+    new Nondeterminism[js.Promise] with MonadError[js.Promise, Any] with Cobind[js.Promise] {
 
-    def cobind[A, B](fa: js.Promise[A])(f: js.Promise[A] => B): js.Promise[B] =
-      point(f(fa))
+      def cobind[A, B](fa: js.Promise[A])(f: js.Promise[A] => B): js.Promise[B] =
+        point(f(fa))
 
-    @inline
-    private def promiseToUnion[A](promise: js.Promise[A]): A | js.Thenable[A]
-      = |.from(promise)(|.Evidence.right(|.Evidence.base))
+      @inline
+      private def promiseToUnion[A](promise: js.Promise[A]): A | js.Thenable[A]
+        = |.from(promise)(|.Evidence.right(|.Evidence.base))
 
-    @inline
-    private def valueToUndefOr[A](a: A): js.UndefOr[A]
-      = |.from(a)(|.Evidence.left(|.Evidence.base))
+      @inline
+      private def valueToUndefOr[A](a: A): js.UndefOr[A]
+        = |.from(a)(|.Evidence.left(|.Evidence.base))
 
-    @inline
-    def point[A](a: => A): js.Promise[A] = js.Promise.resolve[A](a)
-    def bind[A, B](fa: js.Promise[A])(f: A => js.Promise[B]): js.Promise[B] = fa.`then`[B]{ a => promiseToUnion(f(a)) }
+      @inline
+      def point[A](a: => A): js.Promise[A] = js.Promise.resolve[A](a)
+      def bind[A, B](fa: js.Promise[A])(f: A => js.Promise[B]): js.Promise[B] = fa.`then`[B]{ a => promiseToUnion(f(a)) }
 
-    def chooseAny[A](head: js.Promise[A], tail: IList[js.Promise[A]]): js.Promise[(A, IList[js.Promise[A]])] = {
-      chooseAny(ICons(head, tail))
-    }
-
-    private def chooseAny[A](nonEmptyList: ICons[js.Promise[A]]): js.Promise[(A, IList[js.Promise[A]])] = {
-      js.Promise.race(
-        Foldable[IList].toList(nonEmptyList).view.map { jsPromise =>
-          map(jsPromise) { a =>
-            (a, nonEmptyList.filter(_ ne jsPromise))
-          }
-        }.toJSIterable) 
-    }
-
-    override def firstCompletedOf[A](as: Iterable[js.Promise[A]]): Option[js.Promise[A]] =
-      if (as.isEmpty) {
-        None
-      } else {
-        Some(js.Promise.race(as.toJSIterable))
+      def chooseAny[A](head: js.Promise[A], tail: IList[js.Promise[A]]): js.Promise[(A, IList[js.Promise[A]])] = {
+        chooseAny(ICons(head, tail))
       }
 
-    override def gather[A](fs: IList[js.Promise[A]]): js.Promise[IList[A]] =
-      map(js.Promise.all(Foldable[IList].toList(fs).toJSIterable)) { jsIterable =>
-        IList.fromList(jsIterable.toList)
+      private def chooseAny[A](nonEmptyList: ICons[js.Promise[A]]): js.Promise[(A, IList[js.Promise[A]])] = {
+        js.Promise.race(
+          Foldable[IList].toList(nonEmptyList).view.map { jsPromise =>
+            map(jsPromise) { a =>
+              (a, nonEmptyList.filter(_ ne jsPromise))
+            }
+          }.toJSIterable) 
       }
 
-    def raiseError[A](e: Any): js.Promise[A] =
-      js.Promise.reject(e)
+      override def firstCompletedOf[A](as: Iterable[js.Promise[A]]): Option[js.Promise[A]] =
+        if (as.isEmpty) {
+          None
+        } else {
+          Some(js.Promise.race(as.toJSIterable))
+        }
 
-    def handleError[A](fa: js.Promise[A])(f: Any => js.Promise[A]): js.Promise[A] = {
-      val jsFunction: js.Function1[Any, A | js.Thenable[A]] = { e =>
-        promiseToUnion(f(e))
+      override def gather[A](fs: IList[js.Promise[A]]): js.Promise[IList[A]] =
+        map(js.Promise.all(Foldable[IList].toList(fs).toJSIterable)) { jsIterable =>
+          IList.fromList(jsIterable.toList)
+        }
+
+      def raiseError[A](e: Any): js.Promise[A] =
+        js.Promise.reject(e)
+
+      def handleError[A](fa: js.Promise[A])(f: Any => js.Promise[A]): js.Promise[A] = {
+        val jsFunction: js.Function1[Any, A | js.Thenable[A]] = { e =>
+          promiseToUnion(f(e))
+        }
+        fa.`catch`(valueToUndefOr(jsFunction))
       }
-      fa.`catch`(valueToUndefOr(jsFunction))
     }
-  }
 }
