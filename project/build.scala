8cb4aecef2bb6ca755b4104fdfd8d9003814fe1f
kenji yoshida <6b656e6a69@gmail.com>
2024-08-18 16:26:23 +0900

    remove optimize options

--- project/build.scala
+++ project/build.scala
@@ -168,8 +168,6 @@ object build {
             "-Ywarn-numeric-widen",
             "-Ywarn-value-discard",
             "-Xlint:adapted-args",
-            "-opt:l:method,inline",
-            "-opt-inline-from:scalaz.**",
             s"-language:$common",
           ) ++ unusedWarnOptions.value
       }
